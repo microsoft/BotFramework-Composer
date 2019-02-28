2399ad4a0f8eac0af73f2d928c8179e4c164a08b
Chris Whitten <christopher.whitten@microsoft.com>
2019-02-28 11:22:08 -0800

    Adds <Header />

---
+++ Composer/packages/client/src/components/Header/styles.js
@@ -0,0 +1,40 @@
+import { css } from "@emotion/core";
+
+export const header = css`
+  position: relative;
+  line-height: 50px;
+  background: #001f52;
+  font-size: 20px;
+  color: #fff;
+  height: 50px;
+  display: flex;
+`;
+
+export const aside = css`
+  width: 200;
+  margin-left: 100px;
+`;
+
+export const bot = css`
+  color: #eaeaea;
+  width: 500px;
+  position: absolute;
+  right: 0;
+`;
+
+export const botButton = css`
+  color: white;
+  margin-top: 8px;
+`;
+
+export const botMessage = css`
+  margin-left: 5px;
+  font-size: 18px;
+`;
+
+export const actionButton = css`
+  color: #eaeaea;
+  font-size: 16px;
+  margin-top: 2px;
+  margin-left: 15px;
+`;
