56c3ea37ca44bc0b353a3f7c58d6622550c4727d
Hongyang <hond@microsoft.com>
2019-03-01 19:23:36 +0800

    convert server from js to ts

--- Composer/packages/server/launcher-connectors/interface.js
+++
@@ -1,6 +0,0 @@
-// far from final versoin 
-(function (LauncherStatus) {
-    LauncherStatus[LauncherStatus["Running"] = 0] = "Running";
-    LauncherStatus[LauncherStatus["Stopped"] = 1] = "Stopped";
-})(exports.LauncherStatus || (exports.LauncherStatus = {}));
-var LauncherStatus = exports.LauncherStatus;
---
+++ Composer/packages/server/src/config.ts
@@ -0,0 +1,15 @@
+
+export default class config
+{   
+    public static botconfig = 
+    {
+        "bot": {
+            "provider": "localDisk",
+            "path": "../../../SampleBots/SampleBot4/bot4.bot"
+        },
+        "launcherConnector": {
+            "type": "CSharp",
+            "path": "../../../BotProject/CSharp"
+        }
+    };
+}
