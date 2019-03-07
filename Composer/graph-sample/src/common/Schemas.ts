665b3d2492cf1d0e0987d5caf071bf08687c3efe
Axel Suarez Martinez <axsuarez@microsoft.com>
2019-03-06 16:15:32 -0800

    Minimum Graph component sample from CCI Designer

---
+++ Composer/graph-sample/src/common/Schemas.ts
@@ -0,0 +1,24 @@
+// NOTE(lin): The immutability types are adapted from a tutorial by Anders Hejlsberg, the architect of TypeScript:
+// https://github.com/Microsoft/TypeScript/pull/21316
+
+// NOTE(lin): The R<T> type is a recursively read-only version of T.
+//
+// Example:
+// const person: R<Person> = getPerson();
+// person.name = "Peter";             /* Compiler error */
+// person.address.city = "Seattle";   /* Compiler error */
+// person.parents[0].name = "John";   /* Compiler error */
+// person.parents.push(p);            /* Compiler error */
+//
+// If you need to make changes to a R<T> variable, make a copy of it with mutable() or mutableArray().
+export type R<T> =
+    T extends any[] ? ImmutableArray<T[number]> :
+    T extends object ? ImmutableObject<T> :
+    T;
+
+export interface ImmutableArray<T> extends ReadonlyArray<R<T>> { }
+
+export type ImmutableObject<T> = {
+    // tslint:disable-next-line: ban-types
+    readonly [P in keyof T]: T[P] extends Function ? T[P] : R<T[P]>;
+};
