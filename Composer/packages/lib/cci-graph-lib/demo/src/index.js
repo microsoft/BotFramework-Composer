6652ec62083bce39f37f133eb68ac1bb0b64ff0e
Ze Ye <zeye@microsoft.com>
2019-03-13 17:16:36 +0800

    expose props of <SimpleGraph />

--- Composer/packages/lib/cci-graph-lib/demo/src/index.js
+++ Composer/packages/lib/cci-graph-lib/demo/src/index.js
@@ -2,6 +2,34 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import './index.css';
-import SimpleGraph from '../../dist';
+import { SimpleGraph } from '../../dist';
 
-ReactDOM.render(<SimpleGraph />, document.querySelector('#demo'));
+const demoItems = [
+  {
+    id: 'node_0',
+    value: 'green',
+    neighborIds: ['node_1', 'node_2'],
+  },
+  {
+    id: 'node_1',
+    value: 'red',
+    neighborIds: ['node_4'],
+  },
+  {
+    id: 'node_2',
+    value: 'blue',
+    neighborIds: ['node_3'],
+  },
+  {
+    id: 'node_3',
+    value: 'green',
+    neighborIds: ['node_4'],
+  },
+  {
+    id: 'node_4',
+    value: 'blue',
+    neighborIds: [],
+  },
+];
+
+ReactDOM.render(<SimpleGraph items={demoItems} />, document.querySelector('#demo'));
--- Composer/packages/lib/cci-graph-lib/src/SimpleGraph.tsx
+++ Composer/packages/lib/cci-graph-lib/src/SimpleGraph.tsx
@@ -1,141 +1,119 @@
-import * as React from "react";
-import { Graph } from "./graph/Graph";
-import { GraphEdge } from "./graph/GraphSchemas";
-import { GraphNode } from "./graph/Node";
-import "./SimpleGraph.scss";
-import { Color, ItemRenderer, SimpleItem } from "./SimpleItem";
+import * as React from 'react';
+import { Graph } from './graph/Graph';
+import { GraphEdge } from './graph/GraphSchemas';
+import { GraphNode } from './graph/Node';
+import './SimpleGraph.scss';
+import { Color, ItemRenderer, SimpleItem } from './SimpleItem';
 
 interface SimpleGraphState {
-    items: SimpleItem[];
+  items: SimpleItem[];
 }
 
-export class SimpleGraph extends React.Component<{}, SimpleGraphState> {
-    constructor(props: {}) {
-        super(props);
-
-        this.state = {
-            items: [],
-        };
-    }
-
-    public componentDidMount(): void {
-        const items = loadItems();
-        for (const item of items) {
-            item.onClick = this.onClick;
-        }
-
-        this.setState({
-            items,
-        });
-    }
-
-    public render(): React.ReactNode {
-        const nodes = this.getGraphNodes(this.state.items);
-        const edges = this.getGraphEdges(this.state.items);
-
-        return (
-            <div className="graph-container">
-                <Graph
-                    nodeList={nodes}
-                    edgeList={edges}
-                    graphOptions={{
-                        nodeAlignmentStyle: "Symmetric",
-                        marginOffsetTopLeft: {
-                            x: 100,
-                            y: 30,
-                        },
-                        marginOffsetBottomRight: {
-                            x: 100,
-                            y: 100,
-                        },
-                    }}
-                    transformButtonsRenderer={() => undefined}
-                    onEdgesTargetChanged={undefined}
-                />
-            </div>
-        );
-    }
-
-    private onClick = (id: string): void => {
-        const { items } = this.state;
-        const index = items.findIndex((item) => item.id === id);
-        const item = items[index];
-        const color = item.value;
-        const nextColor =
-            color === Color.Red
-                ? Color.Green
-                : color === Color.Green
-                    ? Color.Blue
-                    : Color.Red;
-
-        const updatedItem = {
-            ...item,
-            value: nextColor,
-        };
-
-        const updatedItems = [...items.slice(0, index), updatedItem, ...items.slice(index + 1)];
-        this.setState({
-            items: updatedItems,
-        });
-    }
-
-    private getGraphNodes(items: SimpleItem[]): Array<GraphNode<SimpleItem>> {
-        return items.map((item): GraphNode<SimpleItem> => {
-            return {
-                id: item.id,
-                renderer: ItemRenderer,
-                miniRenderer: ItemRenderer,
-                contentProps: {
-                    nodeId: item.id,
-                    data: item,
-                },
-            };
-        });
-    }
-
-    private getGraphEdges(items: SimpleItem[]): GraphEdge[] {
-        const edges: GraphEdge[] = [];
-
-        for (const item of items) {
-            for (const neighborId of item.neighborIds) {
-                edges.push({
-                    sourceNodeId: item.id,
-                    targetNodeId: neighborId,
-                    localEdgeId: "local",
-                });
-            }
-        }
-
-        return edges;
-    }
+interface GraphPropsType {
+  items: SimpleItem[];
+  width: number;
+  height: number;
 }
 
-function loadItems(): SimpleItem[] {
-    return [
-        {
-            id: "node_0",
-            value: Color.Green,
-            neighborIds: ["node_1", "node_2"],
-        },
-        {
-            id: "node_1",
-            value: Color.Red,
-            neighborIds: ["node_4"],
-        },
-        {
-            id: "node_2",
-            value: Color.Blue,
-            neighborIds: ["node_3"],
-        },
-        {
-            id: "node_3",
-            value: Color.Green,
-            neighborIds: ["node_4"],
-        },
-        {
-            id: "node_4",
-            value: Color.Blue,
-            neighborIds: [],
-        },
-    ];
+export class SimpleGraph extends React.Component<GraphPropsType, SimpleGraphState> {
+  static defaultProps = {
+    width: 600,
+    height: 600,
+  };
+
+  constructor(props: any) {
+    super(props);
+
+    this.state = {
+      items: [],
+    };
+  }
+
+  public componentDidMount(): void {
+    const items = this.props.items;
+    for (const item of items) {
+      item.onClick = this.onClick;
+    }
+
+    this.setState({
+      items,
+    });
+  }
+
+  public render(): React.ReactNode {
+    const nodes = this.getGraphNodes(this.state.items);
+    const edges = this.getGraphEdges(this.state.items);
+
+    return (
+      <div className="graph-container" style={{ width: this.props.width, height: this.props.height }}>
+        <Graph
+          nodeList={nodes}
+          edgeList={edges}
+          graphOptions={{
+            nodeAlignmentStyle: 'Symmetric',
+            marginOffsetTopLeft: {
+              x: 100,
+              y: 30,
+            },
+            marginOffsetBottomRight: {
+              x: 100,
+              y: 100,
+            },
+          }}
+          transformButtonsRenderer={() => undefined}
+          onEdgesTargetChanged={undefined}
+        />
+      </div>
+    );
+  }
+
+  private onClick = (id: string): void => {
+    const { items } = this.state;
+    const index = items.findIndex(item => item.id === id);
+    const item = items[index];
+    const color = item.value;
+    const nextColor = color === Color.Red ? Color.Green : color === Color.Green ? Color.Blue : Color.Red;
+
+    const updatedItem = {
+      ...item,
+      value: nextColor,
+    };
+
+    const updatedItems = [...items.slice(0, index), updatedItem, ...items.slice(index + 1)];
+    this.setState({
+      items: updatedItems,
+    });
+  };
+
+  private getGraphNodes(items: SimpleItem[]): Array<GraphNode<SimpleItem>> {
+    return items.map(
+      (item): GraphNode<SimpleItem> => {
+        return {
+          id: item.id,
+          renderer: ItemRenderer,
+          miniRenderer: ItemRenderer,
+          contentProps: {
+            nodeId: item.id,
+            data: item,
+          },
+        };
+      }
+    );
+  }
+
+  private getGraphEdges(items: SimpleItem[]): GraphEdge[] {
+    const edges: GraphEdge[] = [];
+
+    for (const item of items) {
+      for (const neighborId of item.neighborIds) {
+        edges.push({
+          sourceNodeId: item.id,
+          targetNodeId: neighborId,
+          localEdgeId: 'local',
+        });
+      }
+    }
+
+    return edges;
+  }
 }
