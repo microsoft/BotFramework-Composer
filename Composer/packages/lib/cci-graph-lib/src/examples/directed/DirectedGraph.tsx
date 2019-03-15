eb2f5699ca8c10930bf2fdc5cad0b9aa0346963e
Ze Ye <zeye@microsoft.com>
2019-03-15 16:57:16 +0800

    fix DirectedGraph life cycle to make it reactive

--- Composer/packages/lib/cci-graph-lib/src/examples/directed/DirectedGraph.tsx
+++ Composer/packages/lib/cci-graph-lib/src/examples/directed/DirectedGraph.tsx
@@ -6,7 +6,9 @@ import './DirectedGraph.scss';
 import { DirectedGraphItem } from './DirectedGraphItem';
 
 interface DirectedGraphState {
-  items: DirectedGraphItem[];
+  nodes: GraphNode<DirectedGraphItem>[];
+  edges: GraphEdge[];
+  prevItems: DirectedGraphItem[];
 }
 
 interface DirectedGraphPropsType {
@@ -17,31 +19,66 @@ interface DirectedGraphPropsType {
 
 const EMPTY_ONCLICK_EVENT = () => {};
 
+// TODO: extract node transformer.
+function getGraphNodes(items: DirectedGraphItem[]): Array<GraphNode<DirectedGraphItem>> {
+  return items.map(
+    (item): GraphNode<DirectedGraphItem> => {
+      return {
+        id: item.id,
+        renderer: Node(item.contentRenderer),
+        footerRenderer: item.footerRenderer,
+        miniRenderer: Node(item.contentRenderer),
+        contentProps: {
+          nodeId: item.id,
+          data: {
+            ...item,
+            onClick: item.onClick || EMPTY_ONCLICK_EVENT,
+          },
+        },
+      };
+    }
+  );
+}
+
+function getGraphEdges(items: DirectedGraphItem[]): GraphEdge[] {
+  const edges: GraphEdge[] = [];
+
+  for (const item of items) {
+    for (const neighborId of item.neighborIds) {
+      edges.push({
+        sourceNodeId: item.id,
+        targetNodeId: neighborId,
+        localEdgeId: 'local',
+      });
+    }
+  }
+
+  return edges;
+}
+
 export class DirectedGraph extends React.Component<DirectedGraphPropsType, DirectedGraphState> {
   static defaultProps = {
     width: 600,
     height: 600,
   };
 
-  constructor(props: any) {
-    super(props);
+  state = {
+    nodes: [],
+    edges: [],
+    prevItems: undefined,
+  };
 
-    this.state = {
-      items: [],
-    };
-  }
-
-  public componentDidMount(): void {
-    const items = this.props.items;
-    this.setState({
-      items,
-    });
+  static getDerivedStateFromProps(props, state) {
+    if (props.items !== state.prevItems) {
+      return {
+        nodes: getGraphNodes(props.items),
+        edges: getGraphEdges(props.items),
+      };
+    }
   }
 
   public render(): React.ReactNode {
-    const nodes = this.getGraphNodes(this.state.items);
-    const edges = this.getGraphEdges(this.state.items);
-
+    const { nodes, edges } = this.state;
     return (
       <div className="graph-container" style={{ width: this.props.width, height: this.props.height }}>
         <Graph
@@ -63,41 +100,5 @@ export class DirectedGraph extends React.Component<DirectedGraphPropsType, Direc
         />
       </div>
     );
-  }
-
-  private getGraphNodes(items: DirectedGraphItem[]): Array<GraphNode<DirectedGraphItem>> {
-    return items.map(
-      (item): GraphNode<DirectedGraphItem> => {
-        return {
-          id: item.id,
-          renderer: Node(item.contentRenderer),
-          footerRenderer: item.footerRenderer,
-          miniRenderer: Node(item.contentRenderer),
-          contentProps: {
-            nodeId: item.id,
-            data: {
-              ...item,
-              onClick: item.onClick || EMPTY_ONCLICK_EVENT,
-            },
-          },
-        };
-      }
-    );
-  }
-
-  private getGraphEdges(items: DirectedGraphItem[]): GraphEdge[] {
-    const edges: GraphEdge[] = [];
-
-    for (const item of items) {
-      for (const neighborId of item.neighborIds) {
-        edges.push({
-          sourceNodeId: item.id,
-          targetNodeId: neighborId,
-          localEdgeId: 'local',
-        });
-      }
-    }
-
-    return edges;
   }
 }
