41f50043cbdc1523181390d22b3dc4338fdc472f
Ze Ye <zeye@microsoft.com>
2019-03-15 16:49:39 +0800

    update cci lib demo

--- Composer/packages/lib/cci-graph-lib/demo/src/index.tsx
+++ Composer/packages/lib/cci-graph-lib/demo/src/index.tsx
@@ -26,38 +26,91 @@ const reportNodeId = (nodeId: string) => {
   console.log(`Node ${nodeId} clicked.`);
 };
 
-const demoItems: any[] = [
-  {
-    id: 'node_0',
-    value: 'green',
-    neighborIds: ['node_1', 'node_2'],
-  },
-  {
-    id: 'node_1',
-    value: 'red',
-    neighborIds: ['node_4'],
-  },
-  {
-    id: 'node_2',
-    value: 'blue',
-    neighborIds: ['node_3'],
-  },
-  {
-    id: 'node_3',
-    value: 'green',
-    neighborIds: ['node_4'],
-  },
-  {
-    id: 'node_4',
-    value: 'blue',
-    neighborIds: [],
-  },
+const demoAssets: any[] = [
+  // 1 - Colorful exmaple
+  [
+    {
+      id: 'node_0',
+      value: 'green',
+      neighborIds: ['node_1', 'node_2'],
+    },
+    {
+      id: 'node_1',
+      value: 'red',
+      neighborIds: ['node_4'],
+    },
+    {
+      id: 'node_2',
+      value: 'blue',
+      neighborIds: ['node_3'],
+    },
+    {
+      id: 'node_3',
+      value: 'green',
+      neighborIds: ['node_4'],
+    },
+    {
+      id: 'node_4',
+      value: 'blue',
+      neighborIds: [],
+    },
+  ],
+  // 2 - Integer id
+  [
+    {
+      id: 1,
+      value: 'root',
+      neighborIds: [2, 3],
+    },
+    {
+      id: 2,
+      value: 'leaf',
+      neighborIds: [],
+    },
+    {
+      id: 3,
+      value: 'leaf',
+      neighborIds: [],
+    },
+  ],
 ];
 
-demoItems.forEach(item => {
-  item.contentRenderer = ExampleContent;
-  item.footRenderer = ExampleFooter;
-  item.onClick = reportNodeId;
-});
+// Bind default renderer and click event.
+for (let demoItem of demoAssets) {
+  for (let item of demoItem) {
+    item.contentRenderer = ExampleContent;
+    item.footRenderer = ExampleFooter;
+    item.onClick = reportNodeId;
+  }
+}
 
-ReactDOM.render(<DirectedGraph items={demoItems} width={600} height={700} />, document.querySelector('#demo'));
+class Demo extends React.Component {
+  state = {
+    itemIndex: 0,
+  };
+
+  showNextJson() {
+    let nextIndex = this.state.itemIndex + 1;
+    nextIndex = nextIndex % demoAssets.length;
+    this.setState({ itemIndex: nextIndex });
+  }
+
+  render() {
+    const displayedJson = demoAssets[this.state.itemIndex];
+    return (
+      <div style={{ display: 'flex', flexDirection: 'row' }}>
+        <div>
+          <button onClick={() => this.showNextJson()}>Change Json</button>
+          <DirectedGraph items={displayedJson} width={600} height={700} />
+        </div>
+        <div>
+          <pre style={{ whiteSpace: 'pre-wrap', wordBreak: 'keep-all' }}>
+            {JSON.stringify(displayedJson, null, '\t')}
+          </pre>
+        </div>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Demo />, document.querySelector('#demo'));
