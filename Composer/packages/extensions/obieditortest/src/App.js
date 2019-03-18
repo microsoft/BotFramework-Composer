47d49f7fe08467624cbfbafba44899a937629a82
Chris Whitten <christopher.whitten@microsoft.com>
2019-03-18 14:24:23 -0700

    Render activity field correctly

--- Composer/packages/extensions/obieditortest/src/App.js
+++ Composer/packages/extensions/obieditortest/src/App.js
@@ -64,6 +64,7 @@ const getType = data => {
 
 export const FormEditor = props => {
   const type = getType(props.data);
+
   const [dialogSchema, setDialogSchema] = useState({
     definitions: { ...masterSchema.definitions },
     ...masterSchema.definitions[type],
@@ -88,7 +89,7 @@ export const FormEditor = props => {
         noValidate
         className="schemaForm"
         onChange={onChange}
-        formData={props.data.dialog}
+        formData={props.data.dialog || props.data}
         schema={dialogSchema}
         uiSchema={dialogUiSchema}
       />
