b5d0cdbe92ef547c2ccaefa01f9db4a9feba1410
Wenyi Luo <wenyluo@microsoft.com>
2019-03-19 16:50:08 +0800

    fix comments

--- Composer/packages/server/src/modal/Cache.ts
+++
@@ -1,5 +0,0 @@
-export const globalCache:Cache = {};
-// maybe load lastOpenBot into cache later
-interface Cache{
-    [key:string]: any
-}
--- Composer/packages/server/src/router/projectServer.ts
+++ Composer/packages/server/src/router/projectServer.ts
@@ -1,13 +1,12 @@
 import express, { Router } from 'express';
 import { getFiles } from "../handlers/fileHandler";
-import { globalCache } from "../modal/Cache";
 import storage from "../storage/StorageService";
 
 const router: Router = express.Router({});
 
 // read from memory
 router.get("/opened", function (req: any, res: any, next: any) {
-    let openBot = globalCache.openBot ? globalCache.openBot : null;
+    let openBot = storage.getItem<object>("openBot");
     if (openBot) {
         res.status(200).json(openBot);
     } else {
@@ -16,19 +15,20 @@ router.get("/opened", function (req: any, res: any, next: any) {
 });
 
 router.post("/opened", function (req: any, res: any, next: any) {
-
-    if (req.body.path) {
+    if (req.body.path && req.body.storageId === "default") {
+        // load local project
         let result = getFiles(req.body.path);
         res.status(200).json(result);
         // update recent open bot
-        globalCache.openBot = {
-            storageId: "default",
+        let recentOpenBots: any = storage.getItem<string>('recentAccessedBots');
+        recentOpenBots = recentOpenBots ? JSON.parse(recentOpenBots) : [] as object[];
+        let openBot = {
+            storageId: req.body.storageId,
             path: req.body.path
         };
-        let openBot = storage.getItem<Array<object>>('recentAccessedBots');
-        openBot = openBot ? openBot : [];
-        openBot.push(globalCache.openBot);
-        storage.setItem('recentAccessedBots', openBot);
+        recentOpenBots.push(openBot);
+        storage.setItem("openBot", openBot);
+        storage.setItem('recentAccessedBots', JSON.stringify(recentOpenBots));
     } else {
         res.status(400).json({ error: 'no path' });
     }
