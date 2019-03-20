a881c9f106f3e86b26c3c1d44708d440476d6440
Andy Brown <asbrown002@gmail.com>
2019-03-19 22:39:21 -0500

    Update obieditor playground (#54)
    * remove circular dependency from schema
    
    Circular dependencies are not recommended and cause react-jsonschema-form to loop indefinitely.
    
    http://json-schema.org/latest/json-schema-core.html#rfc.section.8.3
    
    * add type to Microsoft.IDialog definition
    
    * set up demo to be a playground for form development
    
    To use:
    1. Navigate to the obieditor package.
    2. Run `yarn start`
    3. Go to `http://localhost:3000`
    4. Edit the json to update the rendered form
    5. Edit the form to update the json
    
    * set playground port to 3001
    
    in order to have both composer and the obi editor playground running at the same time.

--- Composer/packages/extensions/obieditortest/demo/src/index.js
+++ Composer/packages/extensions/obieditortest/demo/src/index.js
@@ -1,17 +1,77 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { render } from 'react-dom';
+import { Controlled as CodeMirror } from 'react-codemirror2';
 
 import Example from '../../src';
 
-class Demo extends Component {
-  render() {
-    return (
-      <div>
-        <h1>obieditortest Demo</h1>
-        <Example />
+import 'codemirror/mode/javascript/javascript';
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/theme/material.css';
+import 'codemirror/theme/neat.css';
+
+import './styles.scss';
+
+const cmOptions = {
+  theme: 'material',
+  viewportMargin: Infinity,
+  mode: {
+    name: 'javascript',
+    json: true,
+    statementIndent: 2,
+  },
+  lineNumbers: true,
+  lineWrapping: true,
+  indentWithTabs: false,
+  tabSize: 2,
+  smartIndent: true,
+};
+
+const defaultData = {
+  $type: 'Microsoft.WelcomeRule',
+  steps: [
+    {
+      $type: 'Microsoft.SendActivity',
+      activity: 'Hi! I\'m a ToDo bot. Say "add a todo named first one" to get started.',
+    },
+  ],
+};
+
+function Demo() {
+  const [editorData, setEditorData] = useState(JSON.stringify(defaultData, null, 2));
+  const [formData, setFormData] = useState(defaultData);
+  const [isValid, setValid] = useState(true);
+
+  const updateEditorData = (editor, data, value) => {
+    try {
+      const parsed = JSON.parse(value);
+      setFormData(parsed);
+      setValid(true);
+    } catch (err) {
+      setValid(false);
+    }
+  };
+
+  useEffect(() => {
+    setEditorData(JSON.stringify(formData, null, 2));
+  }, [formData]);
+
+  return (
+    <div className="DemoContainer">
+      <CodeMirror
+        value={editorData}
+        options={cmOptions}
+        onBeforeChange={(editor, data, value) => {
+          setEditorData(value);
+        }}
+        onChange={updateEditorData}
+        autoCursor
+        className={isValid ? '' : 'CodeMirror--error'}
+      />
+      <div className="DemoForm">
+        <Example data={formData} onChange={setFormData} />
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 render(<Demo />, document.querySelector('#demo'));
