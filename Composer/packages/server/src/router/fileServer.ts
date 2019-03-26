5a709aa5db1f403fb5b3223ffbb4e8ec5e931d37
Wenyi Luo <wenyluo@microsoft.com>
2019-03-26 21:13:49 +0800

    remove previous useless api

--- Composer/packages/server/src/router/fileServer.ts
+++ Composer/packages/server/src/router/fileServer.ts
@@ -1,5 +1,5 @@
 import express, { Router } from 'express';
-import { getFiles, updateFile, searchFilePath } from '../handlers/fileHandler';
+import { getFiles, updateFile } from '../handlers/fileHandler';
 import setting from '../storage/SettingService';
 import storage from '../storage/StorageService';
 
@@ -28,27 +28,6 @@ router.put('/', function(req: any, res: any, next: any) {
     res.send('OK');
   } catch (error) {
     res.status(400).json({ error: 'save error' });
-  }
-});
-
-router.get('/openbotFile', function(req: any, res: any, next: any) {
-  let fileList: any[] = [];
-  if (!req.query.path) {
-    res.status(400).json({ error: 'no path' });
-  }
-  try {
-    const pickerWorkingDir = storage.getItem<string>('pickerWorkingDir', '');
-    //the limitation of broswer which doens't allows to get the full path of the selected file.
-    const path = searchFilePath(pickerWorkingDir, req.query.path);
-    if (path === '') {
-      res.status(400).json({ error: 'can not find the file' });
-      return;
-    }
-    fileList = getFiles(path);
-    storage.setItem('lastActiveBot', path);
-    res.status(200).json(fileList);
-  } catch (error) {
-    res.status(400).json({ error: 'get file list error' });
   }
 });
 
