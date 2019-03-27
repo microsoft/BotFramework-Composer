0a28db4813cb90b180ef0ed237fe2586c9f4ca51
Wenyi Luo <wenyluo@microsoft.com>
2019-03-27 21:59:37 +0800

    fix problem about getting file or folder without permission

--- Composer/packages/server/src/router/storageServer.ts
+++ Composer/packages/server/src/router/storageServer.ts
@@ -31,18 +31,24 @@ router.get('/:storageId/blobs/:path(*)', function(req: any, res: any, next: any)
       res.status(404).json(err);
       return;
     }
+    // deal with root path incomplete
+    reqPath = reqPath + '/';
     reqPath = path.normalize(reqPath);
-    if (stat.isFile()) {
-      result = fs.readFileSync(reqPath, 'utf-8');
-      result = JSON.parse(result);
-    } else if (stat.isDirectory()) {
-      const folderTree = storageHandler.getFolderTree(reqPath);
+    try {
+      if (stat.isFile()) {
+        result = fs.readFileSync(reqPath, 'utf-8');
+        result = JSON.parse(result);
+      } else if (stat.isDirectory()) {
+        const folderTree = storageHandler.getFolderTree(reqPath);
 
-      result = {
-        name: path.basename(reqPath),
-        parent: path.dirname(reqPath),
-        children: folderTree,
-      };
+        result = {
+          name: path.basename(reqPath),
+          parent: path.dirname(reqPath),
+          children: folderTree,
+        };
+      }
+    } catch (error) {
+      res.status(400).json(error);
     }
     res.status(200).json(result);
   });
@@ -72,25 +78,29 @@ export const storageHandler = {
   getFolderTree: (folderPath: string) => {
     const folderTree = [] as object[];
     const items = fs.readdirSync(folderPath);
-
     for (const item of items) {
-      const itemPath = path.join(folderPath, item);
-      const tempStat = fs.statSync(itemPath);
-
-      if (tempStat.isDirectory()) {
-        folderTree.push({
-          name: item,
-          type: 'folder',
-          path: itemPath,
-        });
-      } else if (tempStat.isFile()) {
-        folderTree.push({
-          name: item,
-          size: tempStat.size,
-          type: 'file',
-          lastModified: tempStat.mtimeMs,
-          path: itemPath,
-        });
+      try {
+        const itemPath = path.join(folderPath, item);
+        const tempStat = fs.statSync(itemPath);
+        if (tempStat.isDirectory()) {
+          folderTree.push({
+            name: item,
+            type: 'folder',
+            path: itemPath,
+          });
+        } else if (tempStat.isFile()) {
+          folderTree.push({
+            name: item,
+            size: tempStat.size,
+            type: 'file',
+            lastModified: tempStat.mtimeMs,
+            path: itemPath,
+          });
+        }
+      } catch (error) {
+        // log error, and continute getting the path which can access
+        console.log(error);
+        continue;
       }
     }
     return folderTree;
