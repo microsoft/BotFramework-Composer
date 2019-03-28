1f501e0832b02d1b2477f419b2f75ccda9e6f2b4
Chris Whitten <christopher.whitten@microsoft.com>
2019-03-27 21:44:20 -0700

    Spacing

--- Composer/packages/extensions/visual-designer/src/components/dialog-flow-editor/DialogNode.js
+++ Composer/packages/extensions/visual-designer/src/components/dialog-flow-editor/DialogNode.js
@@ -106,14 +106,19 @@ export class DialogNode extends Component {
         ) : (
           <div
             style={{
-              display: 'flex',
-              justifyContent: 'center',
-              width: '147px',
+              width: '150px',
               height: '20px',
               backgroundColor: '#ffffff',
             }}
           >
-            <svg width="50" height="20" viewBox="0 0 50 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <svg
+              style={{ marginLeft: '48px' }}
+              width="50"
+              height="20"
+              viewBox="0 0 50 20"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
               <path d="M25 0L50 10L25 20L-2.7865e-06 10L25 0Z" fill="#979797" />
             </svg>
           </div>
