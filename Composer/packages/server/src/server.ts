ed1b6a68cad20139e574fa2a2eded84c6f2c5ee3
Andy <asbrown002@gmail.com>
2019-03-28 12:51:16 -0700

    allow glob paths in botproj

--- Composer/packages/server/src/server.ts
+++ Composer/packages/server/src/server.ts
@@ -1,23 +1,27 @@
-import express, { Express } from 'express';
+import express, { Express, Request, Response, NextFunction } from 'express';
 import bodyParser from 'body-parser';
+import morgan from 'morgan';
 
 import { fileServerRouter } from './router/fileServer';
 import { launcherServerRouter } from './router/launcherServer';
 import { storagesServerRouter } from './router/storageServer';
 import { projectsServerRouter } from './router/projectServer';
+
 const app: Express = express();
 
-app.all('*', function(req: any, res: any, next: any) {
+app.all('*', function(req: Request, res: Response, next: NextFunction) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   next();
 });
 
+app.use(morgan('dev'));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', function(req: any, res: any) {
+app.get('/', function(req: Request, res: Response) {
   res.send('fortest');
 });
 
@@ -26,6 +30,8 @@ app.use('/api/launcher', launcherServerRouter);
 app.use('/api/storages', storagesServerRouter);
 app.use('/api/projects', projectsServerRouter);
 
-app.listen(process.env.PORT || 5000, () => {
-  console.log('Server running');
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Server running on port ${port}`);
 });
