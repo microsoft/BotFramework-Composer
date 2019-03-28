4a9815ede9ab8e62334bc9f40e04973d93cd2577
Andy <asbrown002@gmail.com>
2019-03-28 15:08:49 -0700

    update failing tests

--- Composer/packages/server/__tests__/router/storageServer.test.ts
+++ Composer/packages/server/__tests__/router/storageServer.test.ts
@@ -1,21 +1,22 @@
-import { storageHandler } from '../../src/router/storageServer';
-import { FileStorage } from '../../src/storage/FileStorage';
 import path from 'path';
 
+import { storageHandler } from '../../src/router/storageServer';
+import { FileStorage } from '../../src/storage/FileStorage';
+
 describe('test storage server all method', () => {
-  let mockStorage = new FileStorage(path.resolve(__dirname, '../mockFile/storage.json'), error => {
+  const mockStorage = new FileStorage(path.resolve(__dirname, '../mocks/storage.json'), error => {
     console.log(error);
   });
 
   test('GET /api/storages handler', () => {
-    let result = storageHandler.getStorage(mockStorage);
+    const result = storageHandler.getStorage(mockStorage);
     expect(result).toBeInstanceOf(Array);
     expect(result.length).toBeGreaterThan(0);
     expect(result[0].name).toBe('test');
   });
 
   test('test get folder in one storage', () => {
-    let result = storageHandler.getFolderTree(__dirname);
+    const result = storageHandler.getFolderTree(__dirname);
     expect(result.length).toBeGreaterThan(0);
   });
 });
