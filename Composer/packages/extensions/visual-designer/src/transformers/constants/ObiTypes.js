98b77ffee808271d848e79e68f4daf51e30f1f90
zeye <zeye@microsoft.com>
2019-04-01 17:13:40 +0800

    add more OBI types

--- Composer/packages/extensions/visual-designer/src/transformers/constants/ObiTypes.js
+++ Composer/packages/extensions/visual-designer/src/transformers/constants/ObiTypes.js
@@ -2,15 +2,19 @@ export const ObiTypes = {
   // dialogs
   ObiRuleDialog: 'Microsoft.RuleDialog',
   SequenceDialog: 'Microsoft.SequenceDialog',
+  AdaptiveDialog: 'Microsoft.AdaptiveDialog',
 
   // recognizers
   RegexRecognizer: 'Microsoft.RegexRecognizer',
   LuisRecognizer: 'Microsoft.LuisRecognizer',
+  MultiLanguageRecognizer: 'Microsoft.MultiLanguageRecognizer',
+  QnARecognizer: 'Microsoft.QnARecognizer',
 
   // rules
   WelcomeRule: 'Microsoft.WelcomeRule',
-  FallbackRule: 'Microsoft.FallbackRule',
+  FallbackRule: 'Microsoft.NoMatchRule',
   IntentRule: 'Microsoft.IntentRule',
+  EventRule: 'Microsoft.EventRule',
 
   // steps
   CallDialog: 'Microsoft.CallDialog',
