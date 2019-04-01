fb157a0f2a40e86af23d83b1ef379ad8a4b255e9
Andy <asbrown002@gmail.com>
2019-04-01 14:05:51 -0700

    allow setting breadcrumb when navigating
    allows for deep linking with correct breadcrumb

--- Composer/packages/client/src/store/reducer/index.js
+++ Composer/packages/client/src/store/reducer/index.js
@@ -70,7 +70,7 @@ const getStorageFileSuccess = (state, { response }) => {
   return state;
 };
 
-const navigateTo = (state, { path }) => {
+const navigateTo = (state, { path, breadcrumb }) => {
   if (state.navPath !== path) {
     state.navPath = path;
     state.focusPath = '';
@@ -83,6 +83,9 @@ const navigateTo = (state, { path }) => {
       if (currentOpenFileIndex > 0) {
         state.openFileIndex = currentOpenFileIndex;
       }
+    }
+    if (breadcrumb && Array.isArray(breadcrumb)) {
+      state.navPathHistory = breadcrumb;
     }
     state.navPathHistory.push(path);
   }
