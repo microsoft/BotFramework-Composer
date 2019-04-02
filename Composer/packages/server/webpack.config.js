9f79df7a45a3c3300ab06f04c16ec11949dfb8ba
leilzh <leilzh@microsoft.com>
2019-04-02 17:08:25 +0800

    add webpack config for server

---
+++ Composer/packages/server/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+const SrcPath = path.resolve(__dirname, 'src');
+const BuildPath = path.resolve(__dirname, 'build');
+const ProjPath = __dirname;
+
+module.exports = {
+  target: 'node',
+  mode: 'production',
+  entry: path.resolve(SrcPath, 'server.ts'),
+  output: {
+    path: BuildPath,
+    filename: 'index.js',
+  },
+  module: {
+    rules: [{ test: /\.tsx?$/, exclude: [/node_modules/], loader: 'ts-loader' }],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new CopyPlugin([
+      { from: path.resolve(ProjPath, 'settings.json'), to: BuildPath },
+      { from: path.resolve(ProjPath, 'storage.json'), to: BuildPath },
+    ]),
+  ],
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+};
