6f924265d741f7f057c541d314b4745f1dc1f075
Tianyu You <youtianyu2010@live.cn>
2019-04-03 15:23:33 +0800

    make header stick on the top and base table for lg editor

--- Composer/packages/client/src/pages/content/lg-settings/index.js
+++ Composer/packages/client/src/pages/content/lg-settings/index.js
@@ -1,23 +1,106 @@
+/* eslint-disable react/display-name */
+/* eslint-disable no-unused-vars */
 /** @jsx jsx */
 import { jsx } from '@emotion/core';
 import { Fragment, useContext } from 'react';
+import { TooltipHost } from 'office-ui-fabric-react/lib/Tooltip';
+import { ScrollablePane, ScrollbarVisibility } from 'office-ui-fabric-react/lib/ScrollablePane';
+import { Sticky, StickyPositionType } from 'office-ui-fabric-react/lib/Sticky';
+import { DetailsList, DetailsListLayoutMode, Selection, SelectionMode } from 'office-ui-fabric-react/lib/DetailsList';
 
 import { Store } from '../../../store/index';
+
+import { scrollablePaneRoot, title, label } from './styles';
 
 export function LanguageGenerationSettings() {
   const { state } = useContext(Store);
   const { files } = state;
 
   const lgFiles = files.filter(file => file.name.includes('.lg'));
-  let currentFileContent = '';
-  if (lgFiles && lgFiles.length > 0) {
-    currentFileContent = lgFiles[0].content;
+  const currentFileContent = lgFiles.length > 0 ? lgFiles[0].content.content : '';
+
+  function onRenderDetailsHeader(props, defaultRender) {
+    return (
+      <Sticky stickyPosition={StickyPositionType.Header} isScrollSynced={true}>
+        {defaultRender({
+          ...props,
+          // eslint-disable-next-line react/display-name
+          onRenderColumnHeaderTooltip: tooltipHostProps => <TooltipHost {...tooltipHostProps} />,
+        })}
+      </Sticky>
+    );
   }
+  const templates = [];
+
+  const items = [];
+  if (templates) {
+    templates.forEach(template => {
+      items.push({
+        name: template.name,
+        value: template.name,
+        fileType: template.type,
+        content: template.content,
+      });
+    });
+  }
+
+  const tableColums = [
+    {
+      key: 'name',
+      name: 'Name',
+      fieldName: 'name',
+      minWidth: 50,
+      maxWidth: 90,
+      isRowHeader: true,
+      isResizable: true,
+      data: 'string',
+      onRender: item => {
+        return <span>{item.name}</span>;
+      },
+    },
+    {
+      key: 'type',
+      name: 'Type',
+      fieldName: 'type',
+      minWidth: 50,
+      maxWidth: 90,
+      data: 'string',
+      isPadded: true,
+      onRender: item => {
+        return <span>{item.type}</span>;
+      },
+    },
+    {
+      key: 'phrase',
+      name: 'Sample phrase',
+      fieldName: 'samplePhrase',
+      minWidth: 350,
+      isResizable: true,
+      data: 'string',
+      isPadded: true,
+      onRender: item => {
+        return <span>{item.content}</span>;
+      },
+    },
+  ];
+
   return (
     <Fragment>
       <div>
-        <div>Language Understanding</div>
-        <div>{currentFileContent}</div>
+        <div css={title}>Content &gt; Language Understanding</div>
+        <div css={label}>Templates</div>
+        <ScrollablePane css={scrollablePaneRoot} scrollbarVisibility={ScrollbarVisibility.auto}>
+          <DetailsList
+            items={items}
+            compact={true}
+            columns={tableColums}
+            setKey="key"
+            layoutMode={DetailsListLayoutMode.fixedColumns}
+            onRenderDetailsHeader={onRenderDetailsHeader}
+            selectionMode={SelectionMode.none}
+            selectionPreservedOnEmptyClick={true}
+          />
+        </ScrollablePane>
       </div>
     </Fragment>
   );
