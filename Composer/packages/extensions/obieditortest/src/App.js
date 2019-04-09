c053c5fc006cf42f6c60a483b178832bff05c28a
Andy <asbrown002@gmail.com>
2019-04-09 10:29:29 -0700

    fix lint errors

--- Composer/packages/extensions/obieditortest/src/App.js
+++ Composer/packages/extensions/obieditortest/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { Customizer } from 'office-ui-fabric-react';
 import { FluentCustomizations } from '@uifabric/fluent-theme';
