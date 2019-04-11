dbf230f621478302443b08d48dc41f015a9f394d
zeye <zeye@microsoft.com>
2019-04-11 16:02:39 +0800

    hoist BeginDialog action to rule level

--- Composer/packages/extensions/visual-designer/src/components/nodes/IntentRule.js
+++ Composer/packages/extensions/visual-designer/src/components/nodes/IntentRule.js
@@ -1,19 +1,50 @@
 import React from 'react';
 
+import { ObiTypes } from '../../transformers/constants/ObiTypes';
 import { NodeClickActionTypes } from '../../utils/constant';
+import { normalizeObiStep } from '../../transformers/helpers/elementBuilder';
 
 import { FormCard } from './templates/FormCard';
 import { NodeProps, defaultNodeProps } from './sharedProps';
 
 export class IntentRule extends React.Component {
+  getDetails() {
+    const { steps } = this.props.data;
+    if (Array.isArray(steps) && steps.length === 1) {
+      const normalizedStep = normalizeObiStep(steps[0]);
+      if (normalizedStep.$type === ObiTypes.BeginDialog) {
+        return (
+          <div>
+            Begin Dialog:
+            <span
+              style={{
+                cursor: 'pointer',
+                color: 'blue',
+                marginLeft: 5,
+              }}
+              onClick={e => {
+                e.stopPropagation();
+                this.props.onEvent(NodeClickActionTypes.OpenLink, normalizedStep.dialog);
+              }}
+            >
+              {normalizedStep.dialog}
+            </span>
+          </div>
+        );
+      }
+    }
+    return 'Task';
+  }
+
   render() {
     const { id, data, onEvent } = this.props;
     const { intent, steps } = data;
+    const x = this.getDetails();
     return (
       <FormCard
         themeColor="#0078D4"
-        header="Decision"
-        label={intent}
+        header={intent}
+        label={this.getDetails()}
         onClick={() => {
           if (Array.isArray(steps) && steps.length) {
             onEvent(NodeClickActionTypes.Expand, id);
