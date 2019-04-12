aa565471ced86c16aa95bf3d5a8f76dfe029be51
Chris Whitten <christopher.whitten@microsoft.com>
2019-04-12 13:43:34 -0700

    Introduces format-message for an i18n api (#171)

--- Composer/packages/client/src/components/NotFound/index.js
+++ Composer/packages/client/src/components/NotFound/index.js
@@ -1,5 +1,6 @@
 /** @jsx jsx */
 import { jsx } from '@emotion/core';
+import formatMessage from 'format-message';
 
 import { notfoundbody, notfoundcontainer, description, notfoundicon } from './style';
 
@@ -8,7 +9,7 @@ export const NotFound = () => {
     <div css={notfoundbody}>
       <div css={notfoundcontainer}>
         <div>
-          <div css={description}>{"The page you are looking for can't be found."}</div>
+          <div css={description}>{formatMessage("The page you are looking for can't be found.")}</div>
           <div css={notfoundicon}>{'404'}</div>
         </div>
       </div>
