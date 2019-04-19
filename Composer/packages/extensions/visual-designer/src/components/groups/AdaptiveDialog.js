1ca44c892313357e02e3466d30559d1ed317feb1
zeye <zeye@microsoft.com>
2019-04-19 13:40:38 +0800

    optimize AdaptiveDialog

--- Composer/packages/extensions/visual-designer/src/components/groups/AdaptiveDialog.js
+++ Composer/packages/extensions/visual-designer/src/components/groups/AdaptiveDialog.js
@@ -4,6 +4,8 @@ import { NodeProps, defaultNodeProps } from '../shared/sharedProps';
 import { RootDialogStrategy } from '../../transformers/strategies/RootDialogStrategy';
 import { NodeRenderer } from '../NodeRenderer';
 import { OffsetContainer } from '../OffsetContainer';
+import { GraphNode } from '../shared/GraphNode';
+import { NodeClickActionTypes } from '../../utils/constant';
 
 const ContainerPaddingX = 50;
 const ContainerPaddingY = 50;
@@ -13,19 +15,30 @@ export class AdaptiveDialog extends React.Component {
   width = 0;
   height = 0;
 
-  nodes = {};
+  elements = {
+    recognizerNode: new GraphNode(),
+    eventGroupNode: new GraphNode(),
+    intentGroupNode: new GraphNode(),
+  };
 
   componentDidMount() {
     this.updateLayoutAfterChildrenMount();
     this.forceUpdate();
   }
 
-  updateNodes(recognizer, eventGroup, intentGroup) {
-    const nodes = this.nodes;
-    if (recognizer) nodes.recognizerNode = { ...nodes.recognizerNode, ref: React.createRef() };
-    if (eventGroup) nodes.eventGroupNode = { ...nodes.eventGroupNode, ref: React.createRef() };
-    if (intentGroup) nodes.intentGroupNode = { ...nodes.intentGroupNode, ref: React.createRef() };
-    return nodes;
+  updateElements() {
+    const { elements } = this;
+    const { recognizers, eventGroup, intentGroup } = RootDialogStrategy.selectNodes(this.props.data);
+
+    if (Array.isArray(recognizers) && recognizers.length) {
+      elements.recognizerNode.props = this.buildProps(recognizers[0]);
+    }
+    if (eventGroup) {
+      elements.eventGroupNode.props = this.buildProps(eventGroup);
+    }
+    if (intentGroup) {
+      elements.intentGroupNode.props = this.buildProps(intentGroup);
+    }
   }
 
   getBoundary() {
@@ -38,7 +51,7 @@ export class AdaptiveDialog extends React.Component {
   }
 
   updateLayoutAfterChildrenMount() {
-    const { recognizerNode, eventGroupNode, intentGroupNode } = this.nodes;
+    const { recognizerNode, eventGroupNode, intentGroupNode } = this.elements;
     [recognizerNode, eventGroupNode, intentGroupNode].forEach(x => (x.boundary = x.ref.current.getBoundary()));
 
     const maxWidth = Math.max(...[recognizerNode, eventGroupNode, intentGroupNode].map(x => x.boundary.width));
@@ -70,30 +83,34 @@ export class AdaptiveDialog extends React.Component {
   }
 
   render() {
-    const { data } = this.props;
-    const { eventGroup, intentGroup, recognizers } = RootDialogStrategy.selectNodes(data);
+    this.updateElements();
 
-    this.nodes = this.updateNodes(recognizers[0], eventGroup, intentGroup);
-    const { recognizerNode, eventGroupNode, intentGroupNode } = this.nodes;
+    const { recognizerNode, eventGroupNode, intentGroupNode } = this.elements;
 
-    const recognizer = recognizerNode && (
+    const recognizer = recognizerNode.props && (
       <OffsetContainer offset={recognizerNode.offset}>
-        <NodeRenderer key="recognizer" ref={recognizerNode.ref} {...this.buildProps(recognizers[0])} />
+        <NodeRenderer key="recognizer" ref={recognizerNode.ref} {...recognizerNode.props} />
       </OffsetContainer>
     );
-    const events = eventGroupNode && (
+    const events = eventGroupNode.props && (
       <OffsetContainer offset={eventGroupNode.offset}>
-        <NodeRenderer key="eventGroup" ref={eventGroupNode.ref} {...this.buildProps(eventGroup)} />
+        <NodeRenderer key="eventGroup" ref={eventGroupNode.ref} {...eventGroupNode.props} />
       </OffsetContainer>
     );
-    const intents = intentGroupNode && (
+    const intents = intentGroupNode.props && (
       <OffsetContainer offset={intentGroupNode.offset}>
-        <NodeRenderer key="intentGroup" ref={intentGroupNode.ref} {...this.buildProps(intentGroup)} />
+        <NodeRenderer key="intentGroup" ref={intentGroupNode.ref} {...intentGroupNode.props} />
       </OffsetContainer>
     );
 
     return (
-      <div style={{ width: this.width, height: this.height, position: 'relative' }}>
+      <div
+        style={{ width: this.width, height: this.height, position: 'relative' }}
+        onClick={e => {
+          e.stopPropagation();
+          this.props.onEvent(NodeClickActionTypes.Focus, '');
+        }}
+      >
         {recognizer}
         {events}
         {intents}
---
+++ Composer/packages/extensions/visual-designer/src/components/shared/GraphNode.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export class GraphNode {
+  ref = React.createRef();
+  offset = { x: 0, y: 0 };
+  props;
+}
