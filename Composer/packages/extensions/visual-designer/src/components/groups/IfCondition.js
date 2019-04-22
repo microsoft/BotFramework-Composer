0986a85870dbb9c3d1f3ece964fb6dbae2e4bb6c
zeye <zeye@microsoft.com>
2019-04-22 15:08:15 +0800

    implement IfCondition parser and node layout

--- Composer/packages/extensions/visual-designer/src/components/groups/IfCondition.js
+++ Composer/packages/extensions/visual-designer/src/components/groups/IfCondition.js
@@ -1,20 +1,26 @@
 import React from 'react';
 
+import { transformIfCondtiom } from '../../transformers/transformIfCondition';
 import { NodeClickActionTypes } from '../../shared/NodeClickActionTypes';
 import { Diamond } from '../nodes/templates/Diamond';
 import { NodeProps, defaultNodeProps } from '../shared/sharedProps';
 import { GraphObjectModel } from '../shared/GraphObjectModel';
 import { DynamicLayoutComponent } from '../shared/DynamicLayoutComponent';
+import { OffsetContainer } from '../OffsetContainer';
+import { NodeRenderer } from '../NodeRenderer';
+
+const ChoiceNodeWidth = 150;
+const ChoiceNodeHeight = 50;
+const BranchIntervalX = 30;
+const BranchIntervalY = 30;
 
 export class IfCondition extends DynamicLayoutComponent {
   width = 150;
   height = 20;
 
-  elements = {
-    choiceNode: new GraphObjectModel(),
-    trueNodes: new GraphObjectModel(),
-    falseNodes: new GraphObjectModel(),
-  };
+  choiceNode = null;
+  ifGroupNode = null;
+  elseGroupNode = null;
 
   getBoundary() {
     return {
@@ -25,14 +31,117 @@ export class IfCondition extends DynamicLayoutComponent {
     };
   }
 
-  render() {
+  computeProps(props) {
+    const { id, data, focusedId, onEvent } = props;
+    const { choice, ifGroup, elseGroup } = transformIfCondtiom(data, id);
+
+    const createGraphNode = input => {
+      const result = new GraphObjectModel();
+      result.props = {
+        id: input.id,
+        data: input.json,
+        focusedId: focusedId,
+        onEvent: (...args) => onEvent(...args),
+      };
+      return result;
+    };
+
+    this.choiceNode = createGraphNode(choice);
+    this.ifGroupNode = ifGroup ? createGraphNode(ifGroup) : null;
+    this.elseGroupNode = elseGroup ? createGraphNode(elseGroup) : null;
+  }
+
+  measureLayout() {
+    const { choiceNode, ifGroupNode, elseGroupNode } = this;
+    const branchNodes = [ifGroupNode, elseGroupNode];
+    branchNodes
+      .filter(x => !!x)
+      .forEach(x => {
+        x.boundary = x.ref.current.getBoundary();
+      });
+    const [ifWidth, elseWith] = branchNodes.map(x => (x ? x.boundary.width : 0));
+    const [ifHeight, elseHeight] = branchNodes.map(x => (x ? x.boundary.height : 0));
+
+    const flag = (ifGroupNode ? '1' : '0') + (elseGroupNode ? '1' : '0');
+    switch (flag) {
+      case '11':
+        /**
+         *      <Choice>  ----- [if]
+         *          |            |
+         *        [else]         |
+         *          |-------------
+         */
+        this.width = Math.max(ChoiceNodeWidth, elseWith) + BranchIntervalX + ifWidth;
+        this.height = Math.max(ChoiceNodeHeight + BranchIntervalY + elseHeight, ifHeight + BranchIntervalY);
+
+        choiceNode.offset = { x: (Math.max(ChoiceNodeWidth, elseWith) - ChoiceNodeWidth) / 2, y: 0 };
+        elseGroupNode.offset = {
+          x: (Math.max(ChoiceNodeWidth, elseWith) - elseWith) / 2,
+          y: ChoiceNodeHeight + BranchIntervalY,
+        };
+        ifGroupNode.offset = { x: Math.max(ChoiceNodeWidth, elseWith) + BranchIntervalX, y: 0 };
+        break;
+      case '10':
+        /**
+         *      <Choice>----[if]
+         *         |         |
+         *         |         |
+         *         |----------
+         */
+        this.width = ChoiceNodeWidth + BranchIntervalX + ifWidth;
+        this.height = Math.max(ChoiceNodeHeight, ifHeight) + BranchIntervalY;
+
+        choiceNode.offset = { x: 0, y: 0 };
+        ifGroupNode.offset = { x: ChoiceNodeWidth + BranchIntervalX, y: 0 };
+        break;
+      case '01':
+        /**
+         *      <Choice> ----
+         *          |       |
+         *        [else]    |
+         *          |--------
+         */
+        this.width = Math.max(ChoiceNodeWidth, elseWith) + BranchIntervalX;
+        this.height = ChoiceNodeHeight + BranchIntervalY * 2;
+
+        choiceNode.offset = { x: (Math.max(ChoiceNodeWidth, elseWith) - ChoiceNodeWidth) / 2, y: 0 };
+        elseGroupNode.offset = {
+          x: (Math.max(ChoiceNodeWidth, elseWith) - elseWith) / 2,
+          y: ChoiceNodeHeight + BranchIntervalY,
+        };
+        break;
+      case '00':
+        /**
+         *    <Choice>
+         */
+        this.width = ChoiceNodeWidth;
+        this.height = ChoiceNodeHeight;
+        choiceNode.offset = { x: 0, y: 0 };
+        break;
+      default:
+        break;
+    }
+  }
+
+  renderContent() {
     const { id, onEvent } = this.props;
     return (
-      <Diamond
-        onClick={() => {
-          onEvent(NodeClickActionTypes.Focus, id);
-        }}
-      />
+      <div style={{ width: this.width, height: this.height, position: 'relative' }}>
+        <OffsetContainer offset={this.choiceNode.offset}>
+          <Diamond
+            onClick={() => {
+              onEvent(NodeClickActionTypes.Focus, id);
+            }}
+          />
+        </OffsetContainer>
+        {[this.ifGroupNode, this.elseGroupNode]
+          .filter(x => !!x)
+          .map((x, index) => (
+            <OffsetContainer key={`${id}.branches[${index}].offset`} offset={x.offset}>
+              <NodeRenderer key={`${id}.branches[${index}]`} ref={x.ref} {...x.props} />
+            </OffsetContainer>
+          ))}
+      </div>
     );
   }
 }
---
+++ Composer/packages/extensions/visual-designer/src/transformers/transformIfCondition.js
@@ -0,0 +1,29 @@
+import { ObiTypes } from './constants/ObiTypes';
+import { IndexedNode } from './models/IndexedNode';
+
+export function transformIfCondtiom(input, jsonpath) {
+  if (!input || input.$type !== ObiTypes.IfCondition) return {};
+
+  const result = {
+    choice: new IndexedNode(`${jsonpath}`, {
+      $type: 'Choice',
+      text: input.condition,
+    }),
+  };
+
+  const { ifTrue, ifFalse } = input;
+  if (Array.isArray(ifTrue) && ifTrue.length) {
+    result.ifGroup = new IndexedNode(`${jsonpath}.ifTrue`, {
+      $type: ObiTypes.StepGroup,
+      children: ifTrue.map((x, index) => new IndexedNode(`${jsonpath}.ifTrue[${index}]`, x)),
+    });
+  }
+  if (Array.isArray(ifFalse) && ifFalse.length) {
+    result.elseGroup = new IndexedNode(`${jsonpath}.ifFalse`, {
+      $type: ObiTypes.StepGroup,
+      children: ifFalse.map((x, index) => new IndexedNode(`${jsonpath}.ifFalse[${index}]`, x)),
+    });
+  }
+
+  return result;
+}
