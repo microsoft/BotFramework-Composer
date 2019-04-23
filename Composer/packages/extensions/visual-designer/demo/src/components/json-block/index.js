f90d6634f47e0141c0056656b4938d7c71a5df29
zeye <zeye@microsoft.com>
2019-04-23 16:31:23 +0800

    simplify the demo/json-block component

--- Composer/packages/extensions/visual-designer/demo/src/components/json-block/index.js
+++ Composer/packages/extensions/visual-designer/demo/src/components/json-block/index.js
@@ -6,25 +6,14 @@ export class JsonBlock extends Component {
   state = {
     defaultValue: {},
     displayedText: '',
-    mode: 'controlled',
   };
 
   constructor(props) {
     super(props);
-    if (props.value === undefined && props.onSubmit) {
-      // Uncontrolled component, use 'onSubmit' + 'defaultValue
-      this.state = {
-        defaultValue: props.defaultValue,
-        displayedText: JSON.stringify(props.defaultValue, null, '\t'),
-        mode: 'uncontrolled',
-      };
-    } else {
-      // Controlled component, use 'onChange' + 'value'
-      this.state = {
-        displayedText: JSON.stringify(props.value, null, '\t'),
-        mode: 'controlled',
-      };
-    }
+    this.state = {
+      defaultValue: props.defaultValue,
+      displayedText: JSON.stringify(props.defaultValue, null, '\t'),
+    };
   }
 
   tryParseJson(text) {
@@ -37,65 +26,36 @@ export class JsonBlock extends Component {
 
   onEditText(event) {
     const text = event.target.value;
-    const json = this.tryParseJson(text);
 
-    if (json && this.state.mode === 'controlled') {
-      this.props.onChange(json);
-    } else {
-      this.setState({
-        displayedText: text,
-      });
-    }
+    this.setState({
+      displayedText: text,
+    });
   }
 
-  onSubmitJson() {
-    if (this.state.mode !== 'uncontrolled') return;
-
+  onSubmit() {
     this.props.onSubmit(this.tryParseJson(this.state.displayedText));
   }
 
-  onResetJson() {
-    if (this.state.mode !== 'uncontrolled') return;
-
+  onReset() {
     this.setState({
       displayedText: JSON.stringify(this.state.defaultValue, null, '\t'),
     });
   }
 
-  renderTextarea() {
-    return (
-      <textarea
-        style={{ width: this.props.width, height: this.props.height, font: 'initial', fontSize: '20px' }}
-        value={this.state.displayedText}
-        onChange={this.onEditText.bind(this)}
-      />
-    );
-  }
-
-  renderControlled() {
-    return <Fragment>{this.renderTextarea()}</Fragment>;
-  }
-
-  renderUncontrolled() {
+  render() {
     return (
       <Fragment>
-        {this.renderTextarea()}
+        <textarea
+          style={{ width: this.props.width, height: this.props.height, font: 'initial', fontSize: '20px' }}
+          value={this.state.displayedText}
+          onChange={this.onEditText.bind(this)}
+        />
         <div>
-          <button onClick={this.onSubmitJson.bind(this)}>Submit</button>
-          <button onClick={this.onResetJson.bind(this)}>Reset</button>
+          <button onClick={this.onSubmit.bind(this)}>Submit</button>
+          <button onClick={this.onReset.bind(this)}>Reset</button>
         </div>
       </Fragment>
     );
-  }
-
-  render() {
-    if (this.state.mode === 'controlled') {
-      return this.renderControlled();
-    } else if (this.state.mode === 'uncontrolled') {
-      return this.renderUncontrolled();
-    } else {
-      return <div>{this.state.mode}</div>;
-    }
   }
 }
 
@@ -107,8 +67,6 @@ JsonBlock.defaultProps = {
 
 JsonBlock.propTypes = {
   defaultValue: PropTypes.object,
-  value: PropTypes.object,
-  onChange: PropTypes.func,
   onSubmit: PropTypes.func,
   width: PropTypes.number,
   height: PropTypes.number,
