3d207c62d4e2196b49a63a68f1794808d47d014a
zeye <zeye@microsoft.com>
2019-04-25 19:43:12 +0800

    simplify RecognizerGroup

--- Composer/packages/extensions/visual-designer/src/components/groups/RecognizerGroup.js
+++ Composer/packages/extensions/visual-designer/src/components/groups/RecognizerGroup.js
@@ -1,112 +1,46 @@
 import React from 'react';
 
 import { NodeProps, defaultNodeProps } from '../shared/sharedProps';
-import { NodeRenderer } from '../shared/NodeRenderer';
-import { GraphObjectModel } from '../shared/GraphObjectModel';
 import { NodeClickActionTypes } from '../../shared/NodeClickActionTypes';
-import { DynamicLayoutComponent } from '../shared/DynamicLayoutComponent';
-import { OffsetContainer } from '../shared/OffsetContainer';
-import { Boundary } from '../shared/Boundary';
+import { NodeRenderer } from '../shared/NodeRenderer';
 
 import { EventGroup } from './EventGroup';
 import { IntentGroup } from './IntentGroup';
 
-const ElementIntervalVertical = 30;
+const ElementMargin = 10;
 
-export class RecognizerGroup extends DynamicLayoutComponent {
-  width = 0;
-  height = 0;
-
-  elements = [];
-
-  computeProps(props) {
-    const { recognizer, eventGroup, intentGroup } = props.data;
-
-    const createGraphNode = input => {
-      if (!input) return null;
-      const result = new GraphObjectModel();
-      result.props = {
-        id: input.id,
-        data: input.json,
-        onEvent: (...args) => this.props.onEvent(...args),
-      };
-      return result;
-    };
-
-    this.elements = {
-      recognizer: createGraphNode(recognizer),
-      eventGroup: createGraphNode(eventGroup),
-      intentGroup: createGraphNode(intentGroup),
-    };
-  }
-
-  measureLayout() {
-    const nodes = Object.values(this.elements).filter(x => !!x);
-    // Measure node size
-    nodes.forEach(x => {
-      if (x.ref.current.getBoundary) {
-        x.boundary = x.ref.current.getBoundary();
-      } else {
-        x.boundary = new Boundary(x.ref.current.scrollWidth, x.ref.current.scrollHeight);
-      }
-    });
-
-    // Measure container size
-    this.width = Math.max(0, ...nodes.map(x => x.boundary.width));
-    this.height =
-      nodes.map(x => x.boundary.height).reduce((sum, val) => sum + val, 0) +
-      ElementIntervalVertical * Math.max(nodes.length - 1, 0);
-
-    nodes.reduce((offsetY, node) => {
-      node.offset = { x: (this.width - node.boundary.width) / 2, y: offsetY };
-      return offsetY + node.boundary.height + ElementIntervalVertical;
-    }, 0);
-  }
-
-  renderElement(element, key) {
-    if (!element) return null;
-    let content = null;
-    switch (key) {
-      case 'recognizer':
-        content = <NodeRenderer key={key} focusedId={this.props.focusedId} ref={element.ref} {...element.props} />;
-        break;
-      case 'eventGroup':
-        content = (
-          <div ref={element.ref}>
-            <EventGroup key={key} focusedId={this.props.focusedId} {...element.props} />
-          </div>
-        );
-        break;
-      case 'intentGroup':
-        content = (
-          <div ref={element.ref}>
-            <IntentGroup key={key} focusedId={this.props.focusedId} {...element.props} />
-          </div>
-        );
-        break;
+export const RecognizerGroup = ({ data, focusedId, onEvent }) => {
+  const renderElement = (node, Renderer) => {
+    if (node) {
+      return (
+        <div style={{ margin: ElementMargin }}>
+          <Renderer key={node.id} id={node.id} data={node.json} focusedId={focusedId} onEvent={onEvent} />
+        </div>
+      );
     }
-    if (!content) return null;
-    return (
-      <OffsetContainer key={`${key}.offset`} offset={element.offset}>
-        {content}
-      </OffsetContainer>
-    );
-  }
+    return null;
+  };
 
-  renderContent() {
-    return (
-      <div
-        style={{ width: this.width, height: this.height, position: 'relative' }}
-        onClick={e => {
-          e.stopPropagation();
-          this.props.onEvent(NodeClickActionTypes.Focus, '');
-        }}
-      >
-        {Object.keys(this.elements).map(key => this.renderElement(this.elements[key], key))}
-      </div>
-    );
-  }
-}
+  const { recognizer, eventGroup, intentGroup } = data;
+  return (
+    <div
+      style={{
+        position: 'relative',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+      }}
+      onClick={e => {
+        e.stopPropagation();
+        onEvent(NodeClickActionTypes.Focus, '');
+      }}
+    >
+      {renderElement(recognizer, NodeRenderer)}
+      {renderElement(eventGroup, EventGroup)}
+      {renderElement(intentGroup, IntentGroup)}
+    </div>
+  );
+};
 
 RecognizerGroup.propTypes = NodeProps;
 RecognizerGroup.defaultProps = defaultNodeProps;
