a1f9d04a2cddb848459ebe6f525e6d5bdd6761d4
zeye <zeye@microsoft.com>
2019-04-29 17:54:55 +0800

    support another case schema

--- Composer/packages/extensions/visual-designer/src/transformers/transformSwitchCondition.js
+++ Composer/packages/extensions/visual-designer/src/transformers/transformSwitchCondition.js
@@ -14,16 +14,30 @@ export function transformSwitchCondition(input, jsonpath) {
     cases: [],
   };
 
-  if (cases) {
+  if (!cases) return result;
+
+  let caseList = [];
+  if (Array.isArray(cases)) {
+    caseList = cases.map(({ value, steps }, index) => {
+      const prefix = `${jsonpath}.cases[${index}]`;
+      return new IndexedNode(`${prefix}.stepGroup`, {
+        $type: ObiTypes.StepGroup,
+        label: value,
+        children: steps.map((x, index) => new IndexedNode(`${prefix}[${index}]`, x)),
+      });
+    });
+  } else {
     const orderedCases = Object.keys(cases).sort();
-    const caseNodes = orderedCases.map(caseName => {
+    caseList = orderedCases.map(caseName => {
       const prefix = `${jsonpath}.cases.${caseName}`;
       return new IndexedNode(`${prefix}.stepGroup`, {
         $type: ObiTypes.StepGroup,
+        label: caseName,
         children: cases[caseName].map((x, index) => new IndexedNode(`${prefix}[${index}]`, x)),
       });
     });
-    result.cases = caseNodes;
   }
+
+  result.cases = caseList;
   return result;
 }
