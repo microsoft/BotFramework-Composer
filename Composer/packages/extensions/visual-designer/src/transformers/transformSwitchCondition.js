d06b579294d84bcfef880cfcc23c38e1ca6a52a5
zeye <zeye@microsoft.com>
2019-04-30 12:11:37 +0800

    adapt to new SwitchCondition schema

--- Composer/packages/extensions/visual-designer/src/transformers/transformSwitchCondition.js
+++ Composer/packages/extensions/visual-designer/src/transformers/transformSwitchCondition.js
@@ -14,30 +14,15 @@ export function transformSwitchCondition(input, jsonpath) {
     cases: [],
   };
 
-  if (!cases) return result;
+  if (!cases || !Array.isArray(cases)) return result;
 
-  let caseList = [];
-  if (Array.isArray(cases)) {
-    caseList = cases.map(({ value, steps }, index) => {
-      const prefix = `${jsonpath}.cases[${index}]`;
-      return new IndexedNode(`${prefix}.stepGroup`, {
-        $type: ObiTypes.StepGroup,
-        label: value,
-        children: steps.map((x, index) => new IndexedNode(`${prefix}.steps[${index}]`, x)),
-      });
+  result.cases = cases.map(({ value, steps }, index) => {
+    const prefix = `${jsonpath}.cases[${index}]`;
+    return new IndexedNode(`${prefix}.stepGroup`, {
+      $type: ObiTypes.StepGroup,
+      label: value,
+      children: steps.map((x, index) => new IndexedNode(`${prefix}.steps[${index}]`, x)),
     });
-  } else {
-    const orderedCases = Object.keys(cases).sort();
-    caseList = orderedCases.map(caseName => {
-      const prefix = `${jsonpath}.cases.${caseName}`;
-      return new IndexedNode(`${prefix}.stepGroup`, {
-        $type: ObiTypes.StepGroup,
-        label: caseName,
-        children: cases[caseName].map((x, index) => new IndexedNode(`${prefix}.steps[${index}]`, x)),
-      });
-    });
-  }
-
-  result.cases = caseList;
+  });
   return result;
 }
