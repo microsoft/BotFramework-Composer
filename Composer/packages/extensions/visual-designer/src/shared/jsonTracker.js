478f984a0f0d9f283b04016ab25efb9ee82b3054
zeye <zeye@microsoft.com>
2019-05-10 13:04:36 +0800

    implement deleteNode in jsonTracker

--- Composer/packages/extensions/visual-designer/src/shared/jsonTracker.js
+++ Composer/packages/extensions/visual-designer/src/shared/jsonTracker.js
@@ -1,5 +1,56 @@
+function locateNode(dialog, path) {
+  if (!path) return null;
+
+  const selectors = path.split('.');
+  if (selectors.length === 0) {
+    return null;
+  }
+
+  if (selectors[0] === '$') {
+    selectors.shift();
+  }
+
+  // Locate the manipulated json node
+  let parentData = null;
+  let currentKey = null;
+  let currentData = dialog;
+
+  for (const selector of selectors) {
+    let objSelector = selector;
+    let arrayIndex;
+
+    const parseResult = selector.match(/(\w+)\[(\d+)\]/);
+    if (parseResult) {
+      [, objSelector, arrayIndex] = parseResult;
+      arrayIndex = parseInt(arrayIndex);
+    }
+
+    parentData = currentData;
+    currentData = parentData[objSelector];
+    currentKey = objSelector;
+
+    if (arrayIndex !== undefined) {
+      parentData = currentData;
+      currentData = parentData[arrayIndex];
+      currentKey = arrayIndex;
+    }
+  }
+
+  return { parentData, currentData, currentKey };
+}
+
 export function deleteNode(dialog, path) {
-  delete dialog.steps[0];
-  dialog.steps = dialog.steps.filter(x => x);
-  return { ...dialog };
+  const target = locateNode(dialog, path);
+  if (!target) return dialog;
+
+  const { parentData, currentKey } = target;
+  // Remove targetKey
+  if (Array.isArray(parentData) && typeof currentKey === 'number') {
+    parentData.splice(currentKey, 1);
+  } else {
+    delete parentData[currentKey];
+  }
+
+  // Deep copy to trigger re-render
+  return JSON.parse(JSON.stringify(dialog));
 }
