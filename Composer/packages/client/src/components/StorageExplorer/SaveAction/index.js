83ffb6accde92e5479ad1898e298cfdbddcc437a
Andy Brown <asbrown002@gmail.com>
2019-05-14 12:17:29 -0700

    add create bot and save as e2e tests

--- Composer/packages/client/src/components/StorageExplorer/SaveAction/index.js
+++ Composer/packages/client/src/components/StorageExplorer/SaveAction/index.js
@@ -45,6 +45,7 @@ export function SaveAction(props) {
             setError(false);
           }
         }}
+        data-testid="NewBotProjectInput"
       />{' '}
       {loadingStatus ? (
         <Spinner size={SpinnerSize.medium} css={loading} />
