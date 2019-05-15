7416a9b73a31c820c5f44342545d5b70e2940038
Andy Brown <asbrown002@gmail.com>
2019-05-15 12:44:21 -0700

    copy a bot instead of mutating a sample

--- Composer/cypress/integration/SwitchCondition.spec.js
+++ Composer/cypress/integration/SwitchCondition.spec.js
@@ -3,7 +3,7 @@
 context('SwitchCondition', () => {
   beforeEach(() => {
     cy.visit(Cypress.env('COMPOSER_URL'));
-    cy.openBot('01 - Steps');
+    cy.copyBot('01 - Steps', 'SwitchConditionSpec');
   });
 
   it('can manage cases', () => {
