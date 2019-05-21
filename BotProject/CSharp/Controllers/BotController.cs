ebda6998146cb4c95c9416136cf3336e4fb19a30
Hongyang Du <hond@microsoft.com>
2019-05-21 16:35:56 +0800

    Add file upload API

--- BotProject/CSharp/Controllers/BotController.cs
+++ BotProject/CSharp/Controllers/BotController.cs
@@ -3,11 +3,19 @@
 //
 // Generated with Bot Builder V4 SDK Template for Visual Studio EchoBot v4.3.0
 
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Bot.Builder;
 using Microsoft.Bot.Builder.Dialogs.Debugging;
+using Microsoft.Bot.Builder.Dialogs.Declarative;
+using Microsoft.Bot.Builder.Dialogs.Declarative.Resources;
 using Microsoft.Bot.Builder.Integration.AspNet.Core;
 
 namespace Microsoft.Bot.Builder.TestBot.Json
@@ -29,11 +37,104 @@ namespace Microsoft.Bot.Builder.TestBot.Json
         }
 
         [HttpPost]
-        public async Task PostAsync()
+        public async Task PostAsync(ICollection<IFormFile> files)
         {
-            // Delegate the processing of the HTTP POST to the adapter.
-            // The adapter will invoke the bot.
-            await Adapter.ProcessAsync(Request, Response, Bot);
+            if (files != null && files.Count > 0)
+            {
+                var folder = await HandleFileResource(files.First());
+                if (!string.IsNullOrEmpty(folder) && Adapter is BotFrameworkHttpAdapter bfAdapter)
+                {
+                    var resourceExplorer = new ResourceExplorer();
+                    resourceExplorer.AddFolder(folder);
+
+                    bfAdapter.UseResourceExplorer(resourceExplorer, () => { });
+                }
+            }
+            else
+            {
+                // Delegate the processing of the HTTP POST to the adapter.
+                // The adapter will invoke the bot.
+                await Adapter.ProcessAsync(Request, Response, Bot);
+            }
+        }
+
+
+        private async Task<string> HandleFileResource(IFormFile file)
+        {
+            // TODO should take a folder to place the resource
+            var rootDirectory = @"d:\tmp";
+
+            var resultFolder = string.Empty;
+
+            InitRootDirectory(rootDirectory);
+
+            var fileName = file.FileName;
+            if (IsZipFile(fileName))
+            {
+                var zipFilePath = await SaveFormFile(file, rootDirectory);
+                var directoryPath = Path.Combine(rootDirectory, fileName.Substring(0, fileName.LastIndexOf('.')));
+                resultFolder = ExtractZipFile(zipFilePath, directoryPath);
+            }
+
+            return resultFolder;
+        }
+
+        private bool IsZipFile(string fileName)
+        {
+            if(string.IsNullOrWhiteSpace(fileName))
+            {
+                return false;
+            }
+
+            return fileName.EndsWith(".zip", StringComparison.OrdinalIgnoreCase);
+        }
+
+        private string ExtractZipFile(string zipFilePath, string directoryPath)
+        {
+            CleanDirectory(directoryPath);
+            ZipFile.ExtractToDirectory(zipFilePath, directoryPath);
+            CleanFile(zipFilePath);
+
+            return directoryPath;
+        }
+
+        private async Task<string> SaveFormFile(IFormFile file, string rootDirectory)
+        {
+            var filePath = Path.Combine(rootDirectory, file.FileName);
+            CleanFile(filePath);
+
+            using (var fileStream = new FileStream(filePath, FileMode.OpenOrCreate))
+            {
+                await file.CopyToAsync(fileStream);
+            }
+
+            return filePath;
+        }
+
+        private void InitRootDirectory(string rootDirectory)
+        {
+            var rdInfo = new DirectoryInfo(rootDirectory);
+            if (!rdInfo.Exists)
+            {
+                rdInfo.Create();
+            }
+        }
+
+        private void CleanDirectory(string directoryPath)
+        {
+            var di = new DirectoryInfo(directoryPath);
+            if (di.Exists)
+            {
+                di.Delete(true);
+            }
+        }
+
+        private void CleanFile(string filePath)
+        {
+            if (System.IO.File.Exists(filePath))
+            {
+                System.IO.File.Delete(filePath);
+            }
         }
     }
 }
