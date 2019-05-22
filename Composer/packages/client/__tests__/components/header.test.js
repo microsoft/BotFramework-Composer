229058eee2add5498442078e99f7776dec31803e
Wenyi Luo <wenyluo@microsoft.com>
2019-05-22 16:04:48 +0800

    fix header test

--- Composer/packages/client/__tests__/components/header.test.js
+++ Composer/packages/client/__tests__/components/header.test.js
@@ -1,9 +1,11 @@
 import * as React from 'react';
-import { fireEvent, render } from 'react-testing-library';
+import { cleanup, fireEvent, render, waitForElement } from 'react-testing-library';
 
 import { Header } from '../../src/components/Header';
 
 describe('<Header />', () => {
+  afterEach(cleanup);
+
   it('should render the header', () => {
     const { container } = render(<Header />);
 
@@ -14,25 +16,28 @@ describe('<Header />', () => {
     expect(container).toHaveTextContent('Start');
   });
 
-  it('should open storage explorer', () => {
+  it('should open storage explorer', async () => {
     const mockOpenStorageExplorer = jest.fn(() => null);
     const { getByText } = render(<Header openStorageExplorer={mockOpenStorageExplorer} />);
-    fireEvent.click(getByText(/New/));
-    fireEvent.click(getByText(/Open/));
-    fireEvent.click(getByText(/Save as/));
+    const newButton = await waitForElement(() => getByText(/New/));
+    const openButton = await waitForElement(() => getByText(/Open/));
+    const saveButton = await waitForElement(() => getByText(/Save as/));
+    fireEvent.click(newButton);
+    fireEvent.click(openButton);
+    fireEvent.click(saveButton);
     expect(mockOpenStorageExplorer).toHaveBeenCalledTimes(3);
   });
 
-  it('should set bot status', () => {
+  it('should set bot status', async () => {
     const mockSetBotStatus = jest.fn(() => null);
     const { getByText, rerender } = render(<Header setBotStatus={mockSetBotStatus} botStatus={'stopped'} />);
 
-    const startButton = getByText(/Start/);
+    const startButton = await waitForElement(() => getByText(/Start/));
     fireEvent.click(startButton);
     expect(mockSetBotStatus).toHaveBeenCalled();
 
     rerender(<Header setBotStatus={mockSetBotStatus} botStatus={'running'} />);
-    const connectButton = getByText('Connect and Test');
+    const connectButton = await waitForElement(() => getByText('Connect and Test'));
     fireEvent.click(connectButton);
   });
 });
