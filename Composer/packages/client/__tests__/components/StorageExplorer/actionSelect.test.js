85fd67ec5383fa4929f153a9ac6b31ad7e1f1c15
Wenyi Luo <wenyluo@microsoft.com>
2019-05-22 16:27:15 +0800

    fix some tests

--- Composer/packages/client/__tests__/components/StorageExplorer/actionSelect.test.js
+++ Composer/packages/client/__tests__/components/StorageExplorer/actionSelect.test.js
@@ -1,9 +1,10 @@
 import * as React from 'react';
-import { fireEvent, render } from 'react-testing-library';
+import { cleanup, fireEvent, render, waitForElement } from 'react-testing-library';
 
 import { ActionSelector } from './../../../src/components/StorageExplorer/ActionSelector/index';
 
 describe('<ActionSelect/>', () => {
+  afterEach(cleanup);
   it('should render the new storage modal', () => {
     const { container } = render(<ActionSelector />);
 
@@ -12,12 +13,15 @@ describe('<ActionSelect/>', () => {
     expect(container).toHaveTextContent(/Save As/);
   });
 
-  it('should close the explorer', () => {
+  it('should close the explorer', async () => {
     const mockOnLinkClick = jest.fn(() => null);
     const { getByText } = render(<ActionSelector onLinkClick={mockOnLinkClick} />);
-    fireEvent.click(getByText('New'));
-    fireEvent.click(getByText('Open'));
-    fireEvent.click(getByText('Save As'));
+    const newButton = await waitForElement(() => getByText(/New/));
+    const openButton = await waitForElement(() => getByText(/Open/));
+    const saveButton = await waitForElement(() => getByText(/Save As/));
+    fireEvent.click(newButton);
+    fireEvent.click(openButton);
+    fireEvent.click(saveButton);
     expect(mockOnLinkClick).toHaveBeenCalledTimes(3);
   });
 });
--- Composer/packages/client/__tests__/components/projecttree.test.js
+++ Composer/packages/client/__tests__/components/projecttree.test.js
@@ -1,11 +1,12 @@
 import * as React from 'react';
-import { fireEvent, render, waitForElement } from 'react-testing-library';
+import { fireEvent, render, waitForElement, cleanup } from 'react-testing-library';
 import { SharedColors } from '@uifabric/fluent-theme';
 
 import { dialogs } from '../constants.json';
 import { ProjectTree } from '../../src/components/ProjectTree/index.js';
 
 describe('<ProjectTree/>', () => {
+  afterEach(cleanup);
   it('should render the projecttree', async () => {
     const { getByText } = render(<ProjectTree files={dialogs} />);
 
@@ -18,16 +19,16 @@ describe('<ProjectTree/>', () => {
 
     const node = await waitForElement(() => getByText('ToDoBot.main'));
     fireEvent.click(node);
-    expect(mockFileSelect).toHaveBeenCalled();
+    expect(mockFileSelect).toHaveBeenCalledTimes(1);
   });
 
   it('font blue when project item active', async () => {
     const { rerender, getByText } = render(<ProjectTree files={dialogs} activeNode={0} />);
-    let node = getByText('ToDoBot.main');
+    let node = await waitForElement(() => getByText('ToDoBot.main'));
     expect(node.parentNode).toHaveStyle(`color: ${SharedColors.cyanBlue10};`);
 
     rerender(<ProjectTree files={dialogs} activeNode={1} />);
-    node = getByText('AddToDo');
+    node = await waitForElement(() => getByText('AddToDo'));
     expect(node.parentNode).toHaveStyle(`color: ${SharedColors.cyanBlue10};`);
   });
 });
