0b16607cf6c8b919472b937e58d2b6d6d61e4dd8
Wenyi Luo <wenyluo@microsoft.com>
2019-05-22 22:18:35 +0800

    fix path resolve bug in posix style

--- Composer/packages/server/src/utility/path.ts
+++ Composer/packages/server/src/utility/path.ts
@@ -3,7 +3,12 @@ import path from 'path';
 // handle all the path to unix pattern
 class PathHandler {
   resolve(...params: string[]) {
-    return path.resolve(...params).replace(/\\/g, '/');
+    const pathToTransform = path.join(...params);
+    if (path.isAbsolute(pathToTransform)) {
+      return pathToTransform.replace(/\\/g, '/');
+    } else {
+      return path.resolve(...params).replace(/\\/g, '/');
+    }
   }
   relative(from: string, to: string) {
     return path.relative(from, to).replace(/\\/g, '/');
