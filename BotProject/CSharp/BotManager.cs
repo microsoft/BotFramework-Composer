2ead7c73b0d22dd8b93ed3b564dd30e42b363fc3
Dong Lei <donglei@microsoft.com>
2019-05-25 13:54:49 +0800

    Move the bot switching logic to BotManager

--- BotProject/CSharp/BotManager.cs
+++ BotProject/CSharp/BotManager.cs
@@ -6,7 +6,13 @@ using Microsoft.Bot.Builder.Dialogs.Declarative.Resources;
 using Microsoft.Bot.Builder.Integration.AspNet.Core;
 using Microsoft.Bot.Builder.LanguageGeneration.Renderer;
 using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
 
 namespace Microsoft.Bot.Builder.TestBot.Json
 {
@@ -15,7 +21,7 @@ namespace Microsoft.Bot.Builder.TestBot.Json
         IBotFrameworkHttpAdapter CurrentAdapter { get; }
         IBot CurrentBot { get;  }
 
-        void SetCurrent(BotProject botProj);
+        void SetCurrent(Stream fileStream);
     }
 
     public class BotManager : IBotManager
@@ -24,16 +30,26 @@ namespace Microsoft.Bot.Builder.TestBot.Json
         {
             Config = config;
 
+            // init work dir
+            WorkDir = ConvertPath("tmp");
+            EnsureDirExists(WorkDir);
+            CleanDir(WorkDir);
+
+            // set init bot
             var bot = Config.GetSection("bot").Get<string>();
             var botProj = BotProject.Load(bot);
             SetCurrent(botProj);
         }
 
-        private IConfiguration Config { get; }
+        public IConfiguration Config { get; }
 
         public IBotFrameworkHttpAdapter CurrentAdapter { get; set; }
 
         public IBot CurrentBot { get; set; }
+
+        private string WorkDir { get; }
+
+        private static readonly object locker = new object();
 
         public void SetCurrent(BotProject botProject)
         {
@@ -67,6 +83,85 @@ namespace Microsoft.Bot.Builder.TestBot.Json
             CurrentAdapter = adapter;
 
             CurrentBot = new TestBot(rootDialog, conversationState, resourceExplorer, DebugSupport.SourceRegistry);
+        }
+
+        public void SetCurrent(Stream fileStream)
+        {
+            lock (locker)
+            {
+                // download file as tmp.zip
+                var downloadPath = SaveFile(fileStream, "tmp.zip").GetAwaiter().GetResult();
+
+                // extract to bot folder
+                var extractPath = ExtractFile(downloadPath, GenNewBotDir());
+
+                // locate the proj file
+                var projFile = FindBotProjFile(extractPath);
+
+                var botProj = BotProject.Load(projFile);
+                SetCurrent(botProj);
+            }
+        }
+
+        private string GenNewBotDir()
+        {
+            return System.Guid.NewGuid().ToString("N");
+        }
+
+        private async Task<string> SaveFile(Stream fileStream, string fileName)
+        {
+            EnsureDirExists(WorkDir);
+
+            var filePath = Path.Combine(WorkDir, fileName);
+            using (var outStream = new FileStream(filePath, FileMode.Create))
+            {
+                await fileStream.CopyToAsync(outStream);
+            }
+            return filePath;
+        }
+
+        // Extract file to a dir
+        private string ExtractFile(string filePath, string dstDirPath)
+        {
+            EnsureDirExists(WorkDir);
+
+            var finalDstPath = Path.Combine(WorkDir, dstDirPath);
+
+            ZipFile.ExtractToDirectory(filePath, finalDstPath);
+            return finalDstPath;
+        }
+
+        private string FindBotProjFile(string dir)
+        {
+            string[] projFiles = Directory.GetFiles(dir, "*.botproj");
+            if (projFiles.Length != 1)
+            {
+                throw new Exception("no bot proj file in zip file");
+            }
+            return projFiles[0];
+        }
+
+
+        public static string ConvertPath(string relativePath)
+        {
+            var curDir = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+            return Path.Combine(curDir, relativePath);
+        }
+
+        public static void EnsureDirExists(string dirPath)
+        {
+            var dirInfo = new DirectoryInfo(dirPath);
+            if (!dirInfo.Exists)
+            {
+                dirInfo.Create();
+            }
+        }
+
+        public static void CleanDir(string dirPath)
+        {
+            var dir = new DirectoryInfo(dirPath);
+            dir.GetFiles().ToList().ForEach(f => f.Delete());
+            dir.GetDirectories().ToList().ForEach(d => d.Delete(true));
         }
     }
 }
--- BotProject/CSharp/Controllers/BotAdminController.cs
+++ BotProject/CSharp/Controllers/BotAdminController.cs
@@ -19,36 +19,12 @@ namespace Microsoft.Bot.Builder.TestBot.Json
     [ApiController]
     public class BotAdminController : ControllerBase
     {
-        private static string workDir = ConvertPath("tmp");
         private readonly IBotManager BotManager;
 
         public BotAdminController(IBotManager botManager)
         {
             BotManager = botManager;
         }
-
-        public static string ConvertPath(string relativePath)
-        {
-            var curDir = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
-            return Path.Combine(curDir, relativePath);
-        }
-
-        private void EnsureDirExists(string dirPath)
-        {
-            var dirInfo = new DirectoryInfo(dirPath);
-            if (!dirInfo.Exists)
-            {
-                dirInfo.Create();
-            }
-        }
-
-        private void CleanDir(string dirPath)
-        {
-            var dir = new DirectoryInfo(dirPath);
-            dir.GetFiles().ToList().ForEach(f => f.Delete());
-            dir.GetDirectories().ToList().ForEach(d => dir.Delete(true));
-        }
-
 
         [HttpGet] 
         public IActionResult GetAsync()
@@ -58,66 +34,15 @@ namespace Microsoft.Bot.Builder.TestBot.Json
            
 
         [HttpPost]
-        public async Task<IActionResult> PostAsync(IFormFile file)
+        public IActionResult PostAsync(IFormFile file)
         {
             if (file == null)
             {
                 return BadRequest();
             }
 
-            // download file as tmp.zip
-            var downloadPath = await SaveFile(file, "tmp.zip");
-
-            // extract to bot folder
-            var extractPath = ExtractFile(downloadPath, GenNewBotDir());
-
-            // locate the proj file
-            var projFile = FindBotProjFile(extractPath);
-
-            var botProj = BotProject.Load(projFile);
-            BotManager.SetCurrent(botProj);
+            BotManager.SetCurrent(file.OpenReadStream());
             return Ok();
         }
-
-        private string GenNewBotDir()
-        {
-            return System.Guid.NewGuid().ToString("N");
-        }
-
-        // Download the zip file
-        private async Task<string> SaveFile(IFormFile file, string fileName = null)
-        {
-            EnsureDirExists(workDir);
-
-            var filePath = Path.Combine(workDir, fileName ?? file.Name);
-            using (var outStream = new FileStream(filePath, FileMode.Create))
-            {
-                await file.CopyToAsync(outStream);
-            }
-            return filePath;
-        }
-
-        // Extract file to a dir
-        private string ExtractFile(string filePath, string dstDirPath)
-        {
-            EnsureDirExists(workDir);
-
-            var finalDstPath = Path.Combine(workDir, dstDirPath);
-
-            ZipFile.ExtractToDirectory(filePath, finalDstPath);
-            return finalDstPath;
-        }
-      
-        private string FindBotProjFile(string dir)
-        {
-            string[] projFiles = Directory.GetFiles(dir, "*.botproj");
-            if (projFiles.Length != 1)
-            {
-                throw new Exception("no bot proj file in zip file");
-            }
-            return projFiles[0];
-        }
-
-
     }
 }
