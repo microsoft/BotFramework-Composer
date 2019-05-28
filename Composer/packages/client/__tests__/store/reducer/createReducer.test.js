b499c5ebe3ac8e11600eede0d8373e503e4224cf
Wenyi Luo <wenyluo@microsoft.com>
2019-05-28 15:20:26 +0800

    fix comments

--- Composer/packages/client/__tests__/store/reducer/createReducer.test.js
+++ Composer/packages/client/__tests__/store/reducer/createReducer.test.js
@@ -2,14 +2,19 @@ import createReducer from '../../../src/store/reducer/createReducer';
 describe('test create reducer', () => {
   it('test create reducer with mock action and handler', () => {
     const mockActionHandler = jest.fn((state, payload) => {
-      state.payload = payload;
-      return state;
+      state.a = payload.a;
     });
     const reducer = createReducer({
-      ['mockActionType']: mockActionHandler,
+      mockActionType: mockActionHandler,
     });
-    const result = reducer({}, { type: 'mockActionType', payload: 'mockpayload' });
+    // test else branch
+    const mockState = { a: 2 };
+    const result = reducer(mockState, { type: 'newActionTypeWithoutReducer', payload: 'mock' });
+    expect(result).toBe(mockState);
+    // test if branch
+    const state = reducer(mockState, { type: 'mockActionType', payload: { a: 1 } });
     expect(mockActionHandler).toBeCalled();
-    expect(result.payload).toBe('mockpayload');
+    expect(state).not.toBe(mockState);
+    expect(state.a).toEqual(1);
   });
 });
