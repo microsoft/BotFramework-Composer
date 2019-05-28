2e4a62d4c1b80ad082f858b7229de19d4e961c1a
leileizhang <leilzh@microsoft.com>
2019-05-28 08:25:58 +0800

    Nav update (#300)
    * edit sidebar
    
    * fix test
    
    * clean up keyboard nav and change some styles
    
    * remove tabindex for all elememt

--- Composer/packages/client/src/components/NavItem/index.js
+++ Composer/packages/client/src/components/NavItem/index.js
@@ -1,30 +1,40 @@
-import { Icon } from 'office-ui-fabric-react/lib/Icon';
 /** @jsx jsx */
 import { jsx } from '@emotion/core';
+import { Link } from '@reach/router';
 import { PropTypes } from 'prop-types';
+import { IconButton } from 'office-ui-fabric-react/lib/Button';
+import { CommandBarButton } from 'office-ui-fabric-react/lib/Button';
 
-import { outer, icon, label } from './styles';
-import { NavLink } from './../NavLink';
+import { link, outer, iconButton, commandBarButton } from './styles';
 
-export const NavItem = props => (
-  <NavLink
-    to={props.to}
-    exact={props.exact}
-    style={{ display: 'block', textDecoration: 'none', color: '#4f4f4f' }}
-    activestyle={{ color: '#0083cb' }}
-  >
-    <div css={outer}>
-      <div css={icon}>
-        <Icon iconName={props.iconName} />
+export const NavItem = props => {
+  const { to, exact, labelHide, iconName, labelName } = props;
+  return (
+    <Link
+      to={to}
+      tabIndex={-1}
+      style={link}
+      getProps={({ isCurrent, isPartiallyCurrent }) => {
+        const activeStyle = { style: { ...link, color: '#0083cb' } };
+        const isActive = exact ? isCurrent : isPartiallyCurrent;
+        return isActive ? activeStyle : null;
+      }}
+    >
+      <div tabIndex={-1} css={outer(!labelHide)}>
+        {labelHide ? (
+          <IconButton iconProps={{ iconName }} styles={iconButton} />
+        ) : (
+          <CommandBarButton iconProps={{ iconName }} text={labelName} styles={commandBarButton} />
+        )}
       </div>
-      <div css={label}>{props.label}</div>
-    </div>
-  </NavLink>
-);
+    </Link>
+  );
+};
 
 NavItem.propTypes = {
   to: PropTypes.string,
   iconName: PropTypes.string,
-  label: PropTypes.string,
+  labelName: PropTypes.string,
   exact: PropTypes.bool,
+  labelHide: PropTypes.bool,
 };
