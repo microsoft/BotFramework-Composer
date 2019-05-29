a9942ad2ea09b8fd178a8ef2d5f9e971b5c03c75
yeze322 <2295905420@qq.com>
2019-05-29 16:55:38 +0800

    [Visual Editor] Enable add steps (#312)
    * move common sizes into 'elementSizes'
    
    * increase FormCard size
    
    * update layouters to adapt new design size
    
    * update edge color
    
    * implement EdgeMenu
    
    * apply EdgeMenu to StepGroup
    
    * always let NodeMenu on top of edges
    
    * update IconMenu style
    
    * add new schema
    
    * implement add step e2e
    
    * fix transformer id to 'steps'
    
    * use 'name' as menu props
    
    * transformer: key name 'ruleGroup' -> 'rules' for tracking
    
    * add one more layer 'StepEditor' to enable init adding
    
    * implement StepEditor

---
+++ Composer/packages/extensions/visual-designer/src/components/shared/EdgeMenu.js
@@ -0,0 +1,56 @@
+import React from 'react';
+
+import { IconMenu } from '../nodes/templates/IconMenu';
+import { EdgeAddButtonSize } from '../../shared/elementSizes';
+import { dialogGroups, DialogGroup } from '../../shared/appschema';
+
+const createStepMenu = handleType => {
+  const stepLabels = [
+    DialogGroup.INPUT,
+    DialogGroup.RESPONSE,
+    DialogGroup.MEMORY,
+    DialogGroup.STEP,
+    DialogGroup.CODE,
+    DialogGroup.LOG,
+  ];
+
+  const stepMenuItems = stepLabels.map(x => {
+    const item = dialogGroups[x];
+    return {
+      key: item.label,
+      name: item.label,
+      subMenuProps: {
+        items: item.types.map($type => ({
+          key: $type,
+          name: $type,
+          $type: $type,
+        })),
+        onItemClick: (e, item) => handleType(item.$type),
+      },
+    };
+  });
+
+  return stepMenuItems;
+};
+
+export const EdgeMenu = ({ onClick }) => {
+  return (
+    <div
+      style={{
+        width: EdgeAddButtonSize.width,
+        height: EdgeAddButtonSize.height,
+        borderRadius: '8px',
+        backdropFilter: 'white',
+        boxShadow: '0px 2px 8px rgba(0, 0, 0, 0.1)',
+        overflow: 'hidden',
+        background: 'white',
+      }}
+    >
+      <IconMenu
+        iconName="Add"
+        iconStyles={{ background: 'white', color: '#005CE6', transform: 'scale(0.5)' }}
+        menuItems={createStepMenu($type => onClick($type))}
+      />
+    </div>
+  );
+};
---
+++ Composer/packages/extensions/visual-designer/src/shared/elementSizes.js
@@ -0,0 +1,14 @@
+export const InitNodeSize = {
+  width: 280,
+  height: 80,
+};
+
+export const ElementInterval = {
+  x: 50,
+  y: 60,
+};
+
+export const EdgeAddButtonSize = {
+  width: 16,
+  height: 16,
+};
