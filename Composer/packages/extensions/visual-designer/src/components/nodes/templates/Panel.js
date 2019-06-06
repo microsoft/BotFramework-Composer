606d3b0b723f29244934c776c902d5795ba64b1a
Long Jun <alanlong9278@126.com>
2019-06-06 16:20:31 +0800

    rule column num auto change

--- Composer/packages/extensions/visual-designer/src/components/nodes/templates/Panel.js
+++ Composer/packages/extensions/visual-designer/src/components/nodes/templates/Panel.js
@@ -1,15 +1,42 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { IconButton } from 'office-ui-fabric-react';
 
 import { PanelSize } from '../../../shared/elementSizes';
 
 export const Panel = ({ title, children, collapsedItems }) => {
   const [collapsed, setCollapsed] = useState(false);
+  const [width, setWidth] = useState(PanelSize.defaultWidth);
 
   const collapseFuc = () => {
     setCollapsed(!collapsed);
   };
 
+  useEffect(() => {
+    const PanelWidthList = [
+      { condition: window.matchMedia('(min-width: 1051px)'), length: PanelSize.maxWidth },
+      { condition: window.matchMedia('(max-width: 1050px) and (min-width: 852px)'), length: 824 },
+      { condition: window.matchMedia('(max-width: 851px) and (min-width: 680px)'), length: 624 },
+      { condition: window.matchMedia('(max-width: 679px)'), length: PanelSize.minWidth },
+    ];
+
+    PanelWidthList.forEach(panelWidth => {
+      panelWidth.condition.addListener(e => {
+        if (e.matches) {
+          setWidth(panelWidth.length);
+        }
+      });
+    });
+
+    return () => {
+      PanelWidthList.forEach(panelWidth => {
+        panelWidth.condition.removeListener(e => {
+          if (e.matches) {
+            setWidth(panelWidth.length);
+          }
+        });
+      });
+    };
+  });
   return (
     <div
       style={{
@@ -17,7 +44,7 @@ export const Panel = ({ title, children, collapsedItems }) => {
         boxSizing: 'border-box',
         padding: '24px 0px 12px 24px',
         maxHeight: PanelSize.maxHeight,
-        width: PanelSize.defaultWidth,
+        width: width,
         minWidth: PanelSize.minWidth,
         maxWidth: PanelSize.maxWidth,
         overflow: 'auto',
