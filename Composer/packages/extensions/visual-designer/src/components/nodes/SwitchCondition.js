ffd87db9b70643c9c144d928b5462528db33b673
zeye <zeye@microsoft.com>
2019-06-11 17:51:09 +0800

    update SwitchCondition transformer & layout

--- Composer/packages/extensions/visual-designer/src/components/nodes/SwitchCondition.js
+++ Composer/packages/extensions/visual-designer/src/components/nodes/SwitchCondition.js
@@ -7,28 +7,31 @@ import { OffsetContainer } from '../shared/OffsetContainer';
 import { StepGroup } from '../groups';
 import { Boundary, areBoundariesEqual } from '../shared/Boundary';
 import { Edge } from '../shared/EdgeComponents';
-import { NodeMenu } from '../shared/NodeMenu';
 import { transformSwitchCondition } from '../../transformers/transformSwitchCondition';
 import { switchCaseLayouter } from '../../layouters/switchCaseLayouter';
+import { InitNodeSize } from '../../shared/elementSizes';
 
 import { Diamond } from './templates/Diamond';
+import { DefaultRenderer } from './DefaultRenderer';
 
 const ChoiceNodeWidth = 50;
 const ChoiceNodeHeight = 20;
 
 const calculateNodeMap = (path, data) => {
-  const { condition, cases } = transformSwitchCondition(data, path);
+  const { condition, choice, branches } = transformSwitchCondition(data, path);
   return {
-    switchNode: GraphNode.fromIndexedJson(condition),
-    casesNodes: cases.map(x => GraphNode.fromIndexedJson(x)),
+    conditionNode: GraphNode.fromIndexedJson(condition),
+    choiceNode: GraphNode.fromIndexedJson(choice),
+    branchNodes: branches.map(x => GraphNode.fromIndexedJson(x)),
   };
 };
 
 const calculateLayout = (nodeMap, boundaryMap) => {
-  nodeMap.casesNodes.forEach(x => (x.boundary = boundaryMap[x.id] || new Boundary()));
-  if (nodeMap.switchNode) nodeMap.switchNode.boundary = new Boundary(ChoiceNodeWidth, ChoiceNodeHeight);
+  nodeMap.branchNodes.forEach(x => (x.boundary = boundaryMap[x.id] || new Boundary()));
+  if (nodeMap.choiceNode) nodeMap.choiceNode.boundary = new Boundary(ChoiceNodeWidth, ChoiceNodeHeight);
+  if (nodeMap.conditionNode) nodeMap.conditionNode.boundary = new Boundary(InitNodeSize.width, InitNodeSize.height);
 
-  return switchCaseLayouter(nodeMap.switchNode, nodeMap.casesNodes);
+  return switchCaseLayouter(nodeMap.conditionNode, nodeMap.choiceNode, nodeMap.branchNodes);
 };
 
 export const SwitchCondition = function({ id, data, focusedId, onEvent, onResize }) {
@@ -54,7 +57,16 @@ export const SwitchCondition = function({ id, data, focusedId, onEvent, onResize
   const { boundary, nodeMap, edges } = layout;
   return (
     <div style={{ width: boundary.width, height: boundary.height, position: 'relative' }}>
-      <OffsetContainer offset={nodeMap.switchNode.offset}>
+      <OffsetContainer offset={nodeMap.conditionNode.offset}>
+        <DefaultRenderer
+          key={nodeMap.conditionNode.id}
+          id={nodeMap.conditionNode.id}
+          data={nodeMap.conditionNode.data}
+          focusedId={focusedId}
+          onEvent={onEvent}
+        />
+      </OffsetContainer>
+      <OffsetContainer offset={nodeMap.choiceNode.offset}>
         <Diamond
           data-testid="SwitchConditionDiamond"
           onClick={() => {
@@ -62,7 +74,7 @@ export const SwitchCondition = function({ id, data, focusedId, onEvent, onResize
           }}
         />
       </OffsetContainer>
-      {(nodeMap.caseNodes || []).map(x => (
+      {(nodeMap.branchNodes || []).map(x => (
         <OffsetContainer key={`${x.id}/offset`} offset={x.offset}>
           <StepGroup
             key={x.id}
@@ -77,9 +89,6 @@ export const SwitchCondition = function({ id, data, focusedId, onEvent, onResize
         </OffsetContainer>
       ))}
       {edges ? edges.map(x => <Edge key={x.id} {...x} />) : null}
-      <div style={{ position: 'absolute', top: 0, right: 0 }}>
-        <NodeMenu id={id} onEvent={onEvent} />
-      </div>
     </div>
   );
 };
--- Composer/packages/extensions/visual-designer/src/layouters/switchCaseLayouter.js
+++ Composer/packages/extensions/visual-designer/src/layouters/switchCaseLayouter.js
@@ -1,155 +1,137 @@
 import { Boundary } from '../components/shared/Boundary';
 import { ElementInterval } from '../shared/elementSizes';
+import { GraphNode } from '../components/shared/GraphNode';
 
-const SwitchToBaseline = 10;
-const CaseToBaseline = 30;
-const CaseToBottom = 20;
-const CaseBlockIntervalX = ElementInterval.y;
-const MisalignmentThreshold = 50;
+const ConditionToSwitch = ElementInterval.y / 2;
+const SwitchToBaseline = ElementInterval.y / 2;
+const BaselineToBranch = ElementInterval.y / 2;
+const BranchToBottom = ElementInterval.y / 2;
+const BranchIntervalX = ElementInterval.x;
 
 /**
  *        [switch]
  *           |
- *      ------------
- *      |   |  |   |
+ *           ------------
+ *           |   |  |   |
  */
-export function switchCaseLayouter(switchNode, caseNodes = []) {
-  if (!Array.isArray(caseNodes) || caseNodes.length === 0) {
-    return { boundary: switchNode.boundary, nodeMap: { switchNode, caseNodes: [] }, edges: [] };
+export function switchCaseLayouter(conditionNode, choiceNode, branchNodes = []) {
+  if (!conditionNode) {
+    return { boundary: new Boundary() };
   }
+
+  const firstBranchNode = branchNodes[0] || new GraphNode();
+  const lastBranchNode = branchNodes[branchNodes.length - 1] || new GraphNode();
+
+  const branchGroupBoundary = new Boundary();
+  branchGroupBoundary.width = branchNodes.reduce((acc, x) => acc + x.boundary.width + BranchIntervalX, 0);
+  branchGroupBoundary.height = Math.max(...branchNodes.map(x => x.boundary.height)) + BaselineToBranch + BranchToBottom;
+  branchGroupBoundary.axisX = firstBranchNode.boundary.axisX;
 
   /** Calculate boundary */
+  const containerAxisX = Math.max(conditionNode.boundary.axisX, choiceNode.boundary.axisX, branchGroupBoundary.axisX);
   const containerHeight =
-    switchNode.boundary.height +
+    conditionNode.boundary.height +
+    ConditionToSwitch +
+    choiceNode.boundary.height +
     SwitchToBaseline +
-    CaseToBaseline +
-    Math.max(0, ...caseNodes.map(x => x.boundary.height)) +
-    CaseToBottom;
-  const containerWidth = Math.max(
-    switchNode.boundary.width,
-    caseNodes.reduce((accWidth, caseNode) => accWidth + caseNode.boundary.width + CaseBlockIntervalX, 0)
-  );
-  const containerBoundary = new Boundary(containerWidth, containerHeight);
+    branchGroupBoundary.height;
+  const containerWidth =
+    containerAxisX +
+    Math.max(
+      conditionNode.boundary.width - conditionNode.boundary.axisX,
+      choiceNode.boundary.width - choiceNode.boundary.axisX,
+      branchGroupBoundary.width - branchGroupBoundary.axisX
+    );
+
+  const containerBoundary = new Boundary();
+  containerBoundary.width = containerWidth;
+  containerBoundary.height = containerHeight;
+  containerBoundary.axisX = containerAxisX;
 
   /** Calulate nodes position */
-  // switch node
-  switchNode.offset = {
-    x: (containerWidth - switchNode.boundary.width) / 2,
+  conditionNode.offset = {
+    x: containerAxisX - conditionNode.boundary.axisX,
     y: 0,
   };
+  choiceNode.offset = {
+    x: containerAxisX - choiceNode.boundary.axisX,
+    y: conditionNode.offset.y + conditionNode.boundary.height + ConditionToSwitch,
+  };
 
-  // case nodes
-  const CaseNodeBeginY = switchNode.boundary.height + switchNode.offset.y + SwitchToBaseline + CaseToBaseline;
-  caseNodes.reduce((accOffsetX, x) => {
+  const BaselinePositionY = choiceNode.offset.y + choiceNode.boundary.height + SwitchToBaseline;
+  const BottomelinePositionY = containerHeight;
+
+  branchNodes.reduce((accOffsetX, x) => {
     x.offset = {
       x: accOffsetX,
-      y: CaseNodeBeginY,
+      y: BaselinePositionY + BaselineToBranch,
     };
-    return accOffsetX + CaseBlockIntervalX + x.boundary.width;
-  }, 0);
-
-  /**
-   * Handle misalignment cases
-   *
-   *      [switch]
-   *          |
-   *         --
-   *         |
-   *        [case]
-   *         --
-   *          |
-   * When caseNode[caseNode.length / 2] happened to be possibly aligned with switch node's axis X, align them.
-   * The threshold should be smaller than switchNode.width / 2, otherwise it will change the container's width.
-   */
-  const middleCase = caseNodes[Math.floor(caseNodes.length / 2)];
-  const xAxisDelta =
-    middleCase.offset.x + middleCase.boundary.axisX - (switchNode.offset.x + switchNode.boundary.axisX);
-  if (Math.abs(xAxisDelta) < MisalignmentThreshold) {
-    containerBoundary.axisX += xAxisDelta;
-    switchNode.offset.x += xAxisDelta;
-  }
+    return accOffsetX + BranchIntervalX + x.boundary.width;
+  }, containerAxisX - branchGroupBoundary.axisX);
 
   /** Calculate edges */
   const edges = [];
-  edges.push({
-    id: `edge/${switchNode.id}/switch/switch->baseline`,
-    direction: 'y',
-    x: switchNode.offset.x + switchNode.boundary.axisX,
-    y: switchNode.offset.y + switchNode.boundary.height,
-    length: SwitchToBaseline,
-  });
+  edges.push(
+    {
+      id: `edge/${conditionNode.id}/switch/condition->switch`,
+      direction: 'y',
+      x: containerAxisX,
+      y: conditionNode.offset.y + conditionNode.boundary.height,
+      length: ConditionToSwitch,
+    },
+    {
+      id: `edge/${choiceNode.id}/switch/switch->baseline`,
+      direction: 'y',
+      x: containerAxisX,
+      y: choiceNode.offset.y + choiceNode.boundary.height,
+      length: SwitchToBaseline,
+    }
+  );
 
-  const BaselinePositionY = switchNode.offset.y + switchNode.boundary.height + SwitchToBaseline;
-  const firstCase = caseNodes[0];
-  const lastCase = caseNodes[caseNodes.length - 1];
-
-  const coordXs = [switchNode, firstCase, lastCase].map(x => x.offset.x + x.boundary.axisX);
-  if (coordXs[0] > coordXs[1] && coordXs[2] > coordXs[0]) {
-    /**
-     *        |
-     *    -----------
-     *    |     |    |
-     */
-    edges.push({
-      id: `edge/${switchNode.id}/case/baseline`,
-      direction: 'x',
-      x: firstCase.offset.x + firstCase.boundary.axisX,
-      y: BaselinePositionY,
-      length: lastCase.offset.x + lastCase.boundary.axisX - (firstCase.offset.x + firstCase.boundary.axisX),
-    });
-
-    edges.push({
-      id: `edge/${switchNode.id}/case/bottomline`,
-      direction: 'x',
-      x: firstCase.offset.x + firstCase.boundary.axisX,
-      y: containerBoundary.height,
-      length: lastCase.offset.x + lastCase.boundary.axisX - (firstCase.offset.x + firstCase.boundary.axisX),
-    });
-  } else {
-    /**
-     *         |                |                                     |
-     *   -------     or         -------------         or            ---
-     *   | |                          |      |                      |
-     */
-    const coordXs = [switchNode, firstCase, lastCase].map(x => x.offset.x + x.boundary.axisX);
-    const fromX = Math.min(...coordXs);
-    const toX = Math.max(...coordXs);
-    edges.push({
-      id: `edge/${switchNode.id}/case/baseline`,
-      direction: 'x',
-      x: fromX,
-      y: BaselinePositionY,
-      length: toX - fromX,
-    });
-
-    edges.push({
-      id: `edge/${switchNode.id}/case/bottomline`,
-      direction: 'x',
-      x: fromX,
-      y: containerBoundary.height,
-      length: toX - fromX,
-    });
-  }
-
-  caseNodes.forEach(x => {
+  branchNodes.forEach(x => {
     edges.push(
       {
-        id: `edge/${switchNode.id}/case/baseline->${x.id}`,
+        id: `edge/${choiceNode.id}/case/baseline->${x.id}`,
         direction: 'y',
         x: x.offset.x + x.boundary.axisX,
         y: BaselinePositionY,
-        length: CaseToBaseline,
+        length: BaselineToBranch,
         text: x.data.label,
       },
       {
-        id: `edge/${switchNode.id}/case/${x.id}->bottom`,
+        id: `edge/${choiceNode.id}/case/${x.id}->bottom`,
         direction: 'y',
         x: x.offset.x + x.boundary.axisX,
         y: x.offset.y + x.boundary.height,
-        length: containerBoundary.height - x.offset.y - x.boundary.height,
+        length: BottomelinePositionY - x.offset.y - x.boundary.height,
       }
     );
   });
 
-  return { boundary: containerBoundary, nodeMap: { switchNode, caseNodes }, edges };
+  if (branchNodes.length > 1) {
+    const baseLineLength = lastBranchNode.offset.x + lastBranchNode.boundary.axisX - containerAxisX;
+
+    edges.push(
+      {
+        id: `edge/${conditionNode.id}/baseline`,
+        direction: 'x',
+        x: containerAxisX,
+        y: BaselinePositionY,
+        length: baseLineLength,
+      },
+      {
+        id: `edge/${conditionNode.id}/bottomline`,
+        direction: 'x',
+        x: containerAxisX,
+        y: BottomelinePositionY,
+        length: baseLineLength,
+      }
+    );
+  }
+
+  return {
+    boundary: containerBoundary,
+    nodeMap: { conditionNode, choiceNode, branchNodes },
+    edges,
+  };
 }
