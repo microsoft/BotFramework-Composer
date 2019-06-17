66bb2d63bd5f8c09b3aceacb920d4226b4a8e985
Andy Brown <asbrown002@gmail.com>
2019-06-17 10:09:44 -0700

    focus node when adding in visual designer

--- Composer/packages/extensions/visual-designer/src/components/shared/EventMenu.js
+++ Composer/packages/extensions/visual-designer/src/components/shared/EventMenu.js
@@ -1,15 +1,14 @@
 import React from 'react';
 
 import { IconMenu } from '../nodes/templates/IconMenu';
-import { NodeEventTypes } from '../../shared/NodeEventTypes';
 import { ObiTypes } from '../../shared/ObiTypes';
 
-export const EventMenu = ({ id, onEvent }) => {
+export const EventMenu = ({ onClick }) => {
   const eventTypes = [ObiTypes.IntentRule, ObiTypes.UnknownIntentRule, ObiTypes.EventRule];
   const menuItems = eventTypes.map(x => ({
     key: x,
     name: x,
-    onClick: () => onEvent(NodeEventTypes.Insert, { id, $type: x }),
+    onClick: () => onClick(x),
   }));
   return <IconMenu iconName="CircleAddition" iconStyles={{ color: '#0078D4' }} menuItems={menuItems} />;
 };
