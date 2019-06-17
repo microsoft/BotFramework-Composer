b224338703becedc42bdfb35c5614945c75771b5
Wenyi Luo <wenyluo@microsoft.com>
2019-06-17 17:48:16 +0800

    change e2e test to match naming issue

--- Composer/cypress/integration/CreateNewBot.spec.js
+++ Composer/cypress/integration/CreateNewBot.spec.js
@@ -7,12 +7,13 @@ context('Creating a new bot', () => {
 
   it('can create a bot from the ToDo template', () => {
     cy.getByText('New').click();
+    // cy.pause();
     cy.getByText('ToDoBot').click();
     cy.get('input[data-testid="NewBotProjectInput"]').type('__TestNewProject');
     cy.getByText('Save').click();
     // cy.get('input[data-testid="NewBotProjectInput"]').type('{enter}');
     cy.get('[data-testid="ProjectTree"]').within(() => {
-      cy.getByText('ToDoBot.main').should('exist');
+      cy.getByText('__TestNewProject.main').should('exist');
       cy.getByText('AddToDo').should('exist');
       cy.getByText('ClearToDos').should('exist');
       cy.getByText('DeleteToDo').should('exist');
