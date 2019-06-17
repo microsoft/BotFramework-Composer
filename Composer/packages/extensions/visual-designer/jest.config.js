4dc1df3ae063a2975e822001eec323e8689fdcc3
Andy Brown <asbrown002@gmail.com>
2019-06-17 13:07:55 -0700

    focus node in visual designer after adding (#381)
    * put edge lines behind nodes
    
    fixes #317
    
    * focus node when adding in visual designer
    
    * add e2e test for adding a node in the visual editor

--- Composer/packages/extensions/visual-designer/jest.config.js
+++ Composer/packages/extensions/visual-designer/jest.config.js
@@ -1,7 +1,11 @@
 module.exports = {
-  testPathIgnorePatterns: ['/node_modules/'],
+  testPathIgnorePatterns: ['/node_modules/', '/jestMocks/', '__tests__/setup.js'],
   transform: {
     '^.+\\.(j|t)sx?$': 'babel-jest',
   },
-  roots: ['./tests'],
+  moduleNameMapper: {
+    // Any imports of .scss / .css files will instead import styleMock.js which is an empty object
+    '\\.(s)?css$': '<rootDir>/__tests__/jestMocks/styleMock.js',
+  },
+  transformIgnorePatterns: ['"/node_modules/(?!office-ui-fabric-react).+\\.js$"'],
 };
--- Composer/packages/extensions/visual-designer/src/editors/EventsEditor.js
+++ Composer/packages/extensions/visual-designer/src/editors/EventsEditor.js
@@ -3,16 +3,21 @@ import React from 'react';
 import { Panel } from '../components/nodes/templates/Panel';
 import { RuleGroup, CollapsedRuleGroup } from '../components/groups';
 import { EventMenu } from '../components/shared/EventMenu';
+import { NodeEventTypes } from '../shared/NodeEventTypes';
 
 export const EventsEditor = ({ id, data, focusedId, onEvent }) => {
   const ruleCount = data.children.length;
   const title = `Skills (${ruleCount})`;
 
+  const onClick = $type => {
+    onEvent(NodeEventTypes.Insert, { id, $type, position: ruleCount });
+  };
+
   return (
     <Panel
       title={title}
       collapsedItems={<CollapsedRuleGroup count={ruleCount} />}
-      addMenu={<EventMenu id={id} onEvent={onEvent} />}
+      addMenu={<EventMenu id={id} onClick={onClick} />}
     >
       <RuleGroup key={id} id={id} data={data} focusedId={focusedId} onEvent={onEvent} />
     </Panel>
