9400a426831d9df380e14810718120a584fe12ee
zhixzhan <49866537+zhixzhan@users.noreply.github.com>
2019-06-17 17:41:07 +0800

    add code comments

--- Composer/packages/server/src/utility/jsonWalk.ts
+++ Composer/packages/server/src/utility/jsonWalk.ts
@@ -1,3 +1,9 @@
+/**
+ * visitor function used by JsonWalk
+ * @param path jsonPath string
+ * @param value current node value
+ * @return boolean, true to stop walk deep
+ */
 export interface VisitorFunc {
   (path: string, value: any): boolean;
 }
