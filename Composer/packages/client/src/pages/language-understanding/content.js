4c2978655672855b6b7664c3a225b0b8fc5d42f0
zhixzhan <49866537+zhixzhan@users.noreply.github.com>
2019-06-18 18:43:53 +0800

    update lu all up view

--- Composer/packages/client/src/pages/language-understanding/content.js
+++ Composer/packages/client/src/pages/language-understanding/content.js
@@ -1,26 +1,51 @@
 /** @jsx jsx */
 import { jsx } from '@emotion/core';
-import { Fragment } from 'react';
+import { Fragment, useMemo, useContext } from 'react';
+import { PropTypes } from 'prop-types';
 import lodash from 'lodash';
+import { Store } from '../../store/index';
 import { LuEditor } from 'code-editor';
 
-import FormEditor from './form-editor';
+import TableView from './table-view';
 import { contentEditor } from './styles';
 
+Content.propTypes = {
+  // file: PropTypes.object,
+  onChange: PropTypes.func,
+  textMode: PropTypes.bool,
+  activeDialog: PropTypes.object,
+  onEdit: PropTypes.func,
+};
+
+// TODO: validate here,
+// both form editor and code editor
 export default function Content(props) {
-  const luFile = props.file;
+  // const luFile = props.file;
   const onChange = props.onChange;
   const textMode = props.textMode;
+  const activeDialog = props.activeDialog;
+  const onEdit = props.onEdit;
 
-  return lodash.isEmpty(luFile) === false ? (
-    textMode ? (
-      <div css={contentEditor}>
-        <LuEditor value={luFile.content} onChange={onChange} />
-      </div>
-    ) : (
-      <FormEditor file={luFile} onChange={onChange} />
-    )
-  ) : (
-    <Fragment />
+  const { state } = useContext(Store);
+  const { luFiles } = state;
+
+  const luFile = useMemo(() => {
+    return luFiles.find(luFile => luFile.id === (activeDialog && activeDialog.name));
+  }, [luFiles, activeDialog]);
+
+  // performance optimization, component update should only trigger by luFile change.
+  const memoizedEditor = useMemo(() => {
+    const textContent = lodash.isEmpty(luFile) === false ? luFile.content : '';
+    return <LuEditor value={textContent} onChange={onChange} />;
+  }, [luFile]);
+
+  return (
+    <div css={contentEditor}>
+      {textMode ? (
+        memoizedEditor
+      ) : (
+        <TableView file={luFile} activeDialog={activeDialog} onEdit={onEdit} onChange={onChange} />
+      )}
+    </div>
   );
 }
--- Composer/packages/client/src/pages/language-understanding/table-view.js
+++ Composer/packages/client/src/pages/language-understanding/table-view.js
@@ -2,6 +2,6 @@
 import { jsx } from '@emotion/core';
 import { Fragment } from 'react';
 
-export default function FormEditor() {
+export default function TableView() {
   return <Fragment />;
 }
