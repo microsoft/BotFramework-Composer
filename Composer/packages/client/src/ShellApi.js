1d79ef9713280fcc52c55e1f9b0b61f07e4b2502
Long Jun <alanlong9278@126.com>
2019-06-18 15:42:34 +0800

    refactor

--- Composer/packages/client/src/ShellApi.js
+++ Composer/packages/client/src/ShellApi.js
@@ -80,16 +80,20 @@ export function ShellApi() {
     }, {});
   }, [dialogs]);
 
+  // replace window.frames[num] with window.frames[name].
+  // reason:
+  // 1. iframe position may be changed.
+  // 2. Some Plugins in Chrome cause window.frames contain a global window.
   useEffect(() => {
-    if (window.frames[0]) {
-      const editorWindow = window.frames[0];
+    if (window.frames[VISUAL_EDITOR]) {
+      const editorWindow = window.frames[VISUAL_EDITOR];
       apiClient.apiCallAt('reset', getState(VISUAL_EDITOR), editorWindow);
     }
   }, [dialogs, lgFiles, luFiles, navPath, focusPath]);
 
   useEffect(() => {
-    if (window.frames[1]) {
-      const editorWindow = window.frames[1];
+    if (window.frames[FORM_EDITOR]) {
+      const editorWindow = window.frames[FORM_EDITOR];
       apiClient.apiCallAt('reset', getState(FORM_EDITOR), editorWindow);
     }
   }, [dialogs, lgFiles, luFiles, navPath, focusPath]);
