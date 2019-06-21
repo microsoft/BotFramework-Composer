b461c58c2015bf6c3ef5fec98f9b57ab7995d8dc
zhixzhan <49866537+zhixzhan@users.noreply.github.com>
2019-06-21 17:17:23 +0800

    update E2E test

--- Composer/cypress/integration/LUPage.spec.js
+++ Composer/cypress/integration/LUPage.spec.js
@@ -7,9 +7,30 @@ context('check language understanding page', () => {
   });
 
   it('can open language understanding page', () => {
-    cy.visit(Cypress.env('COMPOSER_URL') + '/language-understanding/ToDoLuis');
-    cy.get('[data-testid="LUEditor"]').within(() => {
-      cy.getByText('ToDoLuisBot.main.lu').should('exist');
-    });
+    cy.visit(Cypress.env('COMPOSER_URL') + '/language-understanding');
+
+    // left nav tree
+    cy.contains('ToDoLuisBot.main');
+    cy.contains('All');
+
+    cy.get('.toggleEditMode button').as('switchButton');
+
+    // all multiple file, edit mode button is disabled.
+    cy.get('@switchButton').should('be.disabled');
+
+    // by default is table view
+    cy.get('[data-testid="LUEditor"] [data-testid="table-view"]').should('exist');
+
+    // nav to ToDoLuisBot.main dialog
+    cy.get('.dialogNavTree button[title="ToDoLuisBot.main"]').click();
+    cy.wait(300);
+
+    // goto edit-mode
+    cy.get('@switchButton').click();
+    cy.get('[data-testid="LUEditor"] .monaco-editor').should('exist');
+
+    // back to all table view
+    cy.get('.dialogNavTree button[title="All"]').click();
+    cy.get('[data-testid="LUEditor"] [data-testid="table-view"]').should('exist');
   });
 });
