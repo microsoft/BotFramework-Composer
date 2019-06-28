1b75466b10a0a8b13f8398fc61b4f936743d5b95
Andy Brown <asbrown002@gmail.com>
2019-06-28 14:16:51 -0700

    render textarea for activity fields (#420)

--- Composer/packages/extensions/obieditortest/src/Form/types.ts
+++ Composer/packages/extensions/obieditortest/src/Form/types.ts
@@ -1,12 +1,13 @@
 import { WidgetProps } from '@bfdesigner/react-jsonschema-form';
 
-import { ShellApi, LuFile } from '../types';
+import { ShellApi, LuFile, LgFile } from '../types';
 
 export interface FormContext {
   editorSchema: any;
   shellApi: ShellApi;
   rootId: string;
   luFiles: LuFile[];
+  lgFiles: LgFile[];
   dialogOptions: string[];
   dialogName: string;
 }
