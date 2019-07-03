7bebe0ba351a364b2e0ed334ff60e191c486ccd4
Ben Brown <benbro@microsoft.com>
2019-07-03 15:11:29 -0500

    Change description of begin dialog

--- Composer/packages/extensions/obieditortest/src/schema/appschema.ts
+++ Composer/packages/extensions/obieditortest/src/schema/appschema.ts
@@ -262,7 +262,7 @@ export const appschema: JSONSchema6 = {
     'Microsoft.BeginDialog': {
       $role: 'unionType(Microsoft.IDialog)',
       title: 'Begin Dialog',
-      description: 'Step which begins another dialog (and when that dialog is done, it will return the caller).',
+      description: 'BeginDialog starts a child dialog that when completed, enables a parent dialog to continue. Included in this action are options to pass to this child dialog.',
       type: 'object',
       properties: {
         $type: {
