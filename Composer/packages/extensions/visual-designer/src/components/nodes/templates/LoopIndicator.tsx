e56cae2b58e7e02f31d1caa29b31c0dc97d2d360
zeye <zeye@microsoft.com>
2019-07-03 17:53:02 +0800

    add loop icon

--- Composer/packages/extensions/visual-designer/src/components/nodes/templates/LoopIndicator.tsx
+++ Composer/packages/extensions/visual-designer/src/components/nodes/templates/LoopIndicator.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Icon } from 'office-ui-fabric-react/lib/Icon';
 
 export const LoopIndicator = ({ onClick }) => {
   return (
@@ -8,11 +9,16 @@ export const LoopIndicator = ({ onClick }) => {
         height: 24,
         borderRadius: 12,
         background: 'grey',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
       }}
       onClick={e => {
         e.stopPropagation();
         onClick();
       }}
-    />
+    >
+      <Icon iconName="Refresh" style={{ color: 'white' }} />
+    </div>
   );
 };
