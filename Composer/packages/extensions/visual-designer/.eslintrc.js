fe92d3ab711bbda13664601eb27a4420b081d6d7
zeye <zeye@microsoft.com>
2019-07-04 10:47:09 +0800

    enable eslint on ts

--- Composer/packages/extensions/visual-designer/.eslintrc.js
+++ Composer/packages/extensions/visual-designer/.eslintrc.js
@@ -1,3 +1,9 @@
 module.exports = {
-  extends: ['../../../.eslintrc.react.js'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: ['../../../.eslintrc.react.js', 'plugin:@typescript-eslint/recommended'],
+  rules: {
+    indent: 'off',
+    '@typescript-eslint/indent': ['error', 2],
+  },
 };
