d8a08fe823fb61ed5286541a2fd80e454ef966b5
Weitian Li <weitli@microsoft.com>
2019-07-09 16:47:40 +0800

    resolve conflict

--- Composer/packages/client/src/router.js
+++ Composer/packages/client/src/router.js
@@ -6,11 +6,8 @@ import DesignPage from './pages/design';
 import { SettingPage } from './pages/setting';
 import { LUPage } from './pages/language-understanding';
 import { LGPage } from './pages/language-generation';
-<<<<<<< HEAD
 import { Home } from './pages/home';
-=======
 import { About } from './pages/about';
->>>>>>> about page design
 import { showDesign, data } from './styles';
 import { NotFound } from './components/NotFound';
 
@@ -29,11 +26,8 @@ const Routes = props => {
               <SettingPage path="setting/*" />
               <LUPage path="language-understanding/*" />
               <LGPage path="language-generation/*" />
-<<<<<<< HEAD
               <Home path="home" />
-=======
               <About path="about" />
->>>>>>> about page design
               <NotFound default />
             </Router>
           )}
