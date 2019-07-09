cd1d2c9b94e4ef603415754c873c5a61ee585140
zeye <zeye@microsoft.com>
2019-07-09 18:26:07 +0800

    add a new example for LogicFlow

--- Composer/packages/extensions/visual-designer/demo/src/stories/LogicFlowDemo/index.js
+++ Composer/packages/extensions/visual-designer/demo/src/stories/LogicFlowDemo/index.js
@@ -5,9 +5,14 @@ import { parseAdaptiveDialog } from '../../../../src/logicflow-obi/parseObi';
 import { renderObiData, measureObiData, renderObiStepInsertionPoint } from '../../../../src/logicflow-obi';
 import AddToDo from '../../samples/todo/AddToDo.json';
 import ObiFlow from '../../samples/logicflow/ObiFlow.json';
+import SimpleFlow from '../../samples/logicflow/SimpleFlow.json';
 import { Boundary } from '../../../../src/shared/Boundary';
 
-const flows = [{ name: 'ObiFlow', data: ObiFlow }, { name: 'AddToDo', data: parseAdaptiveDialog(AddToDo) }];
+const flows = [
+  { name: 'ObiFlow', data: ObiFlow },
+  { name: 'AddToDo', data: parseAdaptiveDialog(AddToDo) },
+  { name: 'SimpleFlow', data: SimpleFlow },
+];
 const renderers = [
   {
     name: 'ObiRenderer',
@@ -18,17 +23,39 @@ const renderers = [
     },
   },
   {
-    name: 'SimpleRenderer',
+    name: 'JsonBoxRenderer',
     renderer: {
-      measureData: () => new Boundary(100, 100),
+      measureData: () => new Boundary(200, 100),
       renderData: function JsonBox(...args) {
         return (
-          <div style={{ width: 100, height: 100, border: '1px solid #999', overflow: 'hidden' }}>
+          <div style={{ width: 200, height: 100, border: '1px solid #999', overflow: 'hidden' }}>
             {JSON.stringify(args)}
           </div>
         );
       },
       renderStepInsertionPoint: undefined,
+    },
+  },
+  {
+    name: 'TextBoxRenderer',
+    renderer: {
+      measureData: () => new Boundary(100, 50),
+      renderData: function TextBox(id, type, data) {
+        return (
+          <div
+            style={{
+              width: 100,
+              height: 50,
+              border: '1px solid lightblue',
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+          >
+            {typeof data === 'string' ? data : '<JSON>'}
+          </div>
+        );
+      },
     },
   },
 ];
