e45342c07a9574181fdc80c0555b259485662c6f
Long Jun <alanlong9278@126.com>
2019-07-11 13:59:46 +0800

    jsonTracker

--- Composer/packages/extensions/visual-designer/__tests__/transformers/jsonTracker.test.ts
+++ Composer/packages/extensions/visual-designer/__tests__/transformers/jsonTracker.test.ts
@@ -1,4 +1,4 @@
-import { insert } from '../../src/shared/jsonTracker';
+import { insert, deleteNode } from '../../src/shared/jsonTracker';
 
 describe('insert', () => {
   const path = 'foo.bar';
@@ -85,6 +85,44 @@ describe('insert', () => {
       const updated = insert(dialog, path, 0, 'newOne');
 
       expect(updated.foo.bar).toEqual([{ $type: 'newOne', $designer: { id: expect.any(String) } }]);
+    });
+  });
+});
+
+describe('deleteNode', () => {
+  let dialog, path, removeLgTemplate;
+  beforeEach(() => {
+    dialog = { foo: { bar: [{ $type: 'firstOne' }, { $type: 'secondOne' }] } };
+    removeLgTemplate = jest.fn();
+  });
+
+  it('when data does not exist', () => {
+    path = null;
+    const result = deleteNode(dialog, path, removeLgTemplate);
+
+    expect(result).toEqual(dialog);
+  });
+
+  describe('when data exist', () => {
+    it('currentKey type is number', () => {
+      path = 'foo.bar[0]';
+      const result = deleteNode(dialog, path, removeLgTemplate);
+
+      expect(result).toEqual({ foo: { bar: [{ $type: 'secondOne' }] } });
+    });
+    it('currentKey type is string', () => {
+      path = 'foo.bar';
+      const result = deleteNode(dialog, path, removeLgTemplate);
+
+      expect(result).toEqual({ foo: {} });
+    });
+    it("removeLgTemplate be called when deleteNode's $type is 'Microsoft.SendActivity' && activity includes '[bfdactivity-'", () => {
+      dialog.foo.activityNode = { $type: 'Microsoft.SendActivity', activity: '[bfdactivity-a]' };
+      path = 'foo.activityNode';
+      const result = deleteNode(dialog, path, removeLgTemplate);
+
+      expect(removeLgTemplate).toBeCalledWith('common', 'bfdactivity-a');
+      expect(result).toEqual({ foo: { bar: [{ $type: 'firstOne' }, { $type: 'secondOne' }] } });
     });
   });
 });
