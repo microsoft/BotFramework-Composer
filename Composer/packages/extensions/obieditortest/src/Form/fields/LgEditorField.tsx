6be14c2b2c4a2030ef09eef896c9e3edd71b50a8
Andy Brown <asbrown002@gmail.com>
2019-07-18 20:13:56 -0700

    show error message when lg is invalid or cannot be saved (#577)

--- Composer/packages/extensions/obieditortest/src/Form/fields/LgEditorField.tsx
+++ Composer/packages/extensions/obieditortest/src/Form/fields/LgEditorField.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { useState, useEffect } from 'react';
 import { LgEditor } from 'code-editor';
 import * as Monaco from 'monaco-editor/esm/vs/editor/editor.api';
+import { SharedColors } from '@uifabric/fluent-theme';
+import formatMessage from 'format-message';
 
 import { BFDFieldProps } from '../types';
 
 import { BaseField } from './BaseField';
 
-const focusEditor = (editor: Monaco.editor.IStandaloneCodeEditor | null) => {
+const focusEditor = (editor: Monaco.editor.IStandaloneCodeEditor | null): void => {
   if (editor !== null) {
     editor.focus();
   }
@@ -25,13 +27,14 @@ const getInitialTemplate = (formData?: string): string => {
   return newTemplate;
 };
 
-export function LgEditorField(props: BFDFieldProps) {
+export const LgEditorField: React.FC<BFDFieldProps> = props => {
   const { formContext } = props;
 
+  const [isInvalid, setIsInvalid] = useState(false);
   const [templateToRender, setTemplateToRender] = useState({ Name: '', Body: '' });
   const lgId = `bfdactivity-${formContext.dialogId}`;
 
-  const ensureTemplate = async (newBody?: string) => {
+  const ensureTemplate = async (newBody?: string): Promise<void> => {
     const templates = await formContext.shellApi.getLgTemplates('common');
     const template = templates.find(template => {
       return template.Name === lgId;
@@ -51,7 +54,7 @@ export function LgEditorField(props: BFDFieldProps) {
     }
   };
 
-  const onChange = data => {
+  const onChange = (data): void => {
     // hit the lg api and replace it's Body with data
     if (formContext.dialogId) {
       let dataToEmit = data.trim();
@@ -61,7 +64,10 @@ export function LgEditorField(props: BFDFieldProps) {
 
       if (dataToEmit.length > 0) {
         setTemplateToRender({ Name: templateToRender.Name, Body: data });
-        formContext.shellApi.updateLgTemplate('common', lgId, dataToEmit);
+        formContext.shellApi
+          .updateLgTemplate('common', lgId, dataToEmit)
+          .then(() => setIsInvalid(false))
+          .catch(() => setIsInvalid(true));
         props.onChange(`[${lgId}]`);
       } else {
         setTemplateToRender({ Name: templateToRender.Name, Body: '' });
@@ -78,17 +84,49 @@ export function LgEditorField(props: BFDFieldProps) {
   const { Body } = templateToRender;
   return (
     <div>
-      <BaseField {...props} />
-      <div style={{ height: '250px' }}>
-        <LgEditor
-          hidePlaceholder
-          value={Body}
-          onChange={onChange}
-          editorDidMount={editor => {
-            focusEditor(editor);
+      <BaseField {...props}>
+        <div
+          style={{
+            height: '250px',
+            border: '1px solid transparent',
+            borderColor: isInvalid ? SharedColors.red20 : 'transparent',
+            transition: `border-color 0.1s ${isInvalid ? 'ease-out' : 'ease-in'}`,
           }}
-        />
-      </div>
+        >
+          <LgEditor
+            hidePlaceholder
+            value={Body}
+            onChange={onChange}
+            editorDidMount={editor => {
+              focusEditor(editor);
+            }}
+          />
+        </div>
+        {isInvalid ? (
+          <span style={{ fontSize: '14px' }}>
+            <span style={{ margin: 0, color: SharedColors.red20, fontSize: '11px' }}>
+              {formatMessage.rich(
+                'This text cannot be saved because there are errors in the LG syntax. Refer to the syntax documentation <a>here</a>.',
+                {
+                  // eslint-disable-next-line react/display-name
+                  a: ({ children }) => (
+                    <a
+                      key="a"
+                      href="https://github.com/microsoft/BotBuilder-Samples/blob/master/experimental/language-generation/docs/lg-file-format.md"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      {children}
+                    </a>
+                  ),
+                }
+              )}
+            </span>
+          </span>
+        ) : (
+          <div style={{ height: '19px' }} />
+        )}
+      </BaseField>
     </div>
   );
-}
+};
