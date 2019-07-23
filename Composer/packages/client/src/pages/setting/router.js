387675e648c3120b4dc763682546788a61a59fb1
Hai Liu <hailiu@gmail.com>
2019-07-23 12:44:08 -0700

    fix dialog settings

--- Composer/packages/client/src/pages/setting/router.js
+++ Composer/packages/client/src/pages/setting/router.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import { Router, Redirect } from '@reach/router';
 
+import { BASEPATH } from '../../constants';
+import { resolveToBasePath } from '../../utils/fileUtil';
+
 import { DialogSettings } from './dialog-settings';
 import { Services } from './services';
 import { ComposerConfiguration } from './composer-configuration/index';
 import { PublishingStaging } from './publishing-staging/index';
 
+const mapNavTo = x => resolveToBasePath(BASEPATH, x);
+
 const Routes = () => (
-  <Router basepath="setting">
-    <Redirect from="*" to="setting/dialog-settings" noThrow />
+  <Router basepath={mapNavTo('setting')}>
+    <Redirect from="*" to={mapNavTo('setting/dialog-settings')} noThrow />
     <DialogSettings path="dialog-settings" />
     <Services path="services" />
     <ComposerConfiguration path="composer-configuration" />
