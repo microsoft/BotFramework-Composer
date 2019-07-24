7d1046dd70ae9a99866b742e22c229fb2bc87ede
zhixzhan <49866537+zhixzhan@users.noreply.github.com>
2019-07-24 15:53:09 +0800

    dialog check isExpression

--- Composer/packages/server/src/models/bot/dialogRules.ts
+++ Composer/packages/server/src/models/bot/dialogRules.ts
@@ -1,30 +1,63 @@
-import { has } from 'lodash';
+import { get } from 'lodash';
+import { ExpressionEngine } from 'botbuilder-expression-parser';
+
+const ExpressionParser = new ExpressionEngine();
 
 interface CheckerFunc {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   (node: { path: string; value: any }): string; // error msg, '' for no error
 }
 
-function Exist(name: string): CheckerFunc {
+function IsExpression(name: string): CheckerFunc {
   return node => {
-    if (has(node.value, name)) return '';
-    return `In ${node.path}: "${node.value.$type}" field "${name}" is missing or empty`;
+    const exp = get(node.value, name);
+    if (!exp) return `In ${node.path}: ${node.value.$type}: ${name} is missing or empty`;
+
+    let message = '';
+    try {
+      ExpressionParser.parse(exp);
+    } catch (error) {
+      message = `In ${node.path}: ${node.value.$type}: ${name} must be an expression`;
+    }
+    return message;
   };
+}
+
+enum EditArrayChangeTypes {
+  Push,
+  Pop,
+  Take,
+  Remove,
+  Clear,
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function EditArrayChecker(node: { path: string; value: any }): string {
+  let message = '';
+
+  const changeType = get(node.value, 'changeType');
+
+  // when push and remove, value is required
+  if (changeType === EditArrayChangeTypes.Push || changeType === EditArrayChangeTypes.Remove) {
+    const exp = get(node.value, 'value');
+    try {
+      ExpressionParser.parse(exp);
+    } catch (error) {
+      message = `In ${node.path}: ${node.value.$type}: ${name} must be an expression`;
+    }
+  }
+
+  return message;
 }
 
 /**
  * Dialog Validation Rules
  */
 export const DialogRules: { [key: string]: CheckerFunc[] } = {
-  'Microsoft.IfCondition': [Exist('condition')],
-  'Microsoft.SwitchCondition': [Exist('condition')],
-  'Microsoft.SetProperty': [Exist('value')],
-  'Microsoft.ForeachPage': [Exist('listProperty')],
-  'Microsoft.Foreach': [Exist('listProperty')],
-  'Microsoft.EditArray': [
-    node => {
-      if (has(node.value, 'value') || has(node.value, 'arrayProperty')) return '';
-      return `Missing Required field at ${node.path}`;
-    },
-  ],
-  'Microsoft.InputDialog': [],
+  'Microsoft.IfCondition': [IsExpression('condition')],
+  'Microsoft.SwitchCondition': [IsExpression('condition')],
+  'Microsoft.SetProperty': [IsExpression('property'), IsExpression('value')],
+  'Microsoft.ForeachPage': [IsExpression('listProperty')],
+  'Microsoft.Foreach': [IsExpression('listProperty')],
+  'Microsoft.EditArray': [IsExpression('arrayProperty'), EditArrayChecker],
 };
