6127f1cc5ca094716a77d3e0ef35298c546887a3
Andy Brown <asbrown002@gmail.com>
2019-07-25 13:58:53 -0700

    rename obieditortest -> obiformeditor (#618)
    * use @bfcomposer scope
    
    * rename obieditortest -> obiformeditor
    
    * fix demo app in code-editor
    
    * ignore format message warnings
    
    * disable fabric icon warnings
    
    * disable fabric icon warnings without initializing for test
    
    * disable formatMessage warnings in test

---
+++ Composer/packages/extensions/obiformeditor/src/Form/ObjectFieldTemplate/index.tsx
@@ -0,0 +1,81 @@
+import React, { useState } from 'react';
+import { PrimaryButton } from 'office-ui-fabric-react';
+import { getUiOptions } from '@bfcomposer/react-jsonschema-form/lib/utils';
+import get from 'lodash.get';
+import omit from 'lodash.omit';
+import { ObjectFieldTemplateProps } from '@bfcomposer/react-jsonschema-form';
+import formatMessage from 'format-message';
+
+import { BaseField } from '../fields/BaseField';
+
+import ObjectItem from './ObjectItem';
+import NewPropertyModal from './NewPropertyModal';
+
+import './styles.scss';
+
+function canExpand({ formData, schema, uiSchema }: ObjectFieldTemplateProps): boolean {
+  if (!schema.additionalProperties) {
+    return false;
+  }
+  const { expandable } = getUiOptions(uiSchema);
+  if (expandable === false) {
+    return expandable;
+  }
+  // if ui:options.expandable was not explicitly set to false, we can add
+  // another property if we have not exceeded maxProperties yet
+  if (schema.maxProperties !== undefined) {
+    return Object.keys(formData).length < schema.maxProperties;
+  }
+  return true;
+}
+
+const ObjectFieldTemplate: React.FunctionComponent<ObjectFieldTemplateProps> = props => {
+  const [showModal, setShowModal] = useState(false);
+  const [editableProperty, setEditableProperty] = useState('');
+
+  const handlePropertyEdit = (newName: string, newValue: string): void => {
+    props.onChange({
+      ...omit(props.formData, editableProperty),
+      [newName]: newValue || get(props.formData, newName || editableProperty, ''),
+    });
+    setShowModal(false);
+    setEditableProperty('');
+  };
+
+  const onEditProperty = (propName: string): void => {
+    setEditableProperty(propName);
+    setShowModal(true);
+  };
+
+  const onDismiss = (): void => {
+    setEditableProperty('');
+    setShowModal(false);
+  };
+
+  return (
+    <div className="ObjectFieldTemplate" key={props.idSchema.__id}>
+      <BaseField {...props}>
+        {props.properties.map(p => (
+          <ObjectItem {...p} key={p.name} onEdit={() => onEditProperty(p.name)} onAdd={() => setShowModal(true)} />
+        ))}
+        {canExpand(props) && (
+          <>
+            <PrimaryButton type="button" onClick={() => setShowModal(true)} styles={{ root: { marginTop: '10px' } }}>
+              {formatMessage('Add')}
+            </PrimaryButton>
+            {showModal && (
+              <NewPropertyModal
+                onSubmit={handlePropertyEdit}
+                onDismiss={onDismiss}
+                name={editableProperty}
+                schema={props.schema || {}}
+              />
+            )}
+          </>
+        )}
+      </BaseField>
+    </div>
+  );
+};
+
+export default ObjectFieldTemplate;
