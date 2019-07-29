728b69925030efc0579c2db6b263a91531980ea2
Chris Whitten <christopher.whitten@microsoft.com>
2019-07-28 21:16:00 -0700

    Fix some issues causing a broken experience in MS Edge (#621)
    * Add cssesc
    
    * do not use trimEnd(), that doesn't exist in MS Edge
    
    * Bump
    
    * Remove cssesc

--- Composer/packages/client/src/store/action/lg.js
+++ Composer/packages/client/src/store/action/lg.js
@@ -113,14 +113,14 @@ export async function updateLgTemplate(dispatch, { file, templateName, template 
   const oldTemplates = lgUtil.parse(file.content);
   if (Array.isArray(oldTemplates) === false) throw new Error('origin lg file is not valid');
 
-  const orignialTemplate = oldTemplates.find(x => x.Name === templateName);
-  let content = file.content.trimEnd();
+  const originalTemplate = oldTemplates.find(x => x.Name === templateName);
+  let content = file.content.replace(/\s+$/, '');
 
-  if (orignialTemplate === undefined) {
+  if (originalTemplate === undefined) {
     content = `${content}${content ? '\n\n' : ''}${textFromTemplates([template])}\n`;
   } else {
-    const startLineNumber = orignialTemplate.ParseTree._start.line;
-    const endLineNumber = orignialTemplate.ParseTree._stop.line;
+    const startLineNumber = originalTemplate.ParseTree._start.line;
+    const endLineNumber = originalTemplate.ParseTree._stop.line;
 
     const lines = content.split('\n');
     const contentBefore = lines.slice(0, startLineNumber - 1).join('\n');
@@ -150,7 +150,7 @@ export async function createLgTemplate(dispatch, { file, template, position }) {
   if (position === 0) {
     content = textFromTemplates([template]) + '\n\n' + content;
   } else {
-    content = content.trimEnd() + '\n\n' + textFromTemplates([template]) + '\n';
+    content = content.replace(/\s+$/, '') + '\n\n' + textFromTemplates([template]) + '\n';
   }
 
   return await updateLgFile(dispatch, { id: file.id, content });
