5cc5d3c58b71468c26abbfd973f1279c71797703
zeye <zeye@microsoft.com>
2019-08-05 11:12:39 +0800

    fiix test spec `index.test.tsx`

--- Composer/packages/extensions/visual-designer/__tests__/index.test.tsx
+++ Composer/packages/extensions/visual-designer/__tests__/index.test.tsx
@@ -3,7 +3,18 @@ import { cleanup, render, waitForElement } from 'react-testing-library';
 
 import VisualDesigner from '../src';
 
-describe.skip('<VisualDesigner />', () => {
+// READ: https://jestjs.io/docs/en/manual-mocks#mocking-methods-which-are-not-implemented-in-jsdom
+window.matchMedia = jest.fn().mockImplementation(query => {
+  return {
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(),
+    removeListener: jest.fn(),
+  };
+});
+
+describe('<VisualDesigner />', () => {
   afterEach(cleanup);
 
   it('should render the visual designer', async () => {
