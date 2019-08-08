cf8a96a2f036a4f6322f052df9ef5fe862e82c7a
leilzh <leilzh@microsoft.com>
2019-08-08 18:16:57 +0800

    fix test

--- Composer/packages/client/__tests__/routers.test.js
+++ Composer/packages/client/__tests__/routers.test.js
@@ -6,7 +6,7 @@ import { StoreProvider } from '../src/store';
 
 import { App } from './../src/App';
 
-function renderWithRouter(ui, { route = '/', history = createHistory(createMemorySource(route)) } = {}) {
+function renderWithRouter(ui, { route = '/dialogs/Main', history = createHistory(createMemorySource(route)) } = {}) {
   return {
     ...render(<LocationProvider history={history}>{ui}</LocationProvider>),
     history,
