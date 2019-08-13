0e4cf71b625f7f3086b4bb59cac84ece97c32aab
zeye <zeye@microsoft.com>
2019-08-13 11:23:45 +0800

    split NodeRenderer to StepRenderer and EventRenderer

---
+++ Composer/packages/extensions/visual-designer/src/components/shared/EventRenderer.tsx
@@ -0,0 +1,63 @@
+/** @jsx jsx */
+import { jsx, css } from '@emotion/core';
+import React, { FC, ComponentClass, useContext } from 'react';
+import classnames from 'classnames';
+
+import { ObiTypes } from '../../shared/ObiTypes';
+import {
+  DefaultRenderer,
+  IntentRule,
+  ConversationUpdateActivityRule,
+  EventRule,
+  UnknownIntentRule,
+} from '../nodes/index';
+import { NodeRendererContext } from '../../store/NodeRendererContext';
+
+import { NodeProps, defaultNodeProps } from './sharedProps';
+
+const rendererByObiType = {
+  [ObiTypes.EventRule]: EventRule,
+  [ObiTypes.IntentRule]: IntentRule,
+  [ObiTypes.UnknownIntentRule]: UnknownIntentRule,
+  [ObiTypes.ConversationUpdateActivityRule]: ConversationUpdateActivityRule,
+};
+const DEFAULT_RENDERER = DefaultRenderer;
+
+function chooseRendererByType($type): FC<NodeProps> | ComponentClass<NodeProps> {
+  const renderer = rendererByObiType[$type] || DEFAULT_RENDERER;
+  return renderer;
+}
+
+const nodeBorderStyle = css`
+  outline: 2px solid grey;
+`;
+
+export const EventRenderer: FC<NodeProps> = ({ id, data, onEvent, onResize }): JSX.Element => {
+  const ChosenRenderer = chooseRendererByType(data.$type);
+
+  const { focusedId, focusedEvent } = useContext(NodeRendererContext);
+  const nodeFocused = focusedId === id || focusedEvent === id;
+
+  return (
+    <div
+      className={classnames('event-renderer-container', { 'event-renderer-container--focused': nodeFocused })}
+      css={css`
+        display: inline-block;
+        position: relative;
+        ${nodeFocused && nodeBorderStyle}
+      `}
+    >
+      <ChosenRenderer
+        id={id}
+        data={data}
+        focused={nodeFocused}
+        onEvent={onEvent}
+        onResize={size => {
+          onResize(size, 'node');
+        }}
+      />
+    </div>
+  );
+};
+
+EventRenderer.defaultProps = defaultNodeProps;
