e9aaab7f77b19f90ad0dafc9face2e84598bb2f2
Andy Brown <asbrown002@gmail.com>
2019-09-04 08:55:22 -0700

    use store to login user in component

--- Composer/packages/client/src/components/RequireAuth/index.tsx
+++ Composer/packages/client/src/components/RequireAuth/index.tsx
@@ -1,19 +1,27 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import { Spinner, SpinnerSize } from 'office-ui-fabric-react';
 import formatMessage from 'format-message';
 
-import { loginUser, getUserToken } from '../../utils/auth';
+import { StoreContext } from '../../store';
 
 import { loading } from './styles';
 
 export const RequireAuth: React.FC = props => {
+  const { state, actions } = useContext(StoreContext);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  // attempt to login user when component mounts if auth is required
   useEffect(() => {
     if (process.env.COMPOSER_REQUIRE_AUTH) {
-      loginUser();
+      actions.loginUser();
     }
   }, []);
 
-  if (process.env.COMPOSER_REQUIRE_AUTH && !getUserToken()) {
+  useEffect(() => {
+    setIsLoading(!state.userToken);
+  }, [state.userToken]);
+
+  if (process.env.COMPOSER_REQUIRE_AUTH && isLoading) {
     return (
       <div css={loading}>
         <Spinner label={formatMessage('Loading...')} size={SpinnerSize.large} />
