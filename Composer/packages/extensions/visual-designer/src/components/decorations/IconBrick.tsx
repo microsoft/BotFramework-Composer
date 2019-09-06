6aa00f05669d271cefce7ea6f03f8ceca30c3de3
zeye <zeye@microsoft.com>
2019-09-06 14:30:21 +0800

    change icon

--- Composer/packages/extensions/visual-designer/src/components/decorations/IconBrick.tsx
+++ Composer/packages/extensions/visual-designer/src/components/decorations/IconBrick.tsx
@@ -1,5 +1,6 @@
 /** @jsx jsx */
 import { jsx } from '@emotion/core';
+import { Icon } from 'office-ui-fabric-react';
 
 import { IconBrickSize } from '../../constants/ElementSizes';
 
@@ -11,10 +12,14 @@ export const IconBrick = ({ onClick }): JSX.Element => {
         background: '#FFFFFF',
         boxShadow: '0px 0.6px 1.8px rgba(0, 0, 0, 0.108), 0px 3.2px 7.2px rgba(0, 0, 0, 0.132)',
         borderRadius: '2px',
+        position: 'relative',
+        cursor: 'pointer',
       }}
       onClick={onClick}
     >
-      X
+      <div css={{ position: 'absolute', left: 4, top: 4 }}>
+        <Icon iconName="ErrorBadge" />
+      </div>
     </div>
   );
 };
