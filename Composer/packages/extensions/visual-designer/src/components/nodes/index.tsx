38baedc8c83311a09fc98a974e354bd7a63ab9e4
zeye <2295905420@qq.com>
2019-09-12 07:50:40 +0800

    [Visual Editor] Selection state optimization (#834)
    * render layout-steps in StepRenderer, steps in ElementRenderer
    
    * compile virtual elements out of BaseInput
    
    * migrate BaseInput to ElementRnederer to adapt to unified selection effect
    
    * fix test failure in type naming convention

--- Composer/packages/extensions/visual-designer/src/components/nodes/index.tsx
+++ Composer/packages/extensions/visual-designer/src/components/nodes/index.tsx
@@ -5,11 +5,14 @@ export * from './steps/Recognizer';
 export * from './steps/ReplaceDialog';
 export * from './steps/ChoiceInput';
 export * from './steps/TextInput';
-export * from './steps/BaseInput';
+export * from './steps/BotAsks';
+export * from './steps/UserAnswers';
+export * from './steps/InvalidPromptBrick';
 
 export * from './layout-steps/Foreach';
 export * from './layout-steps/IfCondition';
 export * from './layout-steps/SwitchCondition';
+export * from './layout-steps/BaseInput';
 
 export * from './events/EventRule';
 export * from './events/IntentRule';
