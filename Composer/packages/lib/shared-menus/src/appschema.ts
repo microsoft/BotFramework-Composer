032987ef80c59658f0c5e3faf5f86b595eefe143
Andy Brown <asbrown002@gmail.com>
2019-09-16 12:45:15 -0700

    add sdk types enum and add missing types to dialog groups

--- Composer/packages/lib/shared-menus/src/appschema.ts
+++ Composer/packages/lib/shared-menus/src/appschema.ts
@@ -4,6 +4,96 @@ import nanoid from 'nanoid/generate';
 import { ConceptLabels } from './labelMap';
 import { seedNewDialog } from './dialogFactory';
 
+// All of the known SDK types. Update this list when we take a schema update.
+// To get this list copy the output of the following commands in a node repl from the project root:
+
+// const schema = JSON.parse(fs.readFileSync('./BotProject/CSharp/Schemas/sdk.schema', 'utf-8'));
+// const types = schema.oneOf.map(t => t.title);
+// let uType = 'export enum SDKTypes {\n';
+// uType += types.map(t => `  ${t.replace('Microsoft.', '')} = '${t}',`).join('\n');
+// uType += '\n}';
+// console.log(uType);
+
+/**
+ * All SDK Types defined by the schema.
+ * All references of the type should be accessed through this enum.
+ * */
+export enum SDKTypes {
+  AdaptiveDialog = 'Microsoft.AdaptiveDialog',
+  AgeEntityRecognizer = 'Microsoft.AgeEntityRecognizer',
+  AttachmentInput = 'Microsoft.AttachmentInput',
+  BeginDialog = 'Microsoft.BeginDialog',
+  CancelAllDialogs = 'Microsoft.CancelAllDialogs',
+  ChoiceInput = 'Microsoft.ChoiceInput',
+  ConditionalSelector = 'Microsoft.ConditionalSelector',
+  ConfirmInput = 'Microsoft.ConfirmInput',
+  ConfirmationEntityRecognizer = 'Microsoft.ConfirmationEntityRecognizer',
+  CurrencyEntityRecognizer = 'Microsoft.CurrencyEntityRecognizer',
+  DateTimeEntityRecognizer = 'Microsoft.DateTimeEntityRecognizer',
+  DateTimeInput = 'Microsoft.DateTimeInput',
+  DebugBreak = 'Microsoft.DebugBreak',
+  DeleteProperty = 'Microsoft.DeleteProperty',
+  DimensionEntityRecognizer = 'Microsoft.DimensionEntityRecognizer',
+  EditActions = 'Microsoft.EditActions',
+  EditArray = 'Microsoft.EditArray',
+  EmailEntityRecognizer = 'Microsoft.EmailEntityRecognizer',
+  EmitEvent = 'Microsoft.EmitEvent',
+  EndDialog = 'Microsoft.EndDialog',
+  EndTurn = 'Microsoft.EndTurn',
+  FirstSelector = 'Microsoft.FirstSelector',
+  Foreach = 'Microsoft.Foreach',
+  ForeachPage = 'Microsoft.ForeachPage',
+  GuidEntityRecognizer = 'Microsoft.GuidEntityRecognizer',
+  HashtagEntityRecognizer = 'Microsoft.HashtagEntityRecognizer',
+  HttpRequest = 'Microsoft.HttpRequest',
+  IfCondition = 'Microsoft.IfCondition',
+  InitProperty = 'Microsoft.InitProperty',
+  IpEntityRecognizer = 'Microsoft.IpEntityRecognizer',
+  LanguagePolicy = 'Microsoft.LanguagePolicy',
+  LogAction = 'Microsoft.LogAction',
+  LuisRecognizer = 'Microsoft.LuisRecognizer',
+  MentionEntityRecognizer = 'Microsoft.MentionEntityRecognizer',
+  MostSpecificSelector = 'Microsoft.MostSpecificSelector',
+  MultiLanguageRecognizer = 'Microsoft.MultiLanguageRecognizer',
+  NumberEntityRecognizer = 'Microsoft.NumberEntityRecognizer',
+  NumberInput = 'Microsoft.NumberInput',
+  NumberRangeEntityRecognizer = 'Microsoft.NumberRangeEntityRecognizer',
+  OAuthInput = 'Microsoft.OAuthInput',
+  OnActivity = 'Microsoft.OnActivity',
+  OnBeginDialog = 'Microsoft.OnBeginDialog',
+  OnConversationUpdateActivity = 'Microsoft.OnConversationUpdateActivity',
+  OnDialogEvent = 'Microsoft.OnDialogEvent',
+  OnEndOfConversationActivity = 'Microsoft.OnEndOfConversationActivity',
+  OnEvent = 'Microsoft.OnEvent',
+  OnEventActivity = 'Microsoft.OnEventActivity',
+  OnHandoffActivity = 'Microsoft.OnHandoffActivity',
+  OnIntent = 'Microsoft.OnIntent',
+  OnInvokeActivity = 'Microsoft.OnInvokeActivity',
+  OnMessageActivity = 'Microsoft.OnMessageActivity',
+  OnMessageDeleteActivity = 'Microsoft.OnMessageDeleteActivity',
+  OnMessageReactionActivity = 'Microsoft.OnMessageReactionActivity',
+  OnMessageUpdateActivity = 'Microsoft.OnMessageUpdateActivity',
+  OnTypingActivity = 'Microsoft.OnTypingActivity',
+  OnUnknownIntent = 'Microsoft.OnUnknownIntent',
+  OrdinalEntityRecognizer = 'Microsoft.OrdinalEntityRecognizer',
+  PercentageEntityRecognizer = 'Microsoft.PercentageEntityRecognizer',
+  PhoneNumberEntityRecognizer = 'Microsoft.PhoneNumberEntityRecognizer',
+  QnAMakerDialog = 'Microsoft.QnAMakerDialog',
+  RandomSelector = 'Microsoft.RandomSelector',
+  RegexEntityRecognizer = 'Microsoft.RegexEntityRecognizer',
+  RegexRecognizer = 'Microsoft.RegexRecognizer',
+  RepeatDialog = 'Microsoft.RepeatDialog',
+  ReplaceDialog = 'Microsoft.ReplaceDialog',
+  SendActivity = 'Microsoft.SendActivity',
+  SetProperty = 'Microsoft.SetProperty',
+  SwitchCondition = 'Microsoft.SwitchCondition',
+  TemperatureEntityRecognizer = 'Microsoft.TemperatureEntityRecognizer',
+  TextInput = 'Microsoft.TextInput',
+  TraceActivity = 'Microsoft.TraceActivity',
+  TrueSelector = 'Microsoft.TrueSelector',
+  UrlEntityRecognizer = 'Microsoft.UrlEntityRecognizer',
+}
+
 export enum DialogGroup {
   RESPONSE = 'RESPONSE',
   INPUT = 'INPUT',
@@ -13,6 +103,7 @@ export enum DialogGroup {
   CODE = 'CODE',
   LOG = 'LOG',
   EVENTS = 'EVENTS',
+  ADVANCED_EVENTS = 'ADVANCED_EVENTS',
   RECOGNIZER = 'RECOGNIZER',
   SELECTOR = 'SELECTOR',
   OTHER = 'OTHER',
@@ -20,85 +111,101 @@ export enum DialogGroup {
 
 export interface DialogGroupItem {
   label: string;
-  types: string[];
+  types: SDKTypes[];
 }
 export type DialogGroupsMap = { [key in DialogGroup]: DialogGroupItem };
 
 export const dialogGroups: DialogGroupsMap = {
   [DialogGroup.RESPONSE]: {
     label: 'Send Messages',
-    types: ['Microsoft.SendActivity'],
+    types: [SDKTypes.SendActivity],
   },
   [DialogGroup.INPUT]: {
     label: 'Ask a Question',
     types: [
-      'Microsoft.TextInput',
-      'Microsoft.NumberInput',
-      'Microsoft.ConfirmInput',
-      'Microsoft.ChoiceInput',
-      'Microsoft.AttachmentInput',
-      'Microsoft.DateTimeInput',
+      SDKTypes.TextInput,
+      SDKTypes.NumberInput,
+      SDKTypes.ConfirmInput,
+      SDKTypes.ChoiceInput,
+      SDKTypes.AttachmentInput,
+      SDKTypes.DateTimeInput,
     ],
   },
   [DialogGroup.BRANCHING]: {
     label: 'Flow',
-    types: ['Microsoft.IfCondition', 'Microsoft.SwitchCondition', 'Microsoft.Foreach', 'Microsoft.ForeachPage'],
+    types: [SDKTypes.IfCondition, SDKTypes.SwitchCondition, SDKTypes.Foreach, SDKTypes.ForeachPage],
   },
   [DialogGroup.MEMORY]: {
     label: 'Memory manipulation',
-    types: ['Microsoft.SetProperty', 'Microsoft.InitProperty', 'Microsoft.DeleteProperty', 'Microsoft.EditArray'],
+    types: [SDKTypes.SetProperty, SDKTypes.InitProperty, SDKTypes.DeleteProperty, SDKTypes.EditArray],
   },
   [DialogGroup.STEP]: {
     label: 'Dialogs',
     types: [
-      'Microsoft.BeginDialog',
-      'Microsoft.EndDialog',
-      'Microsoft.CancelAllDialogs',
-      'Microsoft.EndTurn',
-      'Microsoft.RepeatDialog',
-      'Microsoft.ReplaceDialog',
-      'Microsoft.EditActions',
+      SDKTypes.BeginDialog,
+      SDKTypes.EndDialog,
+      SDKTypes.CancelAllDialogs,
+      SDKTypes.EndTurn,
+      SDKTypes.RepeatDialog,
+      SDKTypes.ReplaceDialog,
+      SDKTypes.EditActions,
+      // SDKTypes.QnAMakerDialog,
     ],
   },
   [DialogGroup.CODE]: {
     label: 'Integrations',
     types: [
-      'Microsoft.HttpRequest',
-      'Microsoft.EmitEvent',
-      'Microsoft.OAuthInput',
-      //  'Microsoft.CodeStep'
+      SDKTypes.HttpRequest,
+      SDKTypes.EmitEvent,
+      SDKTypes.OAuthInput,
+      //  SDKTypes.CodeStep
     ],
   },
   [DialogGroup.LOG]: {
     label: 'Debugging',
-    types: [/* 'Microsoft.DebugBreak', */ 'Microsoft.LogAction', 'Microsoft.TraceActivity'],
+    types: [/* SDKTypes.DebugBreak, */ SDKTypes.LogAction, SDKTypes.TraceActivity],
   },
   [DialogGroup.EVENTS]: {
     label: 'Events',
     types: [
-      'Microsoft.OnEvent',
-      'Microsoft.OnIntent',
-      'Microsoft.OnUnknownIntent',
-      'Microsoft.OnConversationUpdateActivity',
+      SDKTypes.OnEvent,
+      SDKTypes.OnIntent,
+      SDKTypes.OnUnknownIntent,
+      SDKTypes.OnConversationUpdateActivity,
+      SDKTypes.OnBeginDialog,
+    ],
+  },
+  [DialogGroup.ADVANCED_EVENTS]: {
+    label: 'Advanced Events',
+    types: [
+      SDKTypes.OnActivity,
+      SDKTypes.OnEventActivity,
+      SDKTypes.OnHandoffActivity,
+      SDKTypes.OnInvokeActivity,
+      SDKTypes.OnMessageActivity,
+      SDKTypes.OnMessageDeleteActivity,
+      SDKTypes.OnMessageReactionActivity,
+      SDKTypes.OnMessageUpdateActivity,
+      SDKTypes.OnTypingActivity,
     ],
   },
   [DialogGroup.RECOGNIZER]: {
     label: 'Recognizers',
-    types: ['Microsoft.LuisRecognizer', /* 'Microsoft.MultiLanguageRecognizers', */ 'Microsoft.RegexRecognizer'],
+    types: [SDKTypes.LuisRecognizer, /* SDKTypes.MultiLanguageRecognizers, */ SDKTypes.RegexRecognizer],
   },
   [DialogGroup.SELECTOR]: {
     label: 'Selectors',
     types: [
-      'Microsoft.ConditionalSelector',
-      'Microsoft.FirstSelector',
-      'Microsoft.MostSpecificSelector',
-      'Microsoft.RandomSelector',
-      'Microsoft.TrueSelector',
+      SDKTypes.ConditionalSelector,
+      SDKTypes.FirstSelector,
+      SDKTypes.MostSpecificSelector,
+      SDKTypes.RandomSelector,
+      SDKTypes.TrueSelector,
     ],
   },
   [DialogGroup.OTHER]: {
     label: 'Other',
-    types: ['Microsoft.AdaptiveDialog', 'Microsoft.LanguagePolicy', 'Microsoft.QnAMakerDialog'],
+    types: [SDKTypes.AdaptiveDialog, SDKTypes.LanguagePolicy, SDKTypes.QnAMakerDialog],
   },
 };
 
@@ -204,92 +311,3 @@ export function getDialogGroupByType(type) {
 
   return dialogType;
 }
-
-/**
-  All of the known SDK types. Update this list when we take a schema update.
-  To get this list copy the output of the following commands in a node repl from the project root:
-
-  ```js
-    const schema = JSON.parse(fs.readFileSync('./BotProject/CSharp/Schemas/sdk.schema', 'utf-8'));
-    const types = schema.oneOf.map(t => t.title);
-    let uType = 'export type SDKTypes =';
-    types.forEach(t => uType += `\n  | '${t}'`);
-    uType += ';';
-    console.log(uType);
-  ```
-*/
-
-export type SDKTypes =
-  | 'Microsoft.AdaptiveDialog'
-  | 'Microsoft.AgeEntityRecognizer'
-  | 'Microsoft.AttachmentInput'
-  | 'Microsoft.BeginDialog'
-  | 'Microsoft.CancelAllDialogs'
-  | 'Microsoft.ChoiceInput'
-  | 'Microsoft.ConditionalSelector'
-  | 'Microsoft.ConfirmInput'
-  | 'Microsoft.ConfirmationEntityRecognizer'
-  | 'Microsoft.CurrencyEntityRecognizer'
-  | 'Microsoft.DateTimeEntityRecognizer'
-  | 'Microsoft.DateTimeInput'
-  | 'Microsoft.DebugBreak'
-  | 'Microsoft.DeleteProperty'
-  | 'Microsoft.DimensionEntityRecognizer'
-  | 'Microsoft.EditActions'
-  | 'Microsoft.EditArray'
-  | 'Microsoft.EmailEntityRecognizer'
-  | 'Microsoft.EmitEvent'
-  | 'Microsoft.EndDialog'
-  | 'Microsoft.EndTurn'
-  | 'Microsoft.FirstSelector'
-  | 'Microsoft.Foreach'
-  | 'Microsoft.ForeachPage'
-  | 'Microsoft.GuidEntityRecognizer'
-  | 'Microsoft.HashtagEntityRecognizer'
-  | 'Microsoft.HttpRequest'
-  | 'Microsoft.IfCondition'
-  | 'Microsoft.InitProperty'
-  | 'Microsoft.IpEntityRecognizer'
-  | 'Microsoft.LanguagePolicy'
-  | 'Microsoft.LogAction'
-  | 'Microsoft.LuisRecognizer'
-  | 'Microsoft.MentionEntityRecognizer'
-  | 'Microsoft.MostSpecificSelector'
-  | 'Microsoft.MultiLanguageRecognizer'
-  | 'Microsoft.NumberEntityRecognizer'
-  | 'Microsoft.NumberInput'
-  | 'Microsoft.NumberRangeEntityRecognizer'
-  | 'Microsoft.OAuthInput'
-  | 'Microsoft.OnActivity'
-  | 'Microsoft.OnBeginDialog'
-  | 'Microsoft.OnConversationUpdateActivity'
-  | 'Microsoft.OnDialogEvent'
-  | 'Microsoft.OnEndOfConversationActivity'
-  | 'Microsoft.OnEvent'
-  | 'Microsoft.OnEventActivity'
-  | 'Microsoft.OnHandoffActivity'
-  | 'Microsoft.OnIntent'
-  | 'Microsoft.OnInvokeActivity'
-  | 'Microsoft.OnMessageActivity'
-  | 'Microsoft.OnMessageDeleteActivity'
-  | 'Microsoft.OnMessageReactionActivity'
-  | 'Microsoft.OnMessageUpdateActivity'
-  | 'Microsoft.OnTypingActivity'
-  | 'Microsoft.OnUnknownIntent'
-  | 'Microsoft.OrdinalEntityRecognizer'
-  | 'Microsoft.PercentageEntityRecognizer'
-  | 'Microsoft.PhoneNumberEntityRecognizer'
-  | 'Microsoft.QnAMakerDialog'
-  | 'Microsoft.RandomSelector'
-  | 'Microsoft.RegexEntityRecognizer'
-  | 'Microsoft.RegexRecognizer'
-  | 'Microsoft.RepeatDialog'
-  | 'Microsoft.ReplaceDialog'
-  | 'Microsoft.SendActivity'
-  | 'Microsoft.SetProperty'
-  | 'Microsoft.SwitchCondition'
-  | 'Microsoft.TemperatureEntityRecognizer'
-  | 'Microsoft.TextInput'
-  | 'Microsoft.TraceActivity'
-  | 'Microsoft.TrueSelector'
-  | 'Microsoft.UrlEntityRecognizer';
