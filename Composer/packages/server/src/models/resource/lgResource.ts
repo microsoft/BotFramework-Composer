d0316ecfa4f989033ae0a9630ae9cbd5391d0a5e
Dong Lei <donglei@microsoft.com>
2019-09-16 22:22:39 +0800

    Move responsibility from lgIndexer to lgResource

--- Composer/packages/server/src/models/resource/lgResource.ts
+++ Composer/packages/server/src/models/resource/lgResource.ts
@@ -1,16 +1,28 @@
 import { Resource, ResourceType } from './resource';
-import { LGTemplate } from 'botbuilder-lg';
+import { LGTemplate } from '../bot/interface';
+import { LGParser } from 'botbuilder-lg';
+import { Diagnostic } from '../validator/interface';
 
 export class LGResource implements Resource {
   public id: string;
   public content: string;
   public type: ResourceType;
+  public diagnostics: Diagnostic[] = [];
 
-  public templates: LGTemplate[];
+  public templates: LGTemplate[] = [];
 
   constructor(id: string, content: string) {
     this.id = id;
     this.content = content;
     this.type = ResourceType.LG;
+    this.templates = this.parse(this.content, this.id);
+  }
+
+  private parse(content: string, id: string): LGTemplate[] {
+    const resource = LGParser.parse(content, id);
+    const templates = resource.Templates.map(t => {
+      return { Name: t.Name, Body: t.Body, Parameters: t.Parameters };
+    });
+    return templates;
   }
 }
