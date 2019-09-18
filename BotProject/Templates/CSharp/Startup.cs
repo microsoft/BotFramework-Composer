4cd6d181d9ba022900ad1f4ac3c369f5da9d4473
Qi Kang <qika@microsoft.com>
2019-09-18 10:25:39 +0800

    add deployment runtime

---
+++ BotProject/Templates/CSharp/Startup.cs
@@ -0,0 +1,117 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Dialogs.Debugging;
+using Microsoft.Bot.Builder.Dialogs.Declarative.Resources;
+using Microsoft.Bot.Builder.Dialogs.Declarative.Types;
+using Microsoft.Bot.Builder.Integration;
+using Microsoft.Bot.Builder.Integration.AspNet.Core;
+using Microsoft.Bot.Schema;
+using Microsoft.Bot.Connector.Authentication;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging.Debug;
+using System.Diagnostics;
+using System.IO;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Bot.Builder.BotFramework;
+using Microsoft.Bot.Builder.Dialogs.Declarative;
+
+namespace Microsoft.Bot.Builder.TestBot.Json
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env, IConfiguration configuration)
+        {
+            this.HostingEnvironment = env;
+            this.Configuration = configuration;
+        }
+
+        public IHostingEnvironment HostingEnvironment { get; }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddSingleton<IConfiguration>(this.Configuration);
+
+            // Create the credential provider to be used with the Bot Framework Adapter.
+            services.AddSingleton<ICredentialProvider, ConfigurationCredentialProvider>();
+
+            services.AddSingleton<InspectionMiddleware>();
+
+
+            IStorage storage = new MemoryStorage();
+            var userState = new UserState(storage);
+            var conversationState = new ConversationState(storage);
+            var inspectionState = new InspectionState(storage);
+
+
+            var botFile = Configuration.GetSection("bot").Value;
+
+            TypeFactory.Configuration = this.Configuration;
+
+            // manage all bot resources
+            var resourceExplorer = new ResourceExplorer().AddFolder(botFile);
+
+            var credentials = new MicrosoftAppCredentials(this.Configuration["MicrosoftAppId"], this.Configuration["MicrosoftAppPassword"]);
+
+            services.AddSingleton<IBotFrameworkHttpAdapter, BotFrameworkHttpAdapter>((s) =>
+            {
+                var adapter = new BotFrameworkHttpAdapter(new ConfigurationCredentialProvider(this.Configuration));
+                adapter
+                .UseStorage(storage)
+                .UseState(userState, conversationState)
+                .UseLanguageGeneration(resourceExplorer)
+                .UseDebugger(4712)
+                .Use(new InspectionMiddleware(inspectionState, userState, conversationState, credentials))
+                .UseResourceExplorer(resourceExplorer);
+
+                adapter.OnTurnError = async (turnContext, exception) =>
+                {
+                    await turnContext.SendActivityAsync(exception.Message).ConfigureAwait(false);
+
+                    await conversationState.ClearStateAsync(turnContext).ConfigureAwait(false);
+                    await conversationState.SaveChangesAsync(turnContext).ConfigureAwait(false);
+                };
+                return adapter;
+            });
+
+            services.AddSingleton<IBot, TestBot>((sp) => new TestBot("Main.dialog", conversationState, userState, resourceExplorer, DebugSupport.SourceRegistry));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
+            //app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
