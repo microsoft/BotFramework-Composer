b7c53e655c09156317c6a5ca4f69ba6fe6f83304
zeye <zeye@microsoft.com>
2019-09-21 12:03:36 +0800

    Fix Breadcrub CI

--- Composer/cypress/integration/Breadcrumb.spec.js
+++ Composer/cypress/integration/Breadcrumb.spec.js
@@ -1,39 +1,71 @@
 /// <reference types="Cypress" />
 
 context('breadcrumb', () => {
-  beforeEach(() => {
+  before(() => {
     cy.visit(Cypress.env('COMPOSER_URL'));
     cy.openBot('ToDoBot');
   });
 
+  beforeEach(() => {
+    // Return to Main.dialog
+    cy.get('[data-testid="ProjectTree"]').within(() => {
+      cy.getByText('ToDoBot.Main').click();
+    });
+  });
+
   it('can show dialog name in breadcrumb', () => {
+    // Should path = main dialog at first render
     cy.getByTestId('Breadcrumb')
       .invoke('text')
       .should('contain', 'ToDoBot.Main');
 
+    // Click on AddToDo dialog
     cy.get('[data-testid="ProjectTree"]').within(() => {
       cy.getByText('AddToDo').click();
     });
-
     cy.getByTestId('Breadcrumb')
       .invoke('text')
       .should('contain', 'AddToDo');
 
+    // Return to Main.dialog
     cy.get('[data-testid="ProjectTree"]').within(() => {
       cy.getByText('ToDoBot.Main').click();
-      cy.getByText('#AddIntent').click();
     });
 
-    cy.withinEditor('VisualEditor', () => {
-      cy.getByText('AddIntent').click();
+    cy.getByTestId('Breadcrumb')
+      .invoke('text')
+      .should('contain', 'ToDoBot');
+  });
+
+  it('can show event name in breadcrumb', () => {
+    cy.get('[data-testid="ProjectTree"]').within(() => {
+      cy.getByText('AddToDo').click();
       cy.wait(100);
+      cy.getByText('Handle an Event: BeginDialog').click();
+      cy.wait(100);
+    });
+
+    cy.getByTestId('Breadcrumb')
+      .invoke('text')
+      .should('match', /AddToDo.*Handle an Event.*/);
+  });
+
+  it('can show action name in breadcrumb', () => {
+    cy.get('[data-testid="ProjectTree"]').within(() => {
+      cy.getByText('ToDoBot.Main').click();
+      cy.wait(100);
+    });
+
+    // Click on an action
+    cy.withinEditor('VisualEditor', () => {
       cy.getByTestId('RuleEditor').within(() => {
-        cy.getByText('AddToDo').click();
+        cy.getByText('Set a Property').click();
+        cy.wait(100);
       });
     });
 
     cy.getByTestId('Breadcrumb')
       .invoke('text')
-      .should('match', /ToDoBot.+AddToDo/);
+      .should('match', /ToDoBot.+Set a Property/);
   });
 });
