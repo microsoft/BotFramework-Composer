85ca61d1528cc89b820d88546ca6b68414fb83be
zeye <zeye@microsoft.com>
2019-09-21 12:48:20 +0800

    update Breadcrumb CI test

--- Composer/cypress/integration/Breadcrumb.spec.js
+++ Composer/cypress/integration/Breadcrumb.spec.js
@@ -59,13 +59,13 @@ context('breadcrumb', () => {
     // Click on an action
     cy.withinEditor('VisualEditor', () => {
       cy.getByTestId('RuleEditor').within(() => {
-        cy.getByText('Set a Property').click();
+        cy.getByText('Send an Activity').click();
         cy.wait(300);
       });
     });
 
     cy.getByTestId('Breadcrumb')
       .invoke('text')
-      .should('match', /ToDoBot.+Set a Property/);
+      .should('match', /ToDoBot.+Send an Activity/);
   });
 });
