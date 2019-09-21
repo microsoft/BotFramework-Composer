e5e8ada98e4f2b6ff233d2a42abc0a55de7deb68
zeye <zeye@microsoft.com>
2019-09-21 14:29:04 +0800

    Fix CI test VisualDesigner

--- Composer/cypress/integration/VisualDesigner.spec.js
+++ Composer/cypress/integration/VisualDesigner.spec.js
@@ -3,37 +3,12 @@
 context('Visual Designer', () => {
   before(() => {
     cy.visit(Cypress.env('COMPOSER_URL'));
-    cy.startFromTemplate('EmptyBot', 'VisualDesignerTest');
+    cy.openBot('ToDoBot');
   });
 
-  //will remove skip after add trigger is ok
-  it('can add a rule from the visual designer', () => {
-    cy.addEventHandler('Handle a Dialog Event');
-    cy.wait(100);
-
+  it('can find Visual Designer default trigger in container', () => {
     cy.withinEditor('VisualEditor', () => {
-      cy.contains('Handle a Dialog Event').should('exist');
-    });
-
-    cy.addEventHandler('Handle an Intent');
-    cy.wait(100);
-
-    cy.withinEditor('VisualEditor', () => {
-      cy.contains('Handle an Intent').should('exist');
-    });
-
-    cy.addEventHandler('Handle Unknown Intent');
-    cy.wait(100);
-
-    cy.withinEditor('VisualEditor', () => {
-      cy.contains('Handle Unknown Intent').should('exist');
-    });
-
-    cy.addEventHandler('Handle ConversationUpdate');
-    cy.wait(100);
-
-    cy.withinEditor('VisualEditor', () => {
-      cy.contains('Handle ConversationUpdate').should('exist');
+      cy.getByText('Trigger').should('exist');
     });
   });
 });
