487ece868969fa341d352515dc9e1137b3a83c03
zeye <zeye@microsoft.com>
2019-09-21 13:30:46 +0800

    fix cursor move CI test

--- Composer/cypress/integration/CursorMove.spec.js
+++ Composer/cypress/integration/CursorMove.spec.js
@@ -8,64 +8,40 @@ context('Cursor move', () => {
     cy.visit(Cypress.env('COMPOSER_URL'));
     cy.openBot('ToDoBot');
   });
-  it('can move cursor by pressing direction key or tab', () => {
+
+  beforeEach(() => {
     cy.get('[data-testid="ProjectTree"]').within(() => {
-      cy.getByText('AddToDo').click();
+      cy.getByText('ClearToDos').click();
       cy.wait(100);
     });
+  });
 
+  it('can move cursor by pressing direction key', () => {
     cy.withinEditor('VisualEditor', () => {
-      cy.get('[data-is-node=true]').as('selectedNodes');
-      cy.get('@selectedNodes')
-        .eq(1)
+      cy.get('[data-is-node=true]').as('selectableNodes');
+      cy.get('@selectableNodes')
+        .eq(0)
         .click();
+      cy.wait(100);
+
+      cy.get('@selectableNodes')
+        .eq(0)
+        .should('have.class', 'step-renderer-container--selected');
 
       // down arrow
       cy.get('[data-test-id="keyboard-zone"]').type('{downarrow}');
       cy.wait(100);
-      cy.get('@selectedNodes')
-        .eq(2)
-        .should('have.class', 'step-renderer-container--selected');
-
-      cy.wait(100);
-
-      // right arrow
-      cy.get('[data-test-id="keyboard-zone"]').type('{rightarrow}');
-      cy.wait(100);
-      cy.get('@selectedNodes')
-        .eq(4)
-        .should('have.class', 'step-renderer-container--selected');
-
-      cy.wait(100);
-
-      // left arrow
-      cy.get('[data-test-id="keyboard-zone"]').type('{leftarrow}');
-      cy.wait(100);
-      cy.get('@selectedNodes')
-        .eq(5)
-        .should('have.class', 'step-renderer-container--selected');
+      cy.get('@selectableNodes')
+        .eq(0)
+        .should('not.have.class', 'step-renderer-container--selected');
 
       cy.wait(100);
 
       // up arrow
       cy.get('[data-test-id="keyboard-zone"]').type('{uparrow}');
       cy.wait(100);
-      cy.get('@selectedNodes')
-        .eq(3)
-        .should('have.class', 'step-renderer-container--selected');
-      cy.wait(100);
-
-      cy.get('[data-is-selectable=true]').as('selectedElements');
-
-      cy.get('@selectedElements')
-        .eq(1)
-        .click();
-
-      // tab
-      cy.get('[data-test-id="keyboard-zone"]').tab();
-      cy.wait(100);
-      cy.get('@selectedElements')
-        .eq(2)
+      cy.get('@selectableNodes')
+        .eq(0)
         .should('have.class', 'step-renderer-container--selected');
       cy.wait(100);
     });
