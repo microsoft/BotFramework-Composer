951779fa5b0cf257dbf5c285e19f6ef1037e2f3a
Andy Brown <asbrown002@gmail.com>
2019-10-08 09:06:50 -0700

    extract lg editor widget and make template name use field name

--- Composer/packages/extensions/obiformeditor/src/Form/fields/LgEditorField.tsx
+++ Composer/packages/extensions/obiformeditor/src/Form/fields/LgEditorField.tsx
@@ -1,92 +1,19 @@
 import React from 'react';
-import { useState, useEffect } from 'react';
-import { RichEditor } from 'code-editor';
 
 import { BFDFieldProps } from '../types';
+import { LgEditorWidget } from '../widgets/LgEditorWidget';
 
 import { BaseField } from './BaseField';
 
-const LG_HELP =
-  'https://github.com/microsoft/BotBuilder-Samples/blob/master/experimental/language-generation/docs/lg-file-format.md';
-
-const getInitialTemplate = (formData?: string): string => {
-  let newTemplate = formData || '';
-
-  if (newTemplate.indexOf('bfdactivity-') !== -1) {
-    return '';
-  } else if (newTemplate && !newTemplate.startsWith('-')) {
-    newTemplate = `-${newTemplate}`;
-  }
-
-  return newTemplate;
-};
-
 export const LgEditorField: React.FC<BFDFieldProps> = props => {
-  const { formContext } = props;
-  const [templateToRender, setTemplateToRender] = useState({ Name: '', Body: '' });
-  const lgId = `bfdactivity-${formContext.dialogId}`;
-  const [errorMsg, setErrorMsg] = useState('');
-
-  const ensureTemplate = async (newBody?: string): Promise<void> => {
-    const templates = await formContext.shellApi.getLgTemplates('common');
-    const template = templates.find(template => {
-      return template.Name === lgId;
-    });
-    if (template === null || template === undefined) {
-      const newTemplate = getInitialTemplate(newBody);
-
-      if (formContext.dialogId && newTemplate) {
-        formContext.shellApi.updateLgTemplate('common', lgId, newTemplate);
-        props.onChange(`[${lgId}]`);
-      }
-      setTemplateToRender({ Name: `# ${lgId}`, Body: newTemplate });
-    } else {
-      if (templateToRender.Name === '') {
-        setTemplateToRender({ Name: `# ${lgId}`, Body: template.Body });
-      }
-    }
-  };
-
-  const onChange = (data): void => {
-    // hit the lg api and replace it's Body with data
-    if (formContext.dialogId) {
-      let dataToEmit = data.trim();
-      if (dataToEmit.length > 0 && dataToEmit[0] !== '-') {
-        dataToEmit = `-${dataToEmit}`;
-      }
-
-      if (dataToEmit.length > 0) {
-        setTemplateToRender({ Name: templateToRender.Name, Body: data });
-        formContext.shellApi
-          .updateLgTemplate('common', lgId, dataToEmit)
-          .then(() => setErrorMsg(''))
-          .catch(error => setErrorMsg(error));
-        props.onChange(`[${lgId}]`);
-      } else {
-        setTemplateToRender({ Name: templateToRender.Name, Body: '' });
-        formContext.shellApi.removeLgTemplate('common', lgId);
-        props.onChange(undefined);
-      }
-    }
-  };
-
-  useEffect(() => {
-    ensureTemplate(props.formData);
-  }, [formContext.dialogId]);
-
-  const { Body } = templateToRender;
   return (
-    <div>
-      <BaseField {...props}>
-        <div
-          style={{
-            height: '250px',
-            paddingBottom: '19px',
-          }}
-        >
-          <RichEditor errorMsg={errorMsg} value={Body} onChange={onChange} helpURL={LG_HELP} />
-        </div>
-      </BaseField>
-    </div>
+    <BaseField {...props}>
+      <LgEditorWidget
+        name={props.name}
+        formData={props.formData}
+        formContext={props.formContext}
+        onChange={props.onChange}
+      />
+    </BaseField>
   );
 };
