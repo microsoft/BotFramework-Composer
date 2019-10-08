3f1d1b3da545d79bdef8fa87a6f7777819ab2fe6
Long Jun <alanlong9278@126.com>
2019-10-08 18:47:27 +0800

    detach visualApi logic from FrameApi

--- Composer/packages/client/src/messenger/FrameAPI.ts
+++ Composer/packages/client/src/messenger/FrameAPI.ts
@@ -14,15 +14,14 @@ export class FrameAPI {
   /**
    * Initialize the frame ref at first invocation.
    */
-  invoke = (method: string, ...rest) => {
+  setFrameRef = () => {
     if (!this.frameRef) {
       this.frameRef = window.frames[this.frameId];
     }
+  };
 
-    if (this.frameRef && this.frameRef[method]) {
-      return apiClient.apiCall('rpc', [method, ...rest], this.frameRef);
-    }
-    return Promise.reject();
+  invoke = (method: string, ...rest) => {
+    return apiClient.apiCall('rpc', [method, ...rest], this.frameRef);
   };
 }
 
@@ -34,11 +33,18 @@ export const VisualEditorAPI = (() => {
     visualEditorFrameAPI.invoke = () => Promise.resolve(false);
   }
 
+  const invoke = (method: string) => {
+    visualEditorFrameAPI.setFrameRef();
+    if (visualEditorFrameAPI.frameRef && visualEditorFrameAPI.frameRef[method]) {
+      return visualEditorFrameAPI.invoke(method);
+    }
+    return Promise.reject();
+  };
   return {
-    hasElementFocused: () => visualEditorFrameAPI.invoke('hasElementFocused'),
-    hasElementSelected: () => visualEditorFrameAPI.invoke('hasElementSelected'),
-    copySelection: () => visualEditorFrameAPI.invoke('copySelection'),
-    cutSelection: () => visualEditorFrameAPI.invoke('cutSelection'),
-    deleteSelection: () => visualEditorFrameAPI.invoke('deleteSelection'),
+    hasElementFocused: () => invoke('hasElementFocused'),
+    hasElementSelected: () => invoke('hasElementSelected'),
+    copySelection: () => invoke('copySelection'),
+    cutSelection: () => invoke('cutSelection'),
+    deleteSelection: () => invoke('deleteSelection'),
   };
 })();
