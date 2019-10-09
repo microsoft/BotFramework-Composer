9dabe784f6de42b6baddf75685d118525ad13efb
Long Alan <alanlong9278@126.com>
2019-10-09 21:19:26 +0800

    action node ui adjust (#1051)

--- Composer/packages/extensions/visual-designer/src/components/nodes/templates/FormCard.tsx
+++ Composer/packages/extensions/visual-designer/src/components/nodes/templates/FormCard.tsx
@@ -18,8 +18,8 @@ const containerStyle = {
   cursor: 'pointer',
   overflow: 'hidden',
   backgroundColor: 'white',
-  borderRadius: '2px',
-  boxShadow: '0px 1.2px 3.6px rgba(0, 0, 0, 0.108), 0px 6.4px 14.4px rgba(0, 0, 0, 0.132)',
+  borderRadius: '2px 2px 0 0',
+  boxShadow: '0px 2px 8px rgba(0, 0, 0, 0.1)',
 };
 
 interface NodeProps {
