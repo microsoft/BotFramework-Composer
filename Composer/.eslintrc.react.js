e67736ab880f581ede9a44f6c053a27aaed47c69
Andy Brown <asbrown002@gmail.com>
2019-10-09 15:43:49 -0700

    do not use dangerous inner html in react

--- Composer/.eslintrc.react.js
+++ Composer/.eslintrc.react.js
@@ -14,16 +14,17 @@ module.exports = {
     },
   },
   rules: {
-    'react/no-deprecated': 'warn',
-    'react/display-name': 'off',
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'off',
-
-    'react/prop-types': 'warn',
-
     'format-message/literal-pattern': 'error',
     'format-message/no-invalid-pattern': 'error',
     'format-message/no-missing-params': ['error', { allowNonLiteral: false }],
+
+    'react-hooks/exhaustive-deps': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+
+    'react/display-name': 'off',
+    'react/no-danger': 'error',
+    'react/no-deprecated': 'warn',
+    'react/prop-types': 'warn',
   },
   overrides: [
     {
@@ -37,6 +38,7 @@ module.exports = {
       rules: {
         'react/display-name': 'off',
         'react/prop-types': 'off',
+        'react/no-danger': 'off',
       },
     },
   ],
