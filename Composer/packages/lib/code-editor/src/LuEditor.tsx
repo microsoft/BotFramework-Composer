4b608b2eeea2452abe108d4fbfcdbc1c5dd55087
Andy Brown <asbrown002@gmail.com>
2019-10-09 13:18:48 -0700

    use https

--- Composer/packages/lib/code-editor/src/LuEditor.tsx
+++ Composer/packages/lib/code-editor/src/LuEditor.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { RichEditor, RichEditorProps } from './RichEditor';
 
-const LU_HELP = 'http://aka.ms/lu-file-format';
+const LU_HELP = 'https://aka.ms/lu-file-format';
 const placeholder = `> See ${LU_HELP} to learn about supported LU concepts.`;
 
 export function LuEditor(props: RichEditorProps) {
