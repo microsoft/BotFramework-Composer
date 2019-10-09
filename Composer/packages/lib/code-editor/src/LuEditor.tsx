5eaa8736c20ed857fde972074741daed7c9bc443
Andy Brown <asbrown002@gmail.com>
2019-10-09 13:17:08 -0700

    fix lint error

--- Composer/packages/lib/code-editor/src/LuEditor.tsx
+++ Composer/packages/lib/code-editor/src/LuEditor.tsx
@@ -2,10 +2,8 @@ import React from 'react';
 
 import { RichEditor, RichEditorProps } from './RichEditor';
 
-const LU_HELP =
-  'http://aka.ms/lu-file-format';
-const placeholder = `> See ${LU_HELP} to learn about supported LU concepts.
-`;
+const LU_HELP = 'http://aka.ms/lu-file-format';
+const placeholder = `> See ${LU_HELP} to learn about supported LU concepts.`;
 
 export function LuEditor(props: RichEditorProps) {
   return <RichEditor placeholder={placeholder} helpURL={LU_HELP} {...props} />;
