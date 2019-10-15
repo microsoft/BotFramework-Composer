683c9555a58d31fd96fc767054b59d2a6b687e3c
zeye <zeye@microsoft.com>
2019-10-14 17:42:31 -0700

    resolve #1107

--- Composer/packages/extensions/visual-designer/src/constants/ObiTypes.ts
+++ Composer/packages/extensions/visual-designer/src/constants/ObiTypes.ts
@@ -1,57 +1,12 @@
+import { SDKTypes } from 'shared';
+
 export const ObiTypes = {
-  // dialogs
-  AdaptiveDialog: 'Microsoft.AdaptiveDialog',
+  ...SDKTypes,
 
-  // recognizers
-  RegexRecognizer: 'Microsoft.RegexRecognizer',
-  LuisRecognizer: 'Microsoft.LuisRecognizer',
-  MultiLanguageRecognizer: 'Microsoft.MultiLanguageRecognizer',
-  QnARecognizer: 'Microsoft.QnARecognizer',
-
-  // events
-  OnUnknownIntent: 'Microsoft.OnUnknownIntent',
-  OnIntent: 'Microsoft.OnIntent',
-  OnEvent: 'Microsoft.OnEvent',
-  OnConversationUpdateActivity: 'Microsoft.OnConversationUpdateActivity',
-
-  // steps
-  BeginDialog: 'Microsoft.BeginDialog',
-  SendActivity: 'Microsoft.SendActivity',
-
-  InitProperty: 'Microsoft.InitProperty',
-  SetProperty: 'Microsoft.SetProperty',
-  EditArray: 'Microsoft.EditArray',
-  SaveEntity: 'Microsoft.SaveEntity',
-  DeleteProperty: 'Microsoft.DeleteProperty',
-
-  IfCondition: 'Microsoft.IfCondition',
-  SwitchCondition: 'Microsoft.SwitchCondition',
-  Foreach: 'Microsoft.Foreach',
-  ForeachPage: 'Microsoft.ForeachPage',
-
-  AttachmentInput: 'Microsoft.AttachmentInput',
-  ChoiceInput: 'Microsoft.ChoiceInput',
-  ConfirmInput: 'Microsoft.ConfirmInput',
-  DateTimeInput: 'Microsoft.DateTimeInput',
-  NumberInput: 'Microsoft.NumberInput',
-  OAuthInput: 'Microsoft.OAuthInput',
-  TextInput: 'Microsoft.TextInput',
-
-  EndDialog: 'Microsoft.EndDialog',
-  CancelAllDialogs: 'Microsoft.CancelAllDialogs',
-  ReplaceDialog: 'Microsoft.ReplaceDialog',
-  RepeatDialog: 'Microsoft.RepeatDialog',
-  EndTurn: 'Microsoft.EndTurn',
-
-  EditActions: 'Microsoft.EditActions',
-
-  EmitEvent: 'Microsoft.EmitEvent',
-
-  HttpRequest: 'Microsoft.HttpRequest',
+  // TODO: are they outdated?
   CodeStep: 'Microsoft.CodeStep',
-
-  LogAction: 'Microsoft.LogAction',
-  TraceActivity: 'Microsoft.TraceActivity',
+  QnARecognizer: 'Microsoft.QnARecognizer',
+  SaveEntity: 'Microsoft.SaveEntity',
 
   // virtual
   RuleGroup: 'VisualSDK.RuleGroup',
