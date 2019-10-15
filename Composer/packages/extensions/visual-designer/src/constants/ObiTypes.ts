50816c1d33b3bf07f5ea4a0c530b8aff549ea163
zeye <zeye@microsoft.com>
2019-10-15 10:52:40 -0700

    const => enum

--- Composer/packages/extensions/visual-designer/src/constants/ObiTypes.ts
+++ Composer/packages/extensions/visual-designer/src/constants/ObiTypes.ts
@@ -1,18 +1,18 @@
 import { SDKTypes } from 'shared';
 
-const VirtualElementTypes = {
-  RuleGroup: 'VisualSDK.RuleGroup',
-  StepGroup: 'VisualSDK.StepGroup',
-  ChoiceDiamond: 'VisualSDK.ChoiceDiamond',
-  ConditionNode: 'VisualSDK.ConditionNode',
-  LoopIndicator: 'VisualSDK.LoopIndicator',
-  ForeachDetail: 'VisualSDK.ForeachDetail',
-  ForeachPageDetail: 'VisualSDK.ForeachPageDetail',
-  BotAsks: 'VisualSDK.BotAsks',
-  UserAnswers: 'VisualSDK.UserAnswers',
-  InvalidPromptBrick: 'VisualSDK.InvalidPromptBrick',
-  ChoiceInputDetail: 'VisualSDK.ChoiceInputDetail',
-};
+enum VirtualElementTypes {
+  RuleGroup = 'VisualSDK.RuleGroup',
+  StepGroup = 'VisualSDK.StepGroup',
+  ChoiceDiamond = 'VisualSDK.ChoiceDiamond',
+  ConditionNode = 'VisualSDK.ConditionNode',
+  LoopIndicator = 'VisualSDK.LoopIndicator',
+  ForeachDetail = 'VisualSDK.ForeachDetail',
+  ForeachPageDetail = 'VisualSDK.ForeachPageDetail',
+  BotAsks = 'VisualSDK.BotAsks',
+  UserAnswers = 'VisualSDK.UserAnswers',
+  InvalidPromptBrick = 'VisualSDK.InvalidPromptBrick',
+  ChoiceInputDetail = 'VisualSDK.ChoiceInputDetail',
+}
 
 export const ObiTypes = {
   ...SDKTypes,
