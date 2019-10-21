9fc9500b966353c02f297609b197fd926b106b5a
Hai Liu <hailiu@gmail.com>
2019-10-21 14:43:30 -0700

    clean up obfuscate

--- Composer/packages/client/src/utils/objUtil.ts
+++ Composer/packages/client/src/utils/objUtil.ts
@@ -4,12 +4,12 @@
  */
 export const obfuscate = (obj: Record<string, any> | string | number | boolean) => {
   const OBFUSCATED_VALUE = '*****';
-  if (typeof obj !== 'object') {
+  if (!obj || typeof obj !== 'object') {
     return OBFUSCATED_VALUE;
   }
   if (Array.isArray(obj)) {
     return obj.map(obfuscate);
   }
 
-  return obj ? Object.keys(obj).reduce((prev, k) => ({ ...prev, [k]: obfuscate(obj[k]) }), {}) : OBFUSCATED_VALUE;
+  return Object.keys(obj).reduce((prev, k) => ({ ...prev, [k]: obfuscate(obj[k]) }), {});
 };
