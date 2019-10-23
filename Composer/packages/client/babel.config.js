d52d6985ffe201b70c86b05139dd03ef856cddab
Andy Brown <asbrown002@gmail.com>
2019-10-22 17:10:07 -0700

    add emotion babel plugin for test

--- Composer/packages/client/babel.config.js
+++ Composer/packages/client/babel.config.js
@@ -23,6 +23,7 @@ module.exports = {
           },
         ],
         '@babel/preset-react',
+        '@emotion/babel-preset-css-prop',
       ],
       plugins: ['@babel/plugin-proposal-class-properties', '@babel/plugin-transform-runtime'],
     },
