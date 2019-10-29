cdc32cad4b1a3098a3acea3708bff338712c17c8
Chris Anderson (CONVERSATIONAL AI) <chrande@microsoft.com>
2019-10-29 11:46:48 -0700

    chore(BotProject): Manually add license headers

--- BotProject/CSharp/Tests/ToDoBotTests.cs
+++ BotProject/CSharp/Tests/ToDoBotTests.cs
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
 using Microsoft.Bot.Builder;
 using Microsoft.Bot.Builder.Adapters;
 using Microsoft.Bot.Builder.Dialogs;
