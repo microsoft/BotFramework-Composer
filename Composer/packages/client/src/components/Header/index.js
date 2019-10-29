494bcf5c019469fbd6fcf91e2951084e80d6282a
Andy Brown <asbrown002@gmail.com>
2019-10-29 15:33:28 -0700

    move svg assets into images dir

--- Composer/packages/client/src/components/Header/index.js
+++ Composer/packages/client/src/components/Header/index.js
@@ -4,7 +4,7 @@
 import React from 'react';
 import formatMessage from 'format-message';
 
-import composerIcon from '../../composerIcon.svg';
+import composerIcon from '../../images/composerIcon.svg';
 
 import { headerContainer, title, botName } from './styles';
 
