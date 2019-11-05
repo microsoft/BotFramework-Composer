e5795d851891cc9b495205c4fd9f5aa015d2cd15
leilzh <leilzh@microsoft.com>
2019-11-05 15:48:56 +0800

    fix setting can't import corrent

--- Composer/packages/server/src/settings/index.ts
+++ Composer/packages/server/src/settings/index.ts
@@ -6,14 +6,12 @@ import merge from 'lodash.merge';
 import settings from './settings.json';
 
 // overall the guidance in settings.json is to list every item in "development"
-// section with a default value, and override the value for different environment
-// in later sections
+// section with a default value, and override the value for different
+// environment in later sections
 
 const defaultSettings = settings.development;
 const environment = process.env.NODE_ENV || 'development';
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const environmentSettings = (settings as any)[environment];
-
 const finalSettings = merge(defaultSettings, environmentSettings);
-
 export default finalSettings;
