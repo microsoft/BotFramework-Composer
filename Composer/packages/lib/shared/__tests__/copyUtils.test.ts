e8aa0aad08a9d767a19409f2b35add3fbdea958d
zeye <zeye@microsoft.com>
2019-11-05 14:09:52 +0800

    implement the parent copier `copyAdaptiveAction`

--- Composer/packages/lib/shared/__tests__/copyUtils.test.ts
+++ Composer/packages/lib/shared/__tests__/copyUtils.test.ts
@@ -1,26 +1,13 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
 import { copyAdaptiveAction } from '../src/copyUtils';
+import { ExternalApi } from '../src/copyUtils/ExternalApi';
 
 describe('copyAdaptiveAction', () => {
-  const lgTemplate = [{ Name: 'bfdactivity-1234', Body: '-hello' }, { Name: 'bfdprompt-1234', Body: '-hi' }];
-  const externalApi = {
-    updateDesigner: data => {
-      data.$designer = { id: '5678' };
-    },
-    lgApi: {
-      getLgTemplates: (fileId, activityId) => {
-        return Promise.resolve(lgTemplate);
-      },
-      updateLgTemplate: (filedId, activityId, activityBody) => {
-        return Promise.resolve(true);
-      },
-    },
-  };
-  const externalApiWithFailure = {
-    ...externalApi,
-    lgApi: {
-      ...externalApi.lgApi,
-      updateLgTemplate: () => Promise.reject(),
-    },
+  const externalApi: ExternalApi = {
+    getDesignerId: () => ({ id: '5678' }),
+    copyLgTemplate: x => Promise.resolve(x + '(copy)'),
   };
 
   it('should return {} when input is invalid', async () => {
@@ -51,13 +38,7 @@ describe('copyAdaptiveAction', () => {
     expect(await copyAdaptiveAction(sendActivity, externalApi)).toEqual({
       $type: 'Microsoft.SendActivity',
       $designer: { id: '5678' },
-      activity: '[bfdactivity-5678]',
-    });
-
-    expect(await copyAdaptiveAction(sendActivity, externalApiWithFailure)).toEqual({
-      $type: 'Microsoft.SendActivity',
-      $designer: { id: '5678' },
-      activity: '-hello',
+      activity: '[bfdactivity-1234](copy)',
     });
   });
 
@@ -73,6 +54,7 @@ describe('copyAdaptiveAction', () => {
       allowInterruptions: 'true',
       outputFormat: 'none',
       prompt: '[bfdprompt-1234]',
+      invalidPrompt: '[bfdinvalidPrompt-1234]',
     };
 
     expect(await copyAdaptiveAction(promptText, externalApi)).toEqual({
@@ -84,19 +66,168 @@ describe('copyAdaptiveAction', () => {
       alwaysPrompt: false,
       allowInterruptions: 'true',
       outputFormat: 'none',
-      prompt: '[bfdprompt-5678]',
+      prompt: '[bfdprompt-1234](copy)',
+      invalidPrompt: '[bfdinvalidPrompt-1234](copy)',
     });
+  });
 
-    expect(await copyAdaptiveAction(promptText, externalApiWithFailure)).toEqual({
-      $type: 'Microsoft.TextInput',
+  it('can copy IfCondition', async () => {
+    const ifCondition = {
+      $type: 'Microsoft.IfCondition',
+      condition: 'a == b',
+      actions: [
+        {
+          $type: 'Microsoft.BeginDialog',
+          dialog: 'AddToDo',
+        },
+        {
+          $type: 'Microsoft.IfCondition',
+          actions: [
+            {
+              $type: 'Microsoft.SendActivity',
+              activity: '[bfdactivity-1234]',
+            },
+          ],
+        },
+      ],
+      elseActions: [],
+    };
+
+    expect(await copyAdaptiveAction(ifCondition, externalApi)).toEqual({
+      $type: 'Microsoft.IfCondition',
       $designer: {
         id: '5678',
       },
-      maxTurnCount: 3,
-      alwaysPrompt: false,
-      allowInterruptions: 'true',
-      outputFormat: 'none',
-      prompt: '-hi',
+      condition: 'a == b',
+      actions: [
+        {
+          $type: 'Microsoft.BeginDialog',
+          $designer: {
+            id: '5678',
+          },
+          dialog: 'AddToDo',
+        },
+        {
+          $type: 'Microsoft.IfCondition',
+          $designer: {
+            id: '5678',
+          },
+          actions: [
+            {
+              $type: 'Microsoft.SendActivity',
+              $designer: {
+                id: '5678',
+              },
+              activity: '[bfdactivity-1234](copy)',
+            },
+          ],
+        },
+      ],
+      elseActions: [],
+    });
+  });
+
+  it('can copy SwitchCondition', async () => {
+    const switchCondition = {
+      $type: 'Microsoft.SwitchCondition',
+      condition: 'dialog.x',
+      default: [
+        {
+          $type: 'Microsoft.BeginDialog',
+          dialog: 'AddToDo',
+        },
+        {
+          $type: 'Microsoft.IfCondition',
+          actions: [
+            {
+              $type: 'Microsoft.SendActivity',
+              activity: '[bfdactivity-1234]',
+            },
+          ],
+        },
+      ],
+      cases: [
+        {
+          value: '0',
+          actions: [
+            {
+              $type: 'Microsoft.BeginDialog',
+              dialog: 'AddToDo',
+            },
+          ],
+        },
+        {
+          value: '1',
+          actions: [
+            {
+              $type: 'Microsoft.SwitchCondition',
+              condition: 'a.b',
+              default: [],
+              cases: [],
+            },
+          ],
+        },
+      ],
+    };
+
+    expect(await copyAdaptiveAction(switchCondition, externalApi)).toEqual({
+      $type: 'Microsoft.SwitchCondition',
+      $designer: {
+        id: '5678',
+      },
+      condition: 'dialog.x',
+      default: [
+        {
+          $type: 'Microsoft.BeginDialog',
+          $designer: {
+            id: '5678',
+          },
+          dialog: 'AddToDo',
+        },
+        {
+          $type: 'Microsoft.IfCondition',
+          $designer: {
+            id: '5678',
+          },
+          actions: [
+            {
+              $type: 'Microsoft.SendActivity',
+              $designer: {
+                id: '5678',
+              },
+              activity: '[bfdactivity-1234](copy)',
+            },
+          ],
+        },
+      ],
+      cases: [
+        {
+          value: '0',
+          actions: [
+            {
+              $type: 'Microsoft.BeginDialog',
+              $designer: {
+                id: '5678',
+              },
+              dialog: 'AddToDo',
+            },
+          ],
+        },
+        {
+          value: '1',
+          actions: [
+            {
+              $type: 'Microsoft.SwitchCondition',
+              $designer: {
+                id: '5678',
+              },
+              condition: 'a.b',
+              default: [],
+              cases: [],
+            },
+          ],
+        },
+      ],
     });
   });
 });
