60b07ef727d9b9dc690e9a0efcdd70db119e8f3d
zhixzhan <49866537+zhixzhan@users.noreply.github.com>
2019-11-05 14:34:05 +0800

    update demo

--- Composer/packages/tools/language-servers/language-generation/demo/src/main.ts
+++ Composer/packages/tools/language-servers/language-generation/demo/src/main.ts
@@ -1,13 +1,36 @@
 (self as any).MonacoEnvironment = {
   getWorkerUrl: () => './editor.worker.bundle.js',
 };
-import './client.ts';
-// const container = document.getElementById('container');
+// import './client.ts';
+import { startSampleClient } from '../../src/startSampleClient';
 
-// // import { initClient } from './client'
-// // initClient(container)
+const container = document.getElementById('container');
 
-// import { createEditor, registerLGLanguage, startSampleClient } from '../../src/startSampleClient';
-// registerLGLanguage();
-// const editor = createEditor(container)
-// startSampleClient(editor)
+const content = `# Greeting1
+-Good morning
+
+# Greeting2
+-Good afternoon
+
+# Greeting3
+-Good evening
+
+# Greeting4
+-Good midnight`;
+
+// Body will fill in editor
+const template = {
+  Name: 'Greeting2',
+  Body: `-Good afternoon
+-[Greeting3]
+-[Greeting4]`,
+};
+// setting for inline LG template editor
+const file = {
+  uri: 'inmemory://common2.lg',
+  language: 'botbuilderlg',
+  content,
+  template,
+};
+
+startSampleClient(container, file);
