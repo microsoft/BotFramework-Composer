0cc40b25f8ffe82190e98b19682dc73af1b0496b
zeye <zeye@microsoft.com>
2019-11-06 16:17:04 +0800

    add test cases for SwitchCondition and IfCondition

---
+++ Composer/packages/lib/shared/__tests__/copyUtils/copyIfCondition.test.ts
@@ -0,0 +1,56 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import { copyIfCondition } from '../../src/copyUtils/copyIfCondition';
+
+describe('#copyIfCondition', () => {
+  const externalApi = {
+    getDesignerId: () => ({ id: '5678' }),
+    copyLgTemplate: (id, x) => Promise.resolve(x),
+  };
+
+  it('can copy normal input', async () => {
+    const ifCondition = {
+      $type: 'Microsoft.IfCondition',
+      condition: 'a == b',
+      actions: [
+        {
+          $type: 'Microsoft.BeginDialog',
+          dialog: 'AddToDo',
+        },
+      ],
+      elseActions: [
+        {
+          $type: 'Microsoft.SendActivity',
+          activity: '[bfdactivity-1234]',
+        },
+      ],
+    };
+
+    expect(await copyIfCondition(ifCondition, externalApi)).toEqual({
+      $type: 'Microsoft.IfCondition',
+      $designer: {
+        id: '5678',
+      },
+      condition: 'a == b',
+      actions: [
+        {
+          $type: 'Microsoft.BeginDialog',
+          $designer: {
+            id: '5678',
+          },
+          dialog: 'AddToDo',
+        },
+      ],
+      elseActions: [
+        {
+          $type: 'Microsoft.SendActivity',
+          $designer: {
+            id: '5678',
+          },
+          activity: '[bfdactivity-1234]',
+        },
+      ],
+    });
+  });
+});
