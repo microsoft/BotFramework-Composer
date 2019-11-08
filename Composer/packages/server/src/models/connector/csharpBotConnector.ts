922f29a7a58cfaf033ebedef333c7599ddab904f
leilzh <leilzh@microsoft.com>
2019-11-08 11:17:57 +0800

    Fix: Failure to create a bot from scratch

--- Composer/packages/server/src/models/connector/csharpBotConnector.ts
+++ Composer/packages/server/src/models/connector/csharpBotConnector.ts
@@ -55,11 +55,8 @@ export class CSharpBotConnector implements IBotConnector {
       throw new Error('Please publish your Luis models');
     }
 
-    if (luisConfig) {
-      form.append(
-        'endpointKey',
-        luisConfig.endpointKey && luisConfig.endpointKey !== '' ? luisConfig.endpointKey : luisConfig.authoringKey
-      );
+    if (luisConfig && (luisConfig.endpointKey || luisConfig.authoringKey)) {
+      form.append('endpointKey', luisConfig.endpointKey || luisConfig.endpointKey);
     }
 
     config = {
