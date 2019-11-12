7bcdbae113dca9b6929f13953aa929a17edbf779
zhixzhan <49866537+zhixzhan@users.noreply.github.com>
2019-11-12 18:04:20 +0800

    refactor

--- Composer/packages/tools/language-servers/language-generation/src/attachWSToHTTPServer.ts
+++ Composer/packages/tools/language-servers/language-generation/src/attachWSToHTTPServer.ts
@@ -8,36 +8,55 @@ import * as net from 'net';
 import * as ws from 'ws';
 import * as rpc from 'vscode-ws-jsonrpc';
 
-import { launch } from './adapter';
+import { start as startLGServer } from './LGServer';
 
-export function attachLSPServer(server: http.Server, path: string) {
-  const wss = new ws.Server({
-    noServer: true,
-    perMessageDeflate: false,
-  });
+function createSocketHandler(webSocket: any): rpc.IWebSocket {
+  const socket: rpc.IWebSocket = {
+    send: content =>
+      webSocket.send(content, error => {
+        if (error) {
+          throw error;
+        }
+      }),
+    onMessage: cb => webSocket.on('message', cb),
+    onError: cb => webSocket.on('error', cb),
+    onClose: cb => webSocket.on('close', cb),
+    dispose: () => webSocket.close(),
+  };
+  return socket;
+}
+
+function bindWStoHTTP(wss: ws.Server, server: http.Server, path: string, handler: (webSocket) => void) {
   server.on('upgrade', (request: http.IncomingMessage, socket: net.Socket, head: Buffer) => {
     const pathname = request.url ? url.parse(request.url).pathname : undefined;
     if (pathname === path) {
-      wss.handleUpgrade(request, socket, head, webSocket => {
-        const socket: rpc.IWebSocket = {
-          send: content =>
-            webSocket.send(content, error => {
-              if (error) {
-                throw error;
-              }
-            }),
-          onMessage: cb => webSocket.on('message', cb),
-          onError: cb => webSocket.on('error', cb),
-          onClose: cb => webSocket.on('close', cb),
-          dispose: () => webSocket.close(),
-        };
-        // launch the server when the web socket is opened
-        if (webSocket.readyState === webSocket.OPEN) {
-          launch(socket);
-        } else {
-          webSocket.on('open', () => launch(socket));
-        }
-      });
+      wss.handleUpgrade(request, socket, head, handler);
+    }
+  });
+}
+
+function launchLanguageServer(socket: rpc.IWebSocket) {
+  const reader = new rpc.WebSocketMessageReader(socket);
+  const writer = new rpc.WebSocketMessageWriter(socket);
+  startLGServer(reader, writer);
+}
+
+function createWebSocketServer(): ws.Server {
+  return new ws.Server({
+    noServer: true,
+    perMessageDeflate: false,
+  });
+}
+
+export function attachLSPServer(server: http.Server, path: string) {
+  const wss = createWebSocketServer();
+  bindWStoHTTP(wss, server, path, webSocket => {
+    const socketHandler = createSocketHandler(webSocket);
+    // launch language server when the web socket is opened
+    if (webSocket.readyState === webSocket.OPEN) {
+      launchLanguageServer(socketHandler);
+    } else {
+      webSocket.on('open', () => launchLanguageServer(socketHandler));
     }
   });
 }
