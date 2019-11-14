3dda6f6749aa779c427c261a75f7a5cff157d56b
zeye <zeye@microsoft.com>
2019-11-14 10:50:34 +0800

    split CopyConstructorMap to another file

--- Composer/packages/lib/shared/src/copyUtils/copyAdaptiveAction.ts
+++ Composer/packages/lib/shared/src/copyUtils/copyAdaptiveAction.ts
@@ -1,33 +1,10 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-import { SDKTypes } from '../types/schema';
 import { MicrosoftIDialog } from '../types';
 
 import { ExternalApi } from './ExternalApi';
-import { copySendActivity } from './copySendActivity';
-import { copyInputDialog } from './copyInputDialog';
-import { copyIfCondition } from './copyIfCondition';
-import { copySwitchCondition } from './copySwitchCondition';
-import { shallowCopyAdaptiveAction } from './shallowCopyAdaptiveAction';
-import { copyForeach } from './copyForeach';
-import { copyEditActions } from './copyEditActions';
-
-export const CopyConstructorMap = {
-  [SDKTypes.SendActivity]: copySendActivity,
-  [SDKTypes.AttachmentInput]: copyInputDialog,
-  [SDKTypes.ChoiceInput]: copyInputDialog,
-  [SDKTypes.ConfirmInput]: copyInputDialog,
-  [SDKTypes.DateTimeInput]: copyInputDialog,
-  [SDKTypes.NumberInput]: copyInputDialog,
-  [SDKTypes.TextInput]: copyInputDialog,
-  [SDKTypes.IfCondition]: copyIfCondition,
-  [SDKTypes.SwitchCondition]: copySwitchCondition,
-  [SDKTypes.Foreach]: copyForeach,
-  [SDKTypes.ForeachPage]: copyForeach,
-  [SDKTypes.EditActions]: copyEditActions,
-  default: shallowCopyAdaptiveAction,
-};
+import CopyConstructorMap from './CopyConstructorMap';
 
 export async function copyAdaptiveAction(data: MicrosoftIDialog, externalApi: ExternalApi): Promise<MicrosoftIDialog> {
   if (typeof data === 'string') {
