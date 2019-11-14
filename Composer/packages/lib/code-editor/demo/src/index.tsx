94e69d738a4cffe3442bdebc2804216d0e67414a
zhixzhan <49866537+zhixzhan@users.noreply.github.com>
2019-11-14 17:01:47 +0800

    add mixed demo

--- Composer/packages/lib/code-editor/demo/src/index.tsx
+++ Composer/packages/lib/code-editor/demo/src/index.tsx
@@ -4,8 +4,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './singleEditor';
+import App from './lgEditor';
 // import App from './jsonEditor';
+// import App from './luEditor';
+// import App from './lgJsonEditor';
 // import App from './inlineEditor';
 // import App from './multiEditors';
 
