05a51b13a38e54363d2edfcfe081747fad45d47f
Andy Brown <asbrown002@gmail.com>
2019-11-15 10:16:29 -0800

    add docker build and push step

---
+++ Composer/scripts/e2e.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -e
+
+yarn start &
+SERVER_PID=$!
+
+npx cypress run --browser chrome --record --parallel --ci-build-id $BUILD_BUILDNUMBER --group "Azure CI"
+
+function cleanup {
+  kill -9 $SERVER_PID
+}
+
+# kill server process
+trap cleanup EXIT
+
