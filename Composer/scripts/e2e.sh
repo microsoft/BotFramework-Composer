508766cc4dec5c56077e27213a8cb274ff98c284
Andy Brown <asbrown002@gmail.com>
2019-11-18 13:05:58 -0800

    change ci docker cmd to run e2e tests

--- Composer/scripts/e2e.sh
+++ Composer/scripts/e2e.sh
@@ -5,7 +5,7 @@ set -e
 yarn start &
 SERVER_PID=$!
 
-npx cypress run --browser chrome --record --parallel --ci-build-id $BUILD_BUILDNUMBER --group "Azure CI"
+npx cypress run --record --parallel --ci-build-id $BUILD_BUILDNUMBER --group "Azure CI"
 
 function cleanup {
   kill -9 $SERVER_PID
