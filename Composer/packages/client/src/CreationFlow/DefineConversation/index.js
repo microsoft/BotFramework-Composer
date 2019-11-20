27fd3c091daec7f4eae60926eec5bfef1c353875
Weitian Li <weitli@microsoft.com>
2019-11-20 15:11:22 +0800

    fix bug that cannot add customer name

--- Composer/packages/client/src/CreationFlow/DefineConversation/index.js
+++ Composer/packages/client/src/CreationFlow/DefineConversation/index.js
@@ -1,25 +1,34 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-import React, { useState, Fragment } from 'react';
+import React, { useState, useContext, Fragment, useEffect } from 'react';
 import formatMessage from 'format-message';
 import { PrimaryButton, DefaultButton } from 'office-ui-fabric-react/lib/Button';
 import { DialogFooter } from 'office-ui-fabric-react/lib/Dialog';
 import { Stack, StackItem } from 'office-ui-fabric-react/lib/Stack';
 import { TextField } from 'office-ui-fabric-react/lib/TextField';
+import get from 'lodash/get';
 
 import { LocationSelectContent } from '../LocationBrowser/LocationSelectContent';
 import { styles as wizardStyles } from '../StepWizard/styles';
+import { FileTypes } from '../../constants';
 
+import { StoreContext } from './../../store';
 import { name, description } from './styles';
 
 const nameRegex = /^[a-zA-Z0-9-_.]+$/;
 
 const validateForm = data => {
   const errors = {};
-  const { name } = data;
+  const { name, defaultName } = data;
 
-  if (!name || !nameRegex.test(name)) {
+  if (!name && !defaultName) {
+    errors.name = formatMessage(
+      'Spaces and special characters are not allowed. Use letters, numbers, -, or _., numbers, -, and _'
+    );
+  }
+
+  if (name && !nameRegex.test(name)) {
     errors.name = formatMessage(
       'Spaces and special characters are not allowed. Use letters, numbers, -, or _., numbers, -, and _'
     );
@@ -29,10 +38,34 @@ const validateForm = data => {
 };
 
 export function DefineConversation(props) {
+  const { state } = useContext(StoreContext);
   const { onSubmit, onGetErrorMessage, onDismiss, enableLocationBrowse } = props;
-
+  const { templateId, focusedStorageFolder } = state;
   const [formData, setFormData] = useState({ errors: {} });
+  const [location, setLocation] = useState('');
   const [disable, setDisable] = useState(false);
+
+  useEffect(() => {
+    const allFilesInFolder = get(focusedStorageFolder, 'children', []);
+
+    const botsInCurrentFolder = allFilesInFolder.filter(file => {
+      if (file.type === FileTypes.BOT) {
+        return file;
+      }
+    });
+    let i = 0;
+    let defaultName = `${templateId}-${i}`;
+
+    while (
+      botsInCurrentFolder.findIndex(bot => {
+        return bot.name === defaultName;
+      }) > -1
+    ) {
+      i = i + 1;
+      defaultName = `${templateId}-${i}`;
+    }
+    updateForm('defaultName')(null, defaultName);
+  }, [templateId, focusedStorageFolder]);
 
   const updateForm = field => (e, newValue) => {
     setFormData({
@@ -54,9 +87,12 @@ export function DefineConversation(props) {
       return;
     }
 
-    onSubmit({
-      ...formData,
-    });
+    onSubmit(
+      {
+        ...formData,
+      },
+      location
+    );
   };
 
   //disable the next button if the text has errors.
@@ -77,9 +113,13 @@ export function DefineConversation(props) {
     }
   };
 
-  // // update the path in the form and toggle the location picker.
-  const updateLocation = path => {
-    updateForm('location')(null, path);
+  const updateName = (e, name) => {
+    setFormData({
+      ...formData,
+      errors: {},
+      name: name,
+      defaultName: name,
+    });
   };
 
   return (
@@ -90,9 +130,9 @@ export function DefineConversation(props) {
           <StackItem grow={0} styles={wizardStyles.halfstack}>
             <TextField
               label={formatMessage('Name')}
-              value={formData.name}
+              value={formData.name || formData.defaultName}
               styles={name}
-              onChange={updateForm('name')}
+              onChange={updateName}
               errorMessage={formData.errors.name}
               onGetErrorMessage={getErrorMessage}
               data-testid="NewDialogName"
@@ -109,7 +149,7 @@ export function DefineConversation(props) {
             />
           </StackItem>
         </Stack>
-        {enableLocationBrowse && <LocationSelectContent onChange={updateLocation} allowOpeningBot={false} />}
+        {enableLocationBrowse && <LocationSelectContent onChange={setLocation} allowOpeningBot={false} />}
 
         <DialogFooter>
           <DefaultButton onClick={onDismiss} text={formatMessage('Cancel')} />
