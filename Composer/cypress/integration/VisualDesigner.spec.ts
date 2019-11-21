f94ceb1aa74b5895730e75423f7ce9634937992e
Andy Brown <asbrown002@gmail.com>
2019-11-21 09:48:43 -0800

    convert VisualDesigner spec to typescript

--- Composer/cypress/integration/VisualDesigner.spec.ts
+++ Composer/cypress/integration/VisualDesigner.spec.ts
@@ -1,27 +1,22 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-/// <reference types="Cypress" />
-
 context('Visual Designer', () => {
   before(() => {
     cy.visit(Cypress.env('COMPOSER_URL'));
     cy.createBot('TodoSample');
-    cy.wait(100);
   });
 
   beforeEach(() => {
     // Return to Main.dialog
-    cy.get('[data-testid="ProjectTree"]').within(() => {
+    cy.findByTestId('ProjectTree').within(() => {
       cy.findByText('__TestTodoSample.Main').click();
-      cy.wait(100);
     });
   });
 
   it('can find Visual Designer default trigger in container', () => {
-    cy.get('[data-testid="ProjectTree"]').within(() => {
+    cy.findByTestId('ProjectTree').within(() => {
       cy.findByText('Greeting (ConversationUpdate)').click();
-      cy.wait(500);
     });
 
     cy.withinEditor('VisualEditor', () => {
