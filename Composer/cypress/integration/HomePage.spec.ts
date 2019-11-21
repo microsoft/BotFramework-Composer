da9d1fd7b89848b6f4df6293711c3c445f558cfe
Andy Brown <asbrown002@gmail.com>
2019-11-20 16:32:04 -0800

    convert home page spec to typescript

--- Composer/cypress/integration/HomePage.spec.ts
+++ Composer/cypress/integration/HomePage.spec.ts
@@ -1,19 +1,22 @@
-/// <reference types="Cypress" />
-context('check Nav Expandion ', () => {
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+context('Home Page ', () => {
   beforeEach(() => {
     cy.visit(Cypress.env('COMPOSER_URL'));
   });
+
   it('can open buttons in home page', () => {
-    cy.get('[data-testid="LeftNav-CommandBarButtonHome"]').click();
-    cy.get('[data-testid="homePage-ToolBar-New"]').click();
+    cy.findByTestId('LeftNav-CommandBarButtonHome').click();
+    cy.findByTestId('homePage-ToolBar-New').click();
     cy.findByText('Create from scratch?').should('exist');
     cy.findByText('Cancel').should('exist');
     cy.findByText('Cancel').click();
-    cy.get('[data-testid="homePage-ToolBar-Open"]').click();
+    cy.findByTestId('homePage-ToolBar-Open').click();
     cy.findByText('Select a Bot').should('exist');
     cy.findByText('Cancel').should('exist');
     cy.findByText('Cancel').click();
-    cy.get('[data-testid="homePage-body-New"]').click();
+    cy.findByTestId('homePage-body-New').click();
     cy.findByText('Create from scratch?').should('exist');
   });
 });
