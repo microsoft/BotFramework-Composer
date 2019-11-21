507da0f4d55dbc888e4ceb46afb93992db559554
Andy Brown <asbrown002@gmail.com>
2019-11-21 09:56:31 -0800

    remove unused cypress commands

--- Composer/cypress/support/commands.ts
+++ Composer/cypress/support/commands.ts
@@ -15,50 +15,10 @@ Cypress.Commands.add('createBot', (bobotId: string, botName?: string) => {
   cy.wait(1000);
 });
 
-Cypress.Commands.add('openBot', botName => {
-  cy.findByTestId('LeftNav-CommandBarButtonHome').click();
-  cy.findByTestId('homePage-ToolBar-Open').within(() => {
-    cy.findByText('Open').click();
-  });
-  cy.findByTestId('SelectLocation').within(() => {
-    cy.get(`[aria-label="${botName}"]`).click({ force: true });
-    cy.wait(500);
-  });
-  cy.wait(500);
-});
-
 Cypress.Commands.add('withinEditor', (editorName, cb) => {
   cy.get(`iframe[name="${editorName}"]`).then(editor => {
     cy.wrap<HTMLElement>(editor.contents().find('body') as JQuery<HTMLElement>).within(cb);
   });
-});
-
-Cypress.Commands.add('openDialog', dialogName => {
-  cy.findByTestId('ProjectTree').within(() => {
-    cy.findByText(dialogName).click();
-    cy.wait(500);
-  });
-});
-
-Cypress.Commands.add('startFromTemplate', (template, name) => {
-  cy.findByTestId('LeftNav-CommandBarButtonHome').click();
-  cy.findByTestId(`TemplateCopy-${template}`).click();
-  cy.findByTestId('NewDialogName').type(`__Test${name}`);
-  cy.findByTestId('NewDialogName').type('{enter}');
-  cy.wait(1000);
-});
-
-Cypress.Commands.add('addEventHandler', handler => {
-  cy.findByTestId('ProjectTree').within(() => {
-    cy.findByText(/New Trigger ../).click();
-  });
-  cy.findByTestId('triggerTypeDropDown').click();
-  cy.findByText(handler).click();
-  if (handler === 'Dialog trigger') {
-    cy.findByTestId('eventTypeDropDown').click();
-    cy.findByText('consultDialog').click();
-  }
-  cy.findByTestId('triggerFormSubmit').click();
 });
 
 Cypress.Commands.add('visitPage', page => {
