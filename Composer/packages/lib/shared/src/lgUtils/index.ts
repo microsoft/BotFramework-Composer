e975f16f97c764674c8eb2ecd1af99c99aae3d49
zeye <zeye@microsoft.com>
2019-11-26 18:06:54 +0800

    expose them on 'shared' level

--- Composer/packages/lib/shared/src/lgUtils/index.ts
+++ Composer/packages/lib/shared/src/lgUtils/index.ts
@@ -1,2 +1,11 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License
+
+/** models */
+export { default as LgMetaData } from './models/LgMetaData';
+export { default as LgTemplateRef } from './models/LgTemplateRef';
+
+/** parsers */
+export { default as parseLgTemplateName } from './parsers/parseLgTemplateName';
+export { default as parseLgTemplateRef } from './parsers/parseLgTemplateRef';
+export { default as parseLgText } from './parsers/parseLgText';
