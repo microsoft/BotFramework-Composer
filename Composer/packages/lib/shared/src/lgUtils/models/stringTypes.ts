456d08f6aacfe4b690711e2b7c3166a4d92c0a7e
zeye <zeye@microsoft.com>
2019-11-27 19:04:28 +0800

    comments: explain lg strings

--- Composer/packages/lib/shared/src/lgUtils/models/stringTypes.ts
+++ Composer/packages/lib/shared/src/lgUtils/models/stringTypes.ts
@@ -15,3 +15,26 @@ export type LgTemplateRefString = string;
  * example: 'bfdactivity-123456'
  */
 export type LgTemplateName = string;
+
+/**
+ * How we understand LG strings:
+  1. LgText
+  {
+      "activity": "....", // LgText
+      "prompt": "....", // LgText
+      "invalidPrompt": "..." // LgText
+  }
+
+  2. LgTemplateRef
+  '[bfdactivity-1234]'
+  '[greeting]'
+  '[greeting(1)]'
+
+  3. LgTemplateName
+  'bfdactivity-1234' in '[bfdactivity-1234]'
+  'greeting' in '[greeting(1)]'
+
+  4. LgMetaData (Composer-only)
+  'bfdactivity-1234' => { type: 'activity', designerId: '1234' } // LgMetaData
+  'greeting' => NO meta data since its not created by Composer
+*/
