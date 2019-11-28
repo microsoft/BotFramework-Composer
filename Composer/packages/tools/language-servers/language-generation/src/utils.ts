5824cef97613cf8a6e42a30a0df567f9ed42a19f
Shuai Wang <shuwan@microsoft.com>
2019-11-28 16:37:05 +0800

    fix regExp in getWordAtPosition

--- Composer/packages/tools/language-servers/language-generation/src/utils.ts
+++ Composer/packages/tools/language-servers/language-generation/src/utils.ts
@@ -32,7 +32,7 @@ export function getRangeAtPosition(document: TextDocument, position: Position): 
   const pos = position.character;
   const lineText = text.split('\n')[line];
   let match: RegExpMatchArray | null;
-  const wordDefinition = /[a-zA-Z0-9_/-/.]+/g;
+  const wordDefinition = /[a-zA-Z0-9_/.-]+/g;
   while ((match = wordDefinition.exec(lineText))) {
     const matchIndex = match.index || 0;
     if (matchIndex > pos) {
