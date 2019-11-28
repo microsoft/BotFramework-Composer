b7daa7244e4eb8724c047ad6dc7b975551aaa701
zeye <zeye@microsoft.com>
2019-11-28 12:45:38 +0800

    update stringTypes

--- Composer/packages/lib/shared/src/lgUtils/models/stringTypes.ts
+++ Composer/packages/lib/shared/src/lgUtils/models/stringTypes.ts
@@ -2,7 +2,7 @@
 // Licensed under the MIT License
 
 /**
- * example: '- [bfdactivity-123456]'
+ * example: '- hello' | '[bfdactivity-123456]'
  */
 export type LgText = string;
 
@@ -34,7 +34,7 @@ export type LgTemplateName = string;
   'bfdactivity-1234' in '[bfdactivity-1234]'
   'greeting' in '[greeting(1)]'
 
-  4. LgMetaData (Composer-only)
+  4. LgMetaData (Composer-only, can be converted to LgTemplateName)
   'bfdactivity-1234' => { type: 'activity', designerId: '1234' } // LgMetaData
   'greeting' => NO meta data since its not created by Composer
 */
