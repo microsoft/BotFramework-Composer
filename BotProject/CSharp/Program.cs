1f3b631d655e4983928bc07f017cf5a6e438ad4e
Wenyi Luo <wenyluo@microsoft.com>
2019-11-28 17:13:56 +0800

    fix

--- BotProject/CSharp/Program.cs
+++ BotProject/CSharp/Program.cs
@@ -22,12 +22,9 @@ namespace Microsoft.Bot.Builder.ComposerBot.Json
             {
                 var env = hostingContext.HostingEnvironment;
                 var luisAuthoringRegion = Environment.GetEnvironmentVariable("LUIS_AUTHORING_REGION") ?? "westus";
-                var luisSettingFiles = Directory.GetFiles($"ComposerDialogs\\generated", "luis.settings.*.json");
                 config
                     .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
                     .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true)
-                    .AddJsonFile($"ComposerDialogs/settings/appsettings.json", optional: true, reloadOnChange: true)
-                    .AddJsonFile(luisSettingFiles.Length > 0 ? luisSettingFiles[0] : string.Empty, optional: true, reloadOnChange: true)
                     .AddJsonFile($"luis.settings.{env.EnvironmentName}.{luisAuthoringRegion}.json", optional: true, reloadOnChange: true)
                     .AddJsonFile($"luis.settings.{Environment.UserName}.{luisAuthoringRegion}.json", optional: true, reloadOnChange: true);
 
--- BotProject/Templates/CSharp/Program.cs
+++ BotProject/Templates/CSharp/Program.cs
@@ -14,16 +14,18 @@ namespace Microsoft.Bot.Builder.ComposerBot.Json
         {
             BuildWebHost(args).Run();
         }
-
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
             .ConfigureAppConfiguration((hostingContext, config) =>
             {
                 var env = hostingContext.HostingEnvironment;
                 var luisAuthoringRegion = Environment.GetEnvironmentVariable("LUIS_AUTHORING_REGION") ?? "westus";
+                var luisSettingFiles = Directory.GetFiles($"ComposerDialogs\\generated", "luis.settings.*.json");
                 config
                     .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
                     .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true)
+                    .AddJsonFile($"ComposerDialogs/settings/appsettings.json", optional: true, reloadOnChange: true)
+                    .AddJsonFile(luisSettingFiles.Length > 0 ? luisSettingFiles[0] : string.Empty, optional: true, reloadOnChange: true)
                     .AddJsonFile($"luis.settings.{env.EnvironmentName}.{luisAuthoringRegion}.json", optional: true, reloadOnChange: true)
                     .AddJsonFile($"luis.settings.{Environment.UserName}.{luisAuthoringRegion}.json", optional: true, reloadOnChange: true);
 
@@ -37,5 +39,6 @@ namespace Microsoft.Bot.Builder.ComposerBot.Json
                     .AddCommandLine(args);
             }).UseStartup<Startup>()
             .Build();
+
     }
 }
