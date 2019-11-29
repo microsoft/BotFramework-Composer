9f362819909ba9a30fbc2c391d41f934ad9fdb5f
zhixzhan <49866537+zhixzhan@users.noreply.github.com>
2019-11-29 17:45:45 +0800

    update test

--- Composer/packages/tools/language-servers/language-generation/__tests__/app.test.ts
+++ Composer/packages/tools/language-servers/language-generation/__tests__/app.test.ts
@@ -11,19 +11,42 @@ const lgFile = fs.readFileSync(`${__dirname}/mocks/greeting.lg`, 'utf-8');
 const initializeParams = fs.readFileSync(`${__dirname}/mocks/initialize-params.json`, 'utf-8');
 
 const ws = new WebSocket('ws://localhost:50002/lg-language-server');
-let reader;
+
+type messageResolver = (data) => void;
+
+const subscribers: messageResolver[] = [];
+
 ws.on('message', data => {
-  reader(data);
+  const subscriber = subscribers.shift();
+  if (subscriber) {
+    subscriber(data);
+  }
 });
-function send(data, resolver?): Promise<any> {
+
+function send(data, resolvers?: messageResolver[]): Promise<any> {
   ws.send(data);
   return new Promise(resolve => {
-    reader = data => {
-      if (resolver) {
-        resolver(JSON.parse(data), resolve);
-      }
-      resolve(JSON.parse(data));
-    };
+    if (typeof resolvers === 'undefined') {
+      subscribers.push(data => {
+        resolve(data);
+      });
+      return;
+    }
+    if (resolvers.length === 0) {
+      resolve();
+      return;
+    }
+    let count = 0;
+    const readers = resolvers.map(resolver => {
+      return data => {
+        count++;
+        resolver(JSON.parse(data));
+        if (resolvers.length === count) {
+          resolve(JSON.parse(data));
+        }
+      };
+    });
+    subscribers.push(...readers);
   });
 }
 
@@ -52,72 +75,75 @@ describe('lg lsp server', () => {
     done();
   });
   it('websocket should connect server', async () => {
-    await send(
-      `{ "jsonrpc":"2.0","id":0,"method":"initialize","params": ${initializeParams} }`,
-      (response, resolve) => {
-        if (response.method) {
-          expect(response.method).toEqual('window/logMessage');
-          expect(response.params.type).toEqual(4);
-        } else {
-          expect(response.id).toEqual(0);
-          resolve();
-        }
-      }
-    );
+    await send(`{ "jsonrpc":"2.0","id":0,"method":"initialize","params": ${initializeParams} }`, [
+      response => {
+        expect(response.method).toEqual('window/logMessage');
+        expect(response.params.type).toEqual(4);
+      },
+      response => {
+        expect(response.id).toEqual(0);
+      },
+    ]);
     // client initialized
-    send(`{"jsonrpc":"2.0","method":"initialized","params":{}}`);
-    send(
-      `{"jsonrpc":"2.0","method":"textDocument/didOpen","params": {"textDocument":{"uri":"inmemory://model/1","languageId":"botbuilderlg","version":2,"text": "${content}" }}}`
+    await send(`{"jsonrpc":"2.0","method":"initialized","params":{}}`, []);
+    await send(
+      `{"jsonrpc":"2.0","method":"textDocument/didOpen","params": {"textDocument":{"uri":"inmemory://model/1","languageId":"botbuilderlg","version":2,"text": "${content}" }}}`,
+      []
     );
-    await new Promise(resolve => setTimeout(() => resolve(), 10));
   });
 
   it('initialize documents', async () => {
     // initialize, check diagnostics
-    await send(
-      `{"jsonrpc":"2.0","id":1,"method":"initializeDocuments","params":{"uri":"inmemory://model/1"}}`,
-      (response, resolve) => {
-        if (response.method) {
-          expect(response.method).toEqual('textDocument/publishDiagnostics');
-          expect(response.params.diagnostics.length).toEqual(0);
-        } else {
-          expect(response.id).toEqual(1);
-          resolve();
-        }
-      }
-    );
+    await send(`{"jsonrpc":"2.0","id":1,"method":"initializeDocuments","params":{"uri":"inmemory://model/1"}}`, [
+      response => {
+        expect(response.id).toEqual(1);
+      },
+      response => {
+        expect(response.method).toEqual('textDocument/publishDiagnostics');
+        expect(response.params.diagnostics.length).toEqual(0);
+      },
+    ]);
   });
 
   it('hover', async () => {
-    await new Promise(resolve => setTimeout(() => resolve(), 10));
     // didChange
-    const response = await send(
-      `{"jsonrpc":"2.0","id":2,"method":"textDocument/hover","params":{"textDocument":{"uri":"inmemory://model/1"},"position":{"line":6,"character":6}}}`
+    await send(
+      `{"jsonrpc":"2.0","id":2,"method":"textDocument/hover","params":{"textDocument":{"uri":"inmemory://model/1"},"position":{"line":6,"character":6}}}`,
+      [
+        response => {
+          expect(response.id).toEqual(2);
+          expect(response.result.contents[0]).toEqual('-Good evening');
+        },
+      ]
     );
-    expect(response.id).toEqual(2);
-    expect(response.result.contents[0]).toEqual('-Good evening');
   });
 
   it('didChange', async () => {
     // didChange
     const newContent = `${content}-@{G\\r\\n`;
     const payload = `{"jsonrpc":"2.0","method":"textDocument/didChange","params":{"textDocument":{"uri":"inmemory://model/1","version":3},"contentChanges":[{"text": "${newContent}"}]}}`;
-    const response = await send(payload);
-    expect(response.method).toEqual('textDocument/publishDiagnostics');
-    expect(response.params.diagnostics.length).toEqual(0);
+    await send(payload, [
+      response => {
+        expect(response.method).toEqual('textDocument/publishDiagnostics');
+        expect(response.params.diagnostics.length).toEqual(0);
+      },
+    ]);
   });
 
   it('completion', async () => {
     // completion,
     // input G, should suggest Greeting*
     const payload = `{"jsonrpc":"2.0","id":3,"method":"textDocument/completion","params":{"textDocument":{"uri":"inmemory://model/1"},"position":{"line":8,"character":4},"context":{"triggerKind":1}}}`;
-    const response = await send(payload);
-    expect(response.id).toEqual(3);
-    expect(
-      response.result.items
-        .slice(0, 3)
-        .map(item => item.label)
-        .join(',')
-    ).toEqual(['Greeting1', 'Greeting2', 'Greeting3'].join(','));
+    await send(payload, [
+      response => {
+        expect(response.id).toEqual(3);
+        expect(
+          response.result.items
+            .slice(0, 3)
+            .map(item => item.label)
+            .join(',')
+        ).toEqual(['Greeting1', 'Greeting2', 'Greeting3'].join(','));
+      },
+    ]);
   });
 });
