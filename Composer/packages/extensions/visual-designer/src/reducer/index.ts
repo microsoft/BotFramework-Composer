2cd9c99956b6c8e0662cedfdb4fdc07417692287
zeye <zeye@microsoft.com>
2019-12-04 13:06:06 +0800

    + reducer

---
+++ Composer/packages/extensions/visual-designer/src/reducer/index.ts
@@ -0,0 +1,10 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License
+
+import { StoreState } from '../store/store';
+
+const reducer = (state: StoreState, { type, payload }) => {
+  return state;
+};
+
+export default reducer;
