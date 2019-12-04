b19d6f2a6d6d28da1193a498d76f1699e9bdab21
zeye <zeye@microsoft.com>
2019-12-04 13:55:49 +0800

    flatten store

--- Composer/packages/extensions/visual-designer/src/store/store.ts
+++ Composer/packages/extensions/visual-designer/src/store/store.ts
@@ -2,29 +2,17 @@
 // Licensed under the MIT License
 
 export interface StoreState {
-  dialog: {
-    json: any;
-  };
-  editor: {
-    focusedEvent: string;
-    focusedId: string;
-    selectedIds: string[];
-  };
-  clipboard: {
-    actions: any[];
-  };
+  dialog: any;
+  focusedEvent: string;
+  focusedId: string;
+  selectedIds: string[];
+  clipboardActions: any[];
 }
 
 export const initialStore: StoreState = {
-  dialog: {
-    json: {},
-  },
-  editor: {
-    focusedEvent: '',
-    focusedId: '',
-    selectedIds: [],
-  },
-  clipboard: {
-    actions: [],
-  },
+  dialog: {},
+  focusedEvent: '',
+  focusedId: '',
+  selectedIds: [],
+  clipboardActions: [],
 };
