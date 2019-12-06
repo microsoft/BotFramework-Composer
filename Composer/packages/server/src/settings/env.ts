01cd533aceff072d72a0dc50eaf7007596a61c0e
Andy Brown <asbrown002@gmail.com>
2019-12-05 19:07:00 -0800

    build: make docker great again (#1709)
    * wip docker
    
    * fix docker build issues
    
    * add ComposerDialogs folder to bot runtime image
    
    * don't log error when git is missing
    
    * make app data.json path configurable
    
    allows us to save docker state across builds
    
    * add compose override example
    
    * no need to be in composer directory
    
    * finish comment and remove dead code
    
    * use correct syntax for run directive

--- Composer/packages/server/src/settings/env.ts
+++ Composer/packages/server/src/settings/env.ts
@@ -1,6 +1,8 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+import path from 'path';
+
 export const absHosted = process.env.COMPOSER_AUTH_PROVIDER === 'abs-h';
 export const absHostRoot = process.env.WEBSITE_HOSTNAME
   ? `https://${process.env.WEBSITE_HOSTNAME}`
@@ -11,4 +13,7 @@ if (folder && folder.endsWith(':')) {
   folder = folder + '/';
 }
 
+export const environment = process.env.NODE_ENV || 'development';
 export const botsFolder = folder;
+export const botEndpoint = process.env.BOT_ENDPOINT || 'http://localhost:3979';
+export const appDataPath = process.env.COMPOSER_APP_DATA || path.resolve(__dirname, '../../data.json');
