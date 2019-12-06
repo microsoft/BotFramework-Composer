a1e1c7aafcbc9bc4ab89351d5e0509ed56a266fd
zeye <zeye@microsoft.com>
2019-12-06 18:08:55 +0800

    simplify 'AdaptiveDialog'

--- Composer/packages/extensions/visual-designer/src/adaptive/AdaptiveDialog.tsx
+++ Composer/packages/extensions/visual-designer/src/adaptive/AdaptiveDialog.tsx
@@ -3,17 +3,14 @@
 
 /** @jsx jsx */
 import { jsx } from '@emotion/core';
-import { FC, useContext } from 'react';
+import { FC } from 'react';
 import get from 'lodash/get';
 import { MicrosoftAdaptiveDialog } from '@bfc/shared';
 
 import { NodeEventTypes } from '../constants/NodeEventTypes';
-import { EditorContext } from '../store/EditorContext';
-import { Collapse } from '../components/lib/Collapse';
 import { ObiFieldNames } from '../constants/ObiFieldNames';
 
 import { AdaptiveEventList } from './list/AdaptiveEventList';
-import { AdaptiveEvent } from './AdaptiveEvent';
 
 export interface AdaptiveDialogProps {
   path: string;
@@ -25,31 +22,5 @@ export const AdaptiveDialog: FC<AdaptiveDialogProps> = ({ data, onEvent }): JSX.
   const eventsPath = ObiFieldNames.Events;
   const events = get(data, eventsPath, []);
 
-  const FocusedEvent = () => {
-    const { focusedEvent } = useContext(EditorContext);
-    const focusedEventData = focusedEvent ? get(data, focusedEvent) : null;
-    if (!focusedEventData) return null;
-    return <AdaptiveEvent path={focusedEvent || ''} data={focusedEventData} onEvent={onEvent} />;
-  };
-
-  return (
-    <div
-      css={{
-        position: 'relative',
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-      }}
-      onClick={e => {
-        e.stopPropagation();
-        onEvent(NodeEventTypes.Focus, { id: '' });
-      }}
-    >
-      <AdaptiveEventList path={eventsPath} events={events} onEvent={onEvent} />
-      <div className="editor-interval" style={{ height: 50 }} />
-      <Collapse text="Actions">
-        <FocusedEvent />
-      </Collapse>
-    </div>
-  );
+  return <AdaptiveEventList path={eventsPath} events={events} onEvent={onEvent} />;
 };
