5c660fd2b78517b2736a48c700409d7e2595aafc
Kamran Iqbal <29185255+Kaiqb@users.noreply.github.com>
2019-12-09 15:55:49 -0800

    Update utils.ts

--- .github/actions/conventional-pr/src/utils.ts
+++ .github/actions/conventional-pr/src/utils.ts
@@ -1,5 +1,10 @@
 import * as core from '@actions/core';
 
+export interface PullRequestInfo {
+  title: string;
+  body: string;
+  baseRefName: string;
+}
 type ValidationResult = string[];
 
 const validTypes = [
@@ -14,6 +19,7 @@ const validTypes = [
   'ci',
   'chore',
   'revert',
+  'release',
 ];
 
 const typeList = validTypes.map(t => `  - ${t}`).join('\n');
@@ -28,22 +34,46 @@ export function validateTitle(title: string): ValidationResult {
   const hastype = validTypes.some(t => title.startsWith(`${t}: `));
 
   if (!hastype) {
-    core.info(
-      `[Title] Missing type in title. Choose from the following:\n${typeList}`
+    errors.push(
+      `[Title] Must start with type (ex. 'feat: ').\nThe valid types are:\n${typeList}`
     );
-    errors.push("[Title] Must start with type. i.e. 'feat: '");
   }
 
   return errors;
 }
 
 const refMatch = /(refs?|close(d|s)?|fix(ed|es)?) \#\d+/i;
+const helpLink =
+  'https://help.github.com/en/github/managing-your-work-on-github/closing-issues-using-keywords';
 
 export function validateBody(body: string): ValidationResult {
   let errors: ValidationResult = [];
 
   if (!refMatch.test(body)) {
-    errors.push('[Body] Must reference an issue.');
+    errors.push(
+      `[Body] Must reference an issue (ex. 'fixes #1234').\nSee ${helpLink} for more details.`
+    );
+  }
+
+  return errors;
+}
+
+export function isRelease(pr: PullRequestInfo) {
+  return pr.title.startsWith('release: ') && pr.baseRefName === 'stable';
+}
+
+export function validateBaseBranch(
+  title: string,
+  baseBranch: string
+): ValidationResult {
+  let errors: ValidationResult = [];
+
+  if (title.startsWith('release: ') && baseBranch !== 'stable') {
+    errors.push("[Release] Release pull request must target 'stable' branch.");
+  } else if (baseBranch === 'stable') {
+    errors.push(
+      "[Branch] Pull requests cannot target 'stable' branch. Perhaps you meant to create a release or are targeting the wrong branch."
+    );
   }
 
   return errors;
