3cead65cb43d67a11e9bd6a28c1c88ff8c0dcf7c
zeye <zeye@microsoft.com>
2019-12-10 14:42:05 +0800

    enhance visual sdk demo

--- Composer/packages/extensions/visual-designer/demo/src/stories/VisualSDKDemo.js
+++ Composer/packages/extensions/visual-designer/demo/src/stories/VisualSDKDemo.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import { seedNewDialog } from '@bfc/shared';
 
 import { resolveAdaptiveDataRenderer } from '../../../src/adaptive/rendererMap';
+import { EdgeMenu } from '../../../src/components/menus/EdgeMenu';
 import { JsonBlock } from '../components/json-block';
 
 import './story.css';
@@ -21,16 +23,32 @@ export class VisualSDKDemo extends Component {
     }
   }
 
+  seedNewJson($type) {
+    const json = seedNewDialog($type);
+    this.setState({ json });
+  }
+
+  renderActionFactory() {
+    return (
+      <div style={{ height: 100, margin: 20 }}>
+        <h3>Create action by $type</h3>
+        <EdgeMenu id="visual-sdk-demo" onClick={$type => this.seedNewJson($type)} />
+      </div>
+    );
+  }
+
   render() {
     return (
       <div className="story-container">
         <h1 className="story-title">Visual SDK Demo</h1>
         <div className="story-content">
           <div className="block block--left">
+            {this.renderActionFactory()}
             <JsonBlock
+              key={this.state.json && this.state.json.$type}
               styles={{
                 width: '95%',
-                height: 'calc(100% - 100px)',
+                height: '80%',
                 minHeight: '500px',
               }}
               defaultValue={this.state.json}
