4afaa02fae51cf318ce49ef6239d9addb21e2c94
Weitian Li <weitli@microsoft.com>
2019-12-17 16:44:40 +0800

    update new trigger modal according to design

--- Composer/packages/client/src/components/ProjectTree/styles.ts
+++ Composer/packages/client/src/components/ProjectTree/styles.ts
@@ -137,7 +137,7 @@ export const dropdownStyles = {
     fontWeight: FontWeights.semibold,
   },
   dropdown: {
-    width: '300px',
+    width: '400px',
   },
   root: {
     paddingBottom: '20px',
@@ -148,7 +148,7 @@ export const dialogWindow = css`
   display: flex;
   flex-direction: column;
   width: 400px;
-  height: 250px;
+  height: 300px;
 `;
 
 export const textFieldlabel = {
@@ -162,11 +162,16 @@ export const textFieldlabel = {
 };
 
 export const intent = {
-  fieldGroup: {
-    width: 200,
-  },
   root: {
-    height: '90px',
+    width: '400px',
   },
-  subComponentStyles: textFieldlabel,
+};
+
+export const triggerPhrases = {
+  root: {
+    width: '400px',
+  },
+  fieldGroup: {
+    height: 80,
+  },
 };
