b262a699bc95109b1c0c8f069dcf62af51b8eb5e
Leilei Zhang <leilzh@microsoft.com>
2019-12-24 18:39:55 +0800

    navigation

--- Composer/packages/lib/shared/src/convertUtils/parsePathToFocused.ts
+++ Composer/packages/lib/shared/src/convertUtils/parsePathToFocused.ts
@@ -1,14 +1,21 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+import { parsePathToSelected } from './parsePathToSelected';
+
 export function parsePathToFocused(path: string): string {
   //path is like main.trigers[0].actions[0]
 
-  const list = path.split('.');
+  const trigger = parsePathToSelected(path);
 
-  if (list.length > 2) {
-    list.shift();
-    return list.join('.');
+  const actionPatt = /actions\[(\d+)\]/g;
+  let temp: RegExpExecArray | null = null;
+  const matchActions: string[] = [];
+  while ((temp = actionPatt.exec(path)) !== null) {
+    matchActions.push(`actions[${+temp[1]}]`);
+  }
+  if (matchActions.length > 0) {
+    return trigger + '.' + matchActions.join('.');
   }
   return '';
 }
