3161cef2a18c7a959037ab38d0cb58c8d1dfed63
zeye <zeye@microsoft.com>
2019-12-27 20:53:44 +0800

    uischema v0 def (basic types)

--- Composer/packages/extensions/visual-designer/src/components/nodes/utils.ts
+++ Composer/packages/extensions/visual-designer/src/components/nodes/utils.ts
@@ -31,3 +31,21 @@ export function getUserAnswersTitle($type: string): string {
 
   return '';
 }
+
+const truncateSDKType = $type => (typeof $type === 'string' ? $type.split('Microsoft.')[1] : '');
+
+/**
+ * Title priority: $designer.name > override title > title from sdk schema > $type suffix
+ * @param overrideTitle customized title
+ */
+export function generateSDKTitle(overrideTitle?: string) {
+  const titleFromUischema = overrideTitle;
+  return data => {
+    const $type = get(data, '$type');
+    const titleFrom$designer = get(data, '$designer.name');
+    const titleFromShared = get(ConceptLabels, `${$type}.title`);
+    const titleFrom$type = truncateSDKType($type);
+
+    return titleFrom$designer || titleFromUischema || titleFromShared || titleFrom$type;
+  };
+}
