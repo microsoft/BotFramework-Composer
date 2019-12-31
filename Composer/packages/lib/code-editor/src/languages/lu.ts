1d1d42a856585f4c891800b077b9a9f11e131251
Shuai Wang <shuwan@microsoft.com>
2019-12-31 11:39:18 +0800

    add a case in token

--- Composer/packages/lib/code-editor/src/languages/lu.ts
+++ Composer/packages/lib/code-editor/src/languages/lu.ts
@@ -24,7 +24,7 @@ export function registerLULanguage(monaco: typeof monacoEditor) {
         [/^\s*>\s*[\s\S]*$/, { token: 'comments' }],
         [/^\s*-/, { token: 'utterrance-indentifier', next: 'utterrance' }],
         [/^\s*@/, { token: 'entity-identifier', goBack: 1, next: '@entity_mode' }],
-        [/({)(\s*[\w.]*\s*)(=)(\s*[\w.]*\s*)(})/, ['lb', 'pattern', 'equal', 'entity-name', 'rb']],
+        [/({)(\s*[\w.\:\s]*\s*)(=)(\s*[\w.]*\s*)(})/, ['lb', 'pattern', 'equal', 'entity-name', 'rb']],
         [/({\s*@)(\s*[\w.]*\s*)(})/, ['lb', 'entity-name', 'rb']],
         [/^\s*\[[\w\s.]+\]\(.{1,2}\/[\w.*]+(#[\w.?]+)?\)/, 'import-desc'],
         [/./, 'utterance-other'],
