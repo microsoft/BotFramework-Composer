f6b2d0b4cc07945a0e55d430b1e532ecfe2febe7
zeye <zeye@microsoft.com>
2019-12-31 10:50:58 +0800

    fix a title getter bug

--- Composer/packages/extensions/visual-designer/src/components/nodes/utils.ts
+++ Composer/packages/extensions/visual-designer/src/components/nodes/utils.ts
@@ -43,7 +43,7 @@ export function generateSDKTitle(overrideTitle?: string) {
   return data => {
     const $type = get(data, '$type');
     const titleFrom$designer = get(data, '$designer.name');
-    const titleFromShared = get(ConceptLabels, `${$type}.title`);
+    const titleFromShared = get(ConceptLabels, [$type, 'title']);
     const titleFrom$type = truncateSDKType($type);
 
     return titleFrom$designer || titleFromUischema || titleFromShared || titleFrom$type;
