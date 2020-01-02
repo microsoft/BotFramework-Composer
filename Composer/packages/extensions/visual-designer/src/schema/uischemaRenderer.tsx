0190245b5cc673a878761eb50c7f106b4ee7c678
zeye <zeye@microsoft.com>
2020-01-02 21:24:26 +0800

    fix lint warning

--- Composer/packages/extensions/visual-designer/src/schema/uischemaRenderer.tsx
+++ Composer/packages/extensions/visual-designer/src/schema/uischemaRenderer.tsx
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-import React, { FC } from 'react';
+import React from 'react';
 import { BaseSchema } from '@bfc/shared';
 import get from 'lodash/get';
 
