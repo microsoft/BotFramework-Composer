7067f304b5da4c0d841d6a5efd7a4ad3738a2384
Ben Brown <benbro@microsoft.com>
2020-01-09 15:23:08 -0600

    dynamically load in the storage provider via plugin loader use custom storage if one is specified

--- Composer/packages/server/src/models/storage/storageFactory.ts
+++ Composer/packages/server/src/models/storage/storageFactory.ts
@@ -1,22 +1,21 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-import { MongoStorage } from '../../../../../plugins/mongostorage';
+import pluginLoader from '../../services/pluginLoader';
 
+import { LocalDiskStorage } from './localDiskStorage';
 import { StorageConnection, IFileStorage } from './interface';
-// import { LocalDiskStorage } from './localDiskStorage';
-import { AzureBlobStorage } from './azureBlobStorage';
 
 export class StorageFactory {
   public static createStorageClient(conn: StorageConnection): IFileStorage {
-    switch (conn.type) {
-      case 'LocalDisk':
-        return new MongoStorage(conn);
-      // return new LocalDiskStorage();
-      case 'AzureBlobStorage':
-        return new AzureBlobStorage(conn);
-      default:
-        throw new Error(`unknow storage type ${conn.type}`);
+    if (pluginLoader.extensions.storage && pluginLoader.extensions.storage.customStorageClass) {
+      const customStorageClass = pluginLoader.extensions.storage.customStorageClass;
+      if (customStorageClass) {
+        return new customStorageClass(conn) as IFileStorage;
+      }
     }
+
+    // otherwise...
+    return new LocalDiskStorage();
   }
 }
--- Composer/packages/server/src/services/pluginLoader.ts
+++ Composer/packages/server/src/services/pluginLoader.ts
@@ -2,24 +2,52 @@
 // Licensed under the MIT License.
 
 import * as fs from 'fs';
+import * as pathLib from 'path';
+
 import glob from 'globby';
 
 // import { Store } from '../store/store';
 
 class PluginLoader {
+  public extensions: {
+    storage: {
+      [key: string]: any;
+    };
+  };
+
   constructor() {
     // load any plugins present in the default folder
+    // noop for now
+    this.extensions = {
+      storage: {},
+    };
   }
 
-  public loadPlugin(path: string) {
-    // noop yet
-    fs.readFileSync(path);
+  public async loadPlugin(path: string) {
+    console.log('LOAD THE PLUGIN LOCATED IN ', path);
+    const packageJSON = fs.readFileSync(path, 'utf8');
+    const json = JSON.parse(packageJSON);
+
+    if (json.extendsComposer) {
+      const modulePath = path.replace(/package\.json$/, '');
+      const thisPlugin = require(modulePath);
+      console.log('This is a composer plugin!');
+      if (json.extendsComposer.storage) {
+        // grab a pointer to the exported class we want so we can use it later.
+        this.extensions.storage.customStorageClass = thisPlugin[json.extendsComposer.storage.className];
+      }
+    } else {
+      console.log('Not a Composer plugin');
+    }
   }
 
   public async loadPluginsFromFolder(path: string) {
     console.log('LOAD PLUGINS FROM ', path);
     const plugins = await glob('*/package.json', { cwd: path, dot: true });
     console.log('FOUND LOCAL PLUGINS:', plugins);
+    for (const p in plugins) {
+      await this.loadPlugin(pathLib.join(path, plugins[p]));
+    }
   }
 }
 
