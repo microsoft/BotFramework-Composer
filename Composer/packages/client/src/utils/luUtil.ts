973578a1311bdb97a97f9d21960ddffcb73931ba
zhixzhan <49866537+zhixzhan@users.noreply.github.com>
2020-01-13 15:03:58 +0800

    clean up

--- Composer/packages/client/src/utils/luUtil.ts
+++ Composer/packages/client/src/utils/luUtil.ts
@@ -4,7 +4,7 @@
 /**
  * luUtil.ts is a single place use lu-parser handle lu file operation.
  * it's designed have no state, input text file, output text file.
- * for more usage detail, please check /__tests__/utils/luUtil.test.ts
+ * for more usage detail, please check client/__tests__/utils/luUtil.test.ts
  */
 
 import { sectionHandler } from '@bfcomposer/bf-lu/lib/parser';
