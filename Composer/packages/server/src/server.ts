7ce0d07328dc96d0f21804e86e950fb5f5b3ef5d
zhixzhan <49866537+zhixzhan@users.noreply.github.com>
2020-01-16 17:09:18 +0800

    renaming

--- Composer/packages/server/src/server.ts
+++ Composer/packages/server/src/server.ts
@@ -114,13 +114,13 @@ const wss: ws.Server = new ws.Server({
   perMessageDeflate: false,
 });
 
-const { lgImportResolver, staticMemoryResolver } = BotProjectService;
+const { importResolver, staticMemoryResolver } = BotProjectService;
 
 function launchLanguageServer(socket: rpc.IWebSocket) {
   const reader = new rpc.WebSocketMessageReader(socket);
   const writer = new rpc.WebSocketMessageWriter(socket);
   const connection: IConnection = createConnection(reader, writer);
-  const server = new LGServer(connection, lgImportResolver, staticMemoryResolver);
+  const server = new LGServer(connection, importResolver, staticMemoryResolver);
   server.start();
 }
 
