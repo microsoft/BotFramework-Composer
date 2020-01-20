bed2bdcd0c3349f2336c00708147c91f6bcbd6c9
Shuai Wang <shuwan@microsoft.com>
2020-01-21 02:51:36 +0800

    feat: Language-Understanding LSP (#1711)
    * Add LSP of LG
    
    * change folder name
    
    * change folder location
    
    * seperate server and client demo
    
    * add completion and hover for builtin-functions
    
    * change file names
    
    * change dependency
    
    * refactor the package
    
    * update demo readme
    
    * make some refine
    
    * remove npm lock file
    
    * add demos to workspace and run test in 1 command
    
    * remove declaration files
    
    * change tsc outDir to dist, simplify test command
    
    * add syntax highlight in demo and new API
    
    * change naming of the project and move the demo
    
    * remove package.json in demo
    
    * change the content in readme
    
    * fix lg-lsp package publish problem
    
    * fix build command  and redundent d.ts files
    
    * integrate LG LSP server to composer server
    
    * change api in demo
    
    * change the order of commands in build:prod
    
    * delete redundent files generated from build
    
    * change lg-lsp-server api to attachLSPServer
    
    * remove gitignore in lg-lsp-server demo
    
    * remove attachLSPServer in server
    
    * fix token rules and  suggestion context awareness
    
    * init of LU LSP
    
    * add diagnostic in LSP
    
    * add auto suggestions
    
    * fix
    
    * remove redundent in client
    
    * add doc on type config
    
    * fix
    
    * modify doc type format
    
    * fix
    
    * change version of bf-lu
    
    * fix diagnostic
    
    * refactor getRangeAtPosition method
    
    * fix tokens
    
    * remove black line
    
    * fix tokens
    
    * add roles suggestions
    
    * add regex entity seperated line definition
    
    * add suggestions for ml entity
    
    * matching only ML entities
    
    * fix
    
    * update documents
    
    * fix
    
    * integrate lu-lsp to composer
    
    * upgrade botbuilder-expressions
    
    * clean up
    
    * update sample
    
    * add test
    
    * add syntax highlight
    
    * add token rule for {@ expr
    
    * add a case in token
    
    * fix import statement token
    
    * fix composite entity auto complete
    
    * fix entity suggestion and tokens
    
    * add labeling experience of add unlabeled entity
    
    * fix typos
    
    * fix lint
    
    * fix redundent edits
    
    * fix unnecessary space
    
    * fix naming and role suggestions
    
    * fix labeling and error postion
    
    * fix insert text in wrong line, intent usesFeature
    
    * update bf-lu version
    
    * change find a valid luisJson and suggest composite
    
    * add unit test for LU lsp basic funcionalities
    
    Co-authored-by: Hongyang Du (hond) <hond@microsoft.com>
    Co-authored-by: Dong Lei <donglei@microsoft.com>
    Co-authored-by: Zhixiang Zhan <zhixzhan@microsoft.com>

--- Composer/packages/lib/code-editor/src/LuEditor.tsx
+++ Composer/packages/lib/code-editor/src/LuEditor.tsx
@@ -2,12 +2,139 @@
 // Licensed under the MIT License.
 
 import React from 'react';
+import { listen, MessageConnection } from 'vscode-ws-jsonrpc';
+import * as monacoEditor from '@bfcomposer/monaco-editor/esm/vs/editor/editor.api';
+import * as monacoCore from 'monaco-editor-core';
+import get from 'lodash/get';
+import { MonacoServices, MonacoLanguageClient } from 'monaco-languageclient';
 
+import { registerLULanguage } from './languages';
+import { createUrl, createWebSocket, createLanguageClient } from './utils/lspUtil';
 import { RichEditor, RichEditorProps } from './RichEditor';
 
-const LU_HELP = 'https://aka.ms/lu-file-format';
-const placeholder = `> See ${LU_HELP} to learn about supported LU concepts.`;
+const LU_HELP = 'https://github.com/microsoft/botframework-cli/blob/master/packages/lu/docs/lu-file-format.md';
+const placeholder = `> To learn more about the LU file format, read the documentation at
+> ${LU_HELP}`;
 
-export function LuEditor(props: RichEditorProps) {
-  return <RichEditor placeholder={placeholder} helpURL={LU_HELP} {...props} />;
+export interface LUOption {
+  inline: boolean;
+  content: string;
+  template?: {
+    name: string;
+    parameters?: string[];
+    body: string;
+  };
+}
+
+export interface LULSPEditorProps extends RichEditorProps {
+  luOption?: LUOption;
+  languageServer?:
+    | {
+        host?: string;
+        hostname?: string;
+        port?: number | string;
+        path: string;
+      }
+    | string;
+}
+
+const defaultLUServer = {
+  path: '/lu-language-server',
+};
+declare global {
+  interface Window {
+    monacoServiceInstance: MonacoServices;
+    monacoLUEditorInstance: MonacoLanguageClient;
+  }
+}
+
+type ServerEdit = {
+  range: { start: { line: number; character: number }; end: { line: number; character: number } };
+  newText: string;
+};
+
+/*
+convert the edits results from the server to an exectable object in manoco editor
+*/
+function convertEdit(serverEdit: ServerEdit) {
+  return {
+    range: {
+      startLineNumber: serverEdit.range.start.line,
+      startColumn: serverEdit.range.start.character,
+      endLineNumber: serverEdit.range.end.line,
+      endColumn: serverEdit.range.end.character,
+    },
+    text: serverEdit.newText,
+    forceMoveMarkers: true,
+  };
+}
+
+export function LuEditor(props: LULSPEditorProps) {
+  const options = {
+    quickSuggestions: true,
+    wordBasedSuggestions: false,
+    formatOnType: true,
+  };
+
+  const { languageServer, ...restProps } = props;
+  const luServer = languageServer || defaultLUServer;
+
+  const editorWillMount = (monaco: typeof monacoEditor) => {
+    registerLULanguage(monaco);
+    if (typeof props.editorWillMount === 'function') {
+      return props.editorWillMount(monaco);
+    }
+  };
+  const editorDidMount = (editor: monacoEditor.editor.IStandaloneCodeEditor, monaco: typeof monacoEditor) => {
+    if (!window.monacoServiceInstance) {
+      window.monacoServiceInstance = MonacoServices.install(editor as monacoCore.editor.IStandaloneCodeEditor | any);
+    }
+
+    if (!window.monacoLUEditorInstance) {
+      const uri = get(editor.getModel(), 'uri._formatted', '');
+      const url = createUrl(luServer);
+      const webSocket: WebSocket = createWebSocket(url);
+      listen({
+        webSocket,
+        onConnection: (connection: MessageConnection) => {
+          const languageClient = createLanguageClient('LU Language Client', ['lu'], connection);
+          if (!window.monacoLUEditorInstance) {
+            window.monacoLUEditorInstance = languageClient;
+          }
+
+          editor.addCommand(monaco.KeyMod.Shift | monaco.KeyCode.Enter, function() {
+            const position = editor.getPosition();
+            languageClient.sendRequest('labelingExperienceRequest', { uri, position });
+          });
+          languageClient.onReady().then(() =>
+            languageClient.onNotification('addUnlabelUtterance', result => {
+              const edits = result.edits.map(e => {
+                return convertEdit(e);
+              });
+              editor.executeEdits(uri, edits);
+            })
+          );
+          const disposable = languageClient.start();
+          connection.onClose(() => disposable.dispose());
+        },
+      });
+    }
+
+    if (typeof props.editorDidMount === 'function') {
+      return props.editorDidMount(editor, monaco);
+    }
+  };
+
+  return (
+    <RichEditor
+      placeholder={placeholder}
+      helpURL={LU_HELP}
+      {...restProps}
+      theme={'lu'}
+      language={'lu'}
+      options={options}
+      editorWillMount={editorWillMount}
+      editorDidMount={editorDidMount}
+    />
+  );
 }
