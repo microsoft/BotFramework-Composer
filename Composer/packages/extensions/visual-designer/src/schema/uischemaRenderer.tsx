19af26a0681aac1b2c128bd5cd75ddb5b63e755c
zeye <2295905420@qq.com>
2020-01-23 01:38:46 +0800

    chore: migrate TextInput and all other *Input types to uischema (#1874)
    * render Elements with composition strategy (ElementRenderer -> ElementWrapper)
    
    * support recursive uischema
    
    * schema driven prompt inputs
    
    * BaseInput css fix
    
    * add missing onEvent handler
    
    * remove BaseInput
    
    * provide an option 'disableSDKTitle'
    
    * assign fixed title to input part in *Input types

--- Composer/packages/extensions/visual-designer/src/schema/uischemaRenderer.tsx
+++ Composer/packages/extensions/visual-designer/src/schema/uischemaRenderer.tsx
@@ -8,34 +8,41 @@ import get from 'lodash/get';
 import { uiSchema } from './uischema';
 import { UIWidget, UI_WIDGET_KEY, UIWidgetProp, WidgetEventHandler } from './uischema.types';
 
-const buildWidgetProp = (data: BaseSchema, rawPropValue: UIWidgetProp) => {
-  if (typeof rawPropValue === 'function') {
-    const dataTransformer = rawPropValue;
-    const element = dataTransformer(data);
-    return element;
-  }
-
-  return rawPropValue;
-};
-
-const buildWidgetProps = (data: BaseSchema, rawProps) => {
-  return Object.keys(rawProps).reduce((props, propName) => {
-    const propValue = rawProps[propName];
-    props[propName] = buildWidgetProp(data, propValue);
-    return props;
-  }, {});
-};
-
-const parseWidgetSchema = (data: BaseSchema, widgetSchema: UIWidget) => {
-  const { [UI_WIDGET_KEY]: Widget, ...rawProps } = widgetSchema;
-  const props = buildWidgetProps(data, rawProps);
+const parseWidgetSchema = (widgetSchema: UIWidget) => {
+  const { [UI_WIDGET_KEY]: Widget, ...props } = widgetSchema;
   return {
     Widget,
     props,
   };
 };
 
-export interface UISchemaRendererProps {
+const buildWidgetProp = (rawPropValue: UIWidgetProp, context: UISchemaContext) => {
+  if (typeof rawPropValue === 'function') {
+    const dataTransformer = rawPropValue;
+    const element = dataTransformer(context.data);
+    return element;
+  }
+
+  if (typeof rawPropValue === 'object' && rawPropValue[UI_WIDGET_KEY]) {
+    const widgetSchema = rawPropValue as UIWidget;
+    return renderUISchema(widgetSchema, context);
+  }
+
+  return rawPropValue;
+};
+
+const renderUISchema = (schema: UIWidget, context: UISchemaContext): JSX.Element => {
+  const { Widget, props: rawProps } = parseWidgetSchema(schema);
+  const widgetProps = Object.keys(rawProps).reduce((props, propName) => {
+    const propValue = rawProps[propName];
+    props[propName] = buildWidgetProp(propValue, context);
+    return props;
+  }, {});
+
+  return <Widget {...context} {...widgetProps} />;
+};
+
+export interface UISchemaContext {
   /** The uniq id of current schema data. Usually a json path. */
   id: string;
 
@@ -46,9 +53,8 @@ export interface UISchemaRendererProps {
   onEvent: WidgetEventHandler;
 }
 
-export const UISchemaRenderer: FC<UISchemaRendererProps> = ({ id, data, onEvent, ...contextProps }): JSX.Element => {
-  const $type = get(data, '$type');
+export const UISchemaRenderer: FC<UISchemaContext> = (props): JSX.Element => {
+  const $type = get(props.data, '$type');
   const schema = get(uiSchema, $type, uiSchema.default);
-  const { Widget, props } = parseWidgetSchema(data, schema);
-  return <Widget id={id} data={data} onEvent={onEvent} {...contextProps} {...props} />;
+  return renderUISchema(schema, props);
 };
