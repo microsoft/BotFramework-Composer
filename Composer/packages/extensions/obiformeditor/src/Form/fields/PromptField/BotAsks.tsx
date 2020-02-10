0cb28ca3684b9c8dae30164ffd91d2a704337b67
TJ Durnford <tjdford@gmail.com>
2020-02-10 09:28:07 -0800

    Updated form editior padding

--- Composer/packages/extensions/obiformeditor/src/Form/fields/PromptField/BotAsks.tsx
+++ Composer/packages/extensions/obiformeditor/src/Form/fields/PromptField/BotAsks.tsx
@@ -1,6 +1,8 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+/** @jsx jsx */
+import { jsx } from '@emotion/core'
 import React from 'react';
 import formatMessage from 'format-message';
 import { MicrosoftInputDialog } from '@bfc/shared';
@@ -9,6 +11,7 @@ import { LgEditorWidget } from '../../widgets/LgEditorWidget';
 import { WidgetLabel } from '../../widgets/WidgetLabel';
 import { BFDFieldProps } from '../../types';
 
+import { field } from './styles';
 import { GetSchema, PromptFieldChangeHandler } from './types';
 
 interface BotAsksProps extends BFDFieldProps<MicrosoftInputDialog> {
@@ -20,7 +23,7 @@ export const BotAsks: React.FC<BotAsksProps> = props => {
   const { onChange, getSchema, formData, formContext } = props;
 
   return (
-    <>
+    <div css={field}>
       <WidgetLabel label={formatMessage('Prompt')} description={getSchema('prompt').description} />
       <LgEditorWidget
         name="prompt"
@@ -29,6 +32,6 @@ export const BotAsks: React.FC<BotAsksProps> = props => {
         formContext={formContext}
         height={125}
       />
-    </>
+    </div>
   );
 };
