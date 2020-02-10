5b2ac33fbcc0c8aef7b6e82844aa29366ed9227b
TJ Durnford <tjdford@gmail.com>
2020-02-10 09:28:58 -0800

    Add emotion jsx import

--- Composer/packages/extensions/obiformeditor/src/Form/fields/PromptField/BotAsks.tsx
+++ Composer/packages/extensions/obiformeditor/src/Form/fields/PromptField/BotAsks.tsx
@@ -2,7 +2,7 @@
 // Licensed under the MIT License.
 
 /** @jsx jsx */
-import { jsx } from '@emotion/core'
+import { jsx } from '@emotion/core';
 import React from 'react';
 import formatMessage from 'format-message';
 import { MicrosoftInputDialog } from '@bfc/shared';
