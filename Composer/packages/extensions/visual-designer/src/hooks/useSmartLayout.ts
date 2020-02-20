aa2def6e535301643d771c6519461d267b9ab6ea
Ze Ye <zeye@microsoft.com>
2020-02-20 22:17:48 +0800

    add comments in useSmartLayout

--- Composer/packages/extensions/visual-designer/src/hooks/useSmartLayout.ts
+++ Composer/packages/extensions/visual-designer/src/hooks/useSmartLayout.ts
@@ -23,6 +23,19 @@ export function useSmartLayout<T extends string>(
   updateNodeBoundary: (nodeName: T, boundary: Boundary) => void;
 } {
   const [boundaryMap, setBoundaryMap] = useState<BoundaryMap<T>>({} as BoundaryMap<T>);
+  /**
+   * The object `accumulatedPatches` is used to collect all accumulated
+   *  boundary changes happen in a same JS event cyle. After collecting
+   *  them together, they will be submitted to component states to guide
+   *  next redraw.
+   *
+   * We shouldn't use `setState()` here because of `patchBoundary` may be
+   *  fired multiple times (especially at the init render cycle), changes
+   *  will be lost by using `setState()`;
+   *
+   * We shouldn't use `useRef` here since `accumulatedPatches` as a local
+   *  cache needs to be cleared after taking effect in one redraw.
+   */
   const accumulatedPatches = {};
   const patchBoundary = (nodeName: string, boundary: Boundary) => {
     if (!boundaryMap[nodeName] || !areBoundariesEqual(boundaryMap[nodeName], boundary)) {
