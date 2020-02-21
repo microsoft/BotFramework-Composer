738ec852016b47f253f4862f6e547a8266347c01
Andy Brown <asbrown002@gmail.com>
2020-02-20 16:17:08 -0800

    ignore lg lib

--- Composer/packages/ui-plugins/lg/lib/LgField.d.ts
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import { FieldProps } from '@bfc/extension';
-declare const LgField: React.FC<FieldProps<string>>;
-export { LgField };
-//# sourceMappingURL=LgField.d.ts.map
