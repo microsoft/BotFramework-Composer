d2184e844453e30539a191baf28d52bd140d3d2d
Wenyi Luo <wenyluo@microsoft.com>
2020-02-24 14:42:44 +0800

    add environment field into setting

--- Composer/packages/server/src/models/settings/defaultSettingManager.ts
+++ Composer/packages/server/src/models/settings/defaultSettingManager.ts
@@ -20,6 +20,7 @@ export class DefaultSettingManager extends FileSettingManager {
     return {
       MicrosoftAppPassword: '',
       MicrosoftAppId: '',
+      environment: '',
       luis: {
         name: '',
         authoringKey: '',
