a56c5c1661a8f079339c363e024d9c498b989543
Ze Ye <zeye@microsoft.com>
2020-03-03 23:36:37 +0800

    update ListOverview types

--- Composer/packages/extensions/visual-designer/src/components/common/ListOverview.tsx
+++ Composer/packages/extensions/visual-designer/src/components/common/ListOverview.tsx
@@ -5,13 +5,14 @@ import React, { FC } from 'react';
 
 import { SingleLineDiv } from '../elements/styledComponents';
 
-export interface ListOverviewProps {
-  items: string[];
-  renderItem: (item: object | string) => JSX.Element;
+export interface ListOverviewProps<T> {
+  items: T[];
+  renderItem: (item: T) => JSX.Element;
   maxCount: number;
 }
-export const ListOverview: FC<ListOverviewProps> = ({ items, renderItem, maxCount }) => {
-  if (!Array.isArray(items)) {
+
+export const ListOverview: FC<ListOverviewProps<any>> = ({ items, renderItem, maxCount }) => {
+  if (!Array.isArray(items) || !items.length) {
     return null;
   }
   return (
