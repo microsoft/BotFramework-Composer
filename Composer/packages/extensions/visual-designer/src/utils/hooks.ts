45f619eac44da2844b8f4e427e7fcf6a7ba68fc0
Long Alan <julong@microsoft.com>
2020-03-06 18:05:41 +0800

    fix data in toDobotWithLuis sample bot

--- Composer/packages/extensions/visual-designer/src/utils/hooks.ts
+++ Composer/packages/extensions/visual-designer/src/utils/hooks.ts
@@ -36,7 +36,7 @@ export const useLgTemplate = (str?: string, dialogId?: string) => {
         const templateTexts = template.body.split('\n').map(line => (line.startsWith('-') ? line.substring(1) : line));
         let showText = '';
 
-        if (templateTexts[0] && templateTexts[0] === '[activity') {
+        if (templateTexts[0] && templateTexts[0].trim() === '[Activity') {
           showText = templateTexts.find(text => text.includes('Text = '))?.split('Text = ')[1] || '';
         } else {
           showText = templateTexts.join('\n');
