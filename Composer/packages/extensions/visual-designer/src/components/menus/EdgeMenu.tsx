bf0228e816ae2de3820ff60e3910c28a0c337a6b
Ze Ye <zeye@microsoft.com>
2020-03-17 18:19:33 +0800

    filter actions and apply new util

--- Composer/packages/extensions/visual-designer/src/components/menus/EdgeMenu.tsx
+++ Composer/packages/extensions/visual-designer/src/components/menus/EdgeMenu.tsx
@@ -6,7 +6,7 @@ import { jsx, css } from '@emotion/core';
 import { useContext } from 'react';
 import classnames from 'classnames';
 import formatMessage from 'format-message';
-import { createStepMenu, DialogGroup, SDKTypes, appschema } from '@bfc/shared';
+import { createStepMenu, DialogGroup, SDKTypes, appschema, mergeDefinitions } from '@bfc/shared';
 import { IContextualMenu, ContextualMenuItemType } from 'office-ui-fabric-react/lib/ContextualMenu';
 import { FontIcon } from 'office-ui-fabric-react/lib/Icon';
 import get from 'lodash/get';
@@ -33,11 +33,11 @@ const buildEdgeMenuItemsFromClipboardContext = (
   const { clipboardActions, schemas } = context;
   const appschemaDefinitions = get(appschema, 'definitions', {});
   const botprojectDefinitions = get(schemas, 'sdk.content.definitions', {});
-  const extraTypes = Object.keys(botprojectDefinitions)
-    .filter(x => !appschemaDefinitions[x])
-    .filter(x => x.indexOf('Microsoft.test.') === -1)
-    .filter(x => x.indexOf('Microsoft.On') === -1);
-  console.log('visual picked', extraTypes);
+  const { extraTypes } = mergeDefinitions(appschemaDefinitions, botprojectDefinitions);
+  const extraActions = extraTypes
+    .filter(x => x.indexOf('Microsoft.Test.') === -1)
+    .filter(x => x.indexOf('Microsoft.On') === -1)
+    .filter(x => x.indexOf('Recognizer') === -1);
 
   const menuItems = createStepMenu(
     [
@@ -52,7 +52,7 @@ const buildEdgeMenuItemsFromClipboardContext = (
     true,
     (e, item) => onClick(item ? item.$type : null),
     filter,
-    extraTypes
+    extraActions
   );
 
   const enablePaste = Array.isArray(clipboardActions) && clipboardActions.length > 0;
