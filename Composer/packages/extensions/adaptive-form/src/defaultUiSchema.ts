23156c46048a84cae00f8fa49c29fdd700053193
tdurnford <tjdford@gmail.com>
2020-03-19 12:26:35 -0600

    fix: Added helpLinks to defaultUiSchema

--- Composer/packages/extensions/adaptive-form/src/defaultUiSchema.ts
+++ Composer/packages/extensions/adaptive-form/src/defaultUiSchema.ts
@@ -13,6 +13,8 @@ const triggerUiSchema = {
 
 const DefaultUISchema: UISchema = {
   [SDKTypes.AdaptiveDialog]: {
+    description: 'This configures a data driven dialog via a collection of events and actions.',
+    helpLink: 'https://aka.ms/botframework',
     order: ['recognizer', '*'],
     hidden: ['triggers', 'autoEndDialog', 'generator', 'selector', 'schema'],
     properties: {
@@ -25,10 +27,26 @@ const DefaultUISchema: UISchema = {
       },
     },
   },
+  [SDKTypes.AttachmentInput]: {
+    helpLink: 'https://aka.ms/bfc-ask-for-user-input',
+  },
   [SDKTypes.BeginDialog]: {
+    helpLink: 'https://aka.ms/bfc-understanding-dialogs',
     order: ['dialog', 'options', 'resultProperty', 'includeActivity', '*'],
   },
-  [SDKTypes.CancelAllDialogs]: { order: ['dialog', 'property', '*'] },
+  [SDKTypes.CancelAllDialogs]: {
+    helpLink: 'https://aka.ms/bfc-understanding-dialogs',
+    order: ['dialog', 'property', '*'],
+  },
+  [SDKTypes.ChoiceInput]: {
+    helpLink: 'https://aka.ms/bfc-ask-for-user-input',
+  },
+  [SDKTypes.ConfirmInput]: {
+    helpLink: 'https://aka.ms/bfc-ask-for-user-input',
+  },
+  [SDKTypes.DateTimeInput]: {
+    helpLink: 'https://aka.ms/bfc-ask-for-user-input',
+  },
   [SDKTypes.DebugBreak]: { label: () => formatMessage('Debug Break') },
   [SDKTypes.DeleteProperties]: {
     label: () => formatMessage('Delete Properties'),
@@ -36,6 +54,9 @@ const DefaultUISchema: UISchema = {
   },
   [SDKTypes.DeleteProperty]: {
     label: () => formatMessage('Delete a Property'),
+    helpLink: 'https://aka.ms/bfc-using-memory',
+  },
+  [SDKTypes.DeleteProperties]: {
     helpLink: 'https://aka.ms/bfc-using-memory',
   },
   [SDKTypes.EditActions]: { label: () => formatMessage('Modify active dialog') },
@@ -47,16 +68,55 @@ const DefaultUISchema: UISchema = {
     label: () => formatMessage('Emit a custom event'),
     helpLink: 'https://aka.ms/bfc-custom-events',
   },
-  [SDKTypes.Foreach]: { order: ['itemsProperty', '*'], hidden: ['actions'] },
-  [SDKTypes.ForeachPage]: { order: ['itemsProperty', 'pageSize', '*'], hidden: ['actions'] },
-  [SDKTypes.HttpRequest]: { order: ['method', 'url', 'body', 'headers', '*'] },
-  [SDKTypes.IfCondition]: { hidden: ['actions', 'elseActions'] },
+  [SDKTypes.EndDialog]: {
+    helpLink: 'https://aka.ms/bfc-understanding-dialogs',
+  },
+  [SDKTypes.EndDialog]: {
+    helpLink: 'https://aka.ms/bfc-understanding-dialogs',
+  },
+  [SDKTypes.Foreach]: {
+    order: ['itemsProperty', '*'],
+    hidden: ['actions'],
+    helpLink: 'https://aka.ms/bfc-controlling-conversation-flow',
+  },
+  [SDKTypes.ForeachPage]: {
+    order: ['itemsProperty', 'pageSize', '*'],
+    hidden: ['actions'],
+    helpLink: 'https://aka.ms/bfc-controlling-conversation-flow',
+  },
+  [SDKTypes.HttpRequest]: {
+    order: ['method', 'url', 'body', 'headers', '*'],
+    helpLink: 'https://aka.ms/bfc-using-http',
+  },
+  [SDKTypes.IfCondition]: {
+    hidden: ['actions', 'elseActions'],
+    helpLink: 'https://aka.ms/bfc-controlling-conversation-flow',
+  },
+  [SDKTypes.LogAction]: {
+    helpLink: 'https://aka.ms/bfc-debugging-bots',
+  },
+  [SDKTypes.LuisRecognizer]: {
+    helpLink: 'https://aka.ms/BFC-Using-LU',
+  },
+  [SDKTypes.MultiLanguageRecognizer]: {
+    helpLink: 'https://aka.ms/BFC-Using-LU',
+  },
+  [SDKTypes.NumberInput]: {
+    helpLink: 'https://aka.ms/bfc-ask-for-user-input',
+  },
+  [SDKTypes.OAuthInput]: {
+    helpLink: 'https://aka.ms/bfc-using-oauth',
+  },
   [SDKTypes.OAuthInput]: { order: ['connectionName', '*'] },
   [SDKTypes.OnActivity]: { ...triggerUiSchema },
   [SDKTypes.OnBeginDialog]: { ...triggerUiSchema },
   [SDKTypes.OnCancelDialog]: { ...triggerUiSchema },
   [SDKTypes.OnCondition]: { ...triggerUiSchema },
-  [SDKTypes.OnConversationUpdateActivity]: { ...triggerUiSchema },
+  [SDKTypes.OnConversationUpdateActivity]: {
+    helpLink:
+      'https://docs.microsoft.com/en-us/azure/bot-service/bot-builder-conversations?view=azure-bot-service-4.0#conversation-lifetime',
+    ...triggerUiSchema,
+  },
   [SDKTypes.OnCustomEvent]: { ...triggerUiSchema },
   [SDKTypes.OnDialogEvent]: { ...triggerUiSchema },
   [SDKTypes.OnEndOfConversationActivity]: { ...triggerUiSchema },
@@ -80,21 +140,47 @@ const DefaultUISchema: UISchema = {
   [SDKTypes.OnRepromptDialog]: { ...triggerUiSchema },
   [SDKTypes.OnTypingActivity]: { ...triggerUiSchema },
   [SDKTypes.OnUnknownIntent]: { ...triggerUiSchema },
+  [SDKTypes.QnAMakerDialog]: {
+    helpLink: 'https://aka.ms/bfc-using-QnA',
+  },
   [SDKTypes.Recognizer]: { field: RecognizerField },
   [SDKTypes.RegexRecognizer]: { hidden: ['entities'] },
-  [SDKTypes.RepeatDialog]: { order: ['options', 'includeActivity', '*'] },
+  [SDKTypes.RepeatDialog]: {
+    helpLink: 'https://aka.ms/bfc-understanding-dialogs',
+    order: ['options', 'includeActivity', '*'],
+  },
   [SDKTypes.ReplaceDialog]: {
+    helpLink: 'https://aka.ms/bfc-understanding-dialogs',
     order: ['dialog', 'options', 'includeActivity', '*'],
   },
-  [SDKTypes.SendActivity]: { order: ['activity', '*'] },
-  [SDKTypes.SetProperties]: {
-    label: () => formatMessage('Set Properties'),
-    helpLink: 'https://aka.ms/bfc-using-memory',
+  [SDKTypes.SendActivity]: {
+    helpLink: 'https://aka.ms/bfc-send-activity',
+    order: ['activity', '*'],
   },
-  [SDKTypes.SetProperty]: { label: () => formatMessage('Set a Property'), helpLink: 'https://aka.ms/bfc-using-memory' },
+  [SDKTypes.SetProperty]: {
+    helpLink: 'https://aka.ms/bfc-using-memory',
+    label: () => formatMessage('Set a Property'),
+  },
+  [SDKTypes.SetProperties]: {
+    helpLink: 'https://aka.ms/bfc-using-memory',
+    label: () => formatMessage('Set Properties'),
+  },
+  [SDKTypes.SkillDialog]: {
+    helpLink: 'https://aka.ms/bfc-call-skill',
+  },
   [SDKTypes.SwitchCondition]: {
+    helpLink: 'https://aka.ms/bfc-controlling-conversation-flow',
     hidden: ['default'],
     properties: { cases: { hidden: ['actions'] } },
+  },
+  [SDKTypes.TextInput]: {
+    helpLink: 'https://aka.ms/bfc-ask-for-user-input',
+  },
+  [SDKTypes.TraceActivity]: {
+    helpLink: 'https://aka.ms/bfc-debugging-bots',
+  },
+  [SDKTypes.Recognizer]: {
+    helpLink: 'https://aka.ms/BFC-Using-LU',
   },
 };
 
