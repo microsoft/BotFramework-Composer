65966fb126115f10e29ec12a2f9f486a55c66432
tdurnford <tjdford@gmail.com>
2020-03-23 21:24:17 -0600

    Updated jest.config.js

--- Composer/jest.config.js
+++ Composer/jest.config.js
@@ -48,7 +48,7 @@ module.exports = {
     '<rootDir>/packages/tools/language-servers/language-generation',
     '<rootDir>/packages/ui-plugins/json',
     '<rootDir>/packages/ui-plugins/lg',
-    '<rootDir>/packages/ui-plugins/lu',
+    '<rootDir>/packages/ui-plugins/luis',
     '<rootDir>/packages/ui-plugins/select-dialog',
     '<rootDir>/packages/ui-plugins/select-intent',
   ],
