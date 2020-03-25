3c7dbb62b0d80740cf939b2d8bb008de0c4d4a0f
Tony Anziano <tonyanziano5@gmail.com>
2020-03-24 17:21:13 -0700

    Fixed plugin paths.

---
+++ Composer/packages/electron-server/scripts/copy-plugins.js
@@ -0,0 +1,33 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+const fs = require('fs-extra');
+const { resolve } = require('path');
+
+const source = resolve(__dirname, '../../../plugins');
+console.log('[copy-plugins.js] Copying plugins from: ', source);
+
+let destination;
+switch (process.platform) {
+  case 'darwin':
+    // TODO: add mac path
+    break;
+
+  case 'linux':
+    // TODO: add linux path
+    break;
+
+  case 'win32':
+    destination = resolve(__dirname, '../dist/win-unpacked/resources/app.asar.unpacked/build/plugins');
+    console.log('[copy-plugins.js] Windows detected. Copying plugins to: ', destination);
+    break;
+
+  default:
+    console.error('[copy-plugins.js] Detected platform is not Mac / Linux / Windows');
+    process.exit(1);
+}
+
+// copy plugins from /Composer/plugins/ to pre-packaged electron app
+fs.copy(source, destination)
+  .then(console.log('[copy-plugins.js] Copied plugins successfully.'))
+  .catch(console.err('[copy-plugins.js] Error while copying plugins: ', err));
--- Composer/packages/electron-server/scripts/copy-templates.js
+++ Composer/packages/electron-server/scripts/copy-templates.js
@@ -1,4 +1,13 @@
-const fs = require('fs-extra');
-const path = require('path');
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
 
-fs.copy(path.resolve(__dirname, '../../../../BotProject/Templates'), path.resolve(__dirname, '../build/templates'));
+const fs = require('fs-extra');
+const { resolve } = require('path');
+
+const source = resolve(__dirname, '../../../../BotProject/Templates');
+const destination = resolve(__dirname, '../build/templates');
+
+// copy project templates to build directory to be packaged
+fs.copy(source, destination)
+  .then(console.log('[copy-templates.js] Copied templates successfully.'))
+  .catch(console.err('[copy-templates.js] Error while copying templates: ', err));
