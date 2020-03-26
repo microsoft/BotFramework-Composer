19d0de2883f891883a11a9f995539a9c2bb702f0
Srinaath Ravichandran <srravich@microsoft.com>
2020-03-26 16:39:24 -0700

    Builds for mac, linux
    Signed-off-by: Srinaath Ravichandran <srravich@microsoft.com>

--- Composer/packages/electron-server/scripts/copy-templates.js
+++ Composer/packages/electron-server/scripts/copy-templates.js
@@ -8,6 +8,8 @@ const source = resolve(__dirname, '../../../../BotProject/Templates');
 const destination = resolve(__dirname, '../build/templates');
 
 // copy project templates to build directory to be packaged
-fs.copy(source, destination)
-  .then(console.log('[copy-templates.js] Copied templates successfully.'))
-  .catch(console.err('[copy-templates.js] Error while copying templates: ', err));
+// Async with callbacks:
+fs.copy(source, destination, err => {
+  if (err) return console.error('[copy-templates.js] Error while copying templates:');
+  console.log('[copy-templates.js] Copied templates successfully.');
+});
