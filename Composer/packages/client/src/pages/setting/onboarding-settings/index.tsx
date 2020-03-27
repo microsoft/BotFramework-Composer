a6da8dda52bfe21098696818d77961411066ecd4
beyackle <beyackle@microsoft.com>
2020-03-26 17:03:09 -0700

    add callout label to Onboarding toggle

--- Composer/packages/client/src/pages/setting/onboarding-settings/index.tsx
+++ Composer/packages/client/src/pages/setting/onboarding-settings/index.tsx
@@ -2,17 +2,22 @@
 // Licensed under the MIT License.
 
 /** @jsx jsx */
-import { jsx } from '@emotion/core';
-import { useCallback, useContext } from 'react';
+import { jsx, css } from '@emotion/core';
+import { useCallback, useContext, useRef, useState } from 'react';
 import formatMessage from 'format-message';
 import { Toggle } from 'office-ui-fabric-react/lib/Toggle';
 import { Link } from 'office-ui-fabric-react/lib/Link';
+import { Callout } from 'office-ui-fabric-react/lib/Callout';
+import { Icon } from 'office-ui-fabric-react/lib/Icon';
+import { DirectionalHint } from 'office-ui-fabric-react/lib/common/DirectionalHint';
 
 import { StoreContext } from '../../../store';
 
 import { onboardingSettings, onboardingTitle } from './style';
 
 export const OnboardingSettings = () => {
+  const [calloutIsShown, showCallout] = useState(false);
+
   const {
     actions: { onboardingSetComplete },
     state: {
@@ -22,20 +27,56 @@ export const OnboardingSettings = () => {
 
   const onChange = useCallback(() => {
     onboardingSetComplete(!complete);
+    showCallout(complete);
   }, [complete, onboardingSetComplete]);
+
+  const toggleRef = useRef(null);
 
   return (
     <div css={onboardingSettings}>
       <h1 css={onboardingTitle}>{formatMessage('Onboarding')}</h1>
       <p>{formatMessage('Enabling Onboarding will restart the product tour.')}</p>
-      <Toggle
-        data-testid="onboardingToggle"
-        checked={!complete}
-        label={formatMessage('Onboarding')}
-        offText={formatMessage('Disabled')}
-        onChange={onChange}
-        onText={formatMessage('Enabled')}
-      />
+      <div ref={toggleRef}>
+        <Toggle
+          data-testid="onboardingToggle"
+          checked={!complete}
+          label={formatMessage('Onboarding')}
+          offText={formatMessage('Disabled')}
+          onChange={onChange}
+          onText={formatMessage('Enabled')}
+        />
+        <Callout
+          isBeakVisible={false}
+          hidden={!calloutIsShown}
+          role="status"
+          aria-live="assertive"
+          target={toggleRef.current}
+          directionalHint={DirectionalHint.bottomLeftEdge}
+        >
+          <div
+            css={css`
+              margin: 16px;
+              display: flex;
+              justify-content: space-around;
+              align-items: center;
+            `}
+          >
+            <Icon
+              styles={{
+                root: [
+                  {
+                    fontSize: '24px',
+                    marginRight: '8px',
+                    color: '#88f',
+                  },
+                ],
+              }}
+              iconName="SplitObject"
+            />
+            {formatMessage('Please return to Design View to start the Onboarding tutorial.')}
+          </div>
+        </Callout>
+      </div>
       <Link href="https://aka.ms/bfc-onboarding" target="_blank" rel="noopener noreferrer">
         {formatMessage('Learn more')}
       </Link>
