77638b4871d1992d4bf2cfe987155e3583a2bd92
Andy Brown <asbrown002@gmail.com>
2020-04-06 15:42:21 -0700

    chore: do not render extensions in iframe (#2480)
    * [wip] convert shell api from post message
    
    * hoist visual designer out of iframe
    
    * Fixed issues with Onboarding
    
    * Fixed issues with Onboarding
    
    * Fixed teaching bubble placement
    
    * remove copy css step
    
    * convert all usage of shell api to same frame implementation
    
    * remove extension container and related files
    
    * remove unused var
    
    * change FormEditor to PropertyEditor and fix e2e
    
    * show loading spinner when current dialog is not set
    
    * fix build errors
    
    * move test tsconfig into __tests__ directory
    
    * revert moving tsconfig to __tests__
    
    * fix path substitution in client tests
    
    * remount the property editor when focus path changes
    
    * do not memoize shell data
    
    * remove route to serve extension container
    
    * fix vertical scroll on visual designer
    
    * reset current dialog when dialog id changes
    
    * use correct template ref syntax to get template name
    
    * fix lg references in todo sample
    
    Co-authored-by: TJ Durnford <tjdford@gmail.com>
    Co-authored-by: zeye <2295905420@qq.com>

--- Composer/packages/client/src/pages/design/FrameAPI.ts
+++ Composer/packages/client/src/pages/design/FrameAPI.ts
@@ -1,45 +1,28 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-import ApiClient from './ApiClient';
-
-const apiClient = new ApiClient();
-
 export class FrameAPI {
-  frameId: string;
-  frameRef: any;
-
-  constructor(frameId: string) {
-    this.frameId = frameId;
-    this.frameRef = null;
-  }
-
   /**
    * Initialize the frame ref at first invocation.
    */
-  invoke = (method: string, ...rest) => {
-    if (!this.frameRef) {
-      this.frameRef = window.frames[this.frameId];
+  invoke = (method: string, ...rest: any[]) => {
+    if (typeof window[method] === 'function') {
+      return window[method](...rest);
     }
-
-    if (this.frameRef && this.frameRef[method]) {
-      return apiClient.apiCall('rpc', [method, ...rest], this.frameRef);
-    }
-    return Promise.reject();
   };
 }
 
 export const VisualEditorAPI = (() => {
-  const visualEditorFrameAPI = new FrameAPI('VisualEditor');
+  const visualEditorFrameAPI = new FrameAPI();
   // HACK: under cypress env, avoid invoking API inside frame too frequently (especially the `hasEleemntFocused`). It will lead to CI test quite fagile.
   // TODO: remove this hack logic after refactoring state sync logic between shell and editors.
   if ((window as any).Cypress) {
-    visualEditorFrameAPI.invoke = () => Promise.resolve(false);
+    visualEditorFrameAPI.invoke = () => {};
   }
 
   return {
     hasElementFocused: () => visualEditorFrameAPI.invoke('hasElementFocused'),
-    hasElementSelected: () => visualEditorFrameAPI.invoke('hasElementSelected').catch(() => false),
+    hasElementSelected: () => visualEditorFrameAPI.invoke('hasElementSelected'),
     copySelection: () => visualEditorFrameAPI.invoke('copySelection'),
     cutSelection: () => visualEditorFrameAPI.invoke('cutSelection'),
     moveSelection: () => visualEditorFrameAPI.invoke('moveSelection'),
