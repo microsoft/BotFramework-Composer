c0d4162bae0589da294cedb8987d13c0cdd25d7c
Tony Anziano <tonyanziano5@gmail.com>
2020-04-09 15:10:45 -0700

    Added logging.

--- Composer/packages/electron-server/src/appUpdater.ts
+++ Composer/packages/electron-server/src/appUpdater.ts
@@ -1,8 +1,11 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-import { autoUpdater } from 'electron-updater';
+import { autoUpdater, UpdateInfo } from 'electron-updater';
 import { EventEmitter } from 'events';
+
+import logger from './utility/logger';
+const log = logger.extend('app-updater');
 
 export class AppUpdater extends EventEmitter {
   private checkingForUpdate: boolean = false;
@@ -26,6 +29,7 @@ export class AppUpdater extends EventEmitter {
     autoUpdater.on('update-not-available', this.onUpdateNotAvailable);
     autoUpdater.on('download-progress', this.onDownloadProgress);
     autoUpdater.on('update-downloaded', this.onUpdateDownloaded);
+    logger('Initialized');
   }
 
   public checkForUpdates() {
@@ -34,26 +38,33 @@ export class AppUpdater extends EventEmitter {
     }
   }
 
-  private onError() {
+  private onError(err: Error) {
     this.checkingForUpdate = false;
+    logger('Got error while checking for updates: ', err);
   }
 
   private onCheckingForUpdate() {
     // flip some state to change UI in client
     this.checkingForUpdate = true;
+    log('Checking for updates...');
   }
 
-  private onUpdateAvailable() {
+  private onUpdateAvailable(updateInfo: UpdateInfo) {
     this.checkingForUpdate = false;
+    log('Update available: %O', updateInfo);
   }
 
-  private onUpdateNotAvailable() {
+  private onUpdateNotAvailable(updateInfo: UpdateInfo) {
     this.checkingForUpdate = false;
+    log('Update not available: %O', updateInfo);
   }
 
-  private onDownloadProgress() {}
+  private onDownloadProgress(progress: any, ...other) {
+    log('Got update progress: %O %O', progress, other);
+  }
 
-  private onUpdateDownloaded() {
+  private onUpdateDownloaded(updateInfo: UpdateInfo) {
     this.checkingForUpdate = false;
+    log('Update downloaded: %O', updateInfo);
   }
 }
