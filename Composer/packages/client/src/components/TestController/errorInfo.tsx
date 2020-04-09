f25ba33fbf33ad52ff1b51981dd8c3a5cdf4a2b4
leileizhang <leilzh@microsoft.com>
2020-04-10 00:15:31 +0800

    fix: catch the throw error message from the luis file check function (#2557)
    * fix: catch the throw error message from luis file check function
    
    * change the throw error
    
    * refactor the test button
    
    * remove log

---
+++ Composer/packages/client/src/components/TestController/errorInfo.tsx
@@ -0,0 +1,27 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+/** @jsx jsx */
+import { jsx } from '@emotion/core';
+import { IconButton } from 'office-ui-fabric-react/lib/Button';
+
+import { errorButton, errorCount, errorInfo } from './styles';
+
+interface IErrorInfoProps {
+  hidden: boolean;
+  count: number;
+  onClick: () => void;
+}
+
+export const ErrorInfo: React.FC<IErrorInfoProps> = props => {
+  const { hidden, count, onClick } = props;
+
+  if (hidden) return null;
+
+  return (
+    <div css={errorInfo} onClick={onClick} data-testid="notifications-info-button">
+      <span css={errorCount}>{count}</span>
+      <IconButton iconProps={{ iconName: 'ErrorBadge' }} css={errorButton} title="Error" ariaLabel="Error" />
+    </div>
+  );
+};
