5bba96838d6c13840bd05963890e6db75c7c73ae
zeye <zeye@microsoft.com>
2020-04-10 21:18:40 +0800

    move prompts visual schema to 'prompts' plugin

--- Composer/packages/ui-plugins/prompts/src/index.ts
+++ Composer/packages/ui-plugins/prompts/src/index.ts
@@ -4,9 +4,13 @@
 import { PluginConfig } from '@bfc/extension';
 
 import uiSchema from './uiSchema';
+import visualSchema from './visualSchema';
 
 const config: PluginConfig = {
   uiSchema,
+  visual: {
+    schema: visualSchema,
+  },
 };
 
 export default config;
