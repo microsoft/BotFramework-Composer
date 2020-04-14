8218e922e8093e01bc1131e9bad10edbb23a3dd9
zhixzhan <zhixzhan@microsoft.com>
2020-04-14 12:26:57 +0800

    designerId no dash -

--- Composer/packages/lib/shared/src/generateUniqueId.ts
+++ Composer/packages/lib/shared/src/generateUniqueId.ts
@@ -1,8 +1,13 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-import nanoid from 'nanoid';
+import { customAlphabet } from 'nanoid';
+import englishLowercase from 'nanoid-dictionary/lowercase';
+import numbers from 'nanoid-dictionary/numbers';
+import englishUppercase from 'nanoid-dictionary/uppercase';
 
+export const IDDictionnary = [...englishLowercase, ...englishUppercase, ...numbers].join('');
 export function generateUniqueId(size = 6) {
-  return nanoid(size);
+  const nanoid = customAlphabet(IDDictionnary, size);
+  return nanoid();
 }
