51bc571e8df67e854c770135fb50aa1e28a6a80d
Srinaath Ravichandran <srravich@microsoft.com>
2020-04-14 15:12:21 -0700

    safety net
    Signed-off-by: Srinaath Ravichandran <srravich@microsoft.com>
    
    Safe commit for electron
    
    Signed-off-by: Srinaath Ravichandran <srravich@microsoft.com>
    
    Test windows installer
    
    Signed-off-by: Srinaath Ravichandran <srravich@microsoft.com>
    
    Logs remove
    
    Signed-off-by: Srinaath Ravichandran <srravich@microsoft.com>
    
    Reset commits
    
    Signed-off-by: Srinaath Ravichandran <srravich@microsoft.com>
    
    All commits with logs
    
    Signed-off-by: Srinaath Ravichandran <srravich@microsoft.com>
    
    Refactored
    
    Signed-off-by: Srinaath Ravichandran <srravich@microsoft.com>
    
    Mac working
    
    Signed-off-by: Srinaath Ravichandran <srravich@microsoft.com>
    
    Safe commit for Mac and Wimdows
    
    Signed-off-by: Srinaath Ravichandran <srravich@microsoft.com>
    
    Code cleanup
    
    Signed-off-by: Srinaath Ravichandran <srravich@microsoft.com>
    
    Wrapping up open API
    
    Signed-off-by: Srinaath Ravichandran <srravich@microsoft.com>
    
    Revert back to original server
    
    Signed-off-by: Srinaath Ravichandran <srravich@microsoft.com>

---
+++ Composer/packages/electron-server/jest.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+  displayName: 'electron-server',
+  preset: 'ts-jest/presets/js-with-babel',
+  testPathIgnorePatterns: ['/node_modules/'],
+  watchPathIgnorePatterns: ['<rootDir>/__tests__/mocks'],
+
+  moduleNameMapper: {
+    // allow tests to import src code from '@src'
+    '^@src/(.*)$': '<rootDir>/src/$1',
+  },
+
+  globals: {
+    'ts-jest': {
+      tsConfig: path.resolve(__dirname, './tsconfig.json'),
+      diagnostics: {
+        warnOnly: true,
+      },
+    },
+  },
+};
---
+++ Composer/packages/electron-server/src/utility/url.test.ts
@@ -0,0 +1,22 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import { parseDeepLinkUrl } from './url';
+
+describe('Parse URL', () => {
+  it('should parse the url to navigate', () => {
+    const testString =
+      'bfcomposer://open?url=bot%2F88524.65932484581%2Fdialogs%2Fdeleteitem%3Fselected%3Dtriggers%5B0%5D%26focused%3Dtriggers%5B0%5D.actions%5B0%5D&test=ab';
+    const urlToNavigate = parseDeepLinkUrl(testString);
+    expect(urlToNavigate).toBe(
+      'bot/88524.65932484581/dialogs/deleteitem?selected=triggers[0]&focused=triggers[0].actions[0]'
+    );
+  });
+
+  it('should return empty ', () => {
+    const testString =
+      'bfcomposer://noop?url=http%3A%2F%2Flocalhost%3A5000%2Fbot%2F88524.65932484581%2Fdialogs%2Fdeleteitem%3Fselected%3Dtriggers%5B0%5D%26focused%3Dtriggers%5B0%5D.actions%5B0%5D&test=ab';
+    const urlToNavigate = parseDeepLinkUrl(testString);
+    expect(urlToNavigate).toBe('');
+  });
+});
