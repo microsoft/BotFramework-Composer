88fd61bbde939b9141aa709b134097d0ce6b6fed
Lu Han <32191031+luhan2017@users.noreply.github.com>
2020-04-17 11:11:55 +0800

    Fix for skill AutoEndDialog

--- BotProject/Templates/CSharp/ComposerBot.cs
+++ BotProject/Templates/CSharp/ComposerBot.cs
@@ -1,6 +1,8 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using System.Security.Claims;
+using System.Security.Principal;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Bot.Builder.AI.QnA;
@@ -10,6 +12,7 @@ using Microsoft.Bot.Builder.Dialogs.Debugging;
 using Microsoft.Bot.Builder.Dialogs.Declarative;
 using Microsoft.Bot.Builder.Dialogs.Declarative.Resources;
 using Microsoft.Bot.Builder.Skills;
+using Microsoft.Bot.Connector.Authentication;
 
 namespace Microsoft.Bot.Builder.ComposerBot.Json
 {
@@ -38,6 +41,16 @@ namespace Microsoft.Bot.Builder.ComposerBot.Json
         
         public override async Task OnTurnAsync(ITurnContext turnContext, CancellationToken cancellationToken = default(CancellationToken))
         {
+            AdaptiveDialog rootDialog = (AdaptiveDialog)this.dialogManager.RootDialog;
+            if (turnContext.TurnState.Get<IIdentity>(BotAdapter.BotIdentityKey) is ClaimsIdentity claimIdentity && SkillValidation.IsSkillClaim(claimIdentity.Claims))
+            {
+                rootDialog.AutoEndDialog = true;
+            }
+            else
+            {          
+                rootDialog.AutoEndDialog = false;
+            }
+
             await this.dialogManager.OnTurnAsync(turnContext, cancellationToken: cancellationToken);
             await this.conversationState.SaveChangesAsync(turnContext, false, cancellationToken);
             await this.userState.SaveChangesAsync(turnContext, false, cancellationToken);
@@ -46,7 +59,7 @@ namespace Microsoft.Bot.Builder.ComposerBot.Json
         private void LoadRootDialogAsync()
         {
             var rootFile = resourceExplorer.GetResource(rootDialogFile);
-            var rootDialog = resourceExplorer.LoadType<Dialog>(rootFile);
+            var rootDialog = resourceExplorer.LoadType<AdaptiveDialog>(rootFile);
             this.dialogManager = new DialogManager(rootDialog)
                                 .UseResourceExplorer(resourceExplorer)
                                 .UseLanguageGeneration()
