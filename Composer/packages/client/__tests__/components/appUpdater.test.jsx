750040a56dfedf09e2337e1b9545046a085e593f
Tony Anziano <tonyanziano5@gmail.com>
2020-04-20 14:39:28 -0700

    Fixed tests.

--- Composer/packages/client/__tests__/components/appUpdater.test.jsx
+++ Composer/packages/client/__tests__/components/appUpdater.test.jsx
@@ -2,7 +2,7 @@
 // Licensed under the MIT License.
 
 import React from 'react';
-import { render } from 'react-testing-library';
+import { render } from '@bfc/test-utils';
 
 import { StoreContext } from '../../src/store';
 import { AppUpdater } from '../../src/components/AppUpdater';
