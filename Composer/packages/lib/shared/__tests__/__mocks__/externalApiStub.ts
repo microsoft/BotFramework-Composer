8203d6872101cdf409ded6f8e865a5c01c386ebb
leileizhang <leilzh@microsoft.com>
2020-04-21 23:43:26 +0800

    fix: support the cross train corner case (#2673)
    * fix: support the cross train corner case
    
    * update the unit test
    
    * update the config
    
    * add config for all other kinds triggers
    
    Co-authored-by: Dong Lei <donglei@microsoft.com>
    Co-authored-by: Chris Whitten <christopher.whitten@microsoft.com>

--- Composer/packages/lib/shared/__tests__/__mocks__/externalApiStub.ts
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License
-
-import { ExternalApi } from '../../src/copyUtils/ExternalApi';
-
-export const externalApiStub: ExternalApi = {
-  getDesignerId: () => ({ id: '5678' }),
-  transformLgField: (id, data, fieldName, fieldValue) => Promise.resolve(fieldValue || ''),
-};
