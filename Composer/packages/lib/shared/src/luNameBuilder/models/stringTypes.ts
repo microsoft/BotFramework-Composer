5c668aaa47bab98fc64a0bf71bda078377322511
leileizhang <leilzh@microsoft.com>
2020-04-21 23:43:26 +0800

    fix: support the cross train corner case (#2673)
    * fix: support the cross train corner case
    
    * update the unit test
    
    * update the config
    
    * add config for all other kinds triggers
    
    Co-authored-by: Dong Lei <donglei@microsoft.com>
    Co-authored-by: Chris Whitten <christopher.whitten@microsoft.com>

--- Composer/packages/lib/shared/src/luNameBuilder/models/stringTypes.ts
+++
@@ -1,7 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License
-
-/**
- * example: 'TextInput_Response_RuZhXe' => { type: 'TextInput_Response', designerId: 'RuZhXe' } // LuMetaData
- */
-export type LuIntentName = string;
