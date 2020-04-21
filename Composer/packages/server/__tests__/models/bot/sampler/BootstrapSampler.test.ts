e13437740513de166024d6ba065349ab3a92652f
leilzh <leilzh@microsoft.com>
2020-04-21 17:42:55 +0800

    don't do sample for the ratio is ok

--- Composer/packages/server/__tests__/models/bot/sampler/BootstrapSampler.test.ts
+++ Composer/packages/server/__tests__/models/bot/sampler/BootstrapSampler.test.ts
@@ -6,27 +6,20 @@ describe('BootstrapSampler', () => {
   it('balence the utterances ratio in intents after bootstrap sampling', async () => {
     const utterances = [
       { intent: '0', text: '1', entities: [] },
+      { intent: '0', text: '2', entities: [] },
       { intent: '1', text: '3', entities: [] },
       { intent: '1', text: '4', entities: [] },
       { intent: '1', text: '5', entities: [] },
       { intent: '1', text: '6', entities: [] },
       { intent: '1', text: '7', entities: [] },
-      { intent: '1', text: '8', entities: [] },
-      { intent: '1', text: '9', entities: [] },
-      { intent: '1', text: '10', entities: [] },
-      { intent: '1', text: '11', entities: [] },
-      { intent: '1', text: '12', entities: [] },
-      { intent: '1', text: '13', entities: [] },
-      { intent: '1', text: '14', entities: [] },
-      { intent: '1', text: '15', entities: [] },
     ];
-    const sampler = new ComposerBootstrapSampler(utterances, 10);
+    const sampler = new ComposerBootstrapSampler(utterances, 2);
     const result = sampler.getSampledUtterances();
     const intent1 = result.filter(e => e.intent === '1').length;
-    expect((result.length - intent1) / intent1).toBeCloseTo(0.1, 2);
+    expect(2 / intent1).toBeCloseTo(0.5, 2);
     const sampler1 = new ComposerBootstrapSampler(utterances, 5);
     const result1 = sampler1.getSampledUtterances();
     const intent11 = result1.filter(e => e.intent === '1').length;
-    expect((result1.length - intent11) / intent11).toBeCloseTo(0.2, 2);
+    expect(intent11).toBeCloseTo(5);
   });
 });
--- Composer/packages/server/src/models/bot/sampler/BootstrapSampler.ts
+++ Composer/packages/server/src/models/bot/sampler/BootstrapSampler.ts
@@ -2,6 +2,7 @@
 // Licensed under the MIT License.
 
 import { BootstrapSampler } from '@microsoft/bf-dispatcher/lib/mathematics/sampler/BootstrapSampler';
+import { Utility } from '@microsoft/bf-dispatcher/lib/utility/Utility';
 
 const SAMPLE_SIZE_CONFIGURATION = 2;
 
@@ -25,13 +26,17 @@ export class ComposerBootstrapSampler extends BootstrapSampler<number> {
     });
   }
 
-  public computeSamplingNumberInstancesPerLabel(label = ''): number {
+  public computeMaxBalanceNumber(): number {
     const numberInstancesPerLabelReduce: number = this.labels.reduce(
       (mini: number, key: string) => (this.instances[key].length < mini ? this.instances[key].length : mini),
       Number.MAX_SAFE_INTEGER
     );
 
-    return this._maxImbalanceRatio * numberInstancesPerLabelReduce * SAMPLE_SIZE_CONFIGURATION;
+    return this._maxImbalanceRatio * numberInstancesPerLabelReduce;
+  }
+
+  public computeSamplingNumberInstancesPerLabel(label = ''): number {
+    return this.computeMaxBalanceNumber() * SAMPLE_SIZE_CONFIGURATION;
   }
 
   public getSampledUtterances() {
@@ -45,6 +50,26 @@ export class ComposerBootstrapSampler extends BootstrapSampler<number> {
       return Array.from(set).map(index => this._utterances[index]);
     } else {
       return this._utterances;
+    }
+  }
+
+  //do re-sample if the ratio is beigher than the maxImbalanceRatio
+  public *sampleInstances() {
+    for (const key in this.instances) {
+      const instanceArray: number[] = this.instances[key];
+      const numberInstancesPerLabel: number = instanceArray.length;
+      const maxBalanceNumber: number = this.computeMaxBalanceNumber();
+      if (numberInstancesPerLabel > maxBalanceNumber) {
+        const numberSamplingInstancesPerLabel: number = this.computeSamplingNumberInstancesPerLabel(key);
+        for (let i = 0; i < numberSamplingInstancesPerLabel; i++) {
+          const indexRandom = Utility.getRandomInt(numberInstancesPerLabel);
+          yield instanceArray[indexRandom];
+        }
+      } else {
+        for (let i = 0; i < numberInstancesPerLabel; i++) {
+          yield instanceArray[i];
+        }
+      }
     }
   }
 }
