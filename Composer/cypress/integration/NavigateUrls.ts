0cc0d6ed063b2871fa49cdc57faf01124039e96f
Srinaath Ravichandran <srravich@microsoft.com>
2020-04-23 23:33:04 -0700

    Typo fixed
    Signed-off-by: Srinaath Ravichandran <srravich@microsoft.com>

--- Composer/cypress/integration/NavigateUrls.ts
+++ Composer/cypress/integration/NavigateUrls.ts
@@ -6,13 +6,13 @@ context('Navigate Url', () => {
     cy.visit(Cypress.env('COMPOSER_URL'));
   });
 
-  it.only('should open create from scratch/template window from a url', () => {
+  it.only('should open Create From Scratch/Template window from a url', () => {
     cy.visit(`${Cypress.env('COMPOSER_URL')}/home/createProject`);
     cy.get('[data-testid="Create from scratch"]').should('be.visible');
     cy.get('[data-testid="Create from template"]').should('be.visible');
   });
 
-  it.only('should open window to define conversation from a url', () => {
+  it.only('should open Define Conversations window from a url', () => {
     cy.visit(`${Cypress.env('COMPOSER_URL')}/home/create/template/TodoSample`);
     cy.get('[data-testid="NewDialogName"]').should('be.visible');
   });
