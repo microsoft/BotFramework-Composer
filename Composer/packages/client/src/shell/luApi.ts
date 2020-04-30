215c93bad01e3023dfd8e4e3f5810bef77497963
zeye <2295905420@qq.com>
2020-05-01 03:26:52 +0800

    fix: only apply throttle on lu/lg `update` handler (#2823)
    * only apply throttle on lu/lg `update` handler
    
    * fix ts type validation
    
    * apply addLgTemplate
    
    Co-authored-by: Andy Brown <asbrown002@gmail.com>

--- Composer/packages/client/src/shell/luApi.ts
+++ Composer/packages/client/src/shell/luApi.ts
@@ -4,61 +4,65 @@
 import { useContext, useEffect, useState } from 'react';
 import { LuFile, LuIntentSection } from '@bfc/shared';
 import throttle from 'lodash/throttle';
-import mapValues from 'lodash/mapValues';
 
 import * as luUtil from '../utils/luUtil';
 import { State, BoundActionHandlers } from '../store/types';
 import { StoreContext } from '../store';
 
-const createThrottledFunc = fn => throttle(fn, 0, { leading: true, trailing: true });
+const createThrottledFunc = fn => throttle(fn, 1000, { leading: true, trailing: true });
 
 function createLuApi(state: State, actions: BoundActionHandlers, luFileResolver: (id: string) => LuFile | undefined) {
-  const api = {
-    addLuIntent: async (id: string, intentName: string, intent: LuIntentSection) => {
-      const file = luFileResolver(id);
-      if (!file) throw new Error(`lu file ${id} not found`);
-      if (!intentName) throw new Error(`intentName is missing or empty`);
+  const addLuIntent = async (id: string, intentName: string, intent: LuIntentSection) => {
+    const file = luFileResolver(id);
+    if (!file) throw new Error(`lu file ${id} not found`);
+    if (!intentName) throw new Error(`intentName is missing or empty`);
 
-      const content = luUtil.addIntent(file.content, intent);
-      const projectId = state.projectId;
-      return await actions.updateLuFile({ id: file.id, projectId, content });
-    },
-    updateLuIntent: async (id: string, intentName: string, intent: LuIntentSection) => {
-      const file = luFileResolver(id);
-      if (!file) throw new Error(`lu file ${id} not found`);
-      if (!intentName) throw new Error(`intentName is missing or empty`);
-
-      const content = luUtil.updateIntent(file.content, intentName, intent);
-      const projectId = state.projectId;
-      return await actions.updateLuFile({ id: file.id, projectId, content });
-    },
-
-    removeLuIntent: async (id: string, intentName: string) => {
-      const file = luFileResolver(id);
-      if (!file) throw new Error(`lu file ${id} not found`);
-      if (!intentName) throw new Error(`intentName is missing or empty`);
-
-      const content = luUtil.removeIntent(file.content, intentName);
-      const projectId = state.projectId;
-      return await actions.updateLuFile({ id: file.id, projectId, content });
-    },
-
-    getLuIntents: (id: string): LuIntentSection[] => {
-      if (id === undefined) throw new Error('must have a file id');
-      const focusedDialogId = state.focusPath.split('#').shift() || id;
-      const file = luFileResolver(focusedDialogId);
-      if (!file) throw new Error(`lu file ${id} not found`);
-      return file.intents;
-    },
-
-    getLuIntent: (id: string, intentName: string): LuIntentSection | undefined => {
-      const file = luFileResolver(id);
-      if (!file) throw new Error(`lu file ${id} not found`);
-      return file.intents.find(({ Name }) => Name === intentName);
-    },
+    const content = luUtil.addIntent(file.content, intent);
+    const projectId = state.projectId;
+    return await actions.updateLuFile({ id: file.id, projectId, content });
   };
 
-  return mapValues(api, fn => createThrottledFunc(fn));
+  const updateLuIntent = async (id: string, intentName: string, intent: LuIntentSection) => {
+    const file = luFileResolver(id);
+    if (!file) throw new Error(`lu file ${id} not found`);
+    if (!intentName) throw new Error(`intentName is missing or empty`);
+
+    const content = luUtil.updateIntent(file.content, intentName, intent);
+    const projectId = state.projectId;
+    return await actions.updateLuFile({ id: file.id, projectId, content });
+  };
+
+  const removeLuIntent = async (id: string, intentName: string) => {
+    const file = luFileResolver(id);
+    if (!file) throw new Error(`lu file ${id} not found`);
+    if (!intentName) throw new Error(`intentName is missing or empty`);
+
+    const content = luUtil.removeIntent(file.content, intentName);
+    const projectId = state.projectId;
+    return await actions.updateLuFile({ id: file.id, projectId, content });
+  };
+
+  const getLuIntents = (id: string): LuIntentSection[] => {
+    if (id === undefined) throw new Error('must have a file id');
+    const focusedDialogId = state.focusPath.split('#').shift() || id;
+    const file = luFileResolver(focusedDialogId);
+    if (!file) throw new Error(`lu file ${id} not found`);
+    return file.intents;
+  };
+
+  const getLuIntent = (id: string, intentName: string): LuIntentSection | undefined => {
+    const file = luFileResolver(id);
+    if (!file) throw new Error(`lu file ${id} not found`);
+    return file.intents.find(({ Name }) => Name === intentName);
+  };
+
+  return {
+    addLuIntent,
+    getLuIntents,
+    getLuIntent,
+    updateLuIntent: createThrottledFunc(updateLuIntent),
+    removeLuIntent,
+  };
 }
 
 export function useLuApi() {
@@ -73,7 +77,9 @@ export function useLuApi() {
 
     return () => {
       Object.keys(newApi).forEach(apiName => {
-        newApi[apiName].flush();
+        if (typeof newApi[apiName].flush === 'function') {
+          newApi[apiName].flush();
+        }
       });
     };
   }, [projectId, focusPath]);
