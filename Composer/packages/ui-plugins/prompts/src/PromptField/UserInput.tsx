6b9b60e29cae3b8b53ddba3d1882104863cca421
liweitian <liweitian93@outlook.com>
2020-05-01 02:44:50 +0800

    feat: add more comments in all lu editors (#2712)
    * add comments
    
    * handle comments
    
    * handle comments
    
    * update text
    
    * ux updates to lu editor placeholders
    
    - make editors 50% larger
    - add special placeholder for expected responses
    
    Co-authored-by: Andy Brown <asbrown002@gmail.com>

--- Composer/packages/ui-plugins/prompts/src/PromptField/UserInput.tsx
+++ Composer/packages/ui-plugins/prompts/src/PromptField/UserInput.tsx
@@ -20,16 +20,26 @@ const getOptions = (enumSchema: JSONSchema7) => {
   return enumSchema.enum.map(o => o as string);
 };
 
+const expectedResponsesPlaceholder = () =>
+  formatMessage(`> add some expected user responses:
+> - Please remind me to '{itemTitle=buy milk}'
+> - remind me to '{itemTitle}'
+> - add '{itemTitle}' to my todo list
+>
+> entity definitions:
+> @ ml itemTitle
+`);
+
 const UserInput: React.FC<PromptFieldProps<MicrosoftInputDialog>> = props => {
   const { onChange, getSchema, value, id, uiOptions, getError, definitions, depth, schema = {} } = props;
   const { currentDialog, designerId } = useShellApi();
   const { recognizers } = usePluginConfig();
 
-  const { const: $kind } = (schema?.properties?.['$kind'] as any) || {};
+  const { const: $kind } = (schema?.properties?.$kind as { const: string }) || {};
   const intentName = new LuMetaData(new LuType($kind).toString(), designerId).toString();
 
   const type = recognizerType(currentDialog);
-  const Editor: any = type === SDKKinds.LuisRecognizer && recognizers.find(r => r.id === type)?.editor;
+  const Editor = type === SDKKinds.LuisRecognizer && recognizers.find(r => r.id === type)?.editor;
   const intentLabel = formatMessage('Expected responses (intent: #{intentName})', { intentName });
 
   return (
@@ -72,7 +82,7 @@ const UserInput: React.FC<PromptFieldProps<MicrosoftInputDialog>> = props => {
       {Editor && $kind !== SDKKinds.AttachmentInput && (
         <React.Fragment>
           <FieldLabel id={`${id}.intent`} label={intentLabel} />
-          <Editor {...props} onChange={() => {}} />
+          <Editor {...props} onChange={() => {}} placeholder={expectedResponsesPlaceholder()} />
         </React.Fragment>
       )}
       {getSchema('defaultLocale') && (
