214a96520efa4c4297c8a0c2ef5ecee99192a25f
Carlos Castro <ccastro@microsoft.com>
2020-05-01 13:05:01 -0700

    Runtime: Add copyright header to all missing files

--- runtime/dotnet/azurefunctions/Startup.cs
+++ runtime/dotnet/azurefunctions/Startup.cs
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 using Microsoft.ApplicationInsights.Extensibility;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Azure.Functions.Extensions.DependencyInjection;
--- runtime/dotnet/tests/InputsTests.cs
+++ runtime/dotnet/tests/InputsTests.cs
@@ -1,5 +1,4 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
 using Microsoft.Bot.Builder;
