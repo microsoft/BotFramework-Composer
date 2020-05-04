13b631baf6b8ba9025c6fce5b1c7cc780901f7f7
Andy Brown <asbrown002@gmail.com>
2020-05-04 11:07:44 -0700

    memoize starting title in form title

--- Composer/packages/extensions/adaptive-form/src/components/AdaptiveForm/FormTitle.tsx
+++ Composer/packages/extensions/adaptive-form/src/components/AdaptiveForm/FormTitle.tsx
@@ -3,11 +3,10 @@
 
 /** @jsx jsx */
 import { jsx } from '@emotion/core';
-import React from 'react';
+import React, { useMemo } from 'react';
 import { FontWeights } from '@uifabric/styling';
 import { FontSizes } from '@uifabric/fluent-theme';
 import { Link } from 'office-ui-fabric-react/lib/Link';
-import startCase from 'lodash/startCase';
 import formatMessage from 'format-message';
 import { UIOptions, JSONSchema7 } from '@bfc/extension';
 
@@ -19,35 +18,32 @@ interface FormTitleProps {
   description?: string;
   formData: any;
   id: string;
-  name?: string;
-  onChange?: (data: any) => void;
+  onChange: ($designer: object) => void;
   schema: JSONSchema7;
   title?: string;
   uiOptions?: UIOptions;
 }
 
 const FormTitle: React.FC<FormTitleProps> = props => {
-  const { name, description, schema, formData, uiOptions = {} } = props;
+  const { description, schema, formData, uiOptions = {} } = props;
 
   const handleTitleChange = (newTitle?: string): void => {
-    if (props.onChange) {
-      props.onChange({
-        ...formData.$designer,
-        name: newTitle,
-      });
-    }
+    props.onChange({
+      ...formData.$designer,
+      name: newTitle,
+    });
   };
 
   const uiLabel = typeof uiOptions?.label === 'function' ? uiOptions.label(formData) : uiOptions.label;
   const uiSubtitle = typeof uiOptions?.subtitle === 'function' ? uiOptions.subtitle(formData) : uiOptions.subtitle;
-  const getTitle = (): string => {
+  const initialValue = useMemo(() => {
     const designerName = formData.$designer?.name;
 
-    return designerName || uiLabel || schema.title || startCase(name);
-  };
+    return designerName || uiLabel || schema.title;
+  }, [formData.$designer?.name, uiLabel, schema.title]);
 
   const getHelpLinkLabel = (): string => {
-    return (uiLabel || schema.title || startCase(name) || '').toLowerCase();
+    return (uiLabel || schema.title || '').toLowerCase();
   };
 
   const getSubTitle = (): string => {
@@ -86,7 +82,7 @@ const FormTitle: React.FC<FormTitleProps> = props => {
             root: { margin: '5px 0 7px -9px' },
           }}
           uiOptions={{}}
-          value={getTitle()}
+          value={initialValue}
           onChange={handleTitleChange}
           ariaLabel={formatMessage('form title')}
         />
