6376104b457698450d5005feb25f43ddadfd58bd
leileizhang <leilzh@microsoft.com>
2020-05-08 23:36:05 +0800

    fix: improved lu/lg editor performance (#2904)
    * add web worker for parser
    
    * update the lu worker
    
    * add webpack config
    
    * update the import
    
    * remove the webpack config
    
    * update some naming
    
    * add error catch
    
    * add worker-loader to webpack config
    
    * update the jest config to support the worker
    
    * update BaseWorker
    
    * add a generic type for baseworker class
    
    * update the basecalss

---
+++ Composer/packages/client/src/store/parsers/baseWorker.ts
@@ -0,0 +1,43 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import uniqueId from 'lodash/uniqueId';
+
+// Wrapper class
+export class BaseWorker {
+  private worker: Worker;
+  private resolves = {};
+  private rejects = {};
+
+  constructor(worker: Worker) {
+    this.worker = worker;
+    this.worker.onmessage = this.handleMsg;
+  }
+
+  sendMsg = <Payload>(payload: Payload) => {
+    const msgId = uniqueId();
+    const msg = { id: msgId, payload };
+    return new Promise((resolve, reject) => {
+      // save callbacks for later
+      this.resolves[msgId] = resolve;
+      this.rejects[msgId] = reject;
+      this.worker.postMessage(msg);
+    });
+  };
+
+  // Handle incoming calculation result
+  handleMsg = msg => {
+    const { id, error, payload } = msg.data;
+    if (error) {
+      const reject = this.rejects[id];
+      reject(error);
+    } else {
+      const resolve = this.resolves[id];
+      if (resolve) resolve(payload);
+    }
+
+    // purge used callbacks
+    delete this.resolves[id];
+    delete this.rejects[id];
+  };
+}
