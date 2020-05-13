f6998d636bef32c0293999e7f1daf621de00e27a
Andy Brown <asbrown002@gmail.com>
2020-05-13 09:41:45 -0700

    extract schema utils and add tests
    * if def is found in cache, do not attempt to dereference
    * make a per-instance cache
    * check for a circular ref when initially resolving

---
+++ Composer/packages/lib/shared/src/schemaUtils/getRef.ts
@@ -0,0 +1,30 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import get from 'lodash/get';
+import { JSONSchema7Definition } from 'json-schema';
+
+import { SchemaDefinitions, DefinitionCache } from './types';
+import { isCircular } from './circular';
+import { dereference } from './dereference';
+
+export function getRef(ref: string, definitions: SchemaDefinitions, cache: DefinitionCache): JSONSchema7Definition {
+  const defName = ref.replace('#/definitions/', '');
+
+  if (cache.has(defName)) {
+    return cache.get(defName) as JSONSchema7Definition;
+  }
+
+  let def = get(definitions, defName.split('/')) as JSONSchema7Definition | undefined;
+
+  if (!def) {
+    throw new Error(`Definition not found for ${defName}`);
+  }
+
+  if (!isCircular(defName)) {
+    def = dereference(def as JSONSchema7Definition, definitions, cache);
+  }
+  cache.set(defName, def);
+
+  return def;
+}
