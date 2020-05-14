a6c2e3e1d4be452b07c37b07b07124f73f3b9dd6
Andy Brown <asbrown002@gmail.com>
2020-05-14 15:19:05 -0700

    redirect to dialog-settings

--- Composer/packages/client/src/pages/setting/router.tsx
+++ Composer/packages/client/src/pages/setting/router.tsx
@@ -2,7 +2,7 @@
 // Licensed under the MIT License.
 
 import React from 'react';
-import { Router } from '@reach/router';
+import { Router, Redirect } from '@reach/router';
 
 import { ErrorBoundary } from '../../components/ErrorBoundary';
 
@@ -10,10 +10,18 @@ import { DialogSettings } from './dialog-settings';
 import { AppSettings } from './app-settings';
 import { RuntimeSettings } from './runtime-settings';
 
+const getRedirect = () => {
+  const { pathname } = location;
+
+  const path = pathname.endsWith('/') ? pathname.substring(0, pathname.length - 1) : pathname;
+  return `${path}/dialog-settings`;
+};
+
 const Routes = () => {
   return (
     <ErrorBoundary>
       <Router>
+        <Redirect from="/" to={getRedirect()} noThrow />
         <DialogSettings path="dialog-settings" default />
         <AppSettings path="preferences" />
         <RuntimeSettings path="runtime" />
