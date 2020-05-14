9c1db799f8bbfc23e4dc91fbf0723592db688bc2
Andy Brown <asbrown002@gmail.com>
2020-05-14 13:42:21 -0700

    add image assets

---
+++ Composer/packages/client/src/pages/setting/app-settings/images/index.ts
@@ -0,0 +1,9 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import minimap from './minimap.svg';
+import onboarding from './onboarding.svg';
+import rowNumbers from './row-numbers.svg';
+import wordWrap from './word-wrap.svg';
+
+export { minimap, onboarding, rowNumbers, wordWrap };
