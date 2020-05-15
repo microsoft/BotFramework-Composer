8f67351f87764a112c7e4137e2504039c138f4ef
zeye <zeye@microsoft.com>
2020-05-15 18:47:37 +0800

    remove unnecessary if

--- Composer/packages/ui-plugins/lg/src/LgWidget/normalizeLgTemplate.ts
+++ Composer/packages/ui-plugins/lg/src/LgWidget/normalizeLgTemplate.ts
@@ -6,12 +6,7 @@ import { Template } from 'botbuilder-lg';
 export function normalizeLgTemplate(template: Template): string {
   const templateTexts = template.body.split('\n').map(line => (line.startsWith('-') ? line.substring(1) : line));
   let showText = '';
-
-  if (templateTexts[0] && templateTexts[0].trim() === '[Activity') {
-    // TODO: validate enough use cases
-    showText = templateTexts;
-  } else {
-    showText = templateTexts.join('\n');
-  }
+  // TODO: validate enough use cases
+  showText = templateTexts.join('\n');
   return showText;
 }
