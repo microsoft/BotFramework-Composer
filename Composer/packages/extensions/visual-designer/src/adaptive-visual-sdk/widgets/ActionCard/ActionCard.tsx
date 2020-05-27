3976d7e7173a587f0e61ac4e851694ee6448e57d
zeye <zeye@microsoft.com>
2020-05-27 17:56:46 +0800

    move 'CardTemplate' under 'ActionCard'

--- Composer/packages/extensions/visual-designer/src/adaptive-visual-sdk/widgets/ActionCard/ActionCard.tsx
+++ Composer/packages/extensions/visual-designer/src/adaptive-visual-sdk/widgets/ActionCard/ActionCard.tsx
@@ -4,9 +4,9 @@
 import React, { ReactNode } from 'react';
 import { WidgetContainerProps, WidgetComponent } from '@bfc/extension';
 
-import { CardTemplate } from '../../components/nodes/templates/CardTemplate';
+import { ActionHeader } from '../ActionHeader';
 
-import { ActionHeader } from './ActionHeader';
+import { CardTemplate } from './CardTemplate';
 
 export interface ActionCardProps extends WidgetContainerProps {
   header?: ReactNode;
