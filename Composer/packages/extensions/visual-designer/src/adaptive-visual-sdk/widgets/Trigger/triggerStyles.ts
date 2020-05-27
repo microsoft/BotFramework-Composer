adb5882554788335db29724fcf46fcdcf0164071
zeye <zeye@microsoft.com>
2020-05-27 17:59:13 +0800

    move Trigger assets under 'widgets/Trigger'

--- Composer/packages/extensions/visual-designer/src/adaptive-visual-sdk/widgets/Trigger/triggerStyles.ts
+++ Composer/packages/extensions/visual-designer/src/adaptive-visual-sdk/widgets/Trigger/triggerStyles.ts
@@ -1,8 +1,8 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License
 
-import { TriggerSize } from '../../adaptive-visual-sdk/constants/ElementSizes';
-import { ObiColors } from '../../adaptive-visual-sdk/constants/ElementColors';
+import { TriggerSize } from '../../constants/ElementSizes';
+import { ObiColors } from '../../constants/ElementColors';
 
 export const triggerContainerStyle: any = {
   ...TriggerSize,
