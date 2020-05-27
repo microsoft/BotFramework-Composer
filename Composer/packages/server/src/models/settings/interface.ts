1ede6df6ed690c9a0df45624383132b21271dc12
leileizhang <leilzh@microsoft.com>
2020-05-28 01:28:32 +0800

    refactor: clean up the 'slot' abstraction for abs-h (#3193)
    * clean setting slot related code
    
    * remove abshost flag
    
    Co-authored-by: Dong Lei <donglei@microsoft.com>
    Co-authored-by: Chris Whitten <christopher.whitten@microsoft.com>

--- Composer/packages/server/src/models/settings/interface.ts
+++ Composer/packages/server/src/models/settings/interface.ts
@@ -4,7 +4,7 @@
 export const OBFUSCATED_VALUE = '*****';
 
 export interface ISettingManager {
-  get(slot?: string, obfuscate?: boolean): Promise<any | null>;
-  set(slot: string, settings: any): Promise<void>;
+  get(obfuscate?: boolean): Promise<any | null>;
+  set(settings: any): Promise<void>;
   getFileName: () => string;
 }
