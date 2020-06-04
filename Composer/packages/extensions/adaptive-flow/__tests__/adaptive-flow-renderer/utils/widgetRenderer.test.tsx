301452081d09aca0501884ea91d36dfdae5b347b
zeye <zeye@microsoft.com>
2020-06-04 23:02:16 +0800

    enrich test: widgetRenderer

--- Composer/packages/extensions/adaptive-flow/__tests__/adaptive-flow-renderer/utils/widgetRenderer.test.tsx
+++ Composer/packages/extensions/adaptive-flow/__tests__/adaptive-flow-renderer/utils/widgetRenderer.test.tsx
@@ -3,26 +3,47 @@
 
 import React from 'react';
 import { render } from '@bfc/test-utils';
-import { WidgetComponent } from '@bfc/extension';
+import { WidgetComponent, FlowEditorWidgetMap } from '@bfc/extension';
 
-import { renderUIWidget } from '../../../src/adaptive-flow-renderer/utils/visual/widgetRenderer';
+import { renderUIWidget, UIWidgetContext } from '../../../src/adaptive-flow-renderer/utils/visual/widgetRenderer';
+import { FlowWidget } from '../../../src/adaptive-flow-renderer/types/flowRenderer.types';
+
+const renderWidget = (schema: FlowWidget, widgetsMap: FlowEditorWidgetMap, context: UIWidgetContext) => {
+  const TestResult = () => renderUIWidget(schema, widgetsMap, context);
+  return render(<TestResult />);
+};
+
+const TestWidget: WidgetComponent<any> = ({ id, testField }) => <div data-testid="test-widget">{testField}</div>;
 
 describe('renderUIWidget', () => {
-  it('can render with given schema', () => {
-    const TestWidget: WidgetComponent<any> = ({ id, testField }) => <div data-testid="test-widget">{testField}</div>;
+  it('can render with simple schema', () => {
     const schema = {
       widget: 'TestWidget',
       testField: 'test',
     };
 
-    const TestResult = () =>
-      renderUIWidget(
-        schema,
-        { TestWidget },
-        { id: '', data: { $kind: 'Kind' }, onEvent: () => null, onResize: () => null }
-      );
-    const ele = render(<TestResult />);
+    const ele = renderWidget(
+      schema,
+      { TestWidget },
+      { id: '', data: { $kind: 'Kind' }, onEvent: () => null, onResize: () => null }
+    );
     expect(ele.getByTestId('test-widget')).toBeTruthy();
     expect(ele.getAllByText('test')).toHaveLength(1);
+  });
+
+  it('can consume function props to value.', () => {
+    const schema = {
+      widget: 'TestWidget',
+      testField: (data) => data.value,
+    };
+
+    const ele = renderWidget(
+      schema,
+      { TestWidget },
+      { id: '', data: { $kind: 'Kind', value: 'testValue' } as any, onEvent: () => null, onResize: () => null }
+    );
+
+    expect(ele.getByTestId('test-widget')).toBeTruthy();
+    expect(ele.getAllByText('testValue')).toHaveLength(1);
   });
 });
