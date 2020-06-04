b00fbc24ab4faea4a758fea20b3b8aac29ecf46d
Tony Anziano <toanzian@microsoft.com>
2020-06-04 15:07:50 -0700

    fix: Cleaned up handling of external links in Electron. (#3326)
    * Cleaned up handling of external links in Electron.
    
    * Addressed PR comments.

--- Composer/packages/electron-server/src/preload.js
+++ Composer/packages/electron-server/src/preload.js
@@ -1,10 +1,9 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-const { ipcRenderer, shell } = require('electron'); // eslint-disable-line
+const { ipcRenderer } = require('electron'); // eslint-disable-line
 
 // expose ipcRenderer to the browser
 window.ipcRenderer = ipcRenderer;
-window.openExternal = shell.openExternal;
 // flag to distinguish electron client from web app client
 window.__IS_ELECTRON__ = true;
