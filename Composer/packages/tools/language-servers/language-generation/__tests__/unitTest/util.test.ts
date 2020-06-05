29da475aae10ea075948c8a20b76bac1098dd868
zhixzhan <zhixzhan@microsoft.com>
2020-06-05 16:32:51 +0800

    add test

--- Composer/packages/tools/language-servers/language-generation/__tests__/unitTest/util.test.ts
+++ Composer/packages/tools/language-servers/language-generation/__tests__/unitTest/util.test.ts
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-import { textFromTemplate, checkTemplate } from '../../src/utils';
+import { textFromTemplate, checkTemplate, updateTemplate } from '../../src/utils';
 
 describe('LG LSP server util function test', () => {
   it('text from template', function () {
@@ -29,5 +29,14 @@ describe('LG LSP server util function test', () => {
     };
     const diags = checkTemplate(template);
     expect(diags).toHaveLength(1);
+  });
+
+  it('update template', function () {
+    const content = `
+# Greeting
+- hello
+    `;
+    const newContent = updateTemplate(content, 'Greeting', '- hi');
+    expect(newContent).toContain('- hi');
   });
 });
