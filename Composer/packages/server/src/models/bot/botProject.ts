ede909fddd7698254c6e3b569fda7e24281a1e84
Wenyi Luo <wenyluo@microsoft.com>
2020-06-10 15:25:15 +0800

    move plugins folder into packages

--- Composer/packages/server/src/models/bot/botProject.ts
+++ Composer/packages/server/src/models/bot/botProject.ts
@@ -346,7 +346,7 @@ export class BotProject {
   }
 
   private async removeLocalRuntimeData(projectId) {
-    const method = 'localpublish';
+    const method = '@bfc/plugin-localpublish';
     if (pluginLoader.extensions.publish[method]?.methods?.stopBot) {
       const pluginMethod = pluginLoader.extensions.publish[method].methods.stopBot;
       if (typeof pluginMethod === 'function') {
