ff55604a391b7beaa32d6a8a5f973eabcdbbe958
zeye <zeye@microsoft.com>
2020-06-16 20:51:34 +0800

    shallow copy json in IndexedNode to support immutable

--- Composer/packages/extensions/adaptive-flow/src/adaptive-flow-renderer/models/IndexedNode.ts
+++ Composer/packages/extensions/adaptive-flow/src/adaptive-flow-renderer/models/IndexedNode.ts
@@ -1,11 +1,15 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+import clone from 'lodash/clone';
+
 export class IndexedNode {
   id: string;
   json: any;
   constructor(id, payload) {
     this.id = id;
-    this.json = payload;
+    // Adaptive Flow lib leverages origin json to store necessary UI states,
+    // thus shallow clone input json here to support immutable case.
+    this.json = clone(payload);
   }
 }
