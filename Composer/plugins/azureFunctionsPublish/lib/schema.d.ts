de8ba7654ce3b3149202824faa2e210662b0463c
Ben Yackley <61990921+beyackle@users.noreply.github.com>
2020-06-17 14:00:32 -0700

    re-remove plugins directory (moved in another commit)

--- Composer/plugins/azureFunctionsPublish/lib/schema.d.ts
+++
@@ -1,3 +0,0 @@
-import { JSONSchema7 } from '@bfc/plugin-loader';
-declare const schema: JSONSchema7;
-export default schema;
--- Composer/plugins/localPublish/lib/index.d.ts
+++
@@ -1,3 +0,0 @@
-import { ComposerPluginRegistration } from '@bfc/plugin-loader';
-declare const _default: (composer: ComposerPluginRegistration) => Promise<void>;
-export default _default;
