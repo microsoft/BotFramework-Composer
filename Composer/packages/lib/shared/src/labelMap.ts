07cbcc4474f72c6bad1372f76ea578687e5b21a9
Yan Liu <32497439+zxyanliu@users.noreply.github.com>
2020-06-24 00:31:43 -0700

    fix: typo (#3474)
    * Fix a typo
    
    Message recieved -> Message received
    
    * Improve language
    
    A basic brush of the Home page.

--- Composer/packages/lib/shared/src/labelMap.ts
+++ Composer/packages/lib/shared/src/labelMap.ts
@@ -175,7 +175,7 @@ export const ConceptLabels: { [key in ConceptLabelKey]?: LabelOverride } = {
   },
   [SDKKinds.OnMessageActivity]: {
     title: formatMessage('Message received'),
-    subtitle: formatMessage('Message recieved activity'),
+    subtitle: formatMessage('Message received activity'),
   },
   [SDKKinds.OnMessageDeleteActivity]: {
     title: formatMessage('Message deleted'),
