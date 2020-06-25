e54ab3aeda23d325d4bee1dd7ed266326f1a0ca9
liweitian <liweitian93@outlook.com>
2020-06-25 16:15:00 +0800

    handle comments

--- Composer/packages/server/src/services/storage.ts
+++ Composer/packages/server/src/services/storage.ts
@@ -129,26 +129,18 @@ class StorageService {
   };
 
   public createFolder = (path: string) => {
-    try {
-      if (!fs.existsSync(path)) {
-        fs.mkdirSync(path);
-      }
-    } catch (e) {
-      throw new Error(e);
+    if (!fs.existsSync(path)) {
+      fs.mkdirSync(path);
     }
   };
 
   public updateFolder = (path: string, oldName: string, newName: string) => {
     const currentPath = Path.join(path, oldName);
     const newPath = Path.join(path, newName);
-    try {
-      if (fs.existsSync(currentPath)) {
-        fs.renameSync(currentPath, newPath);
-      } else {
-        throw new Error(`The folder ${currentPath} does not exist`);
-      }
-    } catch (e) {
-      throw new Error(e);
+    if (fs.existsSync(currentPath)) {
+      fs.renameSync(currentPath, newPath);
+    } else {
+      throw new Error(`The folder ${currentPath} does not exist`);
     }
   };
 
