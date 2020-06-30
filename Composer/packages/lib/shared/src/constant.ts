1f750ff84c7f731a588592727fa834e24fd5aee2
Long Alan <julong@microsoft.com>
2020-06-30 15:29:44 +0800

    test build

--- Composer/packages/lib/shared/src/constant.ts
+++ Composer/packages/lib/shared/src/constant.ts
@@ -1,7 +1,12 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-export const SensitiveProperties = ['MicrosoftAppPassword', 'luis.authoringKey', 'luis.endpointKey'];
+export const SensitiveProperties = [
+  'MicrosoftAppPassword',
+  'luis.authoringKey',
+  'luis.endpointKey',
+  'qna.subscriptKey',
+];
 export const FieldNames = {
   Events: 'triggers',
   Actions: 'actions',
