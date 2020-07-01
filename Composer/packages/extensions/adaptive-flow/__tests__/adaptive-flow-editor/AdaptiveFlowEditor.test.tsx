234584bd0fe94a4371a351d80bb50b6319597004
zeye <zeye@microsoft.com>
2020-07-01 15:52:16 +0800

    + UT: useEditorEventApi

--- Composer/packages/extensions/adaptive-flow/__tests__/adaptive-flow-editor/AdaptiveFlowEditor.test.tsx
+++ Composer/packages/extensions/adaptive-flow/__tests__/adaptive-flow-editor/AdaptiveFlowEditor.test.tsx
@@ -7,45 +7,14 @@ import { ExtensionContext } from '@bfc/extension';
 
 import AdaptiveFlowEditor from '../../src/adaptive-flow-editor/AdaptiveFlowEditor';
 
-describe('<VisualDesigner/>', () => {
-  const fn = () => ({} as any);
-  const fnList = () => [] as any[];
-  const fnPromise = () => Promise.resolve({} as any);
+import { ShellApiStub } from './stubs/ShellApiStub';
 
+describe('<VisualDesigner/>', () => {
   it('can render.', () => {
     const visualDesigner = render(
       <ExtensionContext.Provider
         value={{
-          shellApi: {
-            getDialog: fn,
-            saveDialog: fn,
-            saveData: fn,
-            navTo: fn,
-            onFocusSteps: fn,
-            onFocusEvent: fn,
-            onSelect: fn,
-            getLgTemplates: fnList,
-            copyLgTemplate: fnPromise,
-            addLgTemplate: fnPromise,
-            updateLgTemplate: fnPromise,
-            removeLgTemplate: fnPromise,
-            removeLgTemplates: fnPromise,
-            getLuIntent: fn,
-            getLuIntents: fnList,
-            addLuIntent: fnPromise,
-            updateLuIntent: fnPromise,
-            removeLuIntent: fn,
-            updateRegExIntent: fn,
-            createDialog: fnPromise,
-            addCoachMarkRef: fn,
-            onCopy: fn,
-            undo: fn,
-            redo: fn,
-            updateUserSettings: fn,
-            addSkillDialog: fnPromise,
-            announce: fn,
-            displayManifestModal: fn,
-          },
+          shellApi: ShellApiStub,
           shellData: {} as any,
           plugins: [],
         }}
