286608cb259e0682887a9079bfe6d8a4ccc5fa5c
zeye <zeye@microsoft.com>
2020-07-01 17:07:45 +0800

    KeyboardZone behavioror test

--- Composer/packages/extensions/adaptive-flow/__tests__/adaptive-flow-editor/components/KeyboardZone.test.tsx
+++ Composer/packages/extensions/adaptive-flow/__tests__/adaptive-flow-editor/components/KeyboardZone.test.tsx
@@ -2,7 +2,7 @@
 // Licensed under the MIT License.
 
 import React from 'react';
-import { render } from '@bfc/test-utils';
+import { render, fireEvent } from '@bfc/test-utils';
 
 import { KeyboardZone } from '../../../src/adaptive-flow-editor/components/KeyboardZone';
 
@@ -15,5 +15,25 @@ describe('KeyboardZone', () => {
     );
     expect(zone).toBeTruthy();
     expect(zone.getByTestId('zone-child')).toBeTruthy();
+  });
+
+  it('can trigger onCommand.', () => {
+    const mockOnCommand = jest.fn();
+    const zone = render(
+      <KeyboardZone onCommand={mockOnCommand}>
+        <span data-testid="zone-child">children</span>
+      </KeyboardZone>
+    ).getByTestId('keyboard-zone');
+
+    fireEvent.focus(zone);
+    fireEvent.keyDown(
+      zone,
+      new KeyboardEvent('keydown', {
+        key: 'C',
+        code: 'C',
+        ctrlKey: true,
+      })
+    );
+    expect(mockOnCommand).toHaveBeenCalled();
   });
 });
