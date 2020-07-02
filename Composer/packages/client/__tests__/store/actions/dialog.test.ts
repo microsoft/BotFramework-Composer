b1802cee97969321e675b07c959804c64c40f073
Ben Yackley <61990921+beyackle@users.noreply.github.com>
2020-07-02 15:28:02 -0700

    add more dialog tests

--- Composer/packages/client/__tests__/store/actions/dialog.test.ts
+++ Composer/packages/client/__tests__/store/actions/dialog.test.ts
@@ -1,10 +1,18 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-import { removeDialog, updateDialogBase, createDialogBegin } from '../../../src/store/action/dialog';
+import {
+  removeDialog,
+  updateDialogBase,
+  createDialogBegin,
+  createDialog,
+  createDialogCancel,
+} from '../../../src/store/action/dialog';
 import { ActionTypes } from '../../../src/constants';
 
 import { runMinorTests } from './testUtils';
+
+jest.useFakeTimers();
 
 const ACTION_TESTS = [
   { action: removeDialog, actionType: ActionTypes.REMOVE_DIALOG, payload: {}, output: { id: {} } },
@@ -18,3 +26,36 @@ const ACTION_TESTS = [
 ];
 
 describe('simple actions', () => runMinorTests(ACTION_TESTS));
+
+describe('the createDialog actions', () => {
+  const mockStore = {
+    getState: jest.fn(),
+    dispatch: jest.fn(),
+  };
+  const dialogCompleteCallback = jest.fn();
+
+  mockStore.getState.mockReturnValue({ onCreateDialogComplete: dialogCompleteCallback });
+
+  it('dispatches createDialog', async () => {
+    const id = '1234';
+    const payload = { id, content: 'content' };
+    await createDialog(mockStore, payload);
+
+    expect(mockStore.dispatch).toHaveBeenCalledWith({
+      type: ActionTypes.CREATE_DIALOG,
+      payload,
+    });
+    jest.runAllTimers();
+    expect(dialogCompleteCallback).toHaveBeenCalledWith(id);
+  });
+
+  it('dispatches createDialogCancel', () => {
+    createDialogCancel(mockStore);
+
+    expect(mockStore.dispatch).toHaveBeenCalledWith({
+      type: ActionTypes.CREATE_DIALOG_CANCEL,
+    });
+    jest.runAllTimers();
+    expect(dialogCompleteCallback).toHaveBeenCalledWith(null);
+  });
+});
