9f7de4c5b04ce1a9806ed182930c96aad6ce0342
leilzh <leilzh@microsoft.com>
2020-07-07 02:01:02 +0800

    add test for file persistence

---
+++ Composer/packages/client/__tests__/recoilModel/persistence/FilePersistence.test.ts
@@ -0,0 +1,114 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+import { DialogInfo, LgFile, LuFile } from '@bfc/shared';
+
+import { BotAssets } from './../../../src/recoilModel/types';
+import filePersistence from './../../../src/recoilModel/persistence/FilePersistence';
+
+jest.mock('axios', () => {
+  return {
+    create: jest.fn(() => {
+      return {
+        put: new Promise((resolve) => setTimeout(() => resolve({ data: {} }), 10)),
+        post: new Promise((resolve) => setTimeout(() => resolve({ data: {} }), 10)),
+        delete: new Promise((resolve) => setTimeout(() => resolve({ data: {} }), 10)),
+      };
+    }),
+  };
+});
+
+describe('test persistence layer', () => {
+  it('test init persistence', async () => {
+    expect(filePersistence.projectId).toBe('');
+    const current = { projectId: '' } as BotAssets;
+    const previous = { projectId: '' } as BotAssets;
+    await filePersistence.notify(current, previous);
+    expect(filePersistence.projectId).toBe('');
+    current.projectId = 'test';
+    await filePersistence.notify(current, previous);
+    expect(filePersistence.projectId).toBe('test');
+  });
+
+  it('test notify update', async () => {
+    const previous = {
+      projectId: 'test',
+      dialogs: [{ id: 'a', content: { a: 'old' } }] as DialogInfo[],
+      lgFiles: [{ id: 'a.en-us', content: '' }] as LgFile[],
+      luFiles: [{ id: 'a.en-us', content: '' }] as LuFile[],
+    } as BotAssets;
+
+    const current = {
+      projectId: 'test',
+      dialogs: [{ id: 'a', content: { a: 'new' } }] as DialogInfo[],
+      lgFiles: [{ id: 'a.en-us', content: 'a.lg' }] as LgFile[],
+      luFiles: [{ id: 'a.en-us', content: 'a.lu' }] as LuFile[],
+    } as BotAssets;
+
+    filePersistence.notify(current, previous);
+    filePersistence.notify(current, previous);
+    expect(JSON.parse(filePersistence.taskQueue['a.dialog'][0].change).a).toBe('new');
+    expect(filePersistence.taskQueue['a.en-us.lg'][0].change).toBe('a.lg');
+    expect(filePersistence.taskQueue['a.en-us.lu'][0].change).toBe('a.lu');
+  });
+
+  it('test notify create', async () => {
+    const previous = {
+      projectId: 'test',
+      dialogs: [{ id: 'a', content: { a: 'a' } }] as DialogInfo[],
+      lgFiles: [{ id: 'a.en-us', content: 'a' }] as LgFile[],
+      luFiles: [{ id: 'a.en-us', content: 'a' }] as LuFile[],
+    } as BotAssets;
+
+    const current = {
+      projectId: 'test',
+      dialogs: [
+        { id: 'a', content: { a: 'a' } },
+        { id: 'b', content: { b: 'b' } },
+      ] as DialogInfo[],
+      lgFiles: [
+        { id: 'a.en-us', content: 'a' },
+        { id: 'b.en-us', content: 'b.lg' },
+      ] as LgFile[],
+      luFiles: [
+        { id: 'a.en-us', content: 'a' },
+        { id: 'b.en-us', content: 'b.lu' },
+      ] as LuFile[],
+    } as BotAssets;
+
+    filePersistence.notify(current, previous);
+    filePersistence.notify(current, previous);
+    expect(JSON.parse(filePersistence.taskQueue['b.dialog'][0].change).b).toBe('b');
+    expect(filePersistence.taskQueue['b.en-us.lg'][0].change).toBe('b.lg');
+    expect(filePersistence.taskQueue['b.en-us.lu'][0].change).toBe('b.lu');
+  });
+
+  it('test notify remove', async () => {
+    const previous = {
+      projectId: 'test',
+      dialogs: [
+        { id: 'a', content: { a: 'a' } },
+        { id: 'b', content: { b: 'b.pre' } },
+      ] as DialogInfo[],
+      lgFiles: [
+        { id: 'a.en-us', content: 'a' },
+        { id: 'b.en-us', content: 'b.pre.lg' },
+      ] as LgFile[],
+      luFiles: [
+        { id: 'a.en-us', content: 'a' },
+        { id: 'b.en-us', content: 'b.pre.lu' },
+      ] as LuFile[],
+    } as BotAssets;
+
+    const current = {
+      projectId: 'test',
+      dialogs: [{ id: 'a', content: { a: 'a' } }] as DialogInfo[],
+      lgFiles: [{ id: 'a.en-us', content: 'a' }] as LgFile[],
+      luFiles: [{ id: 'a.en-us', content: 'a' }] as LuFile[],
+    } as BotAssets;
+    filePersistence.notify(current, previous);
+    filePersistence.notify(current, previous);
+    expect(JSON.parse(filePersistence.taskQueue['b.dialog'][0].change).b).toBe('b.pre');
+    expect(filePersistence.taskQueue['b.en-us.lg'][0].change).toBe('b.pre.lg');
+    expect(filePersistence.taskQueue['b.en-us.lu'][0].change).toBe('b.pre.lu');
+  });
+});
