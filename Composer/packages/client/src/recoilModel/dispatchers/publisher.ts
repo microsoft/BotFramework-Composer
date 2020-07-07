0aedaaf51df57c28a46fb5e088898b754f6835ab
leilzh <leilzh@microsoft.com>
2020-07-07 22:39:35 +0800

    fix publish to profile bug

--- Composer/packages/client/src/recoilModel/dispatchers/publisher.ts
+++ Composer/packages/client/src/recoilModel/dispatchers/publisher.ts
@@ -53,6 +53,46 @@ export const publisherDispatcher = () => {
     set(publishTypesState, data);
   };
 
+  const updatePublishStatus = async (
+    { set, snapshot }: CallbackInterface,
+    projectId: string,
+    target: any,
+    data: any
+  ) => {
+    const { endpointURL, status, id } = data;
+    // the action below only applies to when a bot is being started using the "start bot" button
+    // a check should be added to this that ensures this ONLY applies to the "default" profile.
+    if (target.name === 'default' && endpointURL) {
+      set(botStatusState, BotStatus.connected);
+      set(botEndpointsState, (botEndpoints) => ({
+        ...botEndpoints,
+        [projectId]: `${endpointURL}/api/messages`,
+      }));
+    }
+
+    const publishHistory = await snapshot.getPromise(publishHistoryState);
+    const history = { ...data, target: target };
+    const historys = publishHistory[target.name];
+    let tempHistorys = historys ? [...historys] : [];
+    // if no history exists, create one with the latest status
+    // otherwise, replace the latest publish history with this one
+    if (!historys && status !== 404) {
+      tempHistorys = [history];
+    } else if (status !== 404) {
+      // make sure this status payload represents the same item as item 0 (most of the time)
+      // otherwise, prepend it to the list to indicate a NEW publish has occurred since last loading history
+      if (tempHistorys.length && tempHistorys[0].id === id) {
+        tempHistorys.splice(0, 1, history);
+      } else {
+        tempHistorys.unshift(history);
+      }
+    }
+    set(publishHistoryState, (publishHistory) => ({
+      ...publishHistory,
+      [target.name]: tempHistorys,
+    }));
+  };
+
   const getPublishTargetTypes = useRecoilCallback((callbackHelpers: CallbackInterface) => async () => {
     const { set } = callbackHelpers;
     try {
@@ -118,42 +158,9 @@ export const publisherDispatcher = () => {
     (callbackHelpers: CallbackInterface) => async (projectId: string, target: any) => {
       try {
         const response = await httpClient.get(`/publish/${projectId}/status/${target.name}`);
-        const { set, snapshot } = callbackHelpers;
-        const { endpointURL, status, id } = response.data;
-        // the action below only applies to when a bot is being started using the "start bot" button
-        // a check should be added to this that ensures this ONLY applies to the "default" profile.
-        if (target.name === 'default' && endpointURL) {
-          set(botStatusState, BotStatus.connected);
-          set(botEndpointsState, (botEndpoints) => ({
-            ...botEndpoints,
-            [projectId]: `${endpointURL}/api/messages`,
-          }));
-        }
-
-        const publishHistory = await snapshot.getPromise(publishHistoryState);
-        const history = { ...response.data, target: target };
-        const historys = publishHistory[target.name];
-        let tempHistorys = historys ? [...historys] : [];
-        // if no history exists, create one with the latest status
-        // otherwise, replace the latest publish history with this one
-        if (!historys && status !== 404) {
-          tempHistorys = [history];
-        } else if (status !== 404) {
-          // make sure this status payload represents the same item as item 0 (most of the time)
-          // otherwise, prepend it to the list to indicate a NEW publish has occurred since last loading history
-          if (tempHistorys.length && tempHistorys[0].id === id) {
-            tempHistorys.splice(0, 1, history);
-          } else {
-            tempHistorys.unshift(history);
-          }
-        }
-        set(publishHistoryState, (publishHistory) => ({
-          ...publishHistory,
-          [target.name]: tempHistorys,
-        }));
+        updatePublishStatus(callbackHelpers, projectId, target, response.data);
       } catch (err) {
-        //TODO: error
-        logMessage(callbackHelpers, err.message);
+        updatePublishStatus(callbackHelpers, projectId, target, err.response.data);
       }
     }
   );
