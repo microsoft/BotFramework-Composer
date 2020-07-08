71f2c37cad2bcfb85cff3fefd696460c083c75a3
Ben Yackley <61990921+beyackle@users.noreply.github.com>
2020-07-08 10:16:28 -0700

    fix failing tests

--- Composer/packages/client/__tests__/testUtils/react-recoil-hooks-testing-library.test.tsx
+++ Composer/packages/client/__tests__/testUtils/react-recoil-hooks-testing-library.test.tsx
@@ -3,8 +3,9 @@
 
 import React, { useContext } from 'react';
 import { atom, useRecoilState, useRecoilValue } from 'recoil';
+import { act } from '@bfc/test-utils/lib/hooks';
 
-import { act, renderRecoilHook } from './react-recoil-hooks-testing-library';
+import { renderRecoilHook } from './react-recoil-hooks-testing-library';
 
 const atomA = atom({
   key: 'setMockRecoilState__a',
