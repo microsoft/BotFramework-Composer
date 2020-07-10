015a93f721ccb27ba1c167971a9cb42d37bcbb10
leilzh <leilzh@microsoft.com>
2020-07-10 18:21:42 +0800

    add lu.test

---
+++ Composer/packages/client/src/recoilModel/dispatchers/__tests__/lu.test.tsx
@@ -0,0 +1,113 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import { useRecoilState } from 'recoil';
+import { LuIntentSection, LuFile } from '@bfc/shared';
+import { useRecoilValue } from 'recoil';
+import { act } from '@bfc/test-utils/lib/hooks';
+
+import { renderRecoilHook } from '../../../../__tests__/testUtils';
+import { luFilesState } from '../../atoms';
+import { dispatcherState } from '../../../recoilModel/DispatcherWrapper';
+import { Dispatcher } from '..';
+import { luDispatcher } from '../lu';
+
+jest.mock('../../parsers/luWorker', () => {
+  return {
+    parse: (id, content) => ({ id, content }),
+    addIntent: require('@bfc/indexers/lib/utils/luUtil').addIntent,
+    updateIntent: require('@bfc/indexers/lib/utils/luUtil').updateIntent,
+    removeIntent: require('@bfc/indexers/lib/utils/luUtil').removeIntent,
+  };
+});
+const luFiles = [
+  {
+    id: 'common.en-us',
+    content: `\r\n# Hello\r\n-hi`,
+  },
+] as LuFile[];
+
+const getLuFile = (id, content): LuFile => ({ id, content } as LuFile);
+
+const getLuIntent = (Name, Body): LuIntentSection =>
+  ({
+    Name,
+    Body,
+  } as LuIntentSection);
+
+describe('Lg dispatcher', () => {
+  let renderedComponent, dispatcher: Dispatcher;
+  beforeEach(() => {
+    const useRecoilTestHook = () => {
+      const [luFiles, setLuFiles] = useRecoilState(luFilesState);
+      const currentDispatcher = useRecoilValue(dispatcherState);
+
+      return {
+        luFiles,
+        setLuFiles,
+        currentDispatcher,
+      };
+    };
+
+    const { result } = renderRecoilHook(useRecoilTestHook, {
+      states: [{ recoilState: luFilesState, initialValue: luFiles }],
+      dispatcher: {
+        recoilState: dispatcherState,
+        initialValue: {
+          luDispatcher,
+        },
+      },
+    });
+    renderedComponent = result;
+    dispatcher = renderedComponent.current.currentDispatcher;
+  });
+
+  it('should update a lu file', async () => {
+    await act(async () => {
+      await dispatcher.updateLuFile({
+        id: 'common.en-us',
+        projectId: 'test',
+        content: `\r\n# New\r\n-new`,
+      });
+    });
+
+    expect(renderedComponent.current.luFiles[0].content).toBe(`\r\n# New\r\n-new`);
+  });
+
+  it('should update a lu intent', async () => {
+    await act(async () => {
+      await dispatcher.updateLuIntent({
+        file: getLuFile(luFiles[0].id, luFiles[0].content),
+        intentName: 'Hello',
+        projectId: 'test',
+        intent: getLuIntent('Hello', '-IntentValue'),
+      });
+    });
+
+    expect(renderedComponent.current.luFiles[0].content).toBe(`\r\n# Hello\r\n-IntentValue`);
+  });
+
+  it('should create a lu Intent', async () => {
+    await act(async () => {
+      await dispatcher.createLuIntent({
+        file: getLuFile(luFiles[0].id, luFiles[0].content),
+        intentName: 'Hello',
+        projectId: 'test',
+        intent: getLuIntent('New', '-IntentValue'),
+      });
+    });
+    expect(renderedComponent.current.luFiles[0].content).toBe(`\r\n# Hello\r\n-hi\r\n\r\n# New\r\n-IntentValue`);
+  });
+
+  it('should remove a lg template', async () => {
+    await act(async () => {
+      await dispatcher.removeLuIntent({
+        file: getLuFile(luFiles[0].id, luFiles[0].content),
+        intentName: 'Hello',
+        projectId: 'test',
+      });
+    });
+
+    expect(renderedComponent.current.luFiles[0].content).toBe(``);
+  });
+});
