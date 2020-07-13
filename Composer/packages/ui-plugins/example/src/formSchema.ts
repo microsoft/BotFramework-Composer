68b040ae3e02aca34a4f73eaf769480b64f0bd97
Andy Brown <asbrown002@gmail.com>
2020-07-13 13:33:57 -0500

    update ui plugins to new ui schema

--- Composer/packages/ui-plugins/example/src/formSchema.ts
+++
@@ -1,7 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
-import { FormUISchema } from '@bfc/extension';
-
-const formSchema: FormUISchema = {};
-
-export default formSchema;
