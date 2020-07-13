cadfe94983d6257c542fa5a1c4986098c06db424
leilzh <leilzh@microsoft.com>
2020-07-13 18:34:58 +0800

    update publish luis test

--- Composer/packages/client/__tests__/components/TestController/publish-luis-modal.test.tsx
+++ Composer/packages/client/__tests__/components/TestController/publish-luis-modal.test.tsx
@@ -1,13 +1,13 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 import * as React from 'react';
-import { fireEvent, render } from '@bfc/test-utils';
-import { RecoilRoot } from 'recoil';
+import { fireEvent } from '@bfc/test-utils';
 import { act } from '@bfc/test-utils/lib/hooks';
 
 import { PublishLuis } from '../../../src/pages/language-understanding/publish-luis-modal';
 import { projectIdState, botNameState, settingsState, dispatcherState } from '../../../src/recoilModel';
 import { renderWithRecoil } from '../../testUtils';
+jest.useFakeTimers();
 
 describe('<PublishLuis />', () => {
   it('should render the <PublishLuis />', async () => {
@@ -46,18 +46,20 @@ describe('<PublishLuis />', () => {
       fireEvent.click(publishButton);
     });
 
-    expect(setSettingsMock).toHaveBeenCalledTimes(1);
-    expect(setSettingsMock).toHaveBeenCalledWith('12345', {
-      luis: {
-        authoringEndpoint: 'testAuthoringEndpoint',
-        authoringKey: '12345',
-        authoringRegion: 'westus',
-        defaultLanguage: 'en-us',
-        endpoint: 'testEndpoint',
-        endpointKey: '12345',
-        environment: 'composer',
-        name: 'sampleBot0',
-      },
-    });
+    setTimeout(() => {
+      expect(setSettingsMock).toHaveBeenCalledTimes(1);
+      expect(setSettingsMock).toHaveBeenCalledWith('12345', {
+        luis: {
+          authoringEndpoint: 'testAuthoringEndpoint',
+          authoringKey: '12345',
+          authoringRegion: 'westus',
+          defaultLanguage: 'en-us',
+          endpoint: 'testEndpoint',
+          endpointKey: '12345',
+          environment: 'composer',
+          name: 'sampleBot0',
+        },
+      });
+    }, 10);
   });
 });
