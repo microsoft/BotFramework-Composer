31954b9ca6be99d46b4470a3d541f70a878e0283
TJ <tj.durnford@microsoft.com>
2020-07-13 14:44:03 -0600

    split into multiple ediotrs

--- Composer/packages/ui-plugins/schema-editor/src/AdaptiveDialogField.tsx
+++ Composer/packages/ui-plugins/schema-editor/src/AdaptiveDialogField.tsx
@@ -3,44 +3,57 @@
 
 /** @jsx jsx */
 import { jsx } from '@emotion/core';
-import React from 'react';
+import React, { useMemo } from 'react';
 import { JsonEditor } from '@bfc/code-editor';
 import { FieldLabel, schemaField, ObjectField } from '@bfc/adaptive-form';
 import { FieldProps, useShellApi } from '@bfc/extension';
 import formatMessage from 'format-message';
 
-const getDefaultDialogSchema = (title: string, description = '') => ({
+const getDefaultDialogSchema = (title: string) => ({
   $schema: 'https://raw.githubusercontent.com/microsoft/botframework-sdk/master/schemas/component/component.schema',
   $role: 'implements(Microsoft.IDialog)',
   title,
-  description,
   type: 'object',
   properties: {},
+  $resultValue: {
+    type: 'object',
+    properties: {},
+  },
 });
 
 export const AdaptiveDialogField: React.FC<FieldProps> = (props) => {
   const { dialogs, dialogSchemaFiles, dialogId, shellApi } = useShellApi();
   const { updateDialogSchema } = shellApi;
-  const { content: dialogContent, displayName } = dialogs.find(({ id }) => id === dialogId) || {};
-  const {
-    $designer: { description },
-  } = dialogContent;
+  const { displayName } = dialogs.find(({ id }) => id === dialogId) || {};
   const { content } = dialogSchemaFiles.find(({ id }) => id === dialogId) || {};
+
+  const value = useMemo(
+    () => (typeof content === 'object' ? content : getDefaultDialogSchema(displayName || dialogId)),
+    [content]
+  );
 
   const handleChange = (content) => {
     updateDialogSchema({ content, id: dialogId });
+  };
+
+  const handleValueChange = (properties) => {
+    handleChange({ ...value, properties });
+  };
+
+  const handleResultValueChange = (properties) => {
+    handleChange({ ...value, $resultValue: { ...value.$resultValue, properties } });
   };
 
   return (
     <React.Fragment>
       <ObjectField {...props} />
       <div css={schemaField.container(0)}>
-        <FieldLabel label={formatMessage('Dialog schema')} />
-        <JsonEditor
-          height={300}
-          value={typeof content === 'object' ? content : getDefaultDialogSchema(displayName || dialogId, description)}
-          onChange={handleChange}
-        />
+        <FieldLabel label={formatMessage('Dialog value')} />
+        <JsonEditor height={150} value={value?.properties ?? {}} onChange={handleValueChange} />
+      </div>
+      <div css={schemaField.container(0)}>
+        <FieldLabel label={formatMessage('Dialog result value')} />
+        <JsonEditor height={150} value={value?.$resultValue?.properties ?? {}} onChange={handleResultValueChange} />
       </div>
     </React.Fragment>
   );
