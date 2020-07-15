38f64eb62b0ae07a8a6a056537a59bb11986284d
leilzh <leilzh@microsoft.com>
2020-07-15 12:18:23 +0800

    update lgutils

--- Composer/packages/lib/indexers/src/lgIndexer.ts
+++ Composer/packages/lib/indexers/src/lgIndexer.ts
@@ -1,43 +1,13 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-import { Templates, TemplatesParser, Diagnostic as LGDiagnostic, ImportResolverDelegate } from 'botbuilder-lg';
-import get from 'lodash/get';
-import { LgFile, FileInfo, Diagnostic, Position, Range } from '@bfc/shared';
+import { Templates, TemplatesParser, ImportResolverDelegate } from 'botbuilder-lg';
+import { LgFile, FileInfo } from '@bfc/shared';
 
 import { getBaseName } from './utils/help';
+import { convertTemplatesToLgFile } from './utils/lgUtil';
 
 const { defaultFileResolver } = TemplatesParser;
-
-// NOTE: LGDiagnostic is defined in PascalCase which should be corrected
-function convertLGDiagnostic(d: LGDiagnostic, source: string): Diagnostic {
-  const result = new Diagnostic(d.message, source, d.severity);
-
-  const start: Position = new Position(d.range.start.line, d.range.start.character);
-  const end: Position = new Position(d.range.end.line, d.range.end.character);
-  result.range = new Range(start, end);
-
-  return result;
-}
-
-function convertTemplatesToLgFile(id = '', content: string, templates: Templates): LgFile {
-  const used = templates.toArray().map((t) => {
-    return {
-      name: t.name,
-      body: t.body,
-      parameters: t.parameters,
-      range: {
-        startLineNumber: get(t, 'sourceRange.parseTree.start.line', 0),
-        endLineNumber: get(t, 'sourceRange.parseTree.stop.line', 0),
-      },
-    };
-  });
-  const diagnostics = templates.diagnostics.map((d: LGDiagnostic) => {
-    return convertLGDiagnostic(d, id);
-  });
-
-  return { id, content, templates: used, diagnostics, options: templates.options };
-}
 
 function parse(content: string, id = '', importResolver: ImportResolverDelegate = defaultFileResolver) {
   const lgFile = Templates.parseText(content, id, importResolver);
@@ -59,6 +29,5 @@ function index(files: FileInfo[], importResolver?: ImportResolverDelegate): LgFi
 
 export const lgIndexer = {
   index,
-  convertTemplatesToLgFile,
   parse,
 };
