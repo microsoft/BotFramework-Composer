b3b294d9a97a2ba33c2e287ea6381df51a8c3de7
zidaneymar <kangqidev@gmail.com>
2020-07-16 18:30:04 +0800

    add license header

--- runtime/dotnet/core/ComposerBotPathAdapter.cs
+++ runtime/dotnet/core/ComposerBotPathAdapter.cs
@@ -1,4 +1,7 @@
 using Microsoft.Extensions.Configuration;
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 using System;
 using System.Collections.Generic;
 using System.Text;
