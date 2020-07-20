f2e9b4d4b00512a5b383a45c17cbc5762c45411f
Ben Yackley <61990921+beyackle@users.noreply.github.com>
2020-07-20 12:06:35 -0700

    add error for nameless dialog file (#3666)

--- Composer/packages/lib/indexers/src/utils/help.ts
+++ Composer/packages/lib/indexers/src/utils/help.ts
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-export function getBaseName(filename: string, sep?: string): string | any {
+export function getBaseName(filename: string, sep?: string): string {
   if (sep) return filename.substr(0, filename.lastIndexOf(sep));
   return filename.substring(0, filename.lastIndexOf('.')) || filename;
 }
