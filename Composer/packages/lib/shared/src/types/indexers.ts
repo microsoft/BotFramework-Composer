d0df00a4506bdb2ea71fd69a71b3caec9f64030f
zhixzhan <zhixzhan@microsoft.com>
2020-07-28 15:02:04 +0800

    resolve conflict

--- Composer/packages/lib/shared/src/types/indexers.ts
+++ Composer/packages/lib/shared/src/types/indexers.ts
@@ -4,6 +4,8 @@
 import { Diagnostic } from './diagnostic';
 import { IIntentTrigger } from './dialogUtils';
 
+import { DialogSetting } from './index';
+
 export enum FileExtensions {
   Dialog = '.dialog',
   Manifest = '.json',
@@ -149,45 +151,6 @@ export interface SkillManifest {
   id: string;
   path?: string;
   lastModified?: string;
-}
-
-export interface ILuisConfig {
-  name: string;
-  authoringKey: string;
-  endpointKey: string;
-  endpoint: string;
-  authoringEndpoint: string;
-  authoringRegion: string | 'westus';
-  defaultLanguage: string | 'en-us';
-  environment: string | 'composer';
-}
-
-export interface PublishTarget {
-  name: string;
-  type: string;
-  configuration: string;
-  lastPublished?: Date;
-}
-
-export interface DialogSetting {
-  MicrosoftAppId?: string;
-  MicrosoftAppPassword?: string;
-  luis: ILuisConfig;
-  publishTargets?: PublishTarget[];
-  runtime: {
-    customRuntime: boolean;
-    path: string;
-    command: string;
-  };
-  defaultLanguage: string;
-  languages: string[];
-  skill?: {
-    name: string;
-    manifestUrl: string;
-  }[];
-  botId?: string;
-  skillHostEndpoint?: string;
-  [key: string]: unknown;
 }
 
 export type BotAssets = {
