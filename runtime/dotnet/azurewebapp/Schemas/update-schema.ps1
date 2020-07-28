cb4c2ecb43c8d179e983af1df7fe9e4e1c62975a
Andy Brown <asbrown002@gmail.com>
2020-07-28 15:24:00 -0700

    update schema scripts

--- runtime/dotnet/azurewebapp/Schemas/update-schema.ps1
+++ runtime/dotnet/azurewebapp/Schemas/update-schema.ps1
@@ -2,20 +2,25 @@ param (
   [string]$runtime = "azurewebapp"
 )
 $SCHEMA_FILE="sdk.schema"
+$UISCHEMA_FILE="sdk.schema"
 $BACKUP_SCHEMA_FILE="sdk-backup.schema"
+$BACKUP_UISCHEMA_FILE="sdk-backup.schema"
 
 Write-Host "Running schema merge on $runtime runtime."
 
 Move-Item -Force -Path $SCHEMA_FILE -Destination $BACKUP_SCHEMA_FILE
+Move-Item -Force -Path $UISCHEMA_FILE -Destination $BACKUP_UISCHEMA_FILE
 
-bf dialog:merge "*.schema" "../runtime/$runtime/*.csproj" -o $SCHEMA_FILE -v
+bf dialog:merge "*.schema" "*.uischema" "../runtime/$runtime/*.csproj" -o $SCHEMA_FILE -v
 
 if (Test-Path $SCHEMA_FILE -PathType leaf)
 {
   Remove-Item -Force -Path $BACKUP_SCHEMA_FILE
+  Remove-Item -Force -Path $BACKUP_UISCHEMA_FILE
   Write-Host "Schema merged succesfully."
 }
 else
 {
+  Write-Host "Schema merge failed. Restoring previous versions."
   Move-Item -Force -Path $BACKUP_SCHEMA_FILE -Destination $SCHEMA_FILE
 }
