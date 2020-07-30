6ceb2447a8584720b877948303ae389f93e65ab0
Zhixiang Zhan <zhixzhan@microsoft.com>
2020-07-30 09:51:22 +0800

    Update Composer/packages/client/src/recoilModel/dispatchers/lg.ts
    Co-authored-by: Andy Brown <asbrown002@gmail.com>

--- Composer/packages/client/src/recoilModel/dispatchers/lg.ts
+++ Composer/packages/client/src/recoilModel/dispatchers/lg.ts
@@ -156,7 +156,7 @@ export const lgDispatcher = () => {
       const lgFiles = await snapshot.getPromise(lgFilesState);
       const lgFile = lgFiles.find((file) => file.id === id);
       if (!lgFile) {
-        throw new Error(`lg file ${id} not exist`);
+        throw new Error(formatMessage('lg file {id} does not exist.', { id }));
       }
       const updatedFile = lgUtil.updateTemplate(lgFile, templateName, template, lgFileResolver(lgFiles));
       await updateLgFileState(callbackHelpers, { id, content: updatedFile.content, updatedFile });
