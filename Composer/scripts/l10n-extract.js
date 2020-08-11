ab0184bbab38011bb47b93852b641194ab3816e1
Ben Yackley <61990921+beyackle@users.noreply.github.com>
2020-08-11 10:17:40 -0700

    clean up code and move files into own directory

--- Composer/scripts/l10n-extract.js
+++ Composer/scripts/l10n-extract.js
@@ -2,17 +2,7 @@
 // Licensed under the MIT License.
 
 const fs = require('fs');
-
-let schema;
-const fileName = process.argv[2];
-
-try {
-  // eslint-disable-next-line security/detect-non-literal-fs-filename
-  schema = JSON.parse(fs.readFileSync(fileName));
-} catch (e) {
-  console.error('No valid JSON file found');
-  process.exit(1);
-}
+const path = require('path');
 
 const L10N_FIELDS = ['label', 'description', 'title', 'subtitle'];
 
@@ -31,5 +21,26 @@ function keep(obj, keptFields) {
   return out;
 }
 
-// eslint-disable-next-line security/detect-non-literal-fs-filename
-fs.writeFileSync(fileName + '.en-US.json', JSON.stringify(keep(schema, L10N_FIELDS), null, 4));
+const inFiles = process.argv.slice(2);
+
+for (const file of inFiles) {
+  let schema;
+  const baseName = path.basename(file);
+  const outputDir = path.join(path.normalize(path.dirname(file)), 'locales', 'en-US');
+  console.log(file);
+  // eslint-disable-next-line security/detect-non-literal-fs-filename
+  fs.mkdirSync(outputDir, { recursive: true });
+
+  try {
+    // eslint-disable-next-line security/detect-non-literal-fs-filename
+    schema = JSON.parse(fs.readFileSync(file));
+  } catch (e) {
+    console.error('No valid JSON file found');
+    process.exit(1);
+  }
+
+  const output = keep(schema, L10N_FIELDS);
+
+  // eslint-disable-next-line security/detect-non-literal-fs-filename
+  fs.writeFileSync(outputDir + path.sep + baseName + '.en-US.json', JSON.stringify(output, null, 4));
+}
