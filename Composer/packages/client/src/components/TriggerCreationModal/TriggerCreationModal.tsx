373e453f4457b40e69aa1fe76404c4b8ec51b4d7
zeye <zeye@microsoft.com>
2020-08-11 15:38:46 +0800

    refactor: move TriggerCreationModal path

--- Composer/packages/client/src/components/TriggerCreationModal/TriggerCreationModal.tsx
+++ Composer/packages/client/src/components/TriggerCreationModal/TriggerCreationModal.tsx
@@ -436,5 +436,3 @@ export const TriggerCreationModal: React.FC<TriggerCreationModalProps> = (props)
     </Dialog>
   );
 };
-
-export default TriggerCreationModal;
---
+++ Composer/packages/client/src/components/TriggerCreationModal/index.tsx
@@ -0,0 +1,5 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import { TriggerCreationModal } from './TriggerCreationModal';
+export default TriggerCreationModal;
