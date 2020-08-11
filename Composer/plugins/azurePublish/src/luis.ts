692a711141499b067f78d50f7df0c5521eabeb82
Wenyi Luo <wenyluo@microsoft.com>
2020-08-11 22:40:15 +0800

    polish

--- Composer/plugins/azurePublish/src/luis.ts
+++ Composer/plugins/azurePublish/src/luis.ts
@@ -160,26 +160,6 @@ export class LuisPublish {
         Object.assign(luisAppIds, luisSettings.luis);
       }
 
-      // Create the base LUIS config object
-      // const luisConfig: any = {
-      //   endpoint: luisEndpoint,
-      //   endpointKey: luisEndpointKey,
-      //   authoringRegion: luisAuthoringRegion,
-      //   authoringKey: luisAuthoringRegion,
-      // };
-
-      // // Copy the app IDs into the base config
-      // Object.assign(luisConfig, luisAppIds);
-
-      // // Update deploymentSettings with the luis config
-      // // TODO: This should be handled by the runtime plugin - writing to appsettings.deployment.json
-      // // But in this case the change here is being written into the build folder, not "original" version
-      // const settings: any = await fs.readJson(deploymentSettingsPath);
-      // settings.luis = luisConfig;
-      // await fs.writeJson(deploymentSettingsPath, settings, {
-      //   spaces: 4,
-      // });
-
       // In order for the bot to use the LUIS models, we need to assign a LUIS key to the endpoint of each app
       // First step is to get a list of all the accounts available based on the given luisAuthoringKey.
       let accountList;
