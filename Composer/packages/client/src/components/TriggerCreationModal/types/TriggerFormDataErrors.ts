d574974bef4fe4588b866d98b7738425113484af
zeye <zeye@microsoft.com>
2020-08-12 18:26:07 +0800

    doc: add comments for TriggerFormErrors

--- Composer/packages/client/src/components/TriggerCreationModal/types/TriggerFormDataErrors.ts
+++ Composer/packages/client/src/components/TriggerCreationModal/types/TriggerFormDataErrors.ts
@@ -2,10 +2,21 @@
 // Licensed under the MIT License.
 
 export interface TriggerFormDataErrors {
+  /** Error msg of L1 dropdown 'type of Trigger'. */
   $kind?: string;
-  intent?: string;
+
+  /** Error msg of L2 dropdown 'Dialog events'. */
   event?: string;
-  triggerPhrases?: string;
-  regEx?: string;
+
+  /** Error msg of L2 dropdown 'Activities'. */
   activity?: string;
+
+  /** Error msg of [tirgger=OnIntent, recognizer=RegEx] regex value. */
+  regEx?: string;
+
+  /** Error msg of [tirgger=OnIntent, recognizer=LUIS] intent name. */
+  intent?: string;
+
+  /** Error msg of [tirgger=OnIntent, recognizer=LUIS] lu phrases. */
+  triggerPhrases?: string;
 }
