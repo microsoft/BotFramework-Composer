9765abf6886af660b465e88ab8deab7a6d6fc7d7
zeye <zeye@microsoft.com>
2020-08-12 15:20:59 +0800

    refactor: rename constants

--- Composer/packages/client/src/utils/constants.ts
+++ Composer/packages/client/src/utils/constants.ts
@@ -1,9 +1,7 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-import { SDKKinds } from '@bfc/shared';
-
-export const eventTypeKey: string = SDKKinds.OnDialogEvent;
-export const intentTypeKey: string = SDKKinds.OnIntent;
-export const activityTypeKey: string = SDKKinds.OnActivity;
-export const customEventKey = 'OnCustomEvent';
+export const eventTypeKey = 'DialogEvents';
+export const intentTypeKey = 'IntentRecognized';
+export const activityTypeKey = 'Activities';
+export const customEventKey = 'CustomEvents';
