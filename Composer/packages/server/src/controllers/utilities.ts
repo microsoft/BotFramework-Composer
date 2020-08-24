4eafc82fe03bac22a8f3de581d6c47e6eae2a5e9
liweitian <liweitian93@outlook.com>
2020-08-24 20:33:08 +0800

    code scan error

--- Composer/packages/server/src/controllers/utilities.ts
+++ Composer/packages/server/src/controllers/utilities.ts
@@ -5,6 +5,11 @@ import { Request, Response } from 'express';
 import { parseQnAContent } from '../models/utilities/parser';
 
 async function getQnaContent(req: Request, res: Response) {
+  if (!Array.isArray(req.query.urls)) {
+    res.status(400).json({
+      message: 'Invalid parameters',
+    });
+  }
   try {
     res.status(200).json(await parseQnAContent(req.query.urls));
   } catch (e) {
