576dff9cde36e7c9bbae2968606bb43bfa7dd9ef
zeye <zeye@microsoft.com>
2020-08-28 11:12:11 +0800

    feat: mixed evaluation with LG + Exp

--- Composer/packages/extensions/adaptive-flow/src/adaptive-flow-renderer/utils/visual/widgetPropEvaluator.ts
+++ Composer/packages/extensions/adaptive-flow/src/adaptive-flow-renderer/utils/visual/widgetPropEvaluator.ts
@@ -2,16 +2,45 @@
 // Licensed under the MIT License.
 
 import { Templates } from 'botbuilder-lg';
+import { ExpressionParser } from 'adaptive-expressions';
 
-const LgStringRegex = new RegExp(/\$\{.+\}/);
-export const widgetPropNeedsEvaluation = (widgetProp: string): boolean => {
-  if (typeof widgetProp !== 'string') return false;
-  return LgStringRegex.test(widgetProp);
+// Capture patterns that are strictly '${EXPRESSION}'
+//   - '${action.condition}'
+//   - '${if(action.value, concat(action.value, "(Event)"))}'
+const ExpressionPattern = new RegExp(/^\$\{(.+)\}$/);
+
+// Catpure patterns that include '...${...}...'
+const LgTemplatePattern = new RegExp(/\$\{.+\}/);
+
+export const evaluateWidgetProp = (propValue: string, context: any): string => {
+  if (typeof propValue !== 'string') return propValue;
+
+  if (ExpressionPattern.test(propValue)) {
+    const matchResult = ExpressionPattern.exec(propValue);
+    const expString = matchResult ? matchResult[1] : '';
+    return evaluateAsExpression(expString, context);
+  }
+
+  if (LgTemplatePattern.test(propValue)) {
+    return evaluateAsLGTemplate(propValue, context);
+  }
+
+  return propValue;
 };
+(window as any).evaluateWidgetProp = evaluateWidgetProp;
 
-export const evaluateAsLGTemplate = (propValue: string, scope: any) => {
+export type WidgetPropEvaluator = (propValue: string, scope: any) => string;
+
+export const evaluateAsLGTemplate: WidgetPropEvaluator = (propValue, scope) => {
   const templateId = 'widgetProp';
   const lgContent = `# ${templateId}\r\n- ${propValue}`;
   const lgResult = Templates.parseText(lgContent).evaluate(templateId, scope);
   return lgResult;
+};
+
+const expressionParser = new ExpressionParser();
+export const evaluateAsExpression: WidgetPropEvaluator = (propValue, scope) => {
+  const exp = expressionParser.parse(propValue);
+  const expResult = exp.tryEvaluate(scope);
+  return expResult.value;
 };
