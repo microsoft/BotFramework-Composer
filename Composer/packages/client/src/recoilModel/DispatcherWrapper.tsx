0044e61a3d4cf51642f91270d046a256a1917a3d
Srinaath Ravichandran <srravich@microsoft.com>
2020-08-31 11:33:34 -0700

    Saving the project inside botprojects
    Signed-off-by: Srinaath Ravichandran <srravich@microsoft.com>

--- Composer/packages/client/src/recoilModel/DispatcherWrapper.tsx
+++ Composer/packages/client/src/recoilModel/DispatcherWrapper.tsx
@@ -78,6 +78,9 @@ export const DispatcherWrapper = ({ children }) => {
 
   return (
     <Fragment>
+      {botProjects.map((projectId) => (
+        <UndoRoot key={projectId} projectId={projectId} />
+      ))}
       <InitDispatcher onLoad={setLoaded} />
       {loaded ? children : null}
     </Fragment>
