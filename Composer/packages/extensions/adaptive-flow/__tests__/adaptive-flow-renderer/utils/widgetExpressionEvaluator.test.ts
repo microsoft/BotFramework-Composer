b35585bb665837ddff39baa79eff017ff558732e
zeye <zeye@microsoft.com>
2020-09-01 16:58:48 +0800

    feat: use pure expression way in Flow schema

--- Composer/packages/extensions/adaptive-flow/__tests__/adaptive-flow-renderer/utils/widgetExpressionEvaluator.test.ts
+++ Composer/packages/extensions/adaptive-flow/__tests__/adaptive-flow-renderer/utils/widgetExpressionEvaluator.test.ts
@@ -5,64 +5,77 @@ import { evaluateWidgetExpression } from '../../../src/adaptive-flow-renderer/ut
 
 const evaluate = evaluateWidgetExpression;
 describe('evaluateWidgetExpression()', () => {
-  it('can evaluate basic value access pattern.', () => {
-    expect(evaluate('${action.x}', { action: { x: 1 } })).toEqual(1);
-
-    // Notes: Expression engine behavior. 'null' will be read as 'undefined'
-    expect(evaluate('${action.x}', { action: { x: null } })).toEqual(undefined);
-    expect(evaluate('${action.x}', { action: {} })).toEqual(undefined);
-    expect(evaluate('${action.x}', { action: { x: undefined } })).toEqual(undefined);
-    expect(evaluate('${action.x}', { action: { x: false } })).toEqual(false);
-
-    // Case-insensitive
-    expect(evaluate('${action.x}', { action: { x: 'x' } })).toEqual('x');
-    expect(evaluate('${action.x}', { action: { X: 'x' } })).toEqual('x');
+  it('return origin input when input non-expression string.', () => {
+    ['${action.x}', 'hello', 'action.x'].forEach((x) => {
+      expect(evaluate(x, {})).toEqual(x);
+    });
   });
 
-  describe('can evalueate complicated value access pattern with builtin function', () => {
-    it('foreach', () => {
-      expect(evaluate('${foreach(a.items, x=>x)}', { a: { items: [] } })).toEqual([]);
-      expect(evaluate('${foreach(a.items, x=>x)}', { a: { items: [1, 2] } })).toEqual([1, 2]);
-      expect(evaluate('${foreach(a.items, x=>x+1)}', { a: { items: [1, 2] } })).toEqual([2, 3]);
-      expect(evaluate('${foreach(a.items, x=>x)}', { a: { items: ['1'] } })).toEqual(['1']);
-      expect(evaluate('${foreach(a.items, x=>x+1)}', { a: { items: ['1'] } })).toEqual(['11']);
-      expect(evaluate('${foreach(a.items, x=>x+"1")}', { a: { items: ['1'] } })).toEqual(['11']);
-    });
+  it('can evaluate basic value access pattern.', () => {
+    expect(evaluate('=action.x', { action: { x: 1 } })).toEqual(1);
 
-    it('if', () => {
-      expect(evaluate('${if(a.x, "1", "2")}', { a: { x: true } })).toEqual('1');
-      expect(evaluate('${if(a.x, "1", "2")}', { a: { X: true } })).toEqual('1');
-      expect(evaluate('${if(a.x, "1", "2")}', { a: { x: '' } })).toEqual('1');
-      expect(evaluate('${if(a.x, "1", "2")}', { a: { x: 0 } })).toEqual('1');
-      expect(evaluate('${if(a.x<1, "1", "2")}', { a: { x: 0 } })).toEqual('1');
+    // Notes: Expression engine behavior. 'null' will be read as 'undefined'
+    expect(evaluate('=action.x', { action: { x: null } })).toEqual(undefined);
+    expect(evaluate('=action.x', { action: {} })).toEqual(undefined);
+    expect(evaluate('=action.x', { action: { x: undefined } })).toEqual(undefined);
+    expect(evaluate('=action.x', { action: { x: false } })).toEqual(false);
 
-      expect(evaluate('${if(a.x, "1", "2")}', { a: { x: false } })).toEqual('2');
-      expect(evaluate('${if(a.x, "1", "2")}', { a: {} })).toEqual('2');
-      expect(evaluate('${if(a.x, "1", "2")}', { a: { x: null } })).toEqual('2');
-      expect(evaluate('${if(a.x, "1", "2")}', { a: { x: undefined } })).toEqual('2');
-    });
+    // Convert to string
+    expect(evaluate('=string(action.x)', { action: { x: null } })).toEqual(undefined);
+    expect(evaluate('=string(action.x)', { action: { x: undefined } })).toEqual(undefined);
+    expect(evaluate('=string(action.x)', { action: { x: false } })).toEqual('false');
+
+    // Case-insensitive
+    expect(evaluate('=action.x', { action: { x: 'x' } })).toEqual('x');
+    expect(evaluate('=action.x', { action: { X: 'x' } })).toEqual('x');
+  });
+
+  it('can evaluate expressions with foreach', () => {
+    expect(evaluate('=foreach(a.items, x=>x)', { a: { items: [] } })).toEqual([]);
+    expect(evaluate('=foreach(a.items, x=>x)', { a: { items: [1, 2] } })).toEqual([1, 2]);
+    expect(evaluate('=foreach(a.items, x=>x+1)', { a: { items: [1, 2] } })).toEqual([2, 3]);
+    expect(evaluate('=foreach(a.items, x=>x)', { a: { items: ['1'] } })).toEqual(['1']);
+    expect(evaluate('=foreach(a.items, x=>x+1)', { a: { items: ['1'] } })).toEqual(['11']);
+    expect(evaluate('=foreach(a.items, x=>x+"1")', { a: { items: ['1'] } })).toEqual(['11']);
+  });
+
+  it('can evaluate expressions with if', () => {
+    expect(evaluate('=if(a.x, "1", "2")', { a: { x: true } })).toEqual('1');
+    expect(evaluate('=if(a.x, "1", "2")', { a: { X: true } })).toEqual('1');
+    expect(evaluate('=if(a.x, "1", "2")', { a: { x: '' } })).toEqual('1');
+    expect(evaluate('=if(a.x, "1", "2")', { a: { x: 0 } })).toEqual('1');
+    expect(evaluate('=if(a.x<1, "1", "2")', { a: { x: 0 } })).toEqual('1');
+
+    expect(evaluate('=if(a.x, "1", "2")', { a: { x: false } })).toEqual('2');
+    expect(evaluate('=if(a.x, "1", "2")', { a: {} })).toEqual('2');
+    expect(evaluate('=if(a.x, "1", "2")', { a: { x: null } })).toEqual('2');
+    expect(evaluate('=if(a.x, "1", "2")', { a: { x: undefined } })).toEqual('2');
   });
 
   it('can evaluate string template pattern.', () => {
-    expect(evaluate('${a.x} and ${a.y}', { a: { x: 'x', y: 'y' } })).toEqual('x and y');
-    expect(evaluate('${a.x} and ${a.y}', { a: { X: 'X', Y: 'Y' } })).toEqual('X and Y');
-    expect(evaluate('${a.x} and ${a.y}', { a: { x: 1 } })).toEqual('1 and null');
-    expect(evaluate('${a.x} and ${a.y}', { a: { x: { val: 1 }, y: [1] } })).toEqual('{"val":1} and [1]');
+    expect(evaluate('=concat(a.x, " and ", a.y)', { a: { x: 'x', y: 'y' } })).toEqual('x and y');
+    expect(evaluate('=concat(a.x, " and ", a.y)', { a: { X: 'X', Y: 'Y' } })).toEqual('X and Y');
+    expect(evaluate('=concat(a.x, " and ", a.y)', { a: { x: 1 } })).toEqual('1 and ');
+    expect(evaluate('=concat(string(a.x), " and ", string(a.y))', { a: { x: { val: 1 }, y: [1] } })).toEqual(
+      '{"val":1} and [1]'
+    );
   });
 
   describe('can evaluate real Flow use case', () => {
     it('in Foreach.', () => {
       expect(
-        evaluate('Each value in ${coalesce(action.itemsProperty, "?")}', { action: { itemsProperty: 'user.names' } })
+        evaluate('=concat("Each value in ", coalesce(action.itemsProperty, "?"))', {
+          action: { itemsProperty: 'user.names' },
+        })
       ).toEqual('Each value in user.names');
-      expect(evaluate('Each value in ${coalesce(action.itemsProperty, "?")}', { action: {} })).toEqual(
+      expect(evaluate('=concat("Each value in ", coalesce(action.itemsProperty, "?"))', { action: {} })).toEqual(
         'Each value in ?'
       );
     });
 
     it('in SetProperties.', () => {
       expect(
-        evaluate('${foreach(action.assignments, x => concat(x.property, " : " ,x.value))}', {
+        evaluate('=foreach(action.assignments, x => concat(x.property, " : " ,x.value))', {
           action: {
             assignments: [
               { property: 'a', value: 1 },
--- Composer/packages/extensions/adaptive-flow/src/adaptive-flow-renderer/utils/expression/widgetExpressionEvaluator.ts
+++ Composer/packages/extensions/adaptive-flow/src/adaptive-flow-renderer/utils/expression/widgetExpressionEvaluator.ts
@@ -1,45 +1,18 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-import { Templates } from 'botbuilder-lg';
-import { ExpressionParser } from 'adaptive-expressions';
+import { ValueExpression } from 'adaptive-expressions';
 
-// Capture patterns that are strictly '${EXPRESSION}'
-//   - '${action.condition}'
-//   - '${if(action.value, concat(action.value, "(Event)"))}'
-const ValueAccessPattern = new RegExp(/^\$\{([^}]+)\}$/);
-
-// Catpure patterns that include '...${...}...'
-const StringTemplatePattern = new RegExp(/\$\{.+\}/);
-
-export const evaluateWidgetExpression = (input: string, context: any): string => {
+export const evaluateWidgetExpression = (input: string, context: any): any => {
   if (typeof input !== 'string') return input;
 
-  if (ValueAccessPattern.test(input)) {
-    const matchResult = ValueAccessPattern.exec(input);
-    const expString = matchResult ? matchResult[1] : '';
-    return evaluateAsExpression(expString, context);
+  if (input.startsWith('=')) {
+    return evaluateAsValueExpression(input, context);
   }
-
-  if (StringTemplatePattern.test(input)) {
-    return evaluateAsLGTemplate(input, context);
-  }
-
   return input;
 };
 
-export type WidgetPropEvaluator = (propValue: string, scope: any) => string;
-
-export const evaluateAsLGTemplate: WidgetPropEvaluator = (propValue, scope) => {
-  const templateId = 'widgetProp';
-  const lgContent = `# ${templateId}\r\n- ${propValue}`;
-  const lgResult = Templates.parseText(lgContent).evaluate(templateId, scope);
-  return lgResult;
-};
-
-const expressionParser = new ExpressionParser();
-export const evaluateAsExpression: WidgetPropEvaluator = (propValue, scope) => {
-  const exp = expressionParser.parse(propValue);
-  const expResult = exp.tryEvaluate(scope);
+export const evaluateAsValueExpression = (propValue: string, scope: any): any => {
+  const expResult = new ValueExpression(propValue).tryGetValue(scope);
   return expResult.value;
 };
