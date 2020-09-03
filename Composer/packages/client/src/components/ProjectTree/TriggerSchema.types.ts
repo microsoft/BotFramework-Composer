c328ddd0f806b1c862d7de227bd2bb2045d25bd2
zeye <zeye@microsoft.com>
2020-09-03 15:59:56 +0800

    type: define Trigger UISchema type

---
+++ Composer/packages/client/src/components/ProjectTree/TriggerSchema.types.ts
@@ -0,0 +1,24 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import { SDKKinds } from '@bfc/shared';
+
+export type TriggerUISchema = { [$kind in SDKKinds]: TriggerOption };
+
+export interface TriggerOption {
+  /** Displayed option name. e.g. "Intent recognized" */
+  label: string;
+
+  /** Describes parent path(s) to reach this trigger option.
+   *    1. Submenu[] - A list of Submenu needs to go through from root to this option
+   *    2. Submenu - Equivalent to a Submenu[] with 1 element
+   *    3. string - Equivalent to a Submenu whose 'label' fields equals the string
+   *    2. false - Trigger is at root level
+   */
+  submenu: Submenu[] | Submenu | string | false;
+}
+
+interface Submenu {
+  label: string;
+  prompt?: string;
+}
