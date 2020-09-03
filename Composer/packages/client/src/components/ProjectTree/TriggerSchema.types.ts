3425af9f0d1ba15cc000139864507616925c8b5f
zeye <zeye@microsoft.com>
2020-09-03 16:03:00 +0800

    fix: typo

--- Composer/packages/client/src/components/ProjectTree/TriggerSchema.types.ts
+++ Composer/packages/client/src/components/ProjectTree/TriggerSchema.types.ts
@@ -12,7 +12,7 @@ export interface TriggerOption {
   /** Describes parent path(s) to reach this trigger option.
    *    1. Submenu[] - A list of Submenu needs to go through from root to this option
    *    2. Submenu - Equivalent to a Submenu[] with 1 element
-   *    3. string - Equivalent to a Submenu whose 'label' fields equals the string
+   *    3. string - Equivalent to a Submenu whose 'label' field equals to the string
    *    2. false - Trigger is at root level
    */
   submenu: Submenu[] | Submenu | string | false;
