8d4a9f613a8361381ff6b756f70f7387771b1bd1
Patrick Volum <pavolum@microsoft.com>
2020-09-15 09:47:11 -0700

    Adding va core template plug in with corresponding plugin build steps

---
+++ Composer/plugins/vacore/src/index.ts
@@ -0,0 +1,24 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+import path from 'path';
+import { promisify } from 'util';
+
+import rimraf from 'rimraf';
+import * as fs from 'fs-extra';
+
+import { copyDir } from './copyDir';
+import { IFileStorage } from './interface';
+
+const exec = promisify(require('child_process').exec);
+
+const removeDirAndFiles = promisify(rimraf);
+
+export default async (composer: any): Promise<void> => {
+  // register the base template which will appear in the NEw Bot modal
+  composer.addBotTemplate({
+    id: 'va-core',
+    name: 'VA Core',
+    description: 'The core of your new VA - ready to run, just add skills!',
+    path: path.resolve(__dirname, '../template'),
+  });
+};
