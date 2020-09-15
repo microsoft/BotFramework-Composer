bd783863e9f4784ecd8245962352204187f5c248
Patrick Volum <pavolum@microsoft.com>
2020-09-15 10:27:01 -0700

    Adding env variable feature flag around VA=Core template

--- Composer/plugins/vacore/src/index.ts
+++ Composer/plugins/vacore/src/index.ts
@@ -14,11 +14,13 @@ const exec = promisify(require('child_process').exec);
 const removeDirAndFiles = promisify(rimraf);
 
 export default async (composer: any): Promise<void> => {
-  // register the base template which will appear in the NEw Bot modal
-  composer.addBotTemplate({
-    id: 'va-core',
-    name: 'VA Core',
-    description: 'The core of your new VA - ready to run, just add skills!',
-    path: path.resolve(__dirname, '../template'),
-  });
+  if (process.env.VA_CREATION) {
+    // register the base template which will appear in the NEw Bot modal
+    composer.addBotTemplate({
+      id: 'va-core',
+      name: 'VA Core',
+      description: 'The core of your new VA - ready to run, just add skills!',
+      path: path.resolve(__dirname, '../template'),
+    });
+  }
 };
