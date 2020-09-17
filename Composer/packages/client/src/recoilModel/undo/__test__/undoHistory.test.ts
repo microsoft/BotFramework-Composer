1983e5d7fb60a870bbad4f52572baf356f119d77
Srinaath Ravichandran <srravich@microsoft.com>
2020-09-17 09:00:19 -0700

    PR feedback
    Remove fit, it on tests
    Localization reverts
    
    Signed-off-by: Srinaath Ravichandran <srravich@microsoft.com>

--- Composer/packages/client/src/recoilModel/undo/__test__/undoHistory.test.ts
+++ Composer/packages/client/src/recoilModel/undo/__test__/undoHistory.test.ts
@@ -1,5 +1,5 @@
-// // Copyright (c) Microsoft Corporation.
-// // Licensed under the MIT License.
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
 
 import undoHistoryImpl from '../undoHistory';
 
