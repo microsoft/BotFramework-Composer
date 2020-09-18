b4bb07f7bc4c0cbbb8db2edfcea1f5a932f56d5d
Ben Yackley <61990921+beyackle@users.noreply.github.com>
2020-09-18 13:58:36 -0700

    add TSV creation to l10n scripts

---
+++ Composer/scripts/l10n-makeTSV.js
@@ -0,0 +1,22 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+const fs = require('fs');
+const path = require('path');
+
+const inPath = process.argv[2];
+
+// eslint-disable-next-line security/detect-non-literal-fs-filename
+const inJson = JSON.parse(fs.readFileSync(inPath));
+const outPath = path.join(path.dirname(inPath), 'en-US.tsv');
+
+// eslint-disable-next-line security/detect-non-literal-fs-filename
+const outTSV = fs.openSync(outPath, 'w');
+fs.writeSync(outTSV, 'Key,Message\n');
+
+for (const key in inJson) {
+  fs.writeSync(outTSV, key + '\t' + inJson[key].message.replace(/(\r\n|\r|\n)/g, '\\n') + '\n');
+}
+
+fs.closeSync(outTSV);
