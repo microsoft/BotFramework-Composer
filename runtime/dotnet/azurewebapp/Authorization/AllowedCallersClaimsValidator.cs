3553038bdc55f6a419c16c81ab679f9837bc32cc
Christian Glessner <chris@leitwolf.io>
2020-09-23 17:42:31 +0200

    Skill authorization: little improvements; tests; copyright

--- runtime/dotnet/azurewebapp/Authorization/AllowedCallersClaimsValidator.cs
+++ runtime/dotnet/azurewebapp/Authorization/AllowedCallersClaimsValidator.cs
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 using System;
 using System.Collections.Generic;
 using System.Security.Claims;
@@ -10,21 +13,18 @@ namespace Microsoft.BotFramework.Composer.WebAppTemplates.Authorization
 {
     public class AllowedCallersClaimsValidator : ClaimsValidator
     {
-        private readonly BotSkillSettings _settings;
         private readonly List<string> _allowedCallers;
 
         public AllowedCallersClaimsValidator(BotSkillSettings settings)
         {
-            _settings = settings;
-
             // skill.allowedCallers is the setting in the appsettings.json file
             // that consists of the list of parent bot IDs that are allowed to access the skill.
             // To add a new parent bot, simply edit the AllowedCallers and add
             // the parent bot's Microsoft app ID to the list.
             // In this sample, we allow all callers if AllowedCallers contains an "*".
-            if (_settings == null || _settings.AllowedCallers == null || _settings.AllowedCallers.Length == 0)
+            if (settings?.AllowedCallers == null || settings.AllowedCallers.Length == 0)
             {
-                throw new ArgumentNullException("skill.allowedCallers has to be defined, e.g. ['*'] or ['callerAppId']");
+                throw new ArgumentException($"skill.allowedCallers has to be defined, e.g. ['*'] or ['callerAppId']");
             }
 
             _allowedCallers = new List<string>(settings.AllowedCallers);
@@ -40,7 +40,8 @@ namespace Microsoft.BotFramework.Composer.WebAppTemplates.Authorization
                 var appId = JwtTokenValidation.GetAppIdFromClaims(claims);
                 if (!_allowedCallers.Contains(appId))
                 {
-                    throw new UnauthorizedAccessException($"Received a request from a bot with an app ID of \"{appId}\". To enable requests from this caller, add the app ID to your configuration file.");
+                    throw new UnauthorizedAccessException(
+                        $"Received a request from a bot with an app ID of \"{appId}\". To enable requests from this caller, add the app ID to your configuration file.");
                 }
             }
 
