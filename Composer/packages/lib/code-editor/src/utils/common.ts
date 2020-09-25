f53d86dff3c1f53a8b40c142fcc2c2e2498f8443
zhixzhan <zhixzhan@microsoft.com>
2020-09-25 17:33:12 +0800

    disable ctrl + click open window in electron

--- Composer/packages/lib/code-editor/src/utils/common.ts
+++ Composer/packages/lib/code-editor/src/utils/common.ts
@@ -4,3 +4,8 @@
 export function processSize(size) {
   return !/^\d+$/.test(size) ? size : `${size}px`;
 }
+
+export function isElectron(): boolean {
+  // eslint-disable-next-line no-prototype-builtins
+  return window.hasOwnProperty('__IS_ELECTRON__');
+}
