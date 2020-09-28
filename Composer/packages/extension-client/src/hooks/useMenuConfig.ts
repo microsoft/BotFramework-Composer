3ae77d1fa27ebf6ad43c37e604f56db150068680
Ze Ye <zeye@microsoft.com>
2020-09-28 13:49:27 +0800

    filter $kinds not in sdk.schema from menu config

--- Composer/packages/extension-client/src/hooks/useMenuConfig.ts
+++ Composer/packages/extension-client/src/hooks/useMenuConfig.ts
@@ -8,7 +8,20 @@ import { EditorExtensionContext } from '../EditorExtensionContext';
 import { MenuUISchema } from '../types';
 
 export function useMenuConfig(): MenuUISchema {
-  const { plugins } = useContext(EditorExtensionContext);
+  const { plugins, shellData } = useContext(EditorExtensionContext);
+  const uiSchema = plugins.uiSchema || {};
+  const sdkSchema = shellData.schemas?.sdk;
+  const sdkDefinitions = sdkSchema?.content?.definitions || {};
 
-  return useMemo(() => mapValues(plugins.uiSchema, 'menu'), [plugins.uiSchema]);
+  return useMemo(() => {
+    const menuSchema = mapValues(uiSchema, 'menu') as MenuUISchema;
+    const implementedMenuSchema = Object.entries(menuSchema).reduce((result, [$kind, menuOpt]) => {
+      // Filter out those $kinds implemented in both sdk.schema and uischema
+      if (sdkDefinitions[$kind] && menuOpt) {
+        result[$kind] = menuOpt;
+      }
+      return result;
+    }, {} as MenuUISchema);
+    return implementedMenuSchema;
+  }, [plugins.uiSchema, sdkSchema]);
 }
