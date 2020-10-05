7c0aa1fb5b3bff2a5713a9bc1893101438206ca1
Ben Yackley <61990921+beyackle@users.noreply.github.com>
2020-10-05 12:44:00 -0700

    post-merge fixes pt. 1

--- Composer/packages/client/src/router.tsx
+++ Composer/packages/client/src/router.tsx
@@ -12,13 +12,7 @@ import { resolveToBasePath } from './utils/fileUtil';
 import { data } from './styles';
 import { NotFound } from './components/NotFound';
 import { BASEPATH } from './constants';
-import {
-  dispatcherState,
-  schemasState,
-  botProjectIdsState,
-  botProjectSpaceLoadedState,
-  botOpeningState,
-} from './recoilModel';
+import { dispatcherState, schemasState, botProjectIdsState, botOpeningState } from './recoilModel';
 import { openAlertModal } from './components/Modal/AlertDialog';
 import { dialogStyle } from './components/Modal/dialogStyle';
 import { LoadingSpinner } from './components/LoadingSpinner';
