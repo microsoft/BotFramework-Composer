d1bbca10bd1d88781c7c2eea519b393a46cd855b
TJ <tj.durnford@microsoft.com>
2020-10-08 09:57:29 -1000

    number

--- Composer/packages/adaptive-form/src/components/fields/NumberField.tsx
+++ Composer/packages/adaptive-form/src/components/fields/NumberField.tsx
@@ -9,7 +9,7 @@ import { StringField } from './StringField';
 const floatNumberOfDecimals = 2;
 
 const NumberField: React.FC<FieldProps> = (props) => {
-  const { schema, onChange } = props;
+  const { expression, schema, onChange } = props;
   const { type } = schema;
 
   const [value, setValue] = useState<string | undefined>(props.value);
@@ -17,14 +17,26 @@ const NumberField: React.FC<FieldProps> = (props) => {
   // if the number is a float, we need to convert to a fixed decimal place
   // in order to avoid floating point math rounding errors (ex. 1.2000000001)
   // ex. if step = 0.01, we fix to 2 decimals
-  const handleChange = (value?: string) => {
-    setValue(value);
-
-    if (typeof value === 'undefined' || isNaN(Number(value))) {
-      onChange(value);
-    } else {
-      const newValue = type === 'integer' ? parseInt(value, 10) : +parseFloat(value).toFixed(floatNumberOfDecimals);
-      onChange(newValue);
+  const handleChange = (newValue?: string) => {
+    if (typeof newValue !== 'undefined') {
+      if (
+        (expression && newValue.startsWith('=')) ||
+        (isNaN(Number(newValue)) && newValue.length < (value || '').length)
+      ) {
+        onChange(newValue);
+        setValue(newValue);
+      } else if (!isNaN(Number(newValue))) {
+        if (type === 'number') {
+          const numberValue = +parseFloat(newValue).toFixed(floatNumberOfDecimals);
+          onChange(numberValue);
+          setValue(newValue);
+        } else if (type === 'integer') {
+          const integer = parseInt(newValue, 10);
+          const integerValue = !isNaN(integer) ? integer : '';
+          onChange(integerValue);
+          setValue(integerValue.toString());
+        }
+      }
     }
   };
 
