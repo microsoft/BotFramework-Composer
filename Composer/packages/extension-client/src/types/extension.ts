9b81ae933389c4d32b3c3d7329c8b795c6429229
zeye <zeye@microsoft.com>
2020-10-12 19:22:03 +0800

    share 'LuIntentEditor' via widgets

--- Composer/packages/extension-client/src/types/extension.ts
+++ Composer/packages/extension-client/src/types/extension.ts
@@ -7,11 +7,13 @@ import { UIOptions } from './formSchema';
 import { FlowEditorWidgetMap, FlowWidget } from './flowSchema';
 import { MenuOptions } from './menuSchema';
 import { RecognizerOptions } from './recognizerSchema';
+import { FieldWidget } from './form';
 
 export interface PluginConfig {
   uiSchema?: UISchema;
   widgets?: {
     flow?: FlowEditorWidgetMap;
+    recognizer?: { [name: string]: FieldWidget };
   };
 }
 
