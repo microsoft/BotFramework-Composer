57167d02528a5edd06dd92dbfad49559f160e207
Tony Anziano <tonyanziano5@gmail.com>
2020-10-12 08:21:56 -0700

    Working on auth service

---
+++ Composer/packages/server/src/controllers/auth.ts
@@ -0,0 +1,27 @@
+import { Request, Response } from 'express';
+import { authService } from '../services/auth';
+
+type GetAccessTokenRequest = Request & {
+  query: {
+    clientId: string;
+    scopes: string;
+  };
+};
+
+async function getAccessToken(req: GetAccessTokenRequest, res: Response) {
+  const { clientId, scopes } = req.query;
+  if (!clientId || !scopes) {
+    // bad request 400
+    return res.status(400).send();
+  }
+
+  const accessToken = await authService.getAccessToken({ clientId, scopes });
+
+  res.status(200).json({
+    accessToken,
+  });
+}
+
+export const AuthController = {
+  getAccessToken,
+};
