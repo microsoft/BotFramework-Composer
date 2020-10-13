18d66bd3adcbdbfc6a4d096bc1ed40d45e2cb2de
pavolum <pavolum@outlook.com>
2020-10-13 10:48:39 -0700

    feature: Adding VA template custom creation UI (#4176)
    * Adding va core template plug in with corresponding plugin build steps
    
    * Adding env variable feature flag around VA=Core template
    
    * removing unused code that was initially added for custom runtime which has been removed
    
    * Added creation UI workspace which is linked to and referenced from client package creation flow, Moved some UI assets to shared UI lib for cross workspace reuse, added creation UI experience for va-core template
    
    * Minor styling changes
    
    * Cleaning up code for review:
    Moved to standard styling practices,
    normalized file content structure,
    added formatMessage() to UI strings,
    misc clean up
    
    * Cleaning up unused changes
    
    * removing bot type tile
    
    * removing bot type tile
    
    * Making requested PR changes, general clean up, conversion of interfaces to types, adding error catching
    
    * Converting props types to intersaction with reach router, adding await back to createProject function
    
    * Fixing files to follow repoo linting rules
    
    * Changed post creation logic to execute on useEffect as opposed to reliance on await
    
    * resolve reentry bug
    
    * removing unused function
    
    * Fixing linting errors
    
    * resolve reentry bug
    
    * Fixing reference
    
    * Updating react version in creation extension
    
    * Updating react version in creation extension
    
    * Making PR changes
    
    * Fixed non static formatMessage calls
    
    * minor syntax refactoring
    
    * Refactoring state management for more modular state.
    Implemented local state relationship to prevent excessive rerenders
    
    * Making additional state management changes for better readability and performance.
    minor clean up of interface and file names
    
    * Fixing file naming
    
    * Formatting template strings for localization
    
    * changing ReachRouter version dpendency to match client reach/router version
    
    * update Azure Publish yarn.lock
    
    * Updating reach router version and tamplate name
    
    Co-authored-by: Patrick Volum <pavolum@microsoft.com>

---
+++ Composer/packages/ui-plugins/virtual-assistant-creation/src/components/VirtualAssistantCreationModal.tsx
@@ -0,0 +1,129 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+/** @jsx jsx */
+import { jsx } from '@emotion/core';
+import React, { useEffect, useState } from 'react';
+import { RouteComponentProps, Router } from '@reach/router';
+import { useShellApi } from '@bfc/extension-client';
+import { navigate } from '@reach/router';
+
+import { updatePersonalityQnaFile } from '../util';
+import { RouterPaths } from '../constants';
+import { BotCreationFormData } from '../types';
+import {
+  CustomizeBotPageState,
+  getInitialBotCustomizeState,
+  getInitialSelectBotPageState,
+  SelectBotPageState,
+} from '../models/stateModels';
+
+import { NewBotPage } from './NewBotPage';
+import { CustomizeBotPage } from './CustomizeBotPage';
+import { ConfigSummaryPage } from './ConfigSummaryPage';
+import { ProvisionSummaryPage } from './ProvisionSummaryPage';
+
+// -------------------- VirtualAssistantCreationModal -------------------- //
+type VirtualAssistantCreationModalProps = {
+  onDismiss: () => void;
+  handleCreateNew: (formData: BotCreationFormData, templateId: string) => Promise<void>;
+  formData: BotCreationFormData;
+} & RouteComponentProps<{}>;
+
+const createLgFromString = (text: string) => {
+  if (text && text.length > 0 && text[0] !== '-') {
+    return `- ${text}`;
+  }
+  return text;
+};
+
+export const VirtualAssistantCreationModal: React.FC<VirtualAssistantCreationModalProps> = (props) => {
+  const { onDismiss, handleCreateNew, formData } = props;
+  const [selectBotPageState, setSelectBotPageState] = useState(getInitialSelectBotPageState);
+  const [customizeBotPageState, setCustomizeBotPageState] = useState(getInitialBotCustomizeState);
+  const { shellApi, projectId } = useShellApi();
+  const [initialProjectId] = useState(projectId);
+
+  const createAndConfigureBot = async () => {
+    handleCreateNew(formData, 'va-core');
+  };
+
+  const updateBotResponses = async () => {
+    // VA creation POC initially scoped for english
+    const generatedLgFileId = formData.name.toLowerCase() + '.en-us';
+    await shellApi.updateLgTemplate(
+      generatedLgFileId,
+      'ReturningUserGreeting',
+      createLgFromString(customizeBotPageState.selectedGreetingMessage)
+    );
+    await shellApi.updateLgTemplate(
+      generatedLgFileId,
+      'NewUserGreeting',
+      createLgFromString(customizeBotPageState.selectedGreetingMessage)
+    );
+    await shellApi.updateLgTemplate(
+      generatedLgFileId,
+      'BotName',
+      createLgFromString(customizeBotPageState.selectedBotName)
+    );
+    await shellApi.updateLgTemplate(
+      generatedLgFileId,
+      'FallBackMessage',
+      createLgFromString(customizeBotPageState.selectedFallbackText)
+    );
+  };
+
+  const updateQnaFiles = async () => {
+    updatePersonalityQnaFile(shellApi, customizeBotPageState.selectedPersonality);
+  };
+
+  const onModalDismiss = () => {
+    navigate('./');
+    onDismiss();
+  };
+
+  const updateGlobalCustomizeState = (newCustomizeState: CustomizeBotPageState) => {
+    setCustomizeBotPageState({ ...newCustomizeState });
+  };
+
+  const updateGlobalSelectBotState = (newSelectBotPageState: SelectBotPageState) => {
+    setSelectBotPageState({ ...selectBotPageState, ...newSelectBotPageState });
+  };
+
+  // projectID change indicates that the project files have been created and post create calls can be executed
+  useEffect((): void => {
+    if (projectId !== initialProjectId) {
+      updateQnaFiles();
+      updateBotResponses();
+      navigate('./');
+    }
+  }, [projectId, shellApi]);
+  return (
+    <Router>
+      <NewBotPage
+        default
+        initialState={selectBotPageState}
+        path={RouterPaths.newBotPage}
+        onDismiss={onModalDismiss}
+        onNext={updateGlobalSelectBotState}
+      />
+      <CustomizeBotPage
+        initialState={customizeBotPageState}
+        path={RouterPaths.customizeBotPage}
+        onDismiss={onModalDismiss}
+        onNext={updateGlobalCustomizeState}
+      />
+      <ConfigSummaryPage
+        customizeBotPageState={customizeBotPageState}
+        path={RouterPaths.configSummaryPage}
+        selectBotPageState={selectBotPageState}
+        onDismiss={onModalDismiss}
+      />
+      <ProvisionSummaryPage
+        path={RouterPaths.provisionSummaryPage}
+        onDismiss={onModalDismiss}
+        onSubmit={createAndConfigureBot}
+      />
+    </Router>
+  );
+};
