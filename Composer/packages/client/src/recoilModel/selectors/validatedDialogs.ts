7cb9f20f8103bbdf9869b7b6f40134fdc373b807
Srinaath Ravichandran <srravich@microsoft.com>
2020-10-13 17:11:58 -0700

    Guard selectors against partially loaded project
    Signed-off-by: Srinaath Ravichandran <srravich@microsoft.com>

--- Composer/packages/client/src/recoilModel/selectors/validatedDialogs.ts
+++ Composer/packages/client/src/recoilModel/selectors/validatedDialogs.ts
@@ -5,10 +5,15 @@ import { selectorFamily } from 'recoil';
 import { validateDialog } from '@bfc/indexers';
 
 import { dialogsState, schemasState, lgFilesState, luFilesState } from '../atoms/botState';
+import { botProjectIdsState } from '../atoms';
 
 export const validateDialogSelectorFamily = selectorFamily({
   key: 'validateDialogSelectorFamily',
   get: (projectId: string) => ({ get }) => {
+    const loadedProjects = get(botProjectIdsState);
+    if (!loadedProjects.includes(projectId)) {
+      return [];
+    }
     const dialogs = get(dialogsState(projectId));
     const schemas = get(schemasState(projectId));
     const lgFiles = get(lgFilesState(projectId));
