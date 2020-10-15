517f203d2a6b64d3f1e470e436a6214af2c2c865
Qi Kang <qika@microsoft.com>
2020-10-15 14:55:12 +0800

    fix self contained

--- Composer/plugins/runtimes/src/index.ts
+++ Composer/plugins/runtimes/src/index.ts
@@ -65,7 +65,7 @@ export default async (composer: any): Promise<void> => {
       // do the dotnet publish
       try {
         const { stdout, stderr } = await execAsync(
-          `dotnet publish "${dotnetProjectPath}" -c release -o "${publishFolder}" -v q`,
+          `dotnet publish "${dotnetProjectPath}" -c release -o "${publishFolder}" -v q --self-contained`,
           {
             cwd: runtimePath,
           }
