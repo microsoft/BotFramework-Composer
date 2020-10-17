d3f1ca71f62ddcd523671cf81feb014284b4f87b
zeye <zeye@microsoft.com>
2020-10-17 16:14:10 +0800

    migrate to option tree

--- Composer/packages/client/src/components/TriggerCreationModal/TriggerDropdownGroup.tsx
+++ Composer/packages/client/src/components/TriggerCreationModal/TriggerDropdownGroup.tsx
@@ -1,18 +1,21 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-import React, { FC, useMemo, useState } from 'react';
-import formatMessage from 'format-message';
+import React, { FC, ReactNode, useMemo, useState } from 'react';
 import { Stack } from 'office-ui-fabric-react/lib/Stack';
 import { IDropdownOption } from 'office-ui-fabric-react/lib/Dropdown';
 import { Dropdown } from 'office-ui-fabric-react/lib/Dropdown';
 import { SDKKinds } from '@bfc/shared';
 
-import { activityTypeKey, dialogEventKey } from './constants';
 import { dropdownStyles } from './styles';
-import { getEventOptions, getActivityOptions, checkTriggerOptions, getTriggerOptions } from './getDropdownOptions';
+import { checkTriggerOptions, getTriggerOptions } from './getDropdownOptions';
 import { renderDropdownOption } from './TriggerCreationModal';
-import { generateTriggerOptionTree } from './TriggerOptionTree';
+import {
+  generateTriggerOptionTree,
+  TriggerOptionGroupNode,
+  TriggerOptionLeafNode,
+  TriggerOptionTreeNode,
+} from './TriggerOptionTree';
 import { builtinTriggerUISchema } from './schema/builtinSchema';
 
 export interface TriggerDropwdownGroupProps {
@@ -21,74 +24,66 @@ export interface TriggerDropwdownGroupProps {
   setTriggerType: (type: string) => void;
 }
 
-export const TriggerDropdownGroup: FC<TriggerDropwdownGroupProps> = ({
-  recognizerType,
-  triggerType,
-  setTriggerType,
-}) => {
-  const eventTypes: IDropdownOption[] = getEventOptions();
-  const activityTypes: IDropdownOption[] = getActivityOptions();
+export const TriggerDropdownGroup: FC<TriggerDropwdownGroupProps> = ({ recognizerType, setTriggerType }) => {
   const triggerTypeOptions: IDropdownOption[] = getTriggerOptions();
 
   // Mark out incompatible triggers of current recognizer
   checkTriggerOptions(triggerTypeOptions, recognizerType);
 
-  const [rootKey, setRootKey] = useState(triggerType);
-  const [childKey, setChildKey] = useState('');
-
-  const showEventDropDown = rootKey === dialogEventKey;
-  const showActivityDropDown = rootKey === activityTypeKey;
-
-  const compoundTypes = [activityTypeKey, dialogEventKey];
-  const onSelectTriggerType = (e, option) => {
-    const type = option.key;
-    setRootKey(type);
-    setChildKey('');
-    const isCompound = compoundTypes.some((t) => type === t);
-    setTriggerType(isCompound ? '' : type);
-  };
-  const onChangeChildKey = (e, option) => {
-    setChildKey(option.key);
-    setTriggerType(option.key);
-  };
-
   const triggerOptionTree = useMemo(() => {
     return generateTriggerOptionTree(builtinTriggerUISchema);
   }, []);
+  const [activeNode, setActiveNode] = useState<TriggerOptionTreeNode>(triggerOptionTree);
+  const onClickNode = (node: TriggerOptionTreeNode) => {
+    setActiveNode(node);
+    if (node instanceof TriggerOptionLeafNode) {
+      setTriggerType(node.$kind);
+    } else {
+      setTriggerType('');
+    }
+  };
 
-  return (
-    <Stack>
-      <Dropdown
-        data-testid={'triggerTypeDropDown'}
-        label={formatMessage('What is the type of this trigger?')}
-        options={triggerTypeOptions}
-        selectedKey={rootKey}
-        styles={dropdownStyles}
-        onChange={onSelectTriggerType}
-        onRenderOption={renderDropdownOption}
-      />
-      {showEventDropDown && (
-        <Dropdown
-          data-testid={'eventTypeDropDown'}
-          label={formatMessage('Which event?')}
-          options={eventTypes}
-          placeholder={formatMessage('Select an event type')}
-          selectedKey={childKey}
-          styles={dropdownStyles}
-          onChange={onChangeChildKey}
-        />
-      )}
-      {showActivityDropDown && (
-        <Dropdown
-          data-testid={'activityTypeDropDown'}
-          label={formatMessage('Which activity type?')}
-          options={activityTypes}
-          placeholder={formatMessage('Select an activity type')}
-          selectedKey={childKey}
-          styles={dropdownStyles}
-          onChange={onChangeChildKey}
-        />
-      )}
-    </Stack>
-  );
+  const getDropdownList = (activeNode: TriggerOptionTreeNode) => {
+    const treePath: TriggerOptionTreeNode[] = [activeNode];
+    while (treePath[0].parent) {
+      treePath.unshift(treePath[0].parent);
+    }
+
+    const dropdownList: ReactNode[] = [];
+
+    const getKey = (x: TriggerOptionTreeNode) => (x instanceof TriggerOptionLeafNode ? x.$kind : x.label);
+
+    for (let i = 0; i < treePath.length; i++) {
+      const currentNode: TriggerOptionTreeNode = treePath[i];
+      if (currentNode instanceof TriggerOptionGroupNode) {
+        const nextNode = treePath[i + 1];
+        const selectedKey = nextNode ? getKey(nextNode) : '';
+        const dropdown = (
+          <Dropdown
+            label={currentNode.prompt}
+            options={currentNode.children.map((x) => {
+              return {
+                key: getKey(x),
+                text: x.label,
+                node: x,
+              };
+            })}
+            placeholder={currentNode.placeholder}
+            selectedKey={selectedKey}
+            styles={dropdownStyles}
+            onChange={(e, opt: any) => {
+              onClickNode(opt.node);
+            }}
+            onRenderOption={renderDropdownOption}
+          />
+        );
+        dropdownList.push(dropdown);
+      }
+    }
+
+    return dropdownList;
+  };
+
+  const dropdownList = getDropdownList(activeNode);
+  return <Stack>{...dropdownList}</Stack>;
 };
