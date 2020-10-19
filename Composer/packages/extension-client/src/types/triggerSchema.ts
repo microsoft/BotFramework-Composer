23dd5b87fa4504970fcdc584f6517e608bef5e50
zeye <zeye@microsoft.com>
2020-10-19 17:33:42 +0800

    declare TriggerUISchema in extension

---
+++ Composer/packages/extension-client/src/types/triggerSchema.ts
@@ -0,0 +1,17 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import { SDKKinds } from '@botframework-composer/types';
+
+export interface TriggerUIOption {
+  label: string;
+  submenu?: TriggerSubmenuInfo | string | false;
+}
+
+export interface TriggerSubmenuInfo {
+  label: string;
+  prompt?: string;
+  placeholder?: string;
+}
+
+export type TriggerUISchema = { [key in SDKKinds]?: TriggerUIOption };
