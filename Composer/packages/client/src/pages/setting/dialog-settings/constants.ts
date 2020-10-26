f62ed844805bf60abd3523341b16d8105237be90
Yan Liu <32497439+zxyanliu@users.noreply.github.com>
2020-10-26 14:10:40 -0700

    Updates to Settings page

--- Composer/packages/client/src/pages/setting/dialog-settings/constants.ts
+++ Composer/packages/client/src/pages/setting/dialog-settings/constants.ts
@@ -10,14 +10,16 @@ export const BotSettings = {
   botSettings: formatMessage('Settings'),
   generalTitle: formatMessage('General'),
   botSettingDescription: formatMessage(
-    'Settings contains detailed information about your bot. For security reasons, they are hidden by default. To test your bot or publish to Azure, you may need to provide these settings.'
+    'The Bot Settings page includes detailed configuration information about the selected bot. To test your bot or publish to Azure, you may need to provide these settings.'
   ),
   languageTitle: formatMessage('Bot language'),
-  languagesubTitle: formatMessage(`Select the language that bot will be able to understand (User input) and respond to (Bot responses).
-    To make this bot available in other languages, click “Add’ to create a copy of the default language, and translate the content into the new language.`),
+  languagesubTitle: formatMessage(
+    `To make this bot available in other languages, select "Create copy to translate bot content" and translate the content into the new language.`
+  ),
+  helpLink: 'https://aka.ms/composer-multilingual-support',
   languageBotLanauge: formatMessage('Bot language (active)'),
   languageDefaultLanauge: formatMessage('Default language'),
   languageAddLanauge: formatMessage('Create copy to translate bot content'),
-  learnMore: formatMessage('Learn more.'),
+  learnMore: formatMessage('Learn more'),
   settingsTitle: formatMessage('Bot settings'),
 };
