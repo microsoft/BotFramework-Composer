b7808b920cff7b1c2f2f5f91d5e8a98b94b3c288
TJ <tj.durnford@microsoft.com>
2020-10-28 13:15:12 -1000

    open emulator test

--- Composer/packages/client/__tests__/components/TestController/emulatorOpenButton.test.tsx
+++ Composer/packages/client/__tests__/components/TestController/emulatorOpenButton.test.tsx
@@ -2,34 +2,62 @@
 // Licensed under the MIT License.
 
 import * as React from 'react';
-import { render, fireEvent } from '@botframework-composer/test-utils';
+import { fireEvent } from '@botframework-composer/test-utils';
 
 import { OpenEmulatorButton } from '../../../src/components/TestController/OpenEmulatorButton';
+import { botEndpointsState, botStatusState, settingsState } from '../../../src/recoilModel';
 import { BotStatus } from '../../../src/constants';
+import { renderWithRecoil } from '../../testUtils';
+
+const mockOpenBotInEmulator = jest.fn();
+
+jest.mock('../../../src/utils/navigation', () => ({
+  openInEmulator: mockOpenBotInEmulator,
+}));
+
+jest.mock('office-ui-fabric-react/lib/Button', () => ({
+  ActionButton: ({ onClick, children }) => (
+    <button data-testid="button" onClick={onClick}>
+      {children}
+    </button>
+  ),
+  IconButton: ({ onClick, children }) => (
+    <button data-testid="iconButton" onClick={onClick}>
+      {children}
+    </button>
+  ),
+}));
+
+jest.mock('office-ui-fabric-react/lib/Tooltip', () => ({
+  TooltipHost: ({ children }) => <div>{children}</div>,
+}));
 
 const projectId = '123.abc';
 
+const initialState = ({ currentStatus = BotStatus.connected } = {}) => ({ set }) => {
+  set(botStatusState(projectId), currentStatus);
+  set(botEndpointsState, { [projectId]: 'http://open-in-emulator/api/messages' });
+  set(settingsState(projectId), {});
+};
+
 describe('<OpenEmulatorButton />', () => {
-  it('should show the button to open emulator', () => {
-    const onClick = jest.fn(() => {});
-    const { container, getByText } = render(<OpenEmulatorButton projectId={projectId} />);
+  beforeEach(() => {
+    mockOpenBotInEmulator.mockClear();
+  });
 
-    expect(container).toHaveTextContent('Test in Emulator');
+  fit('should show the button to open emulator', async () => {
+    const { findByTestId } = renderWithRecoil(<OpenEmulatorButton projectId={projectId} />, initialState());
 
-    const button = getByText('Test in Emulator');
+    const button = await findByTestId('button');
     fireEvent.click(button);
-    expect(onClick).toBeCalledTimes(1);
+    expect(mockOpenBotInEmulator).toHaveBeenCalled();
   });
 
-  it('should hidden the button if set hidden', () => {
-    const { container } = render(<OpenEmulatorButton projectId={projectId} />);
-
-    expect(container).not.toHaveTextContent('Test in Emulator');
-  });
-
-  it('should show the button if the bot status is not connected', () => {
-    const { container } = render(<OpenEmulatorButton projectId={projectId} />);
-
+  it('should not show the button if the status is not `BotStatus.connected`', () => {
+    const { container } = renderWithRecoil(
+      <OpenEmulatorButton projectId={projectId} />,
+      initialState({ currentStatus: BotStatus.pending })
+    );
     expect(container).not.toHaveTextContent('Test in Emulator');
   });
 });
