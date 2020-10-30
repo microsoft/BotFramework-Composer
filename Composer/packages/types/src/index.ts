b00fa01a8edfa887d1fc4bb29c093c905ec488a3
Andy Brown <asbrown002@gmail.com>
2020-10-30 09:46:30 -0700

    move extension context to server package

--- Composer/packages/types/src/index.ts
+++ Composer/packages/types/src/index.ts
@@ -5,6 +5,7 @@ export * from './diagnostic';
 export * from './dialogUtils';
 export * from './extension';
 export * from './indexers';
+export * from './project';
 export * from './publish';
 export * from './runtime';
 export * from './schema';
