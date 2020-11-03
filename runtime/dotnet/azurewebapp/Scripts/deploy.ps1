3da5320e5d5d47b00e70f8fed58adefee97664e8
Qi Kang <qika@microsoft.com>
2020-11-03 17:04:15 +0800

    update powershell script to support qna and corsstrain

--- runtime/dotnet/azurewebapp/Scripts/deploy.ps1
+++ runtime/dotnet/azurewebapp/Scripts/deploy.ps1
@@ -1,8 +1,10 @@
 Param(
 	[string] $name,
 	[string] $environment,
+	[string] $hostName,
 	[string] $luisAuthoringKey,
 	[string] $luisAuthoringRegion,
+	[string] $qnaSubscriptionKey,
 	[string] $language,
 	[string] $projFolder = $(Get-Location),
 	[string] $botPath,
@@ -77,6 +79,7 @@ Copy-Item -Path (Get-Item -Path $botPath -Exclude ('runtime', 'generated')).Full
 $settingsPath = $(Join-Path $remoteBotPath settings appsettings.json)
 $settings = Get-Content $settingsPath | ConvertFrom-Json
 $luisSettings = $settings.luis
+$qnaSettings = $settings.qna
 
 if (-not $luisAuthoringKey) {
 	$luisAuthoringKey = $luisSettings.authoringKey
@@ -84,6 +87,13 @@ if (-not $luisAuthoringKey) {
 
 if (-not $luisAuthoringRegion) {
 	$luisAuthoringRegion = $luisSettings.region
+	if (-not $luisAuthoringRegion) {
+		$luisAuthoringRegion = 'westus'
+	}
+}
+
+if (-not $qnaSubscriptionKey) {
+	$qnaSubscriptionKey = $qnaSettings.subscriptionKey
 }
 
 # set feature configuration
@@ -100,39 +110,42 @@ else {
 	$featureConfig["UseCosmosDb"] = $true
 }
 
-# Add Luis Config to appsettings
-if ($luisAuthoringKey -and $luisAuthoringRegion) {
-	Set-Location -Path $remoteBotPath
 
-	$models = Get-ChildItem $remoteBotPath -Recurse -Filter "*.lu" | Resolve-Path -Relative
 
-	# Generate Luconfig.json file
-	$luconfigjson = @{
-		"name"            = $name;
-		"defaultLanguage" = $language;
-		"models"          = $models
+# if luis and qna enabled, crosstrain
+if ($luisAuthoringKey -or $qnaSubscriptionKey) {
+	# if luis or qna enabled, ensure bf cli installed
+	if (Get-Command bf -errorAction SilentlyContinue) {}
+	else {
+		Write-Host "bf command does not exist. Start installation..."
+		npm i -g @microsoft/botframework-cli@next
+		bf plugins:install @microsoft/bf-sampler-cli@beta
+		Write-Host "successfully"
 	}
 
-	$luString = $models | Out-String
-	Write-Host $luString
-
-	$luconfigjson | ConvertTo-Json -Depth 100 | Out-File $(Join-Path $remoteBotPath luconfig.json)
+	Set-Location -Path $remoteBotPath
 
 	# create generated folder if not
 	if (!(Test-Path generated)) {
 		$null = New-Item -ItemType Directory -Force -Path generated
 	}
-
-	# ensure bot cli is installed
-	if (Get-Command bf -errorAction SilentlyContinue) {}
-	else {
-		Write-Host "bf luis:build does not exist. Start installation..."
-		npm i -g @microsoft/botframework-cli
-		Write-Host "successfully"
+	if (!(Test-Path generated\interruption)) {
+		New-Item -ItemType Directory -Force -Path generated\interruption
 	}
 
-	# Execute bf luis:build command
-	bf luis:build --luConfig $(Join-Path $remoteBotPath luconfig.json)  --botName $name --authoringKey $luisAuthoringKey --dialog crosstrained  --out ./generated --suffix $environment -f --region $luisAuthoringRegion
+	bf luis:cross-train --in . --out generated\interruption --config .\settings\cross-train.config.json --force
+
+	if ($luisAuthoringKey) {
+		bf sampler:sampling --in generated\interruption --out generated\interruption --force
+	}
+}
+
+# if luis enabled
+if ($luisAuthoringKey) {
+	Set-Location -Path $remoteBotPath
+
+	# execute lubuild
+	bf luis:build --in generated\interruption --authoringKey $luisAuthoringKey --botName $name --out generated --suffix $environment --force --log
 
 	if ($?) {
 		Write-Host "lubuild succeeded"
@@ -146,8 +159,10 @@ if ($luisAuthoringKey -and $luisAuthoringRegion) {
 
 	$settings = New-Object PSObject
 
+	# get all luis settings
 	$luisConfigFiles = Get-ChildItem -Path $publishFolder -Include "luis.settings*" -Recurse -Force
 
+	# get all luis app ids
 	$luisAppIds = @{ }
 
 	foreach ($luisConfigFile in $luisConfigFiles) {
@@ -164,6 +179,7 @@ if ($luisAuthoringKey -and $luisAuthoringRegion) {
 
 	foreach ($key in $luisAppIds.Keys) { $luisConfig[$key] = $luisAppIds[$key] }
 
+	# add luis settings to the main settings
 	$settings | Add-Member -Type NoteProperty -Force -Name 'luis' -Value $luisConfig
 
 	$tokenResponse = (az account get-access-token) | ConvertFrom-Json
@@ -174,6 +190,7 @@ if ($luisAuthoringKey -and $luisAuthoringRegion) {
 		Break
 	}
 
+	# assign luis key to luis app ids, firstly get luis account by $name-$environment-luis
 	Write-Host "Getting Luis accounts..."
 	$luisAccountEndpoint = "$luisEndpoint/luis/api/v2.0/azureaccounts"
 	$luisAccount = $null
@@ -196,7 +213,7 @@ if ($luisAuthoringKey -and $luisAuthoringRegion) {
 
 	# Assign each luis id in luisAppIds with the endpoint key
 	foreach ($k in $luisAppIds.Keys) {
-		$luisAppId = $luisAppIds.Item($k)
+		$luisAppId = $luisAppIds.Item($k).appId
 		Write-Host "Assigning to Luis app id: $luisAppId"
 		$luisAssignEndpoint = "$luisEndpoint/luis/api/v2.0/apps/$luisAppId/azureaccounts"
 		try {
@@ -210,20 +227,41 @@ if ($luisAuthoringKey -and $luisAuthoringRegion) {
 	}
 }
 
+# if qna enabled
+if ($qnaSubscriptionKey) {
+	Set-Location -Path $remoteBotPath
+	bf qnamaker:build --in generated\interruption --subscriptionKey $qnaSubscriptionKey --botName $name --out generated --suffix $environment --force --log
+	if ($?) {
+		Write-Host "qna build succeeded"
+	}
+	else {
+		Write-Host "qna build failed, please verify your qna models."
+		Break
+	}
+}
+
+
+# add feature config to the settings
 $settings | Add-Member -Type NoteProperty -Force -Name 'feature' -Value $featureConfig
 $settings | ConvertTo-Json -depth 100 | Out-File $settingsPath
 
 $resourceGroup = "$name-$environment"
 
+# if all done, compress the folder and deploy to azure
 if ($?) {
 	# Compress source code
 	Get-ChildItem -Path "$($publishFolder)" | Compress-Archive -DestinationPath "$($zipPath)" -Force | Out-Null
 
 	# Publish zip to Azure
 	Write-Host "> Publishing to Azure ..." -ForegroundColor Green
+
+	if (-not $hostName) {
+		$hostName = "$name-$environment"
+	}
+
 	$deployment = (az webapp deployment source config-zip `
 			--resource-group $resourceGroup `
-			--name "$name-$environment" `
+			--name $hostName `
 			--src $zipPath `
 			--output json) 2>> $logFile
 
