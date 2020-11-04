08ef122517b4b846e1dafb0697403462cef08ee1
GeoffCoxMSFT <gcox@microsoft.com>
2020-11-04 12:34:48 -0800

    Form dialog hidden fixed feature flags

--- Composer/packages/lib/shared/src/featureFlagUtils/index.ts
+++ Composer/packages/lib/shared/src/featureFlagUtils/index.ts
@@ -28,7 +28,7 @@ export const getDefaultFeatureFlags = (): FeatureFlagMap => ({
   FORM_DIALOG: {
     displayName: formatMessage('Show Form Dialog'),
     description: formatMessage('Show form dialog editor in the canvas'),
-    isHidden: false,
+    isHidden: true,
     enabled: false,
   },
 });
