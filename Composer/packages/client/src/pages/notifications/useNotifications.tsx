9272f00983d3c57fe90d303dbf628bc26004e152
zhixzhan <zhixzhan@microsoft.com>
2020-11-06 14:34:00 +0800

    clean up

--- Composer/packages/client/src/pages/notifications/useNotifications.tsx
+++ Composer/packages/client/src/pages/notifications/useNotifications.tsx
@@ -5,8 +5,7 @@ import { useRecoilValue } from 'recoil';
 
 import { messagersSelector } from '../../recoilModel/selectors';
 
-export default function useNotifications(projectId: string, filter?: string) {
-  console.log(projectId);
+export default function useNotifications(_projectId: string, filter?: string) {
   const messagers = useRecoilValue(messagersSelector);
   return filter ? messagers.filter((x) => x.severity === filter) : messagers;
 }
