a6d5111190cf3c06a6258618cc3fe2a442e96905
Ben Yackley <61990921+beyackle@users.noreply.github.com>
2020-11-10 12:48:10 -0800

    fix: correct URL when switching page modes (#4736)
    * Update projectTemplates.ts
    
    * correct links and reading dialogs
    
    * fix typecheck
    
    * fixes from CR
    
    * unify props shape across LG/LU/QnA pages
    
    * fixes from CR

--- Composer/packages/client/src/pages/knowledge-base/QnAPage.tsx
+++ Composer/packages/client/src/pages/knowledge-base/QnAPage.tsx
@@ -21,17 +21,18 @@ import TableView from './table-view';
 
 const CodeEditor = React.lazy(() => import('./code-editor'));
 
-interface QnAPageProps extends RouteComponentProps<{}> {
-  projectId?: string;
-  dialogId?: string;
-}
+const QnAPage: React.FC<RouteComponentProps<{
+  dialogId: string;
+  projectId: string;
+  skillId: string;
+}>> = (props) => {
+  const { dialogId = '', projectId = '', skillId } = props;
 
-const QnAPage: React.FC<QnAPageProps> = (props) => {
-  const { dialogId = '', projectId = '' } = props;
+  const baseURL = skillId == null ? `/bot/${projectId}/` : `/bot/${projectId}/skill/${skillId}/`;
 
   const actions = useRecoilValue(dispatcherState);
-  const dialogs = useRecoilValue(dialogsSelectorFamily(projectId));
-  const qnaFiles = useRecoilValue(qnaFilesState(projectId));
+  const dialogs = useRecoilValue(dialogsSelectorFamily(skillId ?? projectId));
+  const qnaFiles = useRecoilValue(qnaFilesState(skillId ?? projectId));
   //To do: support other languages
   const locale = 'en-us';
   //const locale = useRecoilValue(localeState);
@@ -47,7 +48,7 @@ const QnAPage: React.FC<QnAPageProps> = (props) => {
         id: dialog.id,
         name: dialog.displayName,
         ariaLabel: formatMessage('qna file'),
-        url: `/bot/${projectId}/knowledge-base/${dialog.id}`,
+        url: `${baseURL}knowledge-base/${dialog.id}`,
         menuIconProps: {
           iconName: 'Add',
         },
@@ -81,7 +82,7 @@ const QnAPage: React.FC<QnAPageProps> = (props) => {
       id: 'all',
       name: 'All',
       ariaLabel: formatMessage('all qna files'),
-      url: `/bot/${projectId}/knowledge-base/all`,
+      url: `${baseURL}knowledge-base/all`,
     });
     return newDialogLinks;
   }, [dialogs]);
@@ -90,13 +91,13 @@ const QnAPage: React.FC<QnAPageProps> = (props) => {
     setCreateOnDialogId('');
     const activeDialog = dialogs.find(({ id }) => id === dialogId);
     if (!activeDialog && dialogs.length && dialogId !== 'all') {
-      navigateTo(`/bot/${projectId}/knowledge-base/${dialogId}`);
+      navigateTo(`${baseURL}knowledge-base/${dialogId}`);
     }
   }, [dialogId, dialogs, projectId]);
 
   const onToggleEditMode = useCallback(
     (_e) => {
-      let url = `/bot/${projectId}/knowledge-base/${dialogId}`;
+      let url = `${baseURL}knowledge-base/${dialogId}`;
       if (!edit) url += `/edit`;
       navigateTo(url);
     },
--- Composer/packages/client/src/utils/pageLinks.ts
+++ Composer/packages/client/src/utils/pageLinks.ts
@@ -9,9 +9,14 @@ export const topLinks = (
   projectId: string,
   openedDialogId: string,
   pluginPages: ExtensionPageConfig[],
-  showFormDialog: boolean
+  showFormDialog: boolean,
+  rootProjectId: string | undefined
 ) => {
   const botLoaded = !!projectId;
+  const linkBase =
+    projectId === rootProjectId || rootProjectId == null
+      ? `/bot/${projectId}/`
+      : `/bot/${rootProjectId}/skill/${projectId}/`;
   let links = [
     {
       to: '/home',
@@ -21,28 +26,28 @@ export const topLinks = (
       disabled: false,
     },
     {
-      to: `/bot/${projectId}/dialogs/${openedDialogId}`,
+      to: linkBase + `dialogs/${openedDialogId}`,
       iconName: 'SplitObject',
       labelName: formatMessage('Design'),
       exact: false,
       disabled: !botLoaded,
     },
     {
-      to: `/bot/${projectId}/language-generation`,
+      to: linkBase + `language-generation/${openedDialogId}`,
       iconName: 'Robot',
       labelName: formatMessage('Bot Responses'),
       exact: false,
       disabled: !botLoaded,
     },
     {
-      to: `/bot/${projectId}/language-understanding`,
+      to: linkBase + `language-understanding/${openedDialogId}`,
       iconName: 'People',
       labelName: formatMessage('User Input'),
       exact: false,
       disabled: !botLoaded,
     },
     {
-      to: `/bot/${projectId}/knowledge-base`,
+      to: linkBase + `knowledge-base/${openedDialogId}`,
       iconName: 'QnAIcon',
       labelName: formatMessage('QnA'),
       exact: true,
