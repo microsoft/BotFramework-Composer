82883f81a2b41821a4033624db037b6b34511a0b
Chris Whitten <christopher.whitten@microsoft.com>
2020-11-11 05:43:55 -0800

    Fix schema tests

--- Composer/packages/lib/shared/src/schemaUtils/pvaSchema.ts
+++ Composer/packages/lib/shared/src/schemaUtils/pvaSchema.ts
@@ -2,5 +2,8 @@
 // Licensed under the MIT License.
 
 export const checkForPVASchema = (schema) => {
-  return schema.content.definitions['Microsoft.VirtualAgents.Recognizer'] !== undefined;
+  if (schema && schema.content && schema.content.definitions) {
+    return schema.content.definitions['Microsoft.VirtualAgents.Recognizer'] !== undefined;
+  }
+  return false;
 };
