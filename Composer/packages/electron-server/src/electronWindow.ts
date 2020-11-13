4b573dcf1b45a26d50edb33c9d9e5fa0a2323b62
Soroush <hatpick@gmail.com>
2020-11-13 14:05:31 -0800

    fix: Fixes linux icons on the taskbar (#4806)
    * Fixes linux icons
    
    * Fixes app name on linux on the top panel
    
    Co-authored-by: Soroush <sorgh@microsoft.com>

--- Composer/packages/electron-server/src/electronWindow.ts
+++ Composer/packages/electron-server/src/electronWindow.ts
@@ -8,7 +8,6 @@ import { app, BrowserWindow, screen, shell } from 'electron';
 
 import { isLinux } from './utility/platform';
 import { isDevelopment } from './utility/env';
-import { getUnpackedAsarPath } from './utility/getUnpackedAsarPath';
 import logger from './utility/logger';
 const log = logger.extend('electron-window');
 
@@ -43,7 +42,7 @@ export default class ElectronWindow {
     if (isLinux() && !isDevelopment) {
       // workaround for broken .AppImage icons since electron-builder@21.0.1 removed .AppImage desktop integration
       // (https://github.com/electron-userland/electron-builder/releases/tag/v21.0.1)
-      browserWindowOptions.icon = join(getUnpackedAsarPath(), 'resources/composerIcon_1024x1024.png');
+      browserWindowOptions.icon = join(__dirname, '../resources/composerIcon_1024x1024.png');
     }
     this.currentBrowserWindow = new BrowserWindow(browserWindowOptions);
     this.currentBrowserWindow.on('page-title-updated', (ev) => ev.preventDefault()); // preserve explicit window title
