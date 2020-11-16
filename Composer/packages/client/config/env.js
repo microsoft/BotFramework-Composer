c498a17e70ae4e6fbb9f6a9802c2d05040893070
Andy Brown <asbrown002@gmail.com>
2020-11-16 09:14:17 -0800

    release: v1.2.1
    update version and changelog

--- Composer/packages/client/config/env.js
+++ Composer/packages/client/config/env.js
@@ -88,7 +88,7 @@ function getClientEnvironment(publicUrl) {
         PUBLIC_URL: publicUrl,
         GIT_SHA: getGitSha().toString().replace('\n', ''),
         SDK_PACKAGE_VERSION: '4.11.0', // TODO: change this when Composer supports custom schema/custom runtime
-        COMPOSER_VERSION: '1.2.0',
+        COMPOSER_VERSION: '1.2.1',
         LOCAL_PUBLISH_PATH:
           process.env.LOCAL_PUBLISH_PATH || path.resolve(process.cwd(), '../../../extensions/localPublish/hostedBots'),
       }
--- scripts/generate-changelog.js
+++ scripts/generate-changelog.js
@@ -16,12 +16,13 @@ const AUTHORS = {
   "Kamran Iqbal": "Kaiqb",
   "Long Alan": "alanlong9278",
   "Lu Han": "luhan2017",
-  "mareekuh": "mareekuh",
+  mareekuh: "mareekuh",
   "Pooja Nagpal": "p-nagpal",
   "Qi Kang": "zidaneymar",
-  "sangwoohaan": "srinaath",
+  sangwoohaan: "srinaath",
   "Shuai Wang": "cosmicshuai",
   "Srinaath Ravichandran": "srinaath",
+  Soroush: "hatpick",
   "TJ Durnford": "tdurnford",
   "Tony Anziano": "tonyanziano",
   "Vishwac Sena Kannan": "vishwacsena",
@@ -35,44 +36,43 @@ const AUTHORS = {
   xieofxie: "xieofxie",
   zeye: "yeze322",
   zhixzhan: "zhixzhan",
-  "LouisEugeneMSFT": "leugene-microsoft"
+  LouisEugeneMSFT: "leugene-microsoft",
 };
 
 const getLatestTag = () =>
-  execSync("git describe --tags $(git rev-list --tags --max-count=1)").toString().trim();
+  execSync("git describe --tags $(git rev-list --tags --max-count=1)")
+    .toString()
+    .trim();
 
 const getLog = (tag) =>
   execSync(`git log --pretty=format:'%s | %an' ${tag}..main`)
     .toString()
     .split("\n");
 
-const getDate = () =>
-  execSync('date +"%m-%d-%Y"')
-    .toString()
-    .trim();
+const getDate = () => execSync('date +"%m-%d-%Y"').toString().trim();
 
 const SECTIONS = {
   Added: ["feat"],
   Fixed: ["fix", "a11y"],
   Changed: ["refactor", "style"],
-  Other: []
+  Other: [],
 };
 const tagToSection = Object.keys(SECTIONS).reduce((acc, section) => {
   const groupTags = SECTIONS[section].reduce(
     (s, t) => ({
       ...s,
-      [t]: section
+      [t]: section,
     }),
     {}
   );
 
   return {
     ...acc,
-    ...groupTags
+    ...groupTags,
   };
 }, {});
 
-const getCommitSection = commit => {
+const getCommitSection = (commit) => {
   for (const tag in tagToSection) {
     if (commit.startsWith(tag)) {
       return tagToSection[tag];
@@ -84,7 +84,7 @@ const getCommitSection = commit => {
   return "Other";
 };
 
-const formatMessage = msg =>
+const formatMessage = (msg) =>
   msg.replace(/\(\#(\d+)\)/, `([#$1](${PULL_URL}/$1))`);
 
 const logCache = new Set();
@@ -95,7 +95,7 @@ const logOnce = (key, message) => {
   }
 };
 
-const formatAuthor = author => {
+const formatAuthor = (author) => {
   const username = AUTHORS[author];
 
   if (!username) {
@@ -106,7 +106,7 @@ const formatAuthor = author => {
   return `([@${username}](https://github.com/${username}))`;
 };
 
-const formatCommit = commit => {
+const formatCommit = (commit) => {
   const [message, author] = commit.split(" | ");
 
   return [formatMessage(message), formatAuthor(author)];
@@ -127,7 +127,7 @@ function groupCommits(commits) {
   }, {});
 }
 
-const formatChangeLog = groups => {
+const formatChangeLog = (groups) => {
   const date = getDate();
   let output = `### ${date}`;
 
@@ -135,14 +135,14 @@ const formatChangeLog = groups => {
     if (groups[section] && groups[section].length > 0) {
       output += `\n\n#### ${section}\n\n`;
 
-      output += groups[section].map(c => `- ${c}`).join("\n");
+      output += groups[section].map((c) => `- ${c}`).join("\n");
     }
   }
 
   // Uncategorized
   if (groups.Uncategorized) {
     output += "\n\n Uncategorized\n\n";
-    output += groups.Uncategorized.map(c => `- ${c}`).join("\n");
+    output += groups.Uncategorized.map((c) => `- ${c}`).join("\n");
   }
 
   return output;
