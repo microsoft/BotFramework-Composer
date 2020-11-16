77847945dbeb84cf184ac274d013011084209456
Tony <tonyanziano5@gmail.com>
2020-11-16 13:50:03 -0800

    Fixed up dev config

--- extensions/pvaPublish/webpack.config.js
+++ extensions/pvaPublish/webpack.config.js
@@ -2,100 +2,65 @@ const { resolve } = require('path');
 const svgToMiniDataURI = require('mini-svg-data-uri');
 
 function getWebpackConfigs() {
-  switch (process.env.NODE_ENV) {
-    case 'development':
-      // in the case of development, we want to skip webpack for node compilation and use TSC so we get working sourcemaps
-      console.log('[pvaPublish] Generating development bundles.');
-      return [
-        {
-          entry: './src/ui/index.tsx',
-          mode: 'development',
-          devtool: 'source-map',
-          output: {
-            filename: 'publish-bundle.js',
-            path: resolve(__dirname, 'lib', 'ui'),
-          },
-          externals: {
-            // expect react & react-dom to be available in the extension host iframe
-            react: 'React',
-            'react-dom': 'ReactDOM',
-          },
-          module: {
-            rules: [
-              {
-                test: /\.tsx?$/,
-                loader: 'ts-loader',
-              },
-              {
-                test: /\.svg$/i,
-                loader: 'url-loader',
-                options: {
-                  generator: (content) => svgToMiniDataURI(content.toString()),
-                },
-              },
-            ],
-          },
-          resolve: {
-            extensions: ['.js', '.ts', '.tsx'],
-          },
-          plugins: [],
-        },
-      ];
+  const isDevelopment = process.env.NODE_ENV === 'development';
+  console.log(`[pvaPublish] Generating ${isDevelopment ? 'development' : 'production'} bundles.`);
 
-    case 'production':
-    default:
-      console.log('[pvaPublish] Generating production bundles.');
-      return [
-        {
-          entry: './src/ui/index.tsx',
-          mode: 'production',
-          output: {
-            filename: 'publish-bundle.js',
-            path: resolve(__dirname, 'lib', 'ui'),
+  return [
+    {
+      entry: './src/ui/index.tsx',
+      mode: isDevelopment ? 'development' : 'production',
+      devtool: isDevelopment ? 'source-map' : undefined,
+      output: {
+        filename: 'publish-bundle.js',
+        path: resolve(__dirname, 'lib', 'ui'),
+      },
+      externals: {
+        // expect react & react-dom to be available in the extension host iframe
+        react: 'React',
+        'react-dom': 'ReactDOM',
+      },
+      module: {
+        rules: [
+          {
+            test: /\.tsx?$/,
+            loader: 'ts-loader',
           },
-          externals: {
-            // expect react & react-dom to be available in the extension host iframe
-            react: 'React',
-            'react-dom': 'ReactDOM',
+          {
+            test: /\.svg$/i,
+            loader: 'url-loader',
+            options: {
+              generator: (content) => svgToMiniDataURI(content.toString()),
+            },
           },
-          module: {
-            rules: [
-              {
-                test: /\.tsx?$/,
-                loader: 'ts-loader',
-              },
-              {
-                test: /\.svg$/i,
-                loader: 'url-loader',
-                options: {
-                  generator: (content) => svgToMiniDataURI(content.toString()),
-                },
-              },
-            ],
-          },
-          resolve: {
-            extensions: ['.js', '.ts', '.tsx'],
-          },
-          plugins: [],
-        },
-        {
-          entry: './src/node/index.ts',
-          mode: 'production',
-          target: 'node',
-          output: {
-            path: resolve(__dirname, 'lib', 'node'),
-            filename: 'index.js',
-            libraryTarget: 'commonjs2',
-          },
-          module: {
-            rules: [{ test: /\.tsx?$/, use: 'ts-loader', exclude: [/node_modules/] }],
-          },
-          resolve: {
-            extensions: ['.js', '.ts', '.tsx'],
-          },
-        },
-      ];
-  }
+        ],
+      },
+      resolve: {
+        extensions: ['.js', '.ts', '.tsx'],
+      },
+      plugins: [],
+    },
+    {
+      entry: './src/node/index.ts',
+      mode: isDevelopment ? 'development' : 'production',
+      target: 'node',
+      devtool: isDevelopment ? 'source-map' : undefined,
+      output: {
+        path: resolve(__dirname, 'lib', 'node'),
+        filename: 'index.js',
+        libraryTarget: 'commonjs2',
+        /**
+         * Node files aren't being loaded by Webpack, so we want the source maps to point to the files on disk
+         */
+        devtoolModuleFilenameTemplate: isDevelopment ? '[absolute-resource-path]' : undefined,
+      },
+      module: {
+        rules: [{ test: /\.tsx?$/, use: 'ts-loader', exclude: [/node_modules/] }],
+      },
+      resolve: {
+        extensions: ['.js', '.ts', '.tsx'],
+      },
+    },
+  ];
 }
 
 module.exports = getWebpackConfigs();
