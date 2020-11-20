20ca94d0314f2c6373259e2a616cb41705cdbbdc
Ben Yackley <61990921+beyackle@users.noreply.github.com>
2020-11-19 16:14:50 -0800

    feature: use new ProjectTree in the LG, LU, and QnA pages (#4839)
    * Update projectTemplates.ts
    
    * correct links and reading dialogs
    
    * fix typecheck
    
    * fixes from CR
    
    * unify props shape across LG/LU/QnA pages
    
    * more fixes from PR
    
    * fixes from CR
    
    * start rearranging tree [WIP]
    
    * make tree links do something [WIP]
    
    * tree looks right
    
    * Update navigation.test.tsx
    
    * wire up LG page
    
    * make other pages match
    
    * fix root-bot links
    
    * more fixups
    
    * fix typechecking
    
    * post-merge fixes
    
    * fix LU edit toggle
    
    * route "all" link to URL
    
    * fix test by adding prop to QnA table
    
    * remove "all" links
    
    * Update ProjectTree.tsx
    
    * fixes from CR
    
    * add showRemote
    
    * fix e2e tests
    
    * fix lint stuff
    
    * hide menus in LG/LU/QnA modes
    
    * reinstate pageMode and fixes from CR
    
    * Update Home.tsx
    
    * re-remove pageMode atom
    
    * fix header and unit tests
    
    * remove stray comments in RightPanel
    
    Co-authored-by: Srinaath Ravichandran <srinaath27@gmail.com>

--- Composer/packages/client/src/recoilModel/atoms/appState.ts
+++ Composer/packages/client/src/recoilModel/atoms/appState.ts
@@ -31,17 +31,20 @@ export type CurrentUser = {
   sessionExpired: boolean;
 };
 
+// These values should align with the paths in the app's router
 export type PageMode =
-  | 'home'
-  | 'design'
-  | 'lg'
-  | 'lu'
-  | 'qna'
-  | 'notifications'
+  | 'dialogs' // used for the design page
+  | 'language-understanding'
+  | 'language-generation'
+  | 'knowledge-base'
   | 'publish'
-  | 'skills'
+  | 'botProjectsSettings'
+  | 'forms'
+  | 'diagnostics'
   | 'settings'
-  | 'about';
+  | 'projects'
+  | 'home'
+  | 'botProjectsSettings';
 
 const getFullyQualifiedKey = (value: string) => {
   return `App_${value}_State`;
@@ -203,11 +206,6 @@ export const currentProjectIdState = atom<string>({
   default: '',
 });
 
-export const currentModeState = atom<PageMode>({
-  key: getFullyQualifiedKey('currentMode'),
-  default: 'home',
-});
-
 export const botProjectSpaceLoadedState = atom<boolean>({
   key: getFullyQualifiedKey('botProjectSpaceLoadedState'),
   default: false,
@@ -236,10 +234,10 @@ export const formDialogGenerationProgressingState = atom({
 export const pageElementState = atom<{ [page in PageMode]?: { [key: string]: any } }>({
   key: getFullyQualifiedKey('pageElement'),
   default: {
-    design: {},
-    lg: {},
-    lu: {},
-    qna: {},
+    dialogs: {},
+    'language-generation': {},
+    'language-understanding': {},
+    'knowledge-base': {},
   },
 });
 
