8ba8d4cc69a7c08bd2b7f67727566a77f444d491
TJ <tj.durnford@microsoft.com>
2020-11-24 17:23:25 -1000

    persist machine id

--- Composer/packages/electron-server/src/utility/machineId.ts
+++ Composer/packages/electron-server/src/utility/machineId.ts
@@ -7,8 +7,12 @@ import { v4 as uuid } from 'uuid';
 
 import { getMacAddress } from './getMacAddress';
 
+let machineId;
 export async function getMachineId(): Promise<string> {
-  return (await getMacMachineId()) || uuid.generateUuid(); // fallback, generate a UUID
+  if (!machineId) {
+    machineId = (await getMacMachineId()) || uuid.generateUuid();
+  }
+  return machineId;
 }
 
 async function getMacMachineId(): Promise<string | undefined> {
