0fd09153685da21fe6213fa8a8d02d60e51857de
zeye <zeye@microsoft.com>
2020-11-26 17:25:09 +0800

    shared the query logic of lg tempalte

--- Composer/packages/ui-plugins/lg/src/LgField.tsx
+++ Composer/packages/ui-plugins/lg/src/LgField.tsx
@@ -10,6 +10,8 @@ import { FieldLabel, useFormData } from '@bfc/adaptive-form';
 import { LgMetaData, LgTemplateRef, LgType, CodeEditorSettings } from '@bfc/shared';
 import { filterTemplateDiagnostics } from '@bfc/indexers';
 
+import { locateLgTemplatePosition } from './locateLgTemplatePosition';
+
 const lspServerPath = '/lg-language-server';
 
 const tryGetLgMetaDataType = (lgText: string): string | null => {
@@ -46,12 +48,11 @@ const LgField: React.FC<FieldProps<string>> = (props) => {
   const lgTemplateRef = LgTemplateRef.parse(value);
   const lgName = lgTemplateRef ? lgTemplateRef.name : new LgMetaData(lgType, designerId || '').toString();
 
-  const relatedLgFile = lgFiles.find(
-    (file) => file.templates.find((template) => template.name === lgName) !== undefined
-  );
+  const relatedLgFile = locateLgTemplatePosition(lgFiles, lgName, locale);
 
-  const lgFileId = relatedLgFile ? relatedLgFile.id : `${currentDialog.lgFile}.${locale}`;
-  const lgFile = lgFiles && lgFiles.find((file) => file.id === lgFileId);
+  const fallbackLgFileId = `${currentDialog.lgFile}.${locale}`;
+  const lgFile = relatedLgFile ?? lgFiles.find((f) => f.id === fallbackLgFileId);
+  const lgFileId = lgFile?.id ?? fallbackLgFileId;
 
   const updateLgTemplate = useCallback(
     async (body: string) => {
