405c8d34668e9c28cbfa96de853a2a3dcf0bb682
liweitian <liweitian93@outlook.com>
2020-12-02 20:14:13 +0800

    fix: merge with main (#5048)
    * try-catch around trigger grouping expressions (#5017)
    
    Co-authored-by: Ben Yackley <61990921+beyackle@users.noreply.github.com>
    
    * chore: Updated installOneAuth script to target correct version. (#5020)
    
    * Updated installOneAuth script to target correct version.
    
    * Better refactor
    
    * feat: adaptive expression functions menu (#5015)
    
    * expressions menu
    
    * Fixing aligment of menu
    
    * PR comments
    
    Co-authored-by: Soroush <hatpick@gmail.com>
    
    * fix: Correct handling of focus for Intellisense fields (#5039)
    
    * fix
    
    * Improved handling of focus when leveraging an Intellisense suggestion
    
    * comments on wrong lines
    
    * feat: azure publish with orchestrator (#5011)
    
    * refactor the luis build in azure publish
    
    * update the orchestrator path
    
    * update the cleanup
    
    * fix typo
    
    * catch the build error
    
    * remove qna endpoint
    
    * remove console
    
    * add qnaconfig
    
    Co-authored-by: Geoff Cox (Microsoft) <gcox@microsoft.com>
    Co-authored-by: Ben Yackley <61990921+beyackle@users.noreply.github.com>
    Co-authored-by: Tony Anziano <tonyanziano5@gmail.com>
    Co-authored-by: LouisEugeneMSFT <66701106+LouisEugeneMSFT@users.noreply.github.com>
    Co-authored-by: Soroush <hatpick@gmail.com>
    Co-authored-by: leileizhang <leilzh@microsoft.com>

--- Composer/packages/intellisense/src/components/Intellisense.tsx
+++ Composer/packages/intellisense/src/components/Intellisense.tsx
@@ -16,12 +16,14 @@ export const Intellisense = React.memo(
     id: string;
     value?: any;
     focused?: boolean;
+    completionListOverrideContainerElements?: HTMLDivElement[];
     completionListOverrideResolver?: (value: any) => JSX.Element | null;
     onChange: (newValue: string) => void;
     onBlur?: (id: string) => void;
     children: (renderProps: {
       textFieldValue: any;
       focused?: boolean;
+      cursorPosition?: number;
       onValueChanged: (newValue: any) => void;
       onKeyDownTextField: (event: React.KeyboardEvent<HTMLInputElement>) => void;
       onKeyUpTextField: (event: React.KeyboardEvent<HTMLInputElement>) => void;
@@ -39,6 +41,7 @@ export const Intellisense = React.memo(
       onChange,
       onBlur,
       children,
+      completionListOverrideContainerElements,
     } = props;
 
     const [textFieldValue, setTextFieldValue] = React.useState(value);
@@ -90,6 +93,13 @@ export const Intellisense = React.memo(
           shouldBlur = false;
         }
 
+        if (
+          completionListOverrideContainerElements &&
+          completionListOverrideContainerElements.some((item) => !checkIsOutside(x, y, item))
+        ) {
+          shouldBlur = false;
+        }
+
         if (shouldBlur) {
           setShowCompletionList(false);
           setCursorPosition(-1);
@@ -111,7 +121,7 @@ export const Intellisense = React.memo(
         document.body.removeEventListener('click', outsideClickHandler);
         document.body.removeEventListener('keydown', keydownHandler);
       };
-    }, [focused, onBlur]);
+    }, [focused, onBlur, completionListOverrideContainerElements]);
 
     // When textField value is changed
     const onValueChanged = (newValue: string) => {
@@ -132,6 +142,7 @@ export const Intellisense = React.memo(
             selectedSuggestion +
             textFieldValue.substr(range.end.character);
           onValueChanged(newValue);
+          setCursorPosition(range.start.character + selectedSuggestion.length);
         } else {
           onValueChanged(selectedSuggestion);
         }
@@ -188,7 +199,15 @@ export const Intellisense = React.memo(
 
     return (
       <div onKeyUp={onKeyUpMainComponent} ref={mainContainerRef} style={{ position: 'relative' }}>
-        {children({ textFieldValue, focused, onValueChanged, onKeyDownTextField, onKeyUpTextField, onClickTextField })}
+        {children({
+          textFieldValue,
+          focused,
+          cursorPosition,
+          onValueChanged,
+          onKeyDownTextField,
+          onKeyUpTextField,
+          onClickTextField,
+        })}
 
         {completionListOverride || showCompletionList ? (
           <CompletionList
