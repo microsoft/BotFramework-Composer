aea4ac6952a33b32c295e0866b09563f9b7b64e4
Lu Han <32191031+luhan2017@users.noreply.github.com>
2020-12-09 10:19:51 +0800

    Update webpack.config.js

--- extensions/azurePublish/webpack.config.js
+++ extensions/azurePublish/webpack.config.js
@@ -6,79 +6,35 @@ const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = [
   {
-    entry: './src/ui/index.tsx',
-    mode: 'production',
-    target: 'web',
-    output: {
-      filename: 'publish.js',
-      path: resolve(__dirname, 'lib', 'ui'),
+      entry: './src/node/index.ts',
+      mode: 'production',
+      devtool: 'source-map',
+      target: 'node',
+      node: {
+     __dirname: false,
+     __filename: false,
+     global: false,
     },
-    externals: {
-      react: 'React',
-      'react-dom': 'ReactDOM',
-      'office-ui-fabric-react': 'Fabric',
+    output: {
+      path: resolve(__dirname, 'lib', 'node'),
+      filename: 'index.js',
+      libraryTarget: 'commonjs2',
     },
     module: {
-      rules: [
-        { test: /\.tsx?$/, use: 'ts-loader' },
-        {
-          test: /\.svg$/i,
-          use: [
-            {
-              loader: 'url-loader',
-            },
-          ],
-        },
-      ],
+      rules: [{ test: /\.tsx?$/, use: 'ts-loader', exclude: [/node_modules/] }],
     },
     resolve: {
-      extensions: ['.js', '.ts', '.tsx'],
-      alias: {
-        // Support lsp code editor
-        vscode: require.resolve('monaco-languageclient/lib/vscode-compatibility'),
-      },
+      extensions: ['.js', '.ts', '.tsx', '.json'],
     },
-    // Some libraries import Node modules but don't use them in the browser.
-    // Tell Webpack to provide empty mocks for them so importing them works.
-    node: {
-      module: 'empty',
-      dgram: 'empty',
-      dns: 'mock',
-      fs: 'empty',
-      net: 'empty',
-      tls: 'empty',
+    optimization:  {
+      minimize: true,
+      minimizer: [
+        new TerserPlugin({
+          terserOptions: {
+            keep_fnames: /AbortSignal/,
+          },
+        }),
+      ],
     }
-  },
-  // {
-  //   entry: './src/node/index.ts',
-  //   mode: 'production',
-  //   devtool: 'source-map',
-  //   target: 'node',
-  //   node: {
-  //     __dirname: false,
-  //     __filename: false,
-  //     global: false,
-  //   },
-  //   output: {
-  //     path: resolve(__dirname, 'lib', 'node'),
-  //     filename: 'index.js',
-  //     libraryTarget: 'commonjs2',
-  //   },
-  //   module: {
-  //     rules: [{ test: /\.tsx?$/, use: 'ts-loader', exclude: [/node_modules/] }],
-  //   },
-  //   resolve: {
-  //     extensions: ['.js', '.ts', '.tsx', '.json'],
-  //   },
-  //   optimization:  {
-  //     minimize: true,
-  //     minimizer: [
-  //       new TerserPlugin({
-  //         terserOptions: {
-  //           keep_fnames: /AbortSignal/,
-  //         },
-  //       }),
-  //     ],
-  //   }
-  // }
+  }
 ];
