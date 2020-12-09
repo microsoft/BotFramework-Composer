9eb1121e5b1b11988265d1a1afee707cf27d3fae
Wenyi Luo <wenyluo@microsoft.com>
2020-12-09 11:13:14 +0800

    remove annotation

--- extensions/azurePublish/webpack.config.js
+++ extensions/azurePublish/webpack.config.js
@@ -99,36 +99,4 @@ module.exports = [
       tls: 'empty',
     }
   },
-  // {
-  //   entry: './src/node/index.ts',
-  //   mode: 'production',
-  //   devtool: 'source-map',
-  //   target: 'node',
-  //   node: {
-  //     __dirname: false,
-  //     __filename: false,
-  //     global: false,
-  //   },
-  //   output: {
-  //     path: resolve(__dirname, 'lib', 'node'),
-  //     filename: 'index.js',
-  //     libraryTarget: 'commonjs2',
-  //   },
-  //   module: {
-  //     rules: [{ test: /\.tsx?$/, use: 'ts-loader', exclude: [/node_modules/] }],
-  //   },
-  //   resolve: {
-  //     extensions: ['.js', '.ts', '.tsx', '.json'],
-  //   },
-  //   optimization:  {
-  //     minimize: true,
-  //     minimizer: [
-  //       new TerserPlugin({
-  //         terserOptions: {
-  //           keep_fnames: /AbortSignal/,
-  //         },
-  //       }),
-  //     ],
-  //   }
-  // }
 ];
