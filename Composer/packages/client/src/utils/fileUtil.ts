66e8d7625e3b0a12aab23b02aaf2f65541576be3
Ben Yackley <61990921+beyackle@users.noreply.github.com>
2020-12-18 13:50:36 -0800

    various client-side type cleanups

--- Composer/packages/client/src/utils/fileUtil.ts
+++ Composer/packages/client/src/utils/fileUtil.ts
@@ -12,25 +12,16 @@ import { File } from '../recoilModel/types';
 
 import httpClient from './httpUtil';
 
-export function getExtension(filename: string) {
-  if (typeof filename === 'string') {
-    return filename.substring(filename.lastIndexOf('.') + 1, filename.length) || filename;
-  }
-  return filename;
+export function getExtension(filename: string): string {
+  return filename.substring(filename.lastIndexOf('.') + 1, filename.length) || filename;
 }
 
-export function getBaseName(filename: string) {
-  if (typeof filename === 'string') {
-    return filename.substring(0, filename.lastIndexOf('.')) || filename;
-  }
-  return filename;
+export function getBaseName(filename: string): string {
+  return filename.substring(0, filename.lastIndexOf('.')) || filename;
 }
 
-export function upperCaseName(filename: string) {
-  if (typeof filename === 'string') {
-    return filename.charAt(0).toUpperCase() + filename.slice(1);
-  }
-  return filename;
+export function upperCaseName(filename: string): string {
+  return filename.charAt(0).toUpperCase() + filename.slice(1);
 }
 
 export function resolveToBasePath(base: string, relPath: string) {
