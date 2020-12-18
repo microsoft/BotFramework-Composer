a1a2b71a2b2a6545d9f44989da6b19eac4ddd3a7
Ben Yackley <61990921+beyackle@users.noreply.github.com>
2020-12-18 10:56:22 -0800

    fix: improve type-safety of server code (#5384)
    * improve type safety in server code
    
    * more type improvements
    
    * Update fileSettingManager.ts
    
    * clean up reduce thing
    
    * remove more anys
    
    * fix pre-PR problems
    
    Co-authored-by: Dong Lei <donglei@microsoft.com>

--- Composer/packages/server/src/controllers/asset.ts
+++ Composer/packages/server/src/controllers/asset.ts
@@ -1,9 +1,11 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+import { Request, Response } from 'express';
+
 import AssetService from '../services/asset';
 
-async function getProjTemplates(req: any, res: any) {
+async function getProjTemplates(req: Request, res: Response) {
   try {
     const templates = await AssetService.manager.getProjectTemplates();
     res.status(200).json(templates);
