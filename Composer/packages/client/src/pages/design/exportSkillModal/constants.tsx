896248106f79b2ae5df271ca9c4825aa69694942
TJ <tj.durnford@microsoft.com>
2020-12-28 11:16:46 -0700

    fix unsafe regex

--- Composer/packages/client/src/pages/design/exportSkillModal/constants.tsx
+++ Composer/packages/client/src/pages/design/exportSkillModal/constants.tsx
@@ -21,7 +21,7 @@ import {
   SelectTriggers,
 } from './content';
 
-export const VERSION_REGEX = /\d\.\d(\.(\d+|preview-\d+))?/i;
+export const VERSION_REGEX = /\d\.\d(\.\d+|preview-\d+)|\d\.\d/i;
 
 export const SCHEMA_URIS = [
   'https://schemas.botframework.com/schemas/skills/v2.1/skill-manifest.json',
