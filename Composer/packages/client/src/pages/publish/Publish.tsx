346470265ee0c782b96599954e5c28381c4ae06a
yeze322 <zeye@microsoft.com>
2021-01-07 22:57:08 +0800

    extract publishPageUtils

--- Composer/packages/client/src/pages/publish/Publish.tsx
+++ Composer/packages/client/src/pages/publish/Publish.tsx
@@ -26,70 +26,13 @@ import { BotStatusList } from './BotStatusList';
 import { getPendingNotificationCardProps, getPublishedNotificationCardProps } from './Notifications';
 import { PullDialog } from './pullDialog';
 import { PublishToolbar } from './PublishToolbar';
-import { Bot, BotStatus, BotPublishHistory, BotPropertyType } from './type';
-
-const deleteNotificationInterval = 5000;
-
-const generateComputedData = (botProjectData) => {
-  // fill Settings, status, publishType, publish target for bot from botProjectMeta
-  const botPropertyData: BotPropertyType = {};
-  const botList: Bot[] = [];
-  botProjectData.forEach((bot) => {
-    const botProjectId = bot.projectId;
-    const publishTargets = bot.setting ? bot.setting.publishTargets || [] : [];
-    botPropertyData[botProjectId] = {
-      setting: bot.setting,
-      publishTargets,
-      publishTypes: bot.publishTypes,
-    };
-    const tmpBot = { id: bot.projectId, name: bot.name, publishTarget: '' };
-    if (publishTargets.length > 0) {
-      tmpBot.publishTarget = publishTargets[0].name;
-    }
-    botList.push(tmpBot);
-  });
-  return { botPropertyData, botList };
-};
-
-const generateBotStatusList = (
-  botList: Bot[],
-  botPropertyData: BotPropertyType,
-  botPublishHistoryList: BotPublishHistory
-): BotStatus[] => {
-  const bots = botList.map((bot) => {
-    const botStatus: BotStatus = Object.assign({}, bot);
-    const publishTargets = botPropertyData[bot.id].publishTargets;
-    const publishHistory = botPublishHistoryList[bot.id];
-    if (publishTargets.length > 0 && botStatus.publishTarget && publishHistory) {
-      botStatus.publishTargets = publishTargets;
-      if (publishHistory[botStatus.publishTarget] && publishHistory[botStatus.publishTarget].length > 0) {
-        const history = publishHistory[botStatus.publishTarget][0];
-        botStatus.time = history.time;
-        botStatus.comment = history.comment;
-        botStatus.message = history.message;
-        botStatus.status = history.status;
-      }
-    }
-    return botStatus;
-  });
-  return bots;
-};
-
-// Recover the loading status when switching back to publish page.
-const initUpdaterStatus = (history: BotPublishHistory) => {
-  const status = {};
-  Object.keys(history).forEach((skillId) => {
-    const targetHistory = history[skillId];
-    Object.keys(targetHistory).forEach((targetName) => {
-      const historyData = targetHistory[targetName];
-      if (Array.isArray(historyData) && historyData[0] && historyData[0].status === ApiStatus.Publishing) {
-        status[`${skillId}/${targetName}`] = true;
-      }
-    });
-  });
-
-  return status;
-};
+import { Bot, BotStatus } from './type';
+import {
+  initUpdaterStatus,
+  generateComputedData,
+  generateBotStatusList,
+  deleteNotificationInterval,
+} from './publishPageUtils';
 
 const Publish: React.FC<RouteComponentProps<{ projectId: string; targetName?: string }>> = (props) => {
   const { projectId = '' } = props;
