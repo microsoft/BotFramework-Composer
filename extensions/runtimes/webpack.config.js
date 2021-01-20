991e9374807b4cb0b172c7887b3e3befe807c94d
Andy Brown <asbrown002@gmail.com>
2021-01-20 13:35:20 -0800

    clean up old build script config

--- extensions/runtimes/webpack.config.js
+++
@@ -1,6 +0,0 @@
-const withDefaults = require('../webpack.config.shared');
-
-module.exports = withDefaults({
-  entry: { extension: './src/index.ts' },
-  context: __dirname,
-});
--- extensions/webRoute/webpack.config.js
+++
@@ -1,6 +0,0 @@
-const withDefaults = require('../webpack.config.shared');
-
-module.exports = withDefaults({
-  entry: { extension: './index.js' },
-  context: __dirname,
-});
