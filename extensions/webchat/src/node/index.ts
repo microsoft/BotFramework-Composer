241836d42ed38bc0be9efc572191aa4c86427b9d
yeze322 <zeye@microsoft.com>
2021-01-27 22:16:25 +0800

    webchat boilderpolate

---
+++ extensions/webchat/src/node/index.ts
@@ -0,0 +1,11 @@
+import { IExtensionRegistration } from '@botframework-composer/types';
+
+function initialize(registration: IExtensionRegistration) {
+  registration.addWebRoute('get', '/webchat', (req, res) => {
+    res.send('Webchat!');
+  });
+}
+
+module.exports = {
+  initialize,
+};
