33730a6f520c1e26d219169648e77aab1976e85c
yeze322 <zeye@microsoft.com>
2021-01-27 22:16:25 +0800

    webchat boilderpolate

---
+++ extensions/webchat/webpack.config.js
@@ -0,0 +1,30 @@
+const { resolve } = require('path');
+
+function getWebpackConfigs() {
+  const isDevelopment = process.env.NODE_ENV === 'development';
+  return [
+    {
+      entry: './src/node/index.ts',
+      mode: isDevelopment ? 'development' : 'production',
+      target: 'node',
+      devtool: isDevelopment ? 'source-map' : undefined,
+      output: {
+        path: resolve(__dirname, 'lib', 'node'),
+        filename: 'index.js',
+        libraryTarget: 'commonjs2',
+        /**
+         * Node files aren't being loaded by Webpack, so we want the source maps to point to the files on disk
+         */
+        devtoolModuleFilenameTemplate: isDevelopment ? '[absolute-resource-path]' : undefined,
+      },
+      module: {
+        rules: [{ test: /\.tsx?$/, use: 'ts-loader', exclude: [/node_modules/] }],
+      },
+      resolve: {
+        extensions: ['.js', '.ts', '.tsx'],
+      },
+    },
+  ];
+}
+
+module.exports = getWebpackConfigs();
