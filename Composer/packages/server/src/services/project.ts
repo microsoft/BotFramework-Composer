303744e425d68c90ece5a0ce4dbc047b3937bdab
lei9444 <leilzh@microsoft.com>
2021-02-03 01:26:48 +0800

    fix: refinde the error message when the bot is deleted

--- Composer/packages/server/src/services/project.ts
+++ Composer/packages/server/src/services/project.ts
@@ -254,7 +254,7 @@ export class BotProjectService {
       if (!(await StorageService.checkBlob('default', path, user))) {
         BotProjectService.deleteRecentProject(path);
         BotProjectService.removeProjectIdFromCache(projectId);
-        throw new Error(`file ${path} does not exist`);
+        throw new Error(`${path} doesn't seems to be exists anymore`);
       }
       const project = new BotProject({ storageId: 'default', path: path }, user, eTag);
       await project.init();
@@ -304,7 +304,7 @@ export class BotProjectService {
         if (!(await StorageService.checkBlob('default', path, user))) {
           BotProjectService.deleteRecentProject(path);
           BotProjectService.removeProjectIdFromCache(matchingProjectId);
-          throw new Error(`file ${path} does not exist`);
+          throw new Error(`${path} doesn't seems to be exists anymore`);
         }
         const project = new BotProject({ storageId: 'default', path: path }, user, eTag);
         await project.init();
