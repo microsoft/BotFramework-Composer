814ac4838207fe928951dd352866d7f0df5fcccf
lei9444 <leilzh@microsoft.com>
2021-02-03 09:19:03 +0800

    update the string

--- Composer/packages/server/src/services/project.ts
+++ Composer/packages/server/src/services/project.ts
@@ -254,7 +254,7 @@ export class BotProjectService {
       if (!(await StorageService.checkBlob('default', path, user))) {
         BotProjectService.deleteRecentProject(path);
         BotProjectService.removeProjectIdFromCache(projectId);
-        throw new Error(`${path} doesn't seems to be exists any longer`);
+        throw new Error(`${path} doesn't seem to be exist any longer`);
       }
       const project = new BotProject({ storageId: 'default', path: path }, user, eTag);
       await project.init();
@@ -304,7 +304,7 @@ export class BotProjectService {
         if (!(await StorageService.checkBlob('default', path, user))) {
           BotProjectService.deleteRecentProject(path);
           BotProjectService.removeProjectIdFromCache(matchingProjectId);
-          throw new Error(`${path} doesn't seems to be exists any longer`);
+          throw new Error(`${path} doesn't seem to be exist any longer`);
         }
         const project = new BotProject({ storageId: 'default', path: path }, user, eTag);
         await project.init();
