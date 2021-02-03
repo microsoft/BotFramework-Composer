a497f5db77f0597495ffc8fee015ccc9cfe60370
Patrick Volum <pavolum@microsoft.com>
2021-02-03 13:55:13 -0700

    added unit test ffor new fetchtemplate server endpoint

--- Composer/packages/server/src/controllers/__tests__/asset.test.ts
+++ Composer/packages/server/src/controllers/__tests__/asset.test.ts
@@ -26,3 +26,20 @@ describe('get bot project templates', () => {
     expect(mockRes.status).toHaveBeenCalledWith(200);
   });
 });
+
+describe('get feed driven bot project templates', () => {
+  it('should get project templates', async () => {
+    const mockReq = {
+      params: {},
+      query: {},
+      body: {
+        feedUrls: [
+          'https://registry.npmjs.org/-/v1/search?text=conversationalcore&size=100&from=0&quality=0.65&popularity=0.98&maintenance=0.5',
+        ],
+        getFirstPartyNpm: false,
+      },
+    } as Request;
+    await AssetController.getProjTemplatesV2(mockReq, mockRes);
+    expect(mockRes.status).toHaveBeenCalledWith(200);
+  });
+});
