b05ce16d7720ce1814ff6d1f0018dba129545ede
zeye <zeye@microsoft.com>
2021-02-07 16:53:15 +0800

    add Teams support in runtime template

--- runtime/dotnet/azurefunctions/Startup.cs
+++ runtime/dotnet/azurefunctions/Startup.cs
@@ -13,6 +13,7 @@ using Microsoft.Bot.Builder.Azure.Blobs;
 using Microsoft.Bot.Builder.BotFramework;
 using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Builder.Dialogs.Adaptive;
+using Microsoft.Bot.Builder.Dialogs.Adaptive.Teams;
 using Microsoft.Bot.Builder.Dialogs.Declarative;
 using Microsoft.Bot.Builder.Dialogs.Declarative.Resources;
 using Microsoft.Bot.Builder.Integration.ApplicationInsights.Core;
@@ -75,6 +76,7 @@ namespace Microsoft.BotFramework.Composer.Functions
             ComponentRegistration.Add(new LanguageGenerationComponentRegistration());
             ComponentRegistration.Add(new QnAMakerComponentRegistration());
             ComponentRegistration.Add(new LuisComponentRegistration());
+            ComponentRegistration.Add(new TeamsComponentRegistration());
 
             // This is for custom action component registration.
             //ComponentRegistration.Add(new CustomActionComponentRegistration());
