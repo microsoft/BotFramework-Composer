75e43602e4d642cefaca10d5cac906d908e3f93a
zeye <zeye@microsoft.com>
2021-02-07 13:35:33 +0800

    add Teams kinds in ui-plugin/teams

---
+++ Composer/packages/ui-plugins/teams/src/actions.ts
@@ -0,0 +1,179 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import { UISchema } from '@bfc/extension-client';
+import { SDKKinds } from '@bfc/shared';
+
+export default {
+  [SDKKinds.GetMeetingParticipant]: {
+    menu: {
+      label: '',
+      submenu: ['Teams'],
+    },
+    flow: {
+      widget: 'ActionCard',
+    },
+  },
+  [SDKKinds.GetMember]: {
+    menu: {
+      label: '',
+      submenu: ['Teams'],
+    },
+    flow: {
+      widget: 'ActionCard',
+    },
+  },
+  [SDKKinds.GetPagedMembers]: {
+    menu: {
+      label: '',
+      submenu: ['Teams'],
+    },
+    flow: {
+      widget: 'ActionCard',
+    },
+  },
+  [SDKKinds.GetPagedTeamMembers]: {
+    menu: {
+      label: '',
+      submenu: ['Teams'],
+    },
+    flow: {
+      widget: 'ActionCard',
+    },
+  },
+  [SDKKinds.GetTeamChannels]: {
+    menu: {
+      label: '',
+      submenu: ['Teams'],
+    },
+    flow: {
+      widget: 'ActionCard',
+    },
+  },
+  [SDKKinds.GetTeamDetails]: {
+    menu: {
+      label: '',
+      submenu: ['Teams'],
+    },
+    flow: {
+      widget: 'ActionCard',
+    },
+  },
+  [SDKKinds.GetTeamMember]: {
+    menu: {
+      label: '',
+      submenu: ['Teams'],
+    },
+    flow: {
+      widget: 'ActionCard',
+    },
+  },
+  [SDKKinds.SendAppBasedLinkQueryResponse]: {
+    menu: {
+      label: '',
+      submenu: ['Teams'],
+    },
+    flow: {
+      widget: 'ActionCard',
+    },
+  },
+  [SDKKinds.SendMessageToTeamsChannel]: {
+    menu: {
+      label: '',
+      submenu: ['Teams'],
+    },
+    flow: {
+      widget: 'ActionCard',
+    },
+  },
+  [SDKKinds.SendMessagingExtensionActionResponse]: {
+    menu: {
+      label: '',
+      submenu: ['Teams'],
+    },
+    flow: {
+      widget: 'ActionCard',
+    },
+  },
+  [SDKKinds.SendMessagingExtensionAttachmentsResponse]: {
+    menu: {
+      label: '',
+      submenu: ['Teams'],
+    },
+    flow: {
+      widget: 'ActionCard',
+    },
+  },
+  [SDKKinds.SendMessagingExtensionAuthResponse]: {
+    menu: {
+      label: '',
+      submenu: ['Teams'],
+    },
+    flow: {
+      widget: 'ActionCard',
+    },
+  },
+  [SDKKinds.SendMessagingExtensionBotMessagePreviewResponse]: {
+    menu: {
+      label: '',
+      submenu: ['Teams'],
+    },
+    flow: {
+      widget: 'ActionCard',
+    },
+  },
+  [SDKKinds.SendMessagingExtensionConfigQuerySettingUrlResponse]: {
+    menu: {
+      label: '',
+      submenu: ['Teams'],
+    },
+    flow: {
+      widget: 'ActionCard',
+    },
+  },
+  [SDKKinds.SendMessagingExtensionMessageResponse]: {
+    menu: {
+      label: '',
+      submenu: ['Teams'],
+    },
+    flow: {
+      widget: 'ActionCard',
+    },
+  },
+  [SDKKinds.SendMessagingExtensionSelectItemResponse]: {
+    menu: {
+      label: '',
+      submenu: ['Teams'],
+    },
+    flow: {
+      widget: 'ActionCard',
+    },
+  },
+  [SDKKinds.SendTaskModuleCardResponse]: {
+    menu: {
+      label: '',
+      submenu: ['Teams'],
+    },
+    flow: {
+      widget: 'ActionCard',
+    },
+  },
+  [SDKKinds.SendTaskModuleMessageResponse]: {
+    menu: {
+      label: '',
+      submenu: ['Teams'],
+    },
+    flow: {
+      widget: 'ActionCard',
+    },
+  },
+  [SDKKinds.SendTaskModuleUrlResponse]: {
+    menu: {
+      label: '',
+      submenu: ['Teams'],
+    },
+    flow: {
+      widget: 'ActionCard',
+    },
+  },
+} as UISchema;
---
+++ Composer/packages/ui-plugins/teams/src/triggers.ts
@@ -0,0 +1,170 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import { UISchema } from '@bfc/extension-client';
+import { SDKKinds } from '@bfc/shared';
+
+export default {
+  [SDKKinds.OnTeamsAppBasedLinkQuery]: {
+    trigger: {
+      label: '',
+      submenu: 'Teams',
+    },
+  },
+  [SDKKinds.OnTeamsCardAction]: {
+    trigger: {
+      label: '',
+      submenu: 'Teams',
+    },
+  },
+  [SDKKinds.OnTeamsChannelCreated]: {
+    trigger: {
+      label: '',
+      submenu: 'Teams',
+    },
+  },
+  [SDKKinds.OnTeamsChannelDeleted]: {
+    trigger: {
+      label: '',
+      submenu: 'Teams',
+    },
+  },
+  [SDKKinds.OnTeamsChannelRenamed]: {
+    trigger: {
+      label: '',
+      submenu: 'Teams',
+    },
+  },
+  [SDKKinds.OnTeamsChannelRestored]: {
+    trigger: {
+      label: '',
+      submenu: 'Teams',
+    },
+  },
+  [SDKKinds.OnTeamsFileConsent]: {
+    trigger: {
+      label: '',
+      submenu: 'Teams',
+    },
+  },
+  [SDKKinds.OnTeamsMessagingExtensionCardButtonClicked]: {
+    trigger: {
+      label: '',
+      submenu: 'Teams',
+    },
+  },
+  [SDKKinds.OnTeamsMessagingExtensionConfigurationQuerySettingUrl]: {
+    trigger: {
+      label: '',
+      submenu: 'Teams',
+    },
+  },
+  [SDKKinds.OnTeamsMessagingExtensionConfigurationSetting]: {
+    trigger: {
+      label: '',
+      submenu: 'Teams',
+    },
+  },
+  [SDKKinds.OnTeamsMessagingExtensionFetchTask]: {
+    trigger: {
+      label: '',
+      submenu: 'Teams',
+    },
+  },
+  [SDKKinds.OnTeamsMessagingExtensionQuery]: {
+    trigger: {
+      label: '',
+      submenu: 'Teams',
+    },
+  },
+  [SDKKinds.OnTeamsMessagingExtensionSelectItem]: {
+    trigger: {
+      label: '',
+      submenu: 'Teams',
+    },
+  },
+  [SDKKinds.OnTeamsMessagingExtensionSubmitAction]: {
+    trigger: {
+      label: '',
+      submenu: 'Teams',
+    },
+  },
+  [SDKKinds.OnTeamsO365ConnectorCardAction]: {
+    trigger: {
+      label: '',
+      submenu: 'Teams',
+    },
+  },
+  [SDKKinds.OnTeamsTaskModuleFetch]: {
+    trigger: {
+      label: '',
+      submenu: 'Teams',
+    },
+  },
+  [SDKKinds.OnTeamsTaskModuleSubmit]: {
+    trigger: {
+      label: '',
+      submenu: 'Teams',
+    },
+  },
+  [SDKKinds.OnTeamsTeamArchived]: {
+    trigger: {
+      label: '',
+      submenu: 'Teams',
+    },
+  },
+  [SDKKinds.OnTeamsTeamDeleted]: {
+    trigger: {
+      label: '',
+      submenu: 'Teams',
+    },
+  },
+  [SDKKinds.OnTeamsTeamHardDeleted]: {
+    trigger: {
+      label: '',
+      submenu: 'Teams',
+    },
+  },
+  [SDKKinds.OnTeamsTeamRenamed]: {
+    trigger: {
+      label: '',
+      submenu: 'Teams',
+    },
+  },
+  [SDKKinds.OnTeamsTeamRestored]: {
+    trigger: {
+      label: '',
+      submenu: 'Teams',
+    },
+  },
+  [SDKKinds.OnTeamsTeamUnarchived]: {
+    trigger: {
+      label: '',
+      submenu: 'Teams',
+    },
+  },
+  [SDKKinds.OnTeamsMessagingExtensionBotMessagePreviewEdit]: {
+    trigger: {
+      label: '',
+      submenu: 'Teams',
+    },
+  },
+  [SDKKinds.OnTeamsMessagingExtensionBotMessagePreviewSend]: {
+    trigger: {
+      label: '',
+      submenu: 'Teams',
+    },
+  },
+  [SDKKinds.OnTeamsTabFetch]: {
+    trigger: {
+      label: '',
+      submenu: 'Teams',
+    },
+  },
+  [SDKKinds.OnTeamsTabSubmit]: {
+    trigger: {
+      label: '',
+      submenu: 'Teams',
+    },
+  },
+} as UISchema;
