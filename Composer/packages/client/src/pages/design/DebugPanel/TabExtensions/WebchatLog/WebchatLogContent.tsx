a6c5e5554c6a7449015fa31b10e7abddb85ba7af
Ze Ye <zeye@microsoft.com>
2021-02-09 18:50:54 +0800

    add more event listeners

--- Composer/packages/client/src/pages/design/DebugPanel/TabExtensions/WebchatLog/WebchatLogContent.tsx
+++ Composer/packages/client/src/pages/design/DebugPanel/TabExtensions/WebchatLog/WebchatLogContent.tsx
@@ -2,9 +2,10 @@
 // Licensed under the MIT License.
 
 import { extensionEventEmitter } from '@bfc/extension-client';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 
 export interface WebchatLog {
+  skillId: string;
   conversationId: string;
   type: 'error' | 'warning' | 'info';
   message: string;
@@ -13,15 +14,31 @@ export interface WebchatLog {
 export const WebchatLogContent = () => {
   const [webchatLogs, setWebchatLogs] = useState<WebchatLog[]>([]);
 
+  const appendLog = useCallback((log: WebchatLog) => {
+    setWebchatLogs([...webchatLogs, log]);
+  }, []);
+
+  const clearLogs = useCallback(() => {
+    setWebchatLogs([]);
+  }, []);
+
   useEffect(() => {
-    extensionEventEmitter.on('wechat:log', (log) => {
-      setWebchatLogs([...webchatLogs, log]);
+    extensionEventEmitter.on('wechat:log', (log: WebchatLog) => {
+      appendLog(log);
+    });
+
+    extensionEventEmitter.on('webchat:start', () => {
+      clearLogs();
+    });
+
+    extensionEventEmitter.on('webchat:restart', () => {
+      clearLogs();
     });
   }, []);
 
   return (
     <div>
-      Webchat Content
+      Webchat logs
       <div>
         {webchatLogs.map((log, idx) => (
           <div key={`webchatLog-${idx}`}>
