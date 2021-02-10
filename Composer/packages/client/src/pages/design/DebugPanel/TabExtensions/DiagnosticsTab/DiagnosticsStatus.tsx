46db12fd4d4126f1335e2dc7b83b5608f0af8808
zeye <zeye@microsoft.com>
2021-02-10 13:38:56 +0800

    adjust status icons

--- Composer/packages/client/src/pages/design/DebugPanel/TabExtensions/DiagnosticsTab/DiagnosticsStatus.tsx
+++ Composer/packages/client/src/pages/design/DebugPanel/TabExtensions/DiagnosticsTab/DiagnosticsStatus.tsx
@@ -25,28 +25,35 @@ export const DiagnosticsStatus = () => {
       <DefaultButton
         iconProps={{ iconName: 'StatusErrorFull' }}
         styles={{
-          root: { height: '18px', padding: 0, paddingRight: '3px', minWidth: '20px', marginRight: '8px' },
-          label: { fontSize: '10px', fontFamily: 'Segoe UI', lineHeight: '20px' },
-          icon: { color: '#EB3941', fontSize: '10px', lineHeight: '16px' },
+          root: {
+            height: '36px',
+            padding: 0,
+            paddingRight: '3px',
+            minWidth: '20px',
+            border: 'none',
+            marginRight: '8px',
+          },
+          label: { fontSize: '18px', fontFamily: 'Segoe UI', lineHeight: '20px' },
+          icon: { color: '#EB3941', fontSize: '18px', lineHeight: '18px' },
         }}
         onClick={() => {
           setExpansion(true);
         }}
       >
-        {errorsCount} Errors
+        {errorsCount}
       </DefaultButton>
       <DefaultButton
         iconProps={{ iconName: 'WarningSolid' }}
         styles={{
-          root: { height: '18px', padding: 0, paddingRight: '3px', minWidth: '20px' },
-          label: { fontSize: '10px', fontFamily: 'Segoe UI', lineHeight: '20px' },
-          icon: { color: '#F4BD00', fontSize: '10px', lineHeight: '16px' },
+          root: { height: '36px', padding: 0, paddingRight: '3px', border: 'none', minWidth: '20px' },
+          label: { fontSize: '18px', fontFamily: 'Segoe UI', lineHeight: '20px' },
+          icon: { color: '#F4BD00', fontSize: '18px', lineHeight: '18px' },
         }}
         onClick={() => {
           setExpansion(true);
         }}
       >
-        {warningsCount} Warnings
+        {warningsCount}
       </DefaultButton>
     </div>
   );
