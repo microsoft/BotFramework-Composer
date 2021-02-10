1f4ba71802029914d8d1093a5679eb5931f79af0
zeye <zeye@microsoft.com>
2021-02-10 13:06:32 +0800

    adapt the navigate api

--- Composer/packages/client/src/pages/design/DebugPanel/TabExtensions/DiagnosticsTab/DiagnosticsTabContent.tsx
+++ Composer/packages/client/src/pages/design/DebugPanel/TabExtensions/DiagnosticsTab/DiagnosticsTabContent.tsx
@@ -2,23 +2,29 @@
 // Licensed under the MIT License.
 
 import React from 'react';
-import { useRecoilValue } from 'recoil';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
 
-import { currentProjectIdState } from '../../../../../recoilModel';
+import { currentProjectIdState, exportSkillModalInfoState } from '../../../../../recoilModel';
+import { navigateTo } from '../../../../../utils/navigation';
 import { DiagnosticList } from '../../../../diagnostics/DiagnosticList';
 
 import { useDiagnosticsData } from './useDiagnostics';
 
 export const DiagnosticsContent = () => {
   const skillId = useRecoilValue(currentProjectIdState);
+  const setExportSkillModalInfo = useSetRecoilState(exportSkillModalInfoState);
+
   const diagnostics = useDiagnosticsData();
 
   return (
     <DiagnosticList
       diagnosticItems={diagnostics}
       skillId={skillId}
-      onItemClick={() => {
-        // TODO: migrate the navigation logic
+      onItemClick={(item) => {
+        navigateTo(item.getUrl());
+        if (item.location === 'manifest.json') {
+          setExportSkillModalInfo(item.projectId);
+        }
       }}
     />
   );
