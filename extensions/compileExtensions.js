2c3a738603b08d7ebbf4d12373f4b8af14d1e62b
Andy Brown <asbrown002@gmail.com>
2021-02-12 08:51:09 -0800

    extract build script
    adds ability for each extension to run a build as well as watch mode

--- extensions/compileExtensions.js
+++ extensions/compileExtensions.js
@@ -9,8 +9,7 @@ const path = require('path');
 // eslint-disable-next-line security/detect-child-process
 const { execSync } = require('child_process');
 
-const esbuild = require('esbuild');
-const GlobalsPlugin = require('esbuild-plugin-globals');
+const { compile } = require('./build');
 
 const FORCE = process.argv.includes('--force') || process.argv.includes('-f');
 
@@ -94,104 +93,9 @@ const hasChanges = (name, lastModified) => {
   return buildCache[name] ? new Date(buildCache[name]) < lastModified : true;
 };
 
-const getBundleConfigs = (extPath, packageJSON) => {
-  const buildConfigs = [];
-
-  const defaultConfig = Object.assign(
-    {
-      bundle: true,
-      logLevel: 'error',
-    },
-    packageJSON.esbuild
-  );
-
-  // check for node entry
-  for (const nodeEntry of ['', 'src', 'src/node']) {
-    const p = path.join(extPath, nodeEntry, 'index.ts');
-    if (fs.pathExistsSync(p)) {
-      buildConfigs.push(
-        Object.assign(
-          { ...defaultConfig },
-          {
-            entryPoints: [p],
-            outfile: path.join(extPath, 'dist/extension.js'),
-            platform: 'node',
-            target: ['node12.13.0'],
-          }
-        )
-      );
-    }
-  }
-
-  // for each contrib, create entry for each file
-  const contribs = ['pages', 'publish'];
-
-  for (const contribDir of contribs) {
-    const p = path.join(extPath, 'src', contribDir);
-
-    if (fs.pathExistsSync(p)) {
-      for (const contrib of fs.readdirSync(p, { withFileTypes: true })) {
-        const cPath = path.join(p, contrib.name);
-
-        buildConfigs.push(
-          Object.assign(
-            { ...defaultConfig },
-            {
-              outdir: path.join(extPath, './dist'),
-              entryPoints: [cPath],
-              target: ['es2015'],
-              plugins: [
-                GlobalsPlugin({
-                  react: 'React',
-                  'react-dom': 'ReactDOM',
-                  '@bfc/extension-client': 'ExtensionClient',
-                  'office-ui-fabric-react': 'Fabric',
-                  '@bfc/code-editor': 'CodeEditors',
-                  '@bfc/ui-shared': 'UIShared',
-                }),
-              ],
-              loader: {
-                '.svg': 'dataurl',
-                '.png': 'file',
-              },
-              define: {
-                'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
-              },
-            }
-          )
-        );
-      }
-    }
-  }
-
-  return buildConfigs;
-};
-
-const cleanDist = async (name, extPath) => {
-  console.log('[%s] cleaning dist', name);
-  await fs.emptyDir(path.join(extPath, 'dist'));
-};
-
-const compile = async (name, extPath) => {
-  const packageJSON = JSON.parse(fs.readFileSync(path.join(extPath, 'package.json')));
-  const hasBuild = packageJSON && packageJSON.scripts && packageJSON.scripts.build;
-
-  console.log('[%s] compiling', name);
-  console.log('[%s] yarn install', name);
+const install = async (name, extPath) => {
+  console.log(`[%s] yarn install --production=false --frozen-lockfile ${FORCE ? '--force' : ''}`, name);
   execSync(`yarn --production=false --frozen-lockfile ${FORCE ? '--force' : ''}`, { cwd: extPath, stdio: 'inherit' });
-
-  const service = await esbuild.startService();
-  const work = [];
-
-  try {
-    for (const config of getBundleConfigs(extPath, packageJSON)) {
-      work.push(service.build(config));
-    }
-
-    await Promise.all(work);
-  } finally {
-    await service.stop();
-  }
 };
 
 async function main() {
@@ -212,7 +116,7 @@ async function main() {
       const lastModified = getLastModified(extPath);
       if (FORCE || missingMain(extPath, packageJSON) || hasChanges(entry.name, lastModified)) {
         try {
-          await cleanDist(entry.name, extPath);
+          await install(entry.name, extPath);
           await compile(entry.name, extPath);
           writeToCache(entry.name, lastModified);
         } catch (err) {
