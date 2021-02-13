dbcd27704356bfd830ff6af2b9e3ad2ca0f08174
Andy Brown <asbrown002@gmail.com>
2021-02-12 16:08:32 -0800

    add monaco instance to window when init'ing

--- Composer/packages/lib/code-editor/src/index.ts
+++ Composer/packages/lib/code-editor/src/index.ts
@@ -4,7 +4,11 @@
 import { monaco } from '@monaco-editor/react';
 
 // initialize moanco api as early as possible
-monaco.init();
+monaco.init().then(instance => {
+  console.log('setting monaco on window', instance);
+  // @ts-ignore
+  window.monaco = instance;
+});
 
 export { EditorDidMount } from '@monaco-editor/react';
 export * from './BaseEditor';
--- extensions/adaptiveCardsDesigner/src/pages/Library.tsx
+++ extensions/adaptiveCardsDesigner/src/pages/Library.tsx
@@ -3,15 +3,17 @@
 
 /** @jsx jsx */
 import { jsx } from '@emotion/core';
-import React, { useEffect , useState, useRef } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { render } from '@bfc/extension-client';
+import { CardDesigner } from 'adaptivecards-designer';
+
+import 'adaptivecards-designer/dist/adaptivecards-designer.css';
 
 // import * as monaco from "monaco-editor";
 // import * as markdownit from "markdown-it";
 // import * as ACDesigner from "adaptivecards-designer";
 
 type myDiv = HTMLDivElement;
-
 
 // <script src="https://unpkg.com/adaptivecards@latest/dist/adaptivecards.min.js"></script>
 // <!-- REQUIRED: adaptive-expressions is required by adaptivecards-templating library to enable data binding support in designer -->
@@ -30,77 +32,60 @@ type myDiv = HTMLDivElement;
 // -->
 // <script src="https://unpkg.com/adaptivecards-designer@<VERSION>/dist/adaptivecards-designer.min.js"></script>
 
-
-const scripts = ["https://unpkg.com/adaptivecards@latest/dist/adaptivecards.min.js", "https://unpkg.com/adaptive-expressions@4/lib/browser.js", "https://unpkg.com/adaptivecards-templating@latest/dist/adaptivecards-templating.min.js",  "https://unpkg.com/markdown-it@8.4.0/dist/markdown-it.min.js", "https://unpkg.com/monaco-editor@0.17.1/min/vs/loader.js", "https://unpkg.com/monaco-editor@0.17.1/min/vs/loader.js", "https://unpkg.com/adaptivecards-designer@latest/dist/adaptivecards-designer.min.js" ]
-
-import "adaptivecards-designer/dist/adaptivecards-designer.css";
-
-
+const scripts = [
+  // 'https://unpkg.com/adaptivecards@latest/dist/adaptivecards.min.js',
+  // 'https://unpkg.com/adaptive-expressions@4/lib/browser.js',
+  // 'https://unpkg.com/adaptivecards-templating@latest/dist/adaptivecards-templating.min.js',
+  // 'https://unpkg.com/markdown-it@8.4.0/dist/markdown-it.min.js',
+  // 'https://unpkg.com/monaco-editor@0.17.1/min/vs/loader.js',
+  // 'https://unpkg.com/adaptivecards-designer@latest/dist/adaptivecards-designer.min.js',
+  // 'file:///Users/abrown/Desktop/test.js'
+];
 
 const Library: React.FC = (props) => {
   const [isLoaded, setIsLoaded] = useState(false);
 
-  let myDiv = useRef(null);
+  const myDiv = useRef(null);
 
   useEffect(() => {
-    const promises = [];
-    scripts.forEach((script) => {
-      const s = document.createElement("script");
-      s.src = script;
-
-      const promise = new Promise((resolve) => {s.onload = () => {setTimeout(() => {
-        resolve(undefined)
-      }, 10000)}});
-      promises.push(promise);
-
-      document.body.appendChild(s)
-
-    });
-
-    Promise.all(promises).then(() => {
-      console.log('setting is loaded', window)
-      setIsLoaded(true);
-    })
+    const interval = setInterval(() => {
+      if (window.monaco) {
+        clearInterval(interval);
+        setIsLoaded(true);
+      }
+    }, 500);
   }, []);
 
   useEffect(() => {
     if (isLoaded && myDiv.current) {
-        // const adaptiveCard = new ACDesigner.CardDesigner([new ACDesigner.WebChatContainer("Bot Framework WebChat", "containers/webchat-container.css"),]);
-      type myDiv = HTMLDivElement;
-
-      let hostContainers = [];
+      // const adaptiveCard = new ACDesigner.CardDesigner([new ACDesigner.WebChatContainer("Bot Framework WebChat", "containers/webchat-container.css"),]);
+      const hostContainers = [];
 
       // Optional: add the default Microsoft Host Apps (see docs below)
       // hostContainers = ACDesigner.defaultMicrosoftHosts;
-      console.log('newing up')
+      console.log('newing up');
       // @ts-ignore
-      let designer = new window.ACDesigner.CardDesigner(hostContainers);
+      const designer = new CardDesigner(hostContainers);
 
       // The designer requires various CSS and image assets to work properly,
       // If you've loaded the script from a CDN it needs to know where these assets are located
       // Use the same <VERSION> that you used above
-      designer.assetPath = "https://unpkg.com/adaptivecards-designer@latest/dist";
+      designer.assetPath = 'https://unpkg.com/adaptivecards-designer@latest/dist';
 
       // Initialize monaco-editor for the JSON-editor pane. The simplest way to do this is use the code below
       // @ts-ignore
-      require.config({ paths: { 'vs': 'https://unpkg.com/monaco-editor@0.17.1/min/vs' } });
 
       // @ts-ignore
-      // require(['vs/editor/editor.main'], function () {
-      //   designer.monacoModuleLoaded();
-      // });
-
+      designer.monacoModuleLoaded();
       designer.attachTo(myDiv.current);
-
     }
     // adaptiveCard.attachTo(myDiv);
     // adaptiveCard.monacoModuleLoaded(monaco);
-  }, [isLoaded])
-  return ( <div ref={myDiv} />);
+  }, [isLoaded]);
+  return <div ref={myDiv} />;
 };
 
 render(<Library />);
-
 
 // import * as React from 'react';
 // import * as AdaptiveCards from "microsoft-adaptivecards";
