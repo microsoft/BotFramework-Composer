12fa58f3405ae3cae80596039355b96c519970c8
zeye <zeye@microsoft.com>
2021-02-16 20:11:29 +0800

    fix webchat webchat config

--- extensions/webchat/webpack.config.js
+++ extensions/webchat/webpack.config.js
@@ -1,30 +1,31 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
 const { resolve } = require('path');
+const { DefinePlugin } = require('webpack');
 
-function getWebpackConfigs() {
-  const isDevelopment = process.env.NODE_ENV === 'development';
-  return [
-    {
-      entry: './src/node/index.ts',
-      mode: isDevelopment ? 'development' : 'production',
-      target: 'node',
-      devtool: isDevelopment ? 'source-map' : undefined,
-      output: {
-        path: resolve(__dirname, 'lib', 'node'),
-        filename: 'index.js',
-        libraryTarget: 'commonjs2',
-        /**
-         * Node files aren't being loaded by Webpack, so we want the source maps to point to the files on disk
-         */
-        devtoolModuleFilenameTemplate: isDevelopment ? '[absolute-resource-path]' : undefined,
-      },
-      module: {
-        rules: [{ test: /\.ts$/, use: 'ts-loader', exclude: [/node_modules/] }],
-      },
-      resolve: {
-        extensions: ['.js', '.ts'],
-      },
-    },
-  ];
-}
-
-module.exports = getWebpackConfigs();
+module.exports = {
+  entry: './src/node/index.ts',
+  mode: 'production',
+  devtool: 'source-map',
+  target: 'node',
+  node: {
+    __dirname: false,
+  },
+  output: {
+    path: resolve(__dirname, 'lib'),
+    filename: 'index.js',
+    libraryTarget: 'commonjs2',
+  },
+  module: {
+    rules: [{ test: /\.tsx?$/, use: 'ts-loader', exclude: [/node_modules/] }],
+  },
+  resolve: {
+    extensions: ['.js', '.ts', '.tsx', '.json'],
+    mainFields: ['main'],
+  },
+  plugins: [
+    // reference: https://github.com/node-formidable/formidable/issues/337#issuecomment-153408479
+    new DefinePlugin({ 'global.GENTLY': false })
+  ]
+};
