93f12c963c5e61df3062dbc58f556ae7eb1d17a6
zeye <zeye@microsoft.com>
2021-02-18 15:12:02 +0800

    feat: Debugging panel with integrated diagnostics data (#5686)
    * aadjust two overflow style
    
    * add DebugPanel placeholder
    
    * support collapse/expand
    
    * extension based tab
    
    * render tab content
    
    * update tab ext config comments
    
    * refactor: split DiagnosticsList and DiagnosticsTable
    
    * apply new DiagnosticList in tab extension
    
    * rename tab files to avoid ambiguity with diagnostics page
    
    * contextual diagnostics header
    
    * apply pivot to diagnostics tab header
    
    * tune the collasped tab header of diagnostics
    
    * refactor: add toolbar and aleasy use Pivot as header
    
    * impl V2 design
    
    * add TODOs
    
    * fix eslint
    
    * implement webchat output tab
    
    * subscribe webchat log
    
    * add more event listeners
    
    * filter log by skill ID
    
    * remove the status bar
    
    * update styles
    
    * add indicator
    
    * adapt the navigate api
    
    * update CommandBar status buttons
    
    * inject toolbarWidget from debug extensions
    
    * adjust status icons
    
    * Move diagnostics list
    
    * update diagnostics icon
    
    * hide webchat log
    
    * remove status bar
    
    * remove comments, adjust styles
    
    * fix UT
    
    * update UT mock data
    
    * fix UT
    
    * remove skillId usage in Diagnostics
    
    * clear skillId definitions
    
    * resolve comments
    
    * use standard font size
    
    Co-authored-by: Srinaath Ravichandran <srinaath27@gmail.com>
    Co-authored-by: Dong Lei <donglei@microsoft.com>

---
+++ Composer/packages/client/src/pages/design/DebugPanel/TabExtensions/DiagnosticsTab/useDiagnostics.ts
@@ -0,0 +1,37 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import { useRecoilValue } from 'recoil';
+
+import { IDiagnosticInfo } from '../../../../diagnostics/types';
+import { currentProjectIdState, diagnosticsSelectorFamily } from '../../../../../recoilModel';
+import { DiagnosticSeverity } from '../../../../diagnostics/types';
+
+export const Severity = {
+  Error: DiagnosticSeverity[0],
+  Warning: DiagnosticSeverity[1],
+};
+
+export const useDiagnosticsData = (): IDiagnosticInfo[] => {
+  const projectId = useRecoilValue(currentProjectIdState);
+  const diagnosticData = useRecoilValue(diagnosticsSelectorFamily(projectId));
+
+  return diagnosticData ?? [];
+};
+
+export const useDiagnosticsStatistics = () => {
+  const diagnosticsData = useDiagnosticsData();
+
+  const errorsCount = diagnosticsData.filter((d) => d.severity === Severity.Error).length;
+  const warningsCount = diagnosticsData.filter((d) => d.severity === Severity.Warning).length;
+
+  const hasError = errorsCount > 0;
+  const hasWarning = warningsCount > 0;
+
+  return {
+    errorsCount,
+    warningsCount,
+    hasError,
+    hasWarning,
+  };
+};
---
+++ Composer/packages/client/src/pages/design/DebugPanel/TabExtensions/index.ts
@@ -0,0 +1,9 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import { TabExtensionConfig } from './types';
+import { DiagnosticsTabConfig } from './DiagnosticsTab';
+
+const implementedDebugExtensions: TabExtensionConfig[] = [DiagnosticsTabConfig];
+
+export default implementedDebugExtensions;
