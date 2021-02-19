818455bc3a75171cf84f934a218f8a74c0fe9a12
leilzh <leilzh@microsoft.com>
2021-02-19 18:03:28 +0800

    fix: loadBot will do full index twice

--- Composer/packages/client/src/recoilModel/dispatchers/project.ts
+++ Composer/packages/client/src/recoilModel/dispatchers/project.ts
@@ -478,7 +478,7 @@ export const projectDispatcher = () => {
             // Bot creation successful
             clearInterval(timer);
             callbackHelpers.set(botOpeningMessage, response.data.latestMessage);
-            const { botFiles, projectData } = loadProjectData(response.data.result);
+            const { botFiles, projectData } = await loadProjectData(response.data.result);
             const projectId = response.data.result.id;
             if (settingStorage.get(projectId)) {
               settingStorage.remove(projectId);
