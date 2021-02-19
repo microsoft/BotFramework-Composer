a28b69c2e4941d1ea6dbf0792b6da271e87447d9
zeye <zeye@microsoft.com>
2021-02-19 17:47:17 +0800

    logitem

--- Composer/packages/client/src/pages/design/DebugPanel/TabExtensions/WebchatLog/WebchatLogItem.tsx
+++ Composer/packages/client/src/pages/design/DebugPanel/TabExtensions/WebchatLog/WebchatLogItem.tsx
@@ -10,5 +10,13 @@ export interface WebchatLogItemProps {
 }
 
 export const WebchatLogItem: React.FC<WebchatLogItemProps> = ({ item }) => {
-  return <div css={{}}>{JSON.stringify(item)}</div>;
+  return (
+    <div>
+      [<span css={{ color: '#327e36' }}>{item.timestamp}</span>]
+      <span css={{ marginLeft: 10, color: '#007acc' }}>{item.status}</span>
+      <span css={{ marginLeft: 10, color: '#666' }}>{item.route}</span>
+      <span css={{ marginLeft: 10, color: '#000' }}>{item.message}</span>
+      <span css={{ marginLeft: 10, color: '#000' }}>{item.details}</span>
+    </div>
+  );
 };
