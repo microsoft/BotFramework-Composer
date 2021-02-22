de55f4dd48fb39a38fe1439402e70becf2df8e48
zeye <2295905420@qq.com>
2021-02-22 23:41:51 +0800

    Update diagnosticList.test.tsx

--- Composer/packages/client/__tests__/pages/notifications/diagnosticList.test.tsx
+++ Composer/packages/client/__tests__/pages/notifications/diagnosticList.test.tsx
@@ -124,7 +124,7 @@ describe('<DiagnosticList/>', () => {
 
   it('should render the DiagnosticList', () => {
     const { container } = renderWithRecoil(
-      <DiagnosticList diagnosticItems={state.diagnostics as any} onItemClick={jest.fn} />,
+      <DiagnosticList diagnosticItems={state.diagnostics as any} />,
       initRecoilState
     );
     expect(container).toHaveTextContent('server');
