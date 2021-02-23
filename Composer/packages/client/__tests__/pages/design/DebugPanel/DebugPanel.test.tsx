fd10a09604ad73e728e85747daaa4f5b509612ae
Long Alan <julong@microsoft.com>
2021-02-23 17:25:14 +0800

    lint

--- Composer/packages/client/__tests__/pages/design/DebugPanel/DebugPanel.test.tsx
+++ Composer/packages/client/__tests__/pages/design/DebugPanel/DebugPanel.test.tsx
@@ -1,4 +1,8 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
 import React from 'react';
+
 import { DebugPanel } from '../../../../src/pages/design/DebugPanel/DebugPanel';
 import { DiagnosticsTabKey } from '../../../../src/pages/design/DebugPanel/TabExtensions/DiagnosticsTab/constants';
 import { debugPanelActiveTabState, debugPanelExpansionState } from '../../../../src/recoilModel';
