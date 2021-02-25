270e6df48c82f48fd333a7dfc581c447b5fbd61f
zhixzhan <zhixzhan@microsoft.com>
2021-02-25 09:11:01 +0800

    font align with vscode

--- Composer/packages/client/src/recoilModel/utils/index.ts
+++ Composer/packages/client/src/recoilModel/utils/index.ts
@@ -7,10 +7,51 @@ import merge from 'lodash/merge';
 import storage from '../../utils/storage';
 import { isElectron } from '../../utils/electronUtil';
 
+const getOS = () => {
+  const userAgent = window.navigator.userAgent,
+    platform = window.navigator.platform,
+    macosPlatforms = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'],
+    windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'],
+    iosPlatforms = ['iPhone', 'iPad', 'iPod'];
+  const os = {
+    isMacintosh: false,
+    isLinux: false,
+    isWindows: false,
+    isAndroid: false,
+    isiOS: false,
+  };
+
+  if (macosPlatforms.indexOf(platform) !== -1) {
+    os.isMacintosh = true;
+  } else if (iosPlatforms.indexOf(platform) !== -1) {
+    os.isiOS = true;
+  } else if (windowsPlatforms.indexOf(platform) !== -1) {
+    os.isWindows = true;
+  } else if (/Android/.test(userAgent)) {
+    os.isAndroid = true;
+  } else if (/Linux/.test(platform)) {
+    os.isLinux = true;
+  }
+  return os;
+};
+
+export const PLATFORM = getOS();
+
+// font align with vscode https://github.com/microsoft/vscode/blob/main/src/vs/editor/common/config/editorOptions.ts#L3680
+const DEFAULT_WINDOWS_FONT_FAMILY = "Consolas, 'Courier New', monospace";
+const DEFAULT_MAC_FONT_FAMILY = "Menlo, Monaco, 'Courier New', monospace";
+const DEFAULT_LINUX_FONT_FAMILY = "'Droid Sans Mono', 'monospace', monospace, 'Droid Sans Fallback'";
+
 export const DEFAULT_FONT_SETTINGS = {
-  fontFamily: 'Menlo, Monaco, "Courier New", monospace',
-  fontSize: '14px',
-  fontWeight: '500',
+  fontFamily: PLATFORM.isMacintosh
+    ? DEFAULT_MAC_FONT_FAMILY
+    : PLATFORM.isLinux
+    ? DEFAULT_LINUX_FONT_FAMILY
+    : DEFAULT_WINDOWS_FONT_FAMILY,
+  fontWeight: 'normal',
+  fontSize: PLATFORM.isMacintosh ? 12 : 14,
+  lineHeight: 0,
+  letterSpacing: 0,
 };
 
 export const DEFAULT_USER_SETTINGS = {
