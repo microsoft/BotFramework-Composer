47967195ea853f392aac92e857cf93194aeec989
Wenyi Luo <wenyluo@microsoft.com>
2021-02-25 15:08:27 +0800

    add subscriptionId in profile in create protocol

--- Composer/packages/client/src/utils/electronUtil.ts
+++ Composer/packages/client/src/utils/electronUtil.ts
@@ -1,6 +1,6 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
-import { PublishTarget, PublishProfile } from '@bfc/shared';
+import { PublishTarget } from '@bfc/shared';
 
 /**
  * Returns true if the client is embedded in the Composer Electron environment.
@@ -9,27 +9,48 @@ export function isElectron(): boolean {
   return !!window.__IS_ELECTRON__;
 }
 
+type ABSProfile = {
+  resourceId: string;
+  botName?: string;
+  appId?: string;
+  appPasswordHint: string;
+  subscriptionId?: string;
+  resourceGroup?: string;
+  armEndpoint?: string;
+};
+export type Profile = ABSProfile; // can include PVAProfile or other type of profile by |
 /**
  *
  * @param profile payload from bf's create protocol
  */
-export function getPublishProfileFromPayload(profile: PublishProfile, source: string): PublishTarget | undefined {
+export function getPublishProfileFromPayload(profile: Profile, source: string): PublishTarget | undefined {
   switch (source) {
-    case 'abs':
+    case 'abs': {
       const appId = profile.appId;
-      delete profile.appId;
+      // parse subscriptionId ... from profile
+      const temp = { ...profile };
+      const subs = profile.resourceId.match(/subscriptions\/([\w-]*)\//);
+      const groups = profile.resourceId.match(/resourceGroups\/([^\/]*)/);
+      const names = profile.resourceId.match(/botServices\/([^\/]*)/);
+      temp.subscriptionId = (subs && subs.length > 0 && subs[1]) || '';
+      temp.resourceGroup = (groups && groups.length > 0 && groups[1]) || '';
+      temp.botName = (names && names.length > 0 && names[1]) || '';
+      delete temp.appId;
+
+      console.log(temp);
       return {
         name: `${source}-${profile.botName}`,
         type: 'azurePublish',
         configuration: JSON.stringify({
-          hostname: profile.tag?.webapp || '',
+          hostname: '',
           runtimeIdentifier: 'win-x64',
           settings: {
             MicrosoftAppId: appId,
           },
-          ...profile,
+          ...temp,
         }),
       };
+    }
   }
 }
 
@@ -37,7 +58,7 @@ export function getAliasFromPayload(source: string, payload: string): string | u
   switch (source) {
     case 'abs':
       if (payload) {
-        const profile: PublishProfile = JSON.parse(payload);
+        const profile: ABSProfile = JSON.parse(payload);
         const alias = `${source}-${profile.botName}-${profile.appId}`;
         return alias;
       }
