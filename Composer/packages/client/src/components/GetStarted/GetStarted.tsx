f71fb706f69e6f61c3df72a5611897418309ba4f
Ben Brown <benbro@microsoft.com>
2021-02-26 16:10:45 -0600

    fix tests, fix minor styles, add real links tfor guides

--- Composer/packages/client/src/components/GetStarted/GetStarted.tsx
+++ Composer/packages/client/src/components/GetStarted/GetStarted.tsx
@@ -34,6 +34,17 @@ export const GetStarted: React.FC<GetStartedProps> = (props) => {
   const linkToLGEditor = `/bot/${projectId}/language-generation`;
   const linkToLUEditor = `/bot/${projectId}/language-understanding`;
   const linkToDelete = `/bot/${projectId}/botProjectsSettings/#deleteBot`;
+  const linkToAdaptiveExpressions =
+    'https://docs.microsoft.com/en-us/azure/bot-service/bot-builder-concept-adaptive-expressions?view=azure-bot-service-4.0&tabs=arithmetic';
+  const linkToGetStarted = 'https://docs.microsoft.com/en-us/composer/introduction';
+  const linkToCreateFirstBot = 'https://docs.microsoft.com/en-us/composer/quickstart-create-bot';
+  const linkToTutorials = 'https://docs.microsoft.com/en-us/composer/tutorial/tutorial-introduction';
+  const linkToLGFileFormat =
+    'https://docs.microsoft.com/en-us/azure/bot-service/file-format/bot-builder-lg-file-format?view=azure-bot-service-4.0';
+  const linkToLUFileFormat =
+    'https://docs.microsoft.com/en-us/azure/bot-service/file-format/bot-builder-lu-file-format?view=azure-bot-service-4.0';
+  const linkToPreBuiltExpressions =
+    'https://docs.microsoft.com/en-us/azure/bot-service/adaptive-expressions/adaptive-expressions-prebuilt-functions?view=azure-bot-service-4.0';
 
   const items: IToolbarItem[] = [
     {
@@ -89,9 +100,7 @@ export const GetStarted: React.FC<GetStartedProps> = (props) => {
   ];
 
   const linkClick = (event) => {
-    event.preventDefault();
     // TODO: track telemetry!
-    navigateTo(event.currentTarget.getAttribute('href'));
   };
 
   const buttonClick = (link) => {
@@ -132,11 +141,6 @@ export const GetStarted: React.FC<GetStartedProps> = (props) => {
                     {formatMessage('Train your language model')}
                   </Link>
                 </li>
-              </ul>
-            </Stack.Item>
-            <Stack.Item>
-              Configure
-              <ul style={ulStyle}>
                 <li style={liStyle}>
                   <Link href={linkToConnections} styles={linkStyle} onClick={linkClick}>
                     {formatMessage('Connect your bot to new services')}
@@ -165,13 +169,38 @@ export const GetStarted: React.FC<GetStartedProps> = (props) => {
           <h3 style={h3Style}>Guides and references</h3>
           <ul style={ulStyle}>
             <li style={liStyle}>
-              <Link href={linkToProvision} styles={linkStyle} onClick={linkClick}>
-                {formatMessage('Create a cloud hosting environment')}
+              <Link href={linkToGetStarted} styles={linkStyle} target="_blank" onClick={linkClick}>
+                {formatMessage('Get started with Bot Framework Composer')}
               </Link>
             </li>
             <li style={liStyle}>
-              <Link href={linkToPublish} styles={linkStyle} onClick={linkClick}>
-                {formatMessage('Publish updates to the cloud')}
+              <Link href={linkToCreateFirstBot} styles={linkStyle} target="_blank" onClick={linkClick}>
+                {formatMessage('Create your first bot')}
+              </Link>
+            </li>
+            <li style={liStyle}>
+              <Link href={linkToTutorials} styles={linkStyle} target="_blank" onClick={linkClick}>
+                {formatMessage('Composer tutorials')}
+              </Link>
+            </li>
+            <li style={liStyle}>
+              <Link href={linkToLGFileFormat} styles={linkStyle} target="_blank" onClick={linkClick}>
+                {formatMessage('LG file format and syntax')}
+              </Link>
+            </li>
+            <li style={liStyle}>
+              <Link href={linkToAdaptiveExpressions} styles={linkStyle} target="_blank" onClick={linkClick}>
+                {formatMessage('Learn about Adaptive expressions')}
+              </Link>
+            </li>
+            <li style={liStyle}>
+              <Link href={linkToPreBuiltExpressions} styles={linkStyle} target="_blank" onClick={linkClick}>
+                {formatMessage('Find pre-built Adaptive expressions')}
+              </Link>
+            </li>
+            <li style={liStyle}>
+              <Link href={linkToLUFileFormat} styles={linkStyle} target="_blank" onClick={linkClick}>
+                {formatMessage('LU file format and syntax')}
               </Link>
             </li>
           </ul>
--- Composer/packages/client/src/components/GetStarted/styles.ts
+++ Composer/packages/client/src/components/GetStarted/styles.ts
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 import { FontSizes } from '@uifabric/fluent-theme';
-
+import { FontWeights } from '@uifabric/styling';
 export const linkStyle = {
   root: { color: '#56CCF2', selectors: { ':hover': { color: '#56CCF2' } } },
 };
@@ -14,12 +14,12 @@ export const wrapperStyle = {
 
 export const h2Style = {
   fontSize: FontSizes.size20,
-  fontWeight: 'normal',
+  fontWeight: FontWeights.regular,
   marginTop: 0,
 };
 export const h3Style = {
   fontSize: FontSizes.size16,
-  fontWeight: 'normal',
+  fontWeight: FontWeights.regular,
   marginTop: 0,
 };
 
