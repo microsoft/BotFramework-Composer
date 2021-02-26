b9890528131dbeb8fc001a8a686c7f68a6f34cae
Long Alan <julong@microsoft.com>
2021-02-26 10:48:49 +0800

    fix: debug panel UX improvements (#5928)
    * show all errors in debug panel
    
    * increase indicator size to 10px
    
    * 12px tab header font + 8px indicator
    
    * Chevron on debug panel right corner
    
    * hide status bar when no error & warning
    
    * fix some bugs about resizing panel
    
    * debug panel ux err for settings page
    
    * bot name display err & debug panel display err in settings page
    
    * table sorted
    
    * overflow error table
    
    * fix CI
    
    * update font-family
    
    * useDebugPane
    
    * click debug header to expand diagnostics
    
    * Update to show error or warning
    
    Signed-off-by: Srinaath Ravichandran <srravich@microsoft.com>
    
    Revert colors
    
    Signed-off-by: Srinaath Ravichandran <srravich@microsoft.com>
    
    * use botProjectSpaceSelector
    
    * Revert "update font-family"
    
    This reverts commit 2f3168ab3444604316b4d87979893c6e043944da.
    
    * add drawer open telemetry
    
    * fix tab header fontsize to 12px
    
    * adjust splt range
    
    * + qna debug - diagnostic debug
    
    * update Chevron btn height
    
    * feat: Support unread notification marker on Webchat inspector (#5984)
    
    * Set active tab and unread notification for DebugPanel
    
    Signed-off-by: Srinaath Ravichandran <srravich@microsoft.com>
    
    * Code cleanup
    
    Signed-off-by: Srinaath Ravichandran <srravich@microsoft.com>
    
    * Remove unused ref
    
    Signed-off-by: Srinaath Ravichandran <srravich@microsoft.com>
    
    * refactor: debug panel
    
    * rename webchat logitem states
    
    Co-authored-by: Srinaath Ravichandran <srravich@microsoft.com>
    Co-authored-by: yeze322 <zeye@microsoft.com>
    
    Co-authored-by: zeye <zeye@microsoft.com>
    Co-authored-by: Srinaath Ravichandran <srravich@microsoft.com>
    Co-authored-by: Srinaath Ravichandran <srinaath27@gmail.com>

--- Composer/packages/client/src/components/Page.tsx
+++ Composer/packages/client/src/components/Page.tsx
@@ -62,9 +62,9 @@ export const headerContent = css`
   label: PageHeaderContent;
 `;
 
-export const main = css`
+export const main = (hasRenderHeaderContent) => css`
   margin-left: 2px;
-  height: calc(100vh - 165px);
+  height: ${hasRenderHeaderContent ? 'calc(100vh - 181px)' : 'calc(100vh - 165px)'};
   display: flex;
   flex-grow: 1;
   border-top: 1px solid #dddddd;
@@ -83,7 +83,6 @@ export const content = (shouldShowEditorError: boolean) => css`
   display: flex;
   flex-direction: column;
   position: relative;
-  overflow: auto;
   height: ${shouldShowEditorError ? 'calc(100% - 40px)' : '100%'};
   label: PageContent;
   box-sizing: border-box;
@@ -94,9 +93,9 @@ const contentStyle = css`
   flex-grow: 1;
   height: 0;
   position: relative;
+  overflow: auto;
+  box-sizing: border-box;
 `;
-
-const ShowDebugPanelPageTitle = ['User Input', 'QnA', 'Bot Responses'];
 
 // -------------------- Page -------------------- //
 
@@ -111,6 +110,7 @@ type IPageProps = {
   onRenderHeaderContent?: () => string | JSX.Element | null;
   'data-testid'?: string;
   useNewTree?: boolean;
+  useDebugPane?: boolean;
   navLinks?: INavTreeItem[];
   navLinkClick?: (item: INavTreeItem) => void;
   pageMode: PageMode;
@@ -134,6 +134,7 @@ const Page: React.FC<IPageProps> = (props) => {
     headerStyle = header,
     shouldShowEditorError = false,
     useNewTree,
+    useDebugPane,
     pageMode,
     showCommonLinks = false,
     projectId,
@@ -172,7 +173,7 @@ const Page: React.FC<IPageProps> = (props) => {
           <h1 css={headerTitle}>{title}</h1>
           {onRenderHeaderContent && <div css={headerContent}>{onRenderHeaderContent()}</div>}
         </div>
-        <div css={main} role="main">
+        <div css={main(!!onRenderHeaderContent)} role="main">
           <Split
             resetOnDoubleClick
             initialPrimarySize="20%"
@@ -219,7 +220,7 @@ const Page: React.FC<IPageProps> = (props) => {
               role="region"
             >
               <div css={contentStyle}>{children}</div>
-              {ShowDebugPanelPageTitle.indexOf(title) > -1 && <DebugPanel />}
+              {useDebugPane ? <DebugPanel /> : null}
             </div>
           </Split>
         </div>
