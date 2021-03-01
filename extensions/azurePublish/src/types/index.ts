cba7a75e61a5b94c25d531078bc192098e14fa92
VanyLaw <wenyluo@microsoft.com>
2021-03-01 22:44:51 +0800

    feat: appended provision with a profile (#5741)
    * change edit dialog page 1
    
    * change add and edit page one
    
    * add review page and enable provision resources in current provision
    
    * add some log
    
    * fix test
    
    * add profile merge logic
    
    * add retry logic judging
    
    * remove settings
    
    * remove settings
    
    * fix review page display error
    
    * fix profile update in edit
    
    * remove status and fix provision update
    
    * add missed field in current settings
    
    * Fix some issues
    
    1. fix the logic of merging existing profile with new provisioned configurations.
    2. when hostname or name exist, don't provision new webapp.
    3. add subscriptionId and resourceGroup in the publish profile.
    
    * remove the dependency between webapp and bot channel registration in provision
    
    * fix luis region always westus
    
    * decouple resourceGroup and hostName, fix regex
    
    * Add a botName field for channel service
    
    * fix bug when select luis resource but not select the luis region
    
    * add botName to publish profile
    
    * make hostname disable if name or hostname already have
    
    * fix resourceGroup display error in review page
    
    * remove annotation
    
    * fix regex issue and get location issue
    
    * update publish function getPublishConfig
    
    * fix hostname check issue
    
    * fix UI in add resource page
    
    * fix UI issue
    
    * Fix the config missing issue
    
    * update qna image and resource group check
    
    * add region to profile and show the profile region as default in edit mode
    
    * add display name for app registration
    
    * remove form page if type already have
    
    * fix bug
    
    * fix bug
    
    * remove image
    
    * add image
    
    * fix bugs
    
    * fix: provision process shown in publish list (#6044)
    
    * add jobId for filter publish process
    
    * lint
    
    * delete un-use value
    
    * ut
    
    * refactor
    
    * fix region
    
    * go to edit page if no resources can be added
    
    * fix the region issue
    
    * fix ut
    
    * fix auth dialog popup everytime
    
    Co-authored-by: Qi Kang <qika@microsoft.com>
    Co-authored-by: Lu Han <32191031+luhan2017@users.noreply.github.com>
    Co-authored-by: Ben Brown <benbro@microsoft.com>
    Co-authored-by: Long Alan <julong@microsoft.com>
    Co-authored-by: Dong Lei <donglei@microsoft.com>

--- extensions/azurePublish/src/types/index.ts
+++ extensions/azurePublish/src/types/index.ts
@@ -33,6 +33,20 @@ export enum AzureAPIStatus {
   ERROR = 'ERROR',
 }
 
+export const AzureResourceTypes = {
+  APP_REGISTRATION: 'appRegistration',
+  BOT_REGISTRATION: 'botRegistration',
+  WEBAPP: 'webApp',
+  AZUREFUNCTIONS: 'azureFunctions',
+  COSMOSDB: 'cosmosDb',
+  APPINSIGHTS: 'applicationInsights',
+  LUIS_AUTHORING: 'luisAuthoring',
+  LUIS_PREDICTION: 'luisPrediction',
+  BLOBSTORAGE: 'blobStorage',
+  QNA: 'qna',
+  SERVICE_PLAN: 'servicePlan',
+};
+
 export enum AzureResourceProviderType {
   QnA = 'Microsoft.CognitiveServices',
   Luis = 'Microsoft.CognitiveServices',
