cde52aa4f0e26677f3525e6a390c395736e5d1e4
Wenyi Luo <wenyluo@microsoft.com>
2021-03-01 21:40:02 +0800

    fix ut

--- Composer/packages/client/__tests__/pages/botProjectsSettings/PublishTarget.test.tsx
+++ Composer/packages/client/__tests__/pages/botProjectsSettings/PublishTarget.test.tsx
@@ -8,11 +8,23 @@ import { act, fireEvent } from '@botframework-composer/test-utils';
 import { PublishTargets } from '../../../src/pages/botProject/PublishTargets';
 import { renderWithRecoilAndCustomDispatchers } from '../../testUtils';
 import { dispatcherState, publishTypesState } from '../../../src/recoilModel';
+import { isShowAuthDialog } from '../../../src/utils/auth';
+
+jest.mock('../../../src/utils/auth', () => ({
+  isShowAuthDialog: jest.fn(),
+  getTokenFromCache: jest.fn(),
+  isGetTokenFromUser: jest.fn(),
+}));
+
 const state = {
   projectId: 'test',
   publishTypes: [{ name: 'azureFunctionsPublish', description: 'Publish bot to Azure Functions (Preview)' }],
 };
+
 describe('Publish Target', () => {
+  beforeEach(() => {
+    (isShowAuthDialog as jest.Mock).mockReturnValue(false);
+  });
   const setPublishTargetsMock = jest.fn();
   const initRecoilState = ({ set }) => {
     set(dispatcherState, {
@@ -28,6 +40,7 @@ describe('Publish Target', () => {
       <PublishTargets projectId={state.projectId} />,
       initRecoilState
     );
+
     const addNewPublishProfile = getByTestId('addNewPublishProfile');
     act(() => {
       fireEvent.click(addNewPublishProfile);
