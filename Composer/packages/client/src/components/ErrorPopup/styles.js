b339f4d335dbfc42dfb876bb09a9201a23307a4c
Ben Yackley <61990921+beyackle@users.noreply.github.com>
2021-03-04 13:22:36 -0800

    Update styles.js

--- Composer/packages/client/src/components/ErrorPopup/styles.js
+++ Composer/packages/client/src/components/ErrorPopup/styles.js
@@ -3,15 +3,16 @@
 
 import { css } from '@emotion/core';
 import { FontWeights, FontSizes } from 'office-ui-fabric-react/lib/Styling';
+import { NeutralColors, SharedColors } from '@uifabric/fluent-theme';
 
 export const consoleStyle = css`
-  background: #000;
-  color: #fff;
+  background: ${NeutralColors.black};
+  color: ${NeutralColors.white};
   padding: 15px;
   margin-bottom: 20px;
 
   a {
-    color: #8080ff;
+    color: ${SharedColors.blueMagenta10};
   }
 `;
 export const dialogSubTitle = css`
