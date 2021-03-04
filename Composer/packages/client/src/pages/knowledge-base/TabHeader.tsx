23c9d925da29030d486dbdfcc27213000a688222
Long Alan <julong@microsoft.com>
2021-03-04 12:31:41 +0800

    fix some bug & ux adjust

--- Composer/packages/client/src/pages/knowledge-base/TabHeader.tsx
+++ Composer/packages/client/src/pages/knowledge-base/TabHeader.tsx
@@ -3,6 +3,7 @@
 
 import React, { useState, ReactNode, useMemo } from 'react';
 import { Pivot, PivotItem } from 'office-ui-fabric-react/lib/Pivot';
+
 import { languageListTemplates } from '../../components/MultiLanguage';
 
 export type TabHeaderProps = {
@@ -22,14 +23,29 @@ export const TabHeader: React.FC<TabHeaderProps> = (props) => {
 
   const handleLinkClick = (item?: PivotItem) => {
     if (!item) return;
-    const currentLocale = languageList.find((l) => l.language === item.props.headerText)?.locale as string;
+    const currentLocale = languageList.find(
+      (l) =>
+        (!l.isDefault && l.language === item.props.headerText) ||
+        (l.isDefault && `${l.language}(Default)` === item.props.headerText)
+    )?.locale as string;
     setSelectedKey(languageList.findIndex((l) => l.locale === currentLocale));
     onChangeLocale(currentLocale);
   };
   return (
-    <Pivot aria-label="select locale" selectedKey={String(selectedKey)} onLinkClick={handleLinkClick}>
+    <Pivot
+      aria-label="select locale"
+      selectedKey={String(selectedKey)}
+      style={{ height: '100%' }}
+      styles={{ itemContainer: { height: 'calc(100% - 60px)', marginTop: '10px' } }}
+      onLinkClick={handleLinkClick}
+    >
       {languageList.map((l, index) => (
-        <PivotItem key={index} headerText={`${l.language}${l.isDefault ? '(Default)' : ''}`} itemKey={index.toString()}>
+        <PivotItem
+          key={index}
+          headerText={`${l.language}${l.isDefault ? '(Default)' : ''}`}
+          itemKey={index.toString()}
+          style={{ height: '100%' }}
+        >
           {children}
         </PivotItem>
       ))}
