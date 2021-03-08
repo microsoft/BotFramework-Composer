05a4b1306ef50f1e49432e2cb70c958934c49a0b
Patrick Volum <pavolum@microsoft.com>
2021-03-08 13:06:35 -0500

    Test

--- Composer/packages/server/src/constants.ts
+++ Composer/packages/server/src/constants.ts
@@ -7,7 +7,7 @@ export const BASEURL = (process.env.PUBLIC_URL || '').replace(/\/$/, '');
 
 export const QNA_SUBSCRIPTION_KEY = process.env.QNA_SUBSCRIPTION_KEY ?? '';
 
-export const COGNITIVE_SERVICES_ENDPOINTS = 'https://westus.api.cognitive.microsoft.com/qnamaker/v4.0';
+export const COGNITIVE_SERVICES_ENDPOINTS = "https://westus.api.cognitive.microsoft.com/qnamaker/v4.0";
 
 export const DOC_EXTENSIONS = ['.pdf', '.tsv', '.doc', '.docx', '.xlsx'];
 
