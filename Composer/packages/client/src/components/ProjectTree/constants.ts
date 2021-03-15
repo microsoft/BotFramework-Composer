179b32bcdd857cd529225dcd00c090930238c583
Ben Yackley <61990921+beyackle@users.noreply.github.com>
2021-03-15 14:18:44 -0700

    simplify nested CSS things

--- Composer/packages/client/src/components/ProjectTree/constants.ts
+++ Composer/packages/client/src/components/ProjectTree/constants.ts
@@ -5,3 +5,4 @@ export const SUMMARY_ARROW_SPACE = 28; // the rough pixel size of the dropdown a
 export const INDENT_PER_LEVEL = 16;
 export const ACTION_ICON_WIDTH = 28;
 export const THREE_DOTS_ICON_WIDTH = 28;
+export const ROW_HEIGHT = 24;
