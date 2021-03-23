e5c2ed83bfe9f6da9ae7fc88f0cdcdc59fa3c66b
Qi Kang <kangqidev@gmail.com>
2021-03-23 17:16:40 +0800

    add skill select profile component

---
+++ Composer/packages/client/src/pages/design/exportSkillModal/content/SelectProfile.tsx
@@ -0,0 +1,162 @@
+import { PublishTarget } from '@bfc/shared';
+import formatMessage from 'format-message';
+import { css, Dropdown, Icon, IDropdownOption, PrimaryButton, TextField, TooltipHost } from 'office-ui-fabric-react';
+import React, { useEffect, useMemo, useState } from 'react';
+import { useRecoilValue } from 'recoil';
+import { settingsState } from '../../../../recoilModel';
+import { PublishTargets } from '../../../botProject/PublishTargets';
+import { iconStyle } from '../../../botProject/runtime-settings/style';
+import Publish from '../../../publish/Publish';
+import { ContentProps } from '../constants';
+
+const styles = {
+  container: css`
+    height: 350px;
+    overflow: auto;
+  `,
+};
+
+const onRenderLabel = (props) => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'row',
+        marginBottom: '5px',
+      }}
+    >
+      <div
+        style={{
+          marginRight: '5px',
+          fontWeight: 600,
+          fontSize: '14px',
+        }}
+      >
+        {' '}
+        {props.label}{' '}
+      </div>
+      <TooltipHost content={props.ariaLabel}>
+        <Icon iconName="Info" styles={iconStyle(props.required)} />
+      </TooltipHost>
+    </div>
+  );
+};
+
+export const SelectProfile: React.FC<ContentProps> = ({ errors, value, schema, onChange, projectId }) => {
+  const [publishingTargets, setPublishingTargets] = useState<PublishTarget[]>([]);
+  const [currentTarget, setCurrentTarget] = useState<PublishTarget>();
+  const [endpointUrl, setEndpointUrl] = useState<string>();
+  const [appId, setAppId] = useState<string>();
+  const { endpoints, ...rest } = value;
+
+  const updateCurrentProfile = useMemo(
+    () => (_e, option?: IDropdownOption) => {
+      const target = publishingTargets.find((t) => {
+        return t.name === option?.key;
+      });
+      if (target) {
+        setCurrentTarget(target);
+        const config = JSON.parse(target.configuration);
+        setEndpointUrl(`https://${config.hostname}.azurewebsites.net`);
+        setAppId(config.settings.MicrosoftAppId);
+        onChange({
+          endpoints: [
+            {
+              protocol: 'BotFrameworkV3',
+              name: option?.key,
+              endpointUrl: `https://${config.hostname}.azurewebsites.net`,
+              description: '<description>',
+              msAppId: config.settings.MicrosoftAppId,
+            },
+          ],
+          ...rest,
+        });
+      }
+    },
+    [publishingTargets]
+  );
+
+  const isProfileValid = useMemo(
+    () => () => {
+      if (!publishingTargets) {
+        return false;
+      }
+      const filteredProfile = publishingTargets.filter((item) => {
+        const config = JSON.parse(item.configuration);
+        return (
+          config.settings.MicrosoftAppId &&
+          config.hostname &&
+          config.settings.MicrosoftAppId.length > 0 &&
+          config.hostname.length > 0
+        );
+      });
+      return filteredProfile.length > 0;
+    },
+    [publishingTargets]
+  );
+
+  const publishingOptions = useMemo(() => {
+    return publishingTargets.map((t) => ({
+      key: t.name,
+      text: t.name,
+    }));
+  }, [publishingTargets]);
+
+  const settings = useRecoilValue(settingsState(projectId));
+
+  const OnEndpointChange = useMemo(
+    () => (e, newValue) => {
+      setEndpointUrl(newValue);
+    },
+    [setEndpointUrl]
+  );
+
+  const OnAppIdChange = useMemo(
+    () => (e, newValue) => {
+      setAppId(newValue);
+    },
+    [setAppId]
+  );
+
+  useEffect(() => {
+    setPublishingTargets(settings.publishTargets || []);
+  }, [settings]);
+
+  return isProfileValid() ? (
+    <div css={styles.container}>
+      <Dropdown
+        required
+        ariaLabel={formatMessage('Select a publishing profile')}
+        label={formatMessage('Publishing Profile')}
+        options={publishingOptions}
+        placeholder={'Select one'}
+        styles={{ root: { paddingBottom: '8px' } }}
+        onChange={updateCurrentProfile}
+      />
+      <TextField
+        required
+        ariaLabel={formatMessage('The endpoint url')}
+        label={formatMessage('Endpoint Url')}
+        placeholder={'The endpoint url of your web app resource'}
+        styles={{ root: { paddingBottom: '8px' } }}
+        onChange={OnEndpointChange}
+        value={endpointUrl}
+        onRenderLabel={onRenderLabel}
+      />
+      <TextField
+        required
+        ariaLabel={formatMessage('The app id of your application registration')}
+        label={formatMessage('Microsoft App Id')}
+        placeholder={'The app id'}
+        styles={{ root: { paddingBottom: '8px' } }}
+        onChange={OnAppIdChange}
+        value={appId}
+        onRenderLabel={onRenderLabel}
+      />
+    </div>
+  ) : (
+    <div>
+      <PublishTargets projectId={projectId}></PublishTargets>
+    </div>
+  );
+};
