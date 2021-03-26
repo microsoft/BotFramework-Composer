d17bbbc8587146274a9e37290a7009d9412c7b6c
Long Alan <julong@microsoft.com>
2021-03-26 13:28:33 +0800

    publish notification

--- Composer/packages/client/src/pages/publish/Publish.tsx
+++ Composer/packages/client/src/pages/publish/Publish.tsx
@@ -182,6 +182,10 @@ const Publish: React.FC<RouteComponentProps<{ projectId: string; targetName?: st
     const updater = pollingUpdaterList.find((i) => i.isSameUpdater(botProjectId, targetName));
     const updatedBot = botList.find((bot) => bot.id === botProjectId);
     if (!updatedBot || !updater) return;
+    if (!apiResponse) {
+      stopUpdater(updater);
+      return;
+    }
     const responseData = apiResponse.data;
 
     if (responseData.status !== ApiStatus.Publishing) {
@@ -204,7 +208,7 @@ const Publish: React.FC<RouteComponentProps<{ projectId: string; targetName?: st
 
   const updateUpdaterStatus = (payload) => {
     const { botProjectId, targetName, apiResponse } = payload;
-    const pending = apiResponse.data.status === ApiStatus.Publishing;
+    const pending = apiResponse && apiResponse.data.status === ApiStatus.Publishing;
     setUpdaterStatus({
       ...updaterStatus,
       [`${botProjectId}/${targetName}`]: pending,
