8fe86c3751e87e72395c358e3dfdf5122136a052
Patrick Volum <pavolum@microsoft.com>
2021-03-25 20:59:29 -0400

    Adding modal for node detection on app start

--- Composer/packages/client/src/App.tsx
+++ Composer/packages/client/src/App.tsx
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-import React, { Fragment, useEffect } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import { initializeIcons } from 'office-ui-fabric-react/lib/Icons';
 import { useRecoilValue } from 'recoil';
 
@@ -11,6 +11,7 @@ import { MainContainer } from './components/AppComponents/MainContainer';
 import { dispatcherState, userSettingsState } from './recoilModel';
 import { loadLocale } from './utils/fileUtil';
 import { useInitializeLogger } from './telemetry/useInitializeLogger';
+import { NodeModal } from './components/CreationFlow/v2/NodeModal';
 
 initializeIcons(undefined, { disableWarnings: true });
 
@@ -22,7 +23,7 @@ const Logger = () => {
 export const App: React.FC = () => {
   const { appLocale } = useRecoilValue(userSettingsState);
   const { fetchExtensions, fetchFeatureFlags } = useRecoilValue(dispatcherState);
-
+  const [showNodeModal, setShowNodeModal] = useState(true);
   useEffect(() => {
     loadLocale(appLocale);
   }, [appLocale]);
@@ -34,6 +35,7 @@ export const App: React.FC = () => {
 
   return (
     <Fragment key={appLocale}>
+      {showNodeModal && <NodeModal isOpen={showNodeModal} setIsOpen={setShowNodeModal} />}
       <Logger />
       <Announcement />
       <Header />
