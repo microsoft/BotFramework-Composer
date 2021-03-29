afb4ae61a1616ae7dad799d9cddb8b692d43db3e
Ben Yackley <61990921+beyackle@users.noreply.github.com>
2021-03-29 15:40:29 -0700

    Update WorkingModal.tsx

--- extensions/packageManager/src/components/WorkingModal.tsx
+++ extensions/packageManager/src/components/WorkingModal.tsx
@@ -5,14 +5,25 @@
 import { jsx } from '@emotion/core';
 import { Dialog, DialogType } from 'office-ui-fabric-react';
 import { LoadingSpinner } from '@bfc/ui-shared';
+import { useApplicationApi } from '@bfc/extension-client';
 
 import { modalControlGroup } from './styles';
+import { useEffect } from 'react';
 
 export interface WorkingModalProps {
   hidden: boolean;
   title: string;
 }
 export const WorkingModal: React.FC<WorkingModalProps> = (props) => {
+  const { announce } = useApplicationApi();
+
+  useEffect(() => {
+    async function doAnnouncement() {
+      announce(props.title);
+    }
+    doAnnouncement();
+  }, [props.title]);
+
   return (
     <Dialog
       dialogContentProps={{
