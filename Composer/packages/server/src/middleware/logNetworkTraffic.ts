440cfae3c3d7b7c45653d39625ce7f49b4727d70
Tony <tonyanziano5@gmail.com>
2021-03-31 09:54:05 -0700

    Plumbed network response data into log

--- Composer/packages/server/src/middleware/logNetworkTraffic.ts
+++ Composer/packages/server/src/middleware/logNetworkTraffic.ts
@@ -5,16 +5,30 @@ import { NextFunction, Request, Response } from 'express';
 
 import { WebSocketServer } from '../directline/utils/webSocketServer';
 
+let hasHookedIntoSend = false;
 export function logNetworkTraffic(req: Request, res: Response, next?: NextFunction) {
-  const data = {
-    request: { method: req.method, payload: req.body, route: req.originalUrl },
-    response: {
-      payload: { status: res.statusCode /* TODO: plumb actual response into this */ },
-      statusCode: res.statusCode,
-    },
-    timestamp: new Date().toISOString(),
-    trafficType: 'network' as 'network',
-  };
-  WebSocketServer.sendNetworkTrafficToSubscribers(data);
+  if (!hasHookedIntoSend) {
+    // hook into .send() so that it stores the data it sends on the response object
+    const originalSend = res.send.bind(res);
+    res.send = (data) => {
+      (res as any).sentData = data;
+      return originalSend(data);
+    };
+    hasHookedIntoSend = true;
+  }
+
+  // when the request finishes, log the payload and status code to the client
+  res.once('finish', () => {
+    const data = {
+      request: { method: req.method, payload: req.body, route: req.originalUrl },
+      response: {
+        payload: JSON.parse((res as any).sentData || '{}'),
+        statusCode: res.statusCode,
+      },
+      timestamp: new Date().toISOString(),
+      trafficType: 'network' as 'network',
+    };
+    WebSocketServer.sendNetworkTrafficToSubscribers(data);
+  });
   next?.();
 }
