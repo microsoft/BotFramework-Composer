ce82c69e2f35a52b6354aeb0ffdd77fe29fbf76a
Ben Brown <benbro@microsoft.com>
2021-04-05 18:23:23 -0500

    fix: detect "Get started" dependencies in all projects not just current project (#6686)
    * fix #6649 - detect dependencies in all projects not just curent projcet
    
    * Update Composer/packages/client/src/components/Header.tsx
    
    Co-authored-by: Andy Brown <asbrown002@gmail.com>
    
    * Update Composer/packages/client/src/components/Header.tsx
    
    Co-authored-by: Andy Brown <asbrown002@gmail.com>
    
    Co-authored-by: Andy Brown <asbrown002@gmail.com>

--- Composer/packages/client/src/recoilModel/selectors/project.ts
+++ Composer/packages/client/src/recoilModel/selectors/project.ts
@@ -32,6 +32,7 @@ import {
 } from '../atoms';
 import {
   dialogsSelectorFamily,
+  botAssetsSelectFamily,
   buildEssentialsSelector,
   lgImportsSelectorFamily,
   luImportsSelectorFamily,
@@ -368,5 +369,22 @@ export const webChatEssentialsSelector = selector({
       activeLocale,
       botStatus,
     };
+  },
+});
+
+export const allRequiredRecognizersSelector = selector({
+  key: 'allRequiredRecognizersSelector',
+  get: ({ get }) => {
+    const ids = get(botProjectIdsState);
+    return ids.reduce((result: { projectId: string; requiresLUIS: boolean; requiresQNA: boolean }[], id: string) => {
+      const botAssets = get(botAssetsSelectFamily(id));
+      if (botAssets) {
+        const { dialogs, luFiles, qnaFiles } = botAssets;
+        const requiresLUIS = BotIndexer.shouldUseLuis(dialogs, luFiles);
+        const requiresQNA = BotIndexer.shouldUseQnA(dialogs, qnaFiles);
+        result.push({ projectId: id, requiresLUIS, requiresQNA });
+      }
+      return result;
+    }, []);
   },
 });
