a05b31c2b7104890247b1f587239a3896695e071
zhixzhan <zhixzhan@microsoft.com>
2021-04-06 17:11:14 +0800

    handle aka.ms redirect

--- Composer/packages/server/src/models/bot/feedManager.ts
+++ Composer/packages/server/src/models/bot/feedManager.ts
@@ -1,28 +1,14 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
+import axios from 'axios';
 
-import * as msRest from '@azure/ms-rest-js';
-
-import logger from './../../logger';
-
-const log = logger.extend('feed-manager');
-
-// http client for fetch json feed from url
-const clientOptions: msRest.ServiceClientOptions = {
-  requestPolicyFactories: [msRest.logPolicy(log)],
-};
-const token = process.env.ACCESS_TOKEN || 'token';
-const creds = new msRest.TokenCredentials(token);
-const client = new msRest.ServiceClient(creds, clientOptions);
-
-// TODO: update feed url to main
-const feedUrl = 'https://api.jsonbin.io/b/6066712c7474c23d9027bf10/3';
+// TODO: update feed url
+const feedUrl = 'https://aka.ms/composer-feed-json-test';
 
 export const getFeedUrl = async (): Promise<any> => {
-  const { bodyAsText: content } = await client.sendRequest({
+  const { data: content } = await axios({
+    method: 'get',
     url: feedUrl,
-    method: 'GET',
   });
-
-  return content && typeof content === 'string' ? JSON.parse(content) : {};
+  return content && typeof content === 'string' ? JSON.parse(content) : content;
 };
