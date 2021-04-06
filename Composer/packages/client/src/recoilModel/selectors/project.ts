833a24ead88c82456bf10e5ba94283f6c5281720
Srinaath Ravichandran <srinaath27@gmail.com>
2021-04-05 22:02:46 -0700

    feat: Runtime Logs Viewer (#6673)

--- Composer/packages/client/src/recoilModel/selectors/project.ts
+++ Composer/packages/client/src/recoilModel/selectors/project.ts
@@ -8,6 +8,7 @@ import uniqBy from 'lodash/uniqBy';
 import { selector, selectorFamily } from 'recoil';
 
 import { LanguageFileImport } from '../../../../types/src';
+import { BotStatus } from '../../constants';
 import {
   botDisplayNameState,
   botErrorState,
@@ -56,6 +57,15 @@ export type TreeDataPerProject = {
   isPvaSchema: boolean;
   formDialogSchemas: FormDialogSchema[];
   botError: any;
+};
+
+type WebChatEssentials = {
+  projectId: string;
+  botName: string;
+  secrets: { msAppId: string; msPassword: string };
+  botUrl: string;
+  activeLocale: string;
+  botStatus: BotStatus;
 };
 
 // Actions
@@ -343,13 +353,9 @@ export const projectTreeSelectorFamily = selector<TreeDataPerProject[]>({
   },
 });
 
-export const webChatEssentialsSelector = selector({
+export const webChatEssentialsSelector = selectorFamily<WebChatEssentials, string>({
   key: 'webChatEssentialsSelector',
-  get: ({ get }) => {
-    const projectId = get(rootBotProjectIdSelector);
-    if (!projectId) {
-      return undefined;
-    }
+  get: (projectId: string) => ({ get }) => {
     const settings = get(settingsState(projectId));
     const secrets = {
       msAppId: settings.MicrosoftAppId || '',
@@ -386,5 +392,15 @@ export const allRequiredRecognizersSelector = selector({
       }
       return result;
     }, []);
+  },
+});
+
+export const outputsDebugPanelSelector = selector<WebChatEssentials[]>({
+  key: 'outputsDebugPanelSelector',
+  get: ({ get }) => {
+    const projectIds: string[] = get(botProjectIdsState);
+    return projectIds.map((projectId) => {
+      return get(webChatEssentialsSelector(projectId));
+    });
   },
 });
