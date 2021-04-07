aa91b42c1cca631ec4aed4a0c76b90f9284b57b5
yeze322 <zeye@microsoft.com>
2021-04-07 20:11:37 +0800

    optimization: avoid frequent recoil submission

--- Composer/packages/client/src/pages/design/DebugPanel/TabExtensions/DiagnosticsTab/useAutoFix.ts
+++ Composer/packages/client/src/pages/design/DebugPanel/TabExtensions/DiagnosticsTab/useAutoFix.ts
@@ -17,23 +17,44 @@ export const useAutoFix = () => {
   const botProjectSpace = useRecoilValue(botProjectSpaceSelector);
   const { updateDialog } = useRecoilValue(dispatcherState);
 
+  // Auto fix schema absence by setting 'disabled' to true.
   useEffect(() => {
-    // Auto fix schema absence by setting 'disabled' to true.
+    /**
+     * Caches updated dialogs data as a tree to avoid frequent recoil state submission.
+     *
+     * Example:
+     * {
+     *   // projectId
+     *  '2096.637': {
+     *     // dialogId
+     *     'dialog-1': {...} // updated dialog json
+     *   }
+     * }
+     */
+    const cachedDialogs: { [projectId: string]: { [dialogId: string]: any } } = {};
+
     const schemaDiagnostics = diagnostics.filter((d) => d.type === DiagnosticType.SCHEMA) as SchemaDiagnostic[];
     schemaDiagnostics.forEach((d) => {
-      const { projectId, rootProjectId, id: dialogId, dialogPath } = d;
+      const { projectId, id: dialogId, dialogPath } = d;
 
       const dialogContent = botProjectSpace
         .find((bot) => bot.projectId === projectId)
         ?.dialogs.find((dialog) => dialog.id === dialogId)?.content;
-      if (!dialogContent) return;
 
+      // Contains two cases: 1. action already disabled 2. action doesn't exists on this path.
       if (get(dialogContent, `${dialogPath}.disabled`)) return;
 
-      const copy = cloneDeep(dialogContent);
-      set(copy, `${dialogPath}.disabled`, true);
-      console.log('Autofix: disable', projectId, rootProjectId, dialogPath);
-      updateDialog({ id: dialogId, projectId, content: copy });
+      // Manipulate the dialog content and caches the result
+      const dialogData = get(cachedDialogs, [projectId, dialogId]) ?? cloneDeep(dialogContent);
+      set(dialogData, `${dialogPath}.disabled`, true);
+      set(cachedDialogs, [projectId, dialogId], dialogData);
     });
+
+    // Submit cached dialog updates to recoil store.
+    for (const [projectId, dialogs] of Object.entries(cachedDialogs)) {
+      for (const [dialogId, dialogData] of Object.entries(dialogs)) {
+        updateDialog({ id: dialogId, projectId, content: dialogData });
+      }
+    }
   }, [diagnostics]);
 };
