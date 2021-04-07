d530195eff343a3f3e66756041352feba279c426
yeze322 <zeye@microsoft.com>
2021-04-07 20:38:43 +0800

    optimization: aggregate paths rather than updatedDialog to reduce time complexity

--- Composer/packages/client/src/pages/design/DebugPanel/TabExtensions/DiagnosticsTab/useAutoFix.ts
+++ Composer/packages/client/src/pages/design/DebugPanel/TabExtensions/DiagnosticsTab/useAutoFix.ts
@@ -9,6 +9,7 @@ import { useEffect } from 'react';
 
 import { DiagnosticType, SchemaDiagnostic } from '../../../../diagnostics/types';
 import { botProjectSpaceSelector, dispatcherState } from '../../../../../recoilModel';
+import { dialogsDispatcher } from '../../../../../recoilModel/dispatchers/dialogs';
 
 import { useDiagnosticsData } from './useDiagnostics';
 
@@ -19,41 +20,55 @@ export const useAutoFix = () => {
 
   // Auto fix schema absence by setting 'disabled' to true.
   useEffect(() => {
+    const schemaDiagnostics = diagnostics.filter((d) => d.type === DiagnosticType.SCHEMA) as SchemaDiagnostic[];
     /**
-     * Caches updated dialogs data as a tree to avoid frequent recoil state submission.
+     * Aggregated diagnostic paths where contains schema problem
      *
      * Example:
      * {
      *   // projectId
      *  '2096.637': {
      *     // dialogId
-     *     'dialog-1': {...} // updated dialog json
+     *     'dialog-1': [
+     *       'triggers[0].actions[1]', // diagnostics.dialogPath
+     *       'triggers[2].actions[3]'
+     *     ]
      *   }
      * }
      */
-    const cachedDialogs: { [projectId: string]: { [dialogId: string]: any } } = {};
-
-    const schemaDiagnostics = diagnostics.filter((d) => d.type === DiagnosticType.SCHEMA) as SchemaDiagnostic[];
+    const aggregatedPaths: { [projectId: string]: { [dialogId: string]: string[] } } = {};
     schemaDiagnostics.forEach((d) => {
       const { projectId, id: dialogId, dialogPath } = d;
-
-      const dialogContent = botProjectSpace
-        .find((bot) => bot.projectId === projectId)
-        ?.dialogs.find((dialog) => dialog.id === dialogId)?.content;
-
-      // Contains two cases: 1. action already disabled 2. action doesn't exists on this path.
-      if (get(dialogContent, `${dialogPath}.disabled`)) return;
-
-      // Manipulate the dialog content and caches the result
-      const dialogData = get(cachedDialogs, [projectId, dialogId]) ?? cloneDeep(dialogContent);
-      set(dialogData, `${dialogPath}.disabled`, true);
-      set(cachedDialogs, [projectId, dialogId], dialogData);
+      if (!dialogPath) return;
+      const currentPaths = get(aggregatedPaths, [projectId, dialogId]);
+      if (currentPaths) {
+        currentPaths.push(dialogPath);
+      } else {
+        set(aggregatedPaths, [projectId, dialogId], [dialogPath]);
+      }
     });
 
-    // Submit cached dialog updates to recoil store.
-    for (const [projectId, dialogs] of Object.entries(cachedDialogs)) {
-      for (const [dialogId, dialogData] of Object.entries(dialogs)) {
-        updateDialog({ id: dialogId, projectId, content: dialogData });
+    // Manipulate dialog contnent
+    for (const [projectId, pathsByDialogId] of Object.entries(aggregatedPaths)) {
+      const projectDialogs = botProjectSpace.find((bot) => bot.projectId === projectId)?.dialogs;
+      if (!Array.isArray(projectDialogs)) continue;
+
+      for (const [dialogId, paths] of Object.entries(pathsByDialogId)) {
+        const dialogData = projectDialogs.find((dialog) => dialog.id === dialogId)?.content;
+        if (!dialogData) continue;
+
+        const pathsToUpdate = paths.filter((p) => {
+          // Filters out those paths where action exists and action.disabled !== true
+          const data = get(dialogData, p);
+          return data && !get(data, 'disabled');
+        });
+        if (!pathsToUpdate.length) continue;
+
+        const copy = cloneDeep(dialogData);
+        for (const p of pathsToUpdate) {
+          set(copy, `${p}.disabled`, true);
+        }
+        updateDialog({ id: dialogId, projectId, content: copy });
       }
     }
   }, [diagnostics]);
