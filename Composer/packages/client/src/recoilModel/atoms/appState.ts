72dde8a971e1606e4789411ccb4d84d53fbc22ec
pavolum <pavolum@outlook.com>
2021-04-08 18:27:32 -0400

    fix: check for node on app start on users machine and notify if not installed (#6578)
    * Adding modal for node detection on app start
    
    * - Adding server side checking of node
    - Adding node modal on app start if node is not installed
    
    * Moving node check to creation flow as opposed to app start
    
    * Add isElectron check for node modal
    
    Co-authored-by: Patrick Volum <pavolum@microsoft.com>
    Co-authored-by: Ben Brown <benbro@microsoft.com>
    Co-authored-by: Soroush <hatpick@gmail.com>
    Co-authored-by: Chris Whitten <christopher.whitten@microsoft.com>

--- Composer/packages/client/src/recoilModel/atoms/appState.ts
+++ Composer/packages/client/src/recoilModel/atoms/appState.ts
@@ -344,3 +344,8 @@ export const isWebChatPanelVisibleState = atom<boolean>({
   key: getFullyQualifiedKey('isWebChatPanelVisible'),
   default: false,
 });
+
+export const userHasNodeInstalledState = atom<boolean>({
+  key: getFullyQualifiedKey('userHasNodeInstalled'),
+  default: true,
+});
