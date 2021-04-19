38fd5b58d11328cce6bce604477fecccef962004
pavolum <pavolum@outlook.com>
2021-04-19 17:07:18 -0400

    fix: multiple minor bugs in provision and creation (#7064)
    * Make create from qna url modal blocking
    
    * fix for bug in new tab view:
    add botProjectData as dep to regenerate botStatusList
    
    * Adding prevew tag to node tab in template selection view
    
    * Fix error message for bot name
    
    * fixing typo
    
    Co-authored-by: Chris Whitten <christopher.whitten@microsoft.com>

--- Composer/packages/client/src/pages/publish/Publish.tsx
+++ Composer/packages/client/src/pages/publish/Publish.tsx
@@ -95,7 +95,7 @@ const Publish: React.FC<RouteComponentProps<{ projectId: string; targetName?: st
 
   const botStatusList = useMemo(() => {
     return generateBotStatusList(currentBotList, botPropertyData, publishHistoryList);
-  }, [currentBotList, botPropertyData, publishHistoryList]);
+  }, [currentBotList, botPropertyData, publishHistoryList, botProjectData]);
 
   const isPublishPending = useMemo(() => {
     return Object.values(updaterStatus).some(Boolean);
