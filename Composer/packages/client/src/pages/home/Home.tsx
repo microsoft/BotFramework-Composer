996d681a8a1f329bc760cc376f37d5771fd220dc
zhixzhan <zhixzhan@microsoft.com>
2021-04-20 15:09:53 +0800

    lint

--- Composer/packages/client/src/pages/home/Home.tsx
+++ Composer/packages/client/src/pages/home/Home.tsx
@@ -202,7 +202,7 @@ const Home: React.FC<RouteComponentProps> = () => {
           </div>
           <div css={home.videosContainer}>
             <div css={home.rowContainer}>
-              <Pivot aria-label="Videos and articles" linkSize={PivotLinkSize.large} css={home.pivotContainer}>
+              <Pivot aria-label="Videos and articles" css={home.pivotContainer} linkSize={PivotLinkSize.large}>
                 {feed.tabs.map((tab, index) => (
                   <PivotItem key={index} headerText={tab.title}>
                     {tab.viewAllLinkText && (
