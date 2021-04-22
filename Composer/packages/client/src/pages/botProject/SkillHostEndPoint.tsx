b6ea77651db61f45f11bddd47174fe24f8ac5d89
pavolum <pavolum@outlook.com>
2021-04-21 21:50:45 -0400

    fix: update UI in bot settings page to final R13 design spec (#7065)
    * tab name changes
    
    * Updating to latest UI: first/overview tab in settings page
    
    * Adding (root) suffix to root bot in settings view nav pane
    
    * removing old table view of bot settings page
    
    * Fixing Luis and Qna styling
    
    * Fixing fluentUI imports and skills settings page UI
    
    * Fixing UI for appID component in settings page
    
    * Fixing connections tab UI
    
    * Fixing skills tab UI
    
    * language tab UI fixes
    
    * Adding teams callout when teams is enabled
    
    * removing unused variables and deps
    
    * Changing text with links to use formatMessage.rich
    
    * Fix column width
    
    * Fixing luis provision modal read me link
    
    * Updating locale file
    
    * Remove unused import
    
    * Update unit test to reflect new tab text
    
    * Add links to new hyperlinks in settings page
    
    * Adding learnmore links to new qna/luis modal implementation
    
    * Adding keys to rich format message
    
    * updating locale file
    
    * Making minor changes requested in PR
    
    Co-authored-by: Soroush <hatpick@gmail.com>

--- Composer/packages/client/src/pages/botProject/SkillHostEndPoint.tsx
+++ Composer/packages/client/src/pages/botProject/SkillHostEndPoint.tsx
@@ -2,7 +2,7 @@
 // Licensed under the MIT License.
 
 /** @jsx jsx */
-import React from 'react';
+import React, { Fragment } from 'react';
 import { jsx, css } from '@emotion/core';
 import { useRecoilValue } from 'recoil';
 import { TextField } from 'office-ui-fabric-react/lib/TextField';
@@ -11,13 +11,13 @@ import { Icon } from 'office-ui-fabric-react/lib/Icon';
 import formatMessage from 'format-message';
 import { FontSizes } from 'office-ui-fabric-react/lib/Styling';
 import { SharedColors } from '@uifabric/fluent-theme';
+import { Link } from 'office-ui-fabric-react/lib/components/Link';
 
 import { dispatcherState, settingsState } from '../../recoilModel';
-import { CollapsableWrapper } from '../../components/CollapsableWrapper';
 import { rootBotProjectIdSelector } from '../../recoilModel/selectors/project';
 import { mergePropertiesManagedByRootBot } from '../../recoilModel/dispatchers/utils/project';
 
-import { title } from './styles';
+import { subtext, title } from './styles';
 // -------------------- Styles -------------------- //
 
 const labelContainer = css`
@@ -70,7 +70,20 @@ export const SkillHostEndPoint: React.FC<SkillHostEndPointProps> = (props) => {
   const { skillHostEndpoint } = useRecoilValue(settingsState(projectId));
 
   return (
-    <CollapsableWrapper title={formatMessage('Skill host endpoint')} titleStyle={title}>
+    <Fragment>
+      <div css={title}>{formatMessage('Call skills')}</div>
+      <div css={subtext}>
+        {formatMessage.rich(
+          'Add a skill host endpoint so your skills can reliably connect to your root bot. <a>Learn more</a>.',
+          {
+            a: ({ children }) => (
+              <Link key="skills-settings-page" href={'https://aka.ms/composer-skills-learnmore'} target="_blank">
+                {children}
+              </Link>
+            ),
+          }
+        )}
+      </div>
       <TextField
         ariaLabel={formatMessage('Skill host endpoint url')}
         data-testid={'SkillHostEndPointTextField'}
@@ -85,6 +98,6 @@ export const SkillHostEndPoint: React.FC<SkillHostEndPointProps> = (props) => {
         }}
         onRenderLabel={onRenderLabel}
       />
-    </CollapsableWrapper>
+    </Fragment>
   );
 };
