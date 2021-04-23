6e872ca3eff3642bfd4624b68fc26fcd12a6a0da
Andy Brown <asbrown002@gmail.com>
2021-04-23 12:54:08 -0700

    fix: refactor way we get COMPOSER_VERSION (#7326)
    Co-authored-by: Chris Whitten <christopher.whitten@microsoft.com>

--- Composer/packages/electron-server/src/preload.js
+++ Composer/packages/electron-server/src/preload.js
@@ -1,11 +1,9 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-const { app, ipcRenderer } = require('electron'); // eslint-disable-line
+const { ipcRenderer } = require('electron'); // eslint-disable-line
 
 // expose ipcRenderer to the browser
 window.ipcRenderer = ipcRenderer;
-// get the app version to hand into the client
-window.appVersion = app.getVersion();
 // flag to distinguish electron client from web app client
 window.__IS_ELECTRON__ = true;
