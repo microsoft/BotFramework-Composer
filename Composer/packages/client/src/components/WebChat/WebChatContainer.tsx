0ccc30dd5d30af153c2b0afb2e185a8521a5a702
Srinaath Ravichandran <srravich@microsoft.com>
2021-04-26 12:47:01 -0700

    Z index set
    Signed-off-by: Srinaath Ravichandran <srravich@microsoft.com>

--- Composer/packages/client/src/components/WebChat/WebChatContainer.tsx
+++ Composer/packages/client/src/components/WebChat/WebChatContainer.tsx
@@ -5,11 +5,10 @@ import { jsx } from '@emotion/core';
 import { NeutralColors } from '@uifabric/fluent-theme';
 import { useRecoilValue } from 'recoil';
 
+import { zIndices } from '../../utils/zIndices';
 import { isWebChatPanelVisibleState, webChatEssentialsSelector, rootBotProjectIdSelector } from '../../recoilModel';
 import { BASEPATH } from '../../constants';
 import { WebChatPanel } from '../../components/WebChat/WebChatPanel';
-
-const webChatPanelZIndex = 1;
 
 export const WebChatContainer = () => {
   const rootBotId = useRecoilValue(rootBotProjectIdSelector) ?? '';
@@ -23,7 +22,7 @@ export const WebChatContainer = () => {
         height: '100%',
         width: '395px',
         right: 0,
-        zIndex: webChatPanelZIndex,
+        zIndex: zIndices.webChatContainer,
         display: isWebChatPanelVisible ? 'block' : 'none',
         background: `${NeutralColors.white}`,
         boxShadow: '-4px 0px 6px 1px rgb(0 0 0 / 10%)',
