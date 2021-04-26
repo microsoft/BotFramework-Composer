262856ea01cbf24b651c001c35867b4fc3988408
Long Alan <julong@microsoft.com>
2021-04-26 16:49:28 +0800

    lint

--- Composer/packages/client/src/pages/design/exportSkillModal/content/AddCallers.tsx
+++ Composer/packages/client/src/pages/design/exportSkillModal/content/AddCallers.tsx
@@ -7,7 +7,7 @@ import { SharedColors } from '@uifabric/fluent-theme';
 import formatMessage from 'format-message';
 import { ActionButton } from 'office-ui-fabric-react/lib/Button';
 import { FontWeights } from 'office-ui-fabric-react/lib/Styling';
-import { ITextField, ITextFieldProps, TextField, TextFieldBase } from 'office-ui-fabric-react/lib/TextField';
+import { ITextField, ITextFieldProps, TextField } from 'office-ui-fabric-react/lib/TextField';
 import React, { useEffect, useRef, useState } from 'react';
 
 import { tableColumnHeader, tableRow, tableRowItem } from '../../../botProject/styles';
