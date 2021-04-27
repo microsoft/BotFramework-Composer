c2e523279a6cbb4a7e1f1217185b73070370411c
Ben Yackley <61990921+beyackle@users.noreply.github.com>
2021-04-27 09:12:42 -0700

    Update About.tsx

--- Composer/packages/client/src/pages/about/About.tsx
+++ Composer/packages/client/src/pages/about/About.tsx
@@ -36,11 +36,11 @@ export const About: React.FC<RouteComponentProps> = () => {
             <p4>The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.</p4>
             <p5>THE SOFTWARE IS PROVIDED AS IS, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.</p5>`,
             {
-              p1: ({ children }) => <p key="p1">{children}</p>,
-              p2: ({ children }) => <p key="p2">{children}</p>,
-              p3: ({ children }) => <p key="p3">{children}</p>,
-              p4: ({ children }) => <p key="p4">{children}</p>,
-              p5: ({ children }) => <p key="p5">{children}</p>,
+              p1: ({ children }) => <p key="copyright">{children}</p>,
+              p2: ({ children }) => <p key="title">{children}</p>,
+              p3: ({ children }) => <p key="permission-granted">{children}</p>,
+              p4: ({ children }) => <p key="license-inclusion-clause">{children}</p>,
+              p5: ({ children }) => <p key="as-is-disclaimer">{children}</p>,
             }
           )}
         </div>
