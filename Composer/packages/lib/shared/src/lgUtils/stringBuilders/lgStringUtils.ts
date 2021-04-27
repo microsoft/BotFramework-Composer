10f058b4bea2c0976850bd817e3c070e257ec2db
Soroush <sorgh@microsoft.com>
2021-04-27 13:48:28 -0700

    added comments

--- Composer/packages/lib/shared/src/lgUtils/stringBuilders/lgStringUtils.ts
+++ Composer/packages/lib/shared/src/lgUtils/stringBuilders/lgStringUtils.ts
@@ -7,6 +7,9 @@ export type TemplateBodyItem = {
   value: string;
 };
 
+/**
+ * Returns the template kind based on the item text.
+ */
 const getItemKind = (itemText: string): TemplateBodyItem['kind'] => {
   const firstChar = itemText.charAt(0);
   switch (firstChar) {
@@ -19,6 +22,9 @@ const getItemKind = (itemText: string): TemplateBodyItem['kind'] => {
   }
 };
 
+/**
+ * Cleans up the text of the item based on its kind.
+ */
 const getItemText = (itemText: string, kind: TemplateBodyItem['kind']) => {
   switch (kind) {
     case 'comment':
@@ -30,6 +36,7 @@ const getItemText = (itemText: string, kind: TemplateBodyItem['kind']) => {
   }
 };
 
+// This regex can be used to split a template body by newline followed by -, > or another new line
 const splitRegex = /\r?\n(?=-|>|\r?\n)/g;
 export const parseTemplateBody = (body?: string): TemplateBodyItem[] => {
   /**
@@ -40,11 +47,17 @@ export const parseTemplateBody = (body?: string): TemplateBodyItem[] => {
   let multiLineItem = '';
   const fixedItems: string[] = [];
 
+  /**
+   * This block fixes the variations that are multiline and include - or >
+   */
   for (let i = 0; i < items.length; i++) {
     const item = items[i];
     if (multiLineItem || item.startsWith('-')) {
+      // Check if this item is the start of a multiline variation
       if (/^-( )*```/.test(item)) {
         multiLineItem = item;
+        // Check if multiline variation ends in the same line (one line)
+        // If yes, it will start keeping track of the multiline variation
         if(/```$/.test(item)) {
           fixedItems.push(multiLineItem);
           multiLineItem = '';
@@ -54,6 +67,8 @@ export const parseTemplateBody = (body?: string): TemplateBodyItem[] => {
         continue;
       }
 
+      // Check if this item is the end of a multiline variation
+      // If yes, it will add the multiline variation into the result array
       if (/```$/.test(item)) {
         multiLineItem += item;
         fixedItems.push(multiLineItem);
