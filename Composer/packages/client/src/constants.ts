245e34e822416f6f6e9ad519c45fe93e73d3a14e
Andy Brown <asbrown002@gmail.com>
2021-04-29 13:41:06 -0700

    update v2 invalid name char

--- Composer/packages/client/src/constants.ts
+++ Composer/packages/client/src/constants.ts
@@ -421,7 +421,7 @@ export const nameRegex = /^[a-zA-Z0-9-_]+$/;
 export const nameRegexV2 = /^[a-zA-Z0-9_]+$/;
 
 export const invalidNameCharRegex = /[^a-zA-Z0-9-_]/g;
-export const invalidNameCharRegexV2 = /[^a-zA-Z0-9-_]/g;
+export const invalidNameCharRegexV2 = /[^a-zA-Z0-9_]/g;
 
 export const authConfig = {
   // for web login
