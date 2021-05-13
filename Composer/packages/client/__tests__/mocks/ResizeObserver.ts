a788889d4ce3255ed3f4c3fa8aad447f166e1b7c
zhixzhan <zhixzhan@microsoft.com>
2021-05-13 11:26:48 +0800

    fix type in test

---
+++ Composer/packages/client/__tests__/mocks/ResizeObserver.ts
@@ -0,0 +1,11 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+export default class ResizeObserver {
+  observe() {
+    // do nothing
+  }
+  disconnect() {
+    // do nothing
+  }
+}
