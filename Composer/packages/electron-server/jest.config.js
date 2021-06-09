a4cc7a373fd1b45a129a874064ccf196f26bc6f6
Andy Brown <asbrown002@gmail.com>
2021-06-09 09:01:25 -0700

    fix jest-haste-map warning

--- Composer/packages/electron-server/jest.config.js
+++ Composer/packages/electron-server/jest.config.js
@@ -3,4 +3,5 @@ const { createConfig } = require('@botframework-composer/test-utils');
 module.exports = createConfig('electron-server', 'node', {
   setupFilesAfterEnv: ['./__tests__/setupTests.js'],
   testPathIgnorePatterns: ['/node_modules/', '/__tests__/setupTests.js', 'dist'],
+  modulePathIgnorePatterns: ['dist'],
 });
