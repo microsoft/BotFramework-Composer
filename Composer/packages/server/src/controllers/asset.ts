de481e05d8cc95c7c39be5af3917ff359e9c5eb0
Patrick Volum <pavolum@outlook.com>
2021-06-10 12:25:07 -0400

    Remove unused import

--- Composer/packages/server/src/controllers/asset.ts
+++ Composer/packages/server/src/controllers/asset.ts
@@ -1,7 +1,6 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-import { Request, Response } from 'express';
 import { BotTemplate, emptyBotNpmTemplateName, localTemplateId, QnABotTemplateId } from '@bfc/shared';
 import formatMessage from 'format-message';
 
