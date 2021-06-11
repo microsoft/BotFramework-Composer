7db26a4ceb724dbb1d816042a832ca973ac049cd
Soroush <hatpick@fmail.com>
2021-06-11 13:54:40 -0700

    Add list entity creation dialog to LU editor

--- Composer/packages/lib/code-editor/src/lu/types.ts
+++ Composer/packages/lib/code-editor/src/lu/types.ts
@@ -4,3 +4,22 @@
 export const toolbarSupportedLuEntityTypes = ['prebuilt', 'ml', 'list', 'composite', 'regex'] as const;
 export type ToolbarLuEntityType = typeof toolbarSupportedLuEntityTypes[number];
 export type LuToolbarButtonKind = 'useEntity' | 'defineEntity';
+
+type CommonEntity = {
+  entityType: ToolbarLuEntityType;
+};
+
+export type ListEntityItem = {
+  id: number;
+  normalizedValue: string;
+  synonyms: string[];
+};
+
+export type ListEntity = CommonEntity & {
+  entityType: 'list';
+  name: string;
+  items: ListEntityItem[];
+};
+
+// This type can be expanded to other types of entities
+export type Entity = ListEntity;
---
+++ Composer/packages/lib/ui-shared/src/components/tags/ContentEditable.tsx
@@ -0,0 +1,121 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+/* eslint-disable react/jsx-sort-props */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable react/no-danger */
+
+import * as React from 'react';
+
+import { safeHtmlString } from './utils';
+
+type ContentEditableProps = {
+  value: string;
+  className?: string;
+  innerEditableRef: React.RefObject<HTMLDivElement>;
+  inputRef: React.RefObject<HTMLInputElement>;
+  removeOnBackspace?: boolean;
+  style?: React.CSSProperties;
+  onChange: (value: string) => void;
+  onRemove: () => void;
+  onValidate?: (value: string) => boolean;
+};
+
+export const ContentEditable = (props: ContentEditableProps) => {
+  const {
+    value,
+    className = '',
+    style,
+    innerEditableRef,
+    inputRef,
+    removeOnBackspace,
+    onChange,
+    onRemove,
+    onValidate,
+  } = props;
+
+  const removed = React.useRef(false);
+  const preFocusedValue = React.useRef('');
+
+  const getValue = () => innerEditableRef.current?.innerText || '';
+
+  const focusInputRef = React.useCallback(() => {
+    if (inputRef?.current) {
+      inputRef.current.focus();
+    }
+  }, [inputRef]);
+
+  React.useEffect(() => {
+    preFocusedValue.current = getValue();
+  }, []);
+
+  const onPaste = React.useCallback((e: React.ClipboardEvent<HTMLDivElement>) => {
+    // Cancel paste event
+    e.preventDefault();
+
+    // Remove formatting from clipboard contents
+    const text = e.clipboardData.getData('text/plain');
+
+    // Insert text manually from paste command
+    document.execCommand('insertHTML', false, safeHtmlString(text));
+  }, []);
+
+  const onFocus = React.useCallback(() => {
+    preFocusedValue.current = getValue();
+  }, []);
+
+  const onBlur = React.useCallback(() => {
+    if (!removed && innerEditableRef.current) {
+      // On blur, if no content in tag, remove it
+      if (innerEditableRef.current.innerText === '') {
+        props.onRemove();
+        return;
+      }
+
+      // Validate input if needed
+      if (onValidate) {
+        const valid = onValidate(getValue());
+        // If invalidate, switch ref back to pre focused value
+        if (!valid) {
+          innerEditableRef.current.innerText = preFocusedValue.current;
+          return;
+        }
+      }
+      onChange(innerEditableRef.current.innerText);
+    }
+  }, [onChange, onRemove, onValidate, innerEditableRef]);
+
+  const onKeyDown = React.useCallback(
+    (e: React.KeyboardEvent<HTMLDivElement>) => {
+      // On enter, focus main tag input
+      if (e.key === 'Enter') {
+        e.preventDefault();
+        focusInputRef();
+        return;
+      }
+
+      const currentVal = getValue();
+      if (removeOnBackspace && e.key === 'Backspace' && currentVal === '') {
+        removed.current = true;
+        onRemove();
+        focusInputRef();
+        return;
+      }
+    },
+    [removeOnBackspace, onRemove, focusInputRef]
+  );
+
+  return (
+    <div
+      style={style}
+      dangerouslySetInnerHTML={{ __html: safeHtmlString(value) }}
+      ref={innerEditableRef}
+      className={className}
+      contentEditable
+      onBlur={onBlur}
+      onFocus={onFocus}
+      onKeyDown={onKeyDown}
+      onPaste={onPaste}
+    />
+  );
+};
