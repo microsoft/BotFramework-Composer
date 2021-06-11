d2e62f00d2007a7a9ee93c6e231b793227c54a85
Andy Brown <asbrown002@gmail.com>
2021-06-11 11:04:09 -0700

    move integration tests into own workspace
    this is required due to a dependency resolution conflict within @testing-library/dom. See https://github.com/testing-library/dom-testing-library/issues/963

---
+++ Composer/packages/integration-tests/scripts/e2e-ci.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+function cleanup {
+  kill $SERVER_PID
+}
+
+# move up to composer root dir
+cd ../..
+yarn start >> e2e.log 2>&1 &
+SERVER_PID=$!
+
+cd packages/integration-tests
+npx cypress run
+EXIT_CODE=$?
+
+# kill server process
+trap cleanup EXIT
+
+exit $EXIT_CODE
