6a160b29e76bf293586c07358c370228b4d8fc79
Ben Yackley <61990921+beyackle@users.noreply.github.com>
2021-06-15 14:47:18 -0700

    Update constants.tsx

--- Composer/packages/client/src/constants.tsx
+++ Composer/packages/client/src/constants.tsx
@@ -526,8 +526,7 @@ export const defaultTeamsManifest: TeamsManifest = {
 export const defaultBotPort = 3979;
 export const defaultBotEndpoint = `http://localhost:${defaultBotPort}/api/messages`;
 
-const DAYS_IN_MS = 1000 * 60 * 60 * 24;
-
+// const DAYS_IN_MS = 1000 * 60 * 60 * 24;
 // export const SURVEY_PARAMETERS = {
 //   daysUntilEligible: 5,
 //   timeUntilNextSurvey: 90 * DAYS_IN_MS,
