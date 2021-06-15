f02d87dd3bbb2434c70c697afa0dcbe4ac461a1d
natalgar <garcia.natalie@microsoft.com>
2021-06-15 14:14:52 -0700

    error handling and retry logic for resource groups

--- extensions/azurePublishNew/src/services/resourceGroupService.ts
+++ extensions/azurePublishNew/src/services/resourceGroupService.ts
@@ -5,17 +5,62 @@ import { TokenCredentials } from '@azure/ms-rest-js';
 
 import { throwNotImplementedError } from './throwNotImplementedError';
 
-const createResourceGroupService = (token: string, subscriptionId: string) => {
+const sleep = (waitTimeInMs: number) => new Promise((resolve) => setTimeout(resolve, waitTimeInMs));
+
+const runWithRetry = async (requestOperation: () => Promise<any>, numOfRetries = 2) => {
+  let result;
+  let retryCount = numOfRetries;
+
+  while (retryCount >= 0) {
+    try {
+      result = await requestOperation();
+    } catch (err) {
+      if (retryCount == 0) {
+        const errorMessage = JSON.stringify(err, Object.getOwnPropertyNames(err));
+        throw new Error(errorMessage);
+      } else {
+        // if auth expired, don't retry...
+        await sleep(3000);
+        retryCount--;
+        continue;
+      }
+    }
+
+    break;
+  }
+
+  return result;
+};
+
+const wrapWithRetry = (operation: () => Promise<any>) => {
+  return () => runWithRetry(operation);
+};
+
+const runWithErrorHandling = async (operation: () => Promise<any>) => {
+  try {
+    const result = await operation();
+    return result;
+  } catch (e) {
+    return e;
+  }
+};
+
+const runWithRetryAndErrorHandling = (operation: () => Promise<any>) => {
+  return runWithErrorHandling(wrapWithRetry(operation));
+};
+
+export const createResourceGroupService = (token: string, subscriptionId: string) => {
   const tokenCredentials = new TokenCredentials(token);
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const client = new ResourceManagementClient(tokenCredentials, subscriptionId);
 
   const list = async () => {
-    throwNotImplementedError();
+    const listOperation = () => client.resourceGroups.list();
+    return await runWithRetryAndErrorHandling(listOperation);
   };
 
-  const get = async () => {
-    throwNotImplementedError();
+  const get = async (resourceGroupName: string) => {
+    const getOperation = () => client.resourceGroups.get(resourceGroupName);
+    return await runWithRetryAndErrorHandling(getOperation);
   };
 
   const update = async () => {
@@ -30,8 +75,9 @@ const createResourceGroupService = (token: string, subscriptionId: string) => {
     throwNotImplementedError();
   };
 
-  const checkNameAvailability = async () => {
-    throwNotImplementedError();
+  const checkNameAvailability = async (resourceGroupName: string) => {
+    const checkExistenceOperation = () => client.resourceGroups.checkExistence(resourceGroupName);
+    return await runWithRetryAndErrorHandling(checkExistenceOperation);
   };
 
   return {
