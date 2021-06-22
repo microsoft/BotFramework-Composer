d304c721e3b5b86f75c234e7f179279d52315de6
Lu Han <32191031+luhan2017@users.noreply.github.com>
2021-06-22 09:25:17 +0800

    Update deploy.ts

--- extensions/azurePublish/src/node/deploy.ts
+++ extensions/azurePublish/src/node/deploy.ts
@@ -64,7 +64,7 @@ export class BotProjectDeploy {
       }
 
       // Update skill host endpoint
-      if (Object.keys(settings?.skill).length > 0) {
+      if (settings?.skill && Object.keys(settings?.skill).length > 0) {
         settings.skillHostEndpoint = `https://${hostname}.azurewebsites.net/api/skills`;
       }
 
