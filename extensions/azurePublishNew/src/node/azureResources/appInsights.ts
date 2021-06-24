9986da570fc55b4f90e7171ca0aded32bd243215
Natalie Garcia <82851479+natalgar@users.noreply.github.com>
2021-06-24 11:47:19 -0700

    feat: Implement provision servies for azure resources (#8174)
    * set up provision services
    
    * add appregistration test
    
    * change Azure Hosting to App Services
    
    * refactor invalid to notallowed
    
    * refactor types
    
    * use bfc-shared package
    
    * refactor resource definitions
    
    * change webapp text to App Services string
    
    * refactor getResources
    
    * fix blobstorage tier description
    
    * refactor resourcedefinition group names and tier names
    
    * refactor webapp and appregistration provision methods
    
    * fix dependencies
    
    Co-authored-by: natalgar <natgarcia@microsoft.com>
    Co-authored-by: natalgar <natalie.garcia@microsoft.com>

---
+++ extensions/azurePublishNew/src/node/azureResources/appInsights.ts
@@ -0,0 +1,24 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import { ProvisionMethod, ProvisionWorkingSet, ResourceProvisionService } from '../types';
+
+import { AppInsightsConfigNew } from './types';
+
+const getAppInsightsProvisionMethod = (): ProvisionMethod => {
+  return (config: AppInsightsConfigNew, workingSet: ProvisionWorkingSet): Promise<ProvisionWorkingSet> => {
+    const provisionResult = { instrumentationKey: '', connectionString: '' };
+    return {
+      ...workingSet,
+      appInsights: provisionResult,
+    };
+  };
+};
+
+export const getAppInsightsProvisionService = (): ResourceProvisionService => {
+  return {
+    getDependencies: () => ['appRegistration', 'botRegistration'],
+    getRecommendationForProject: (project) => 'optional',
+    provision: getAppInsightsProvisionMethod(),
+  };
+};
---
+++ extensions/azurePublishNew/src/node/azureResources/luisPrediction.ts
@@ -0,0 +1,22 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import { ProvisionMethod, ProvisionWorkingSet, ResourceProvisionService } from '../types';
+
+import { LuisPredictionConfig } from './types';
+
+const getLuisPredictionProvisionMethod = (): ProvisionMethod => {
+  return (config: LuisPredictionConfig, workingSet: ProvisionWorkingSet): Promise<ProvisionWorkingSet> => {
+    return workingSet;
+  };
+};
+
+export const getLuisPredictionProvisionService = (): ResourceProvisionService => {
+  return {
+    getDependencies: () => [],
+    getRecommendationForProject: (project) => {
+      return project.requiresLuisPrediction; // tbd
+    },
+    provision: getLuisPredictionProvisionMethod(),
+  };
+};
