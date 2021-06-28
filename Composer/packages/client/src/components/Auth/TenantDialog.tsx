8f0b3a6156d69ca1ec0261db5e81fdc55c2efbdd
Ben Brown <benbro@microsoft.com>
2021-06-28 14:37:19 -0500

    adjust ui of tenant dialog, select current/default tenant

--- Composer/packages/client/src/components/Auth/TenantDialog.tsx
+++ Composer/packages/client/src/components/Auth/TenantDialog.tsx
@@ -4,6 +4,8 @@
 /** @jsx jsx */
 import { jsx } from '@emotion/core';
 import { DialogWrapper, DialogTypes } from '@bfc/ui-shared';
+import { FontSizes } from '@uifabric/fluent-theme';
+import { FontWeights } from 'office-ui-fabric-react/lib/Styling';
 import formatMessage from 'format-message';
 import { DialogFooter } from 'office-ui-fabric-react/lib/Dialog';
 import { PrimaryButton } from 'office-ui-fabric-react/lib/Button';
@@ -11,7 +13,7 @@ import { ChoiceGroup } from 'office-ui-fabric-react/lib/ChoiceGroup';
 import { useState } from 'react';
 import { useRecoilValue } from 'recoil';
 
-import { availableTenantsState, dispatcherState } from '../../recoilModel/atoms';
+import { availableTenantsState, dispatcherState, currentTenantIdState } from '../../recoilModel/atoms';
 
 export interface TenantDialogProps {
   onDismiss: () => void;
@@ -19,28 +21,52 @@ export interface TenantDialogProps {
 export const TenantDialog: React.FC<TenantDialogProps> = (props) => {
   const availableTenants = useRecoilValue(availableTenantsState);
   const { setCurrentTenant } = useRecoilValue(dispatcherState);
-  const [tenant, setTenant] = useState<string>('');
+  const currentTenant = useRecoilValue(currentTenantIdState);
+  // select current or default to first
+  const [tenant, setTenant] = useState<string>(currentTenant || availableTenants[0]?.tenantId);
 
   return (
     <DialogWrapper
       isBlocking
       isOpen
-      dialogType={DialogTypes.CreateFlow}
+      customerStyle={{
+        dialog: {
+          title: {
+            fontWeight: FontWeights.bold,
+            fontSize: FontSizes.size20,
+            paddingTop: '14px',
+            paddingBottom: '11px',
+          },
+          subText: {
+            fontSize: FontSizes.size14,
+          },
+        },
+        modal: {
+          main: {
+            maxWidth: '80% !important',
+            width: '492px !important',
+          },
+        },
+      }}
+      dialogType={DialogTypes.Customer}
       subText={formatMessage(
         'The directory you choose will impact subscription, resource group, and region filters that are available when you select or create Azure resources. You can change this later.'
       )}
       title={formatMessage('Select directory')}
       onDismiss={props.onDismiss}
     >
-      <ChoiceGroup
-        required
-        options={availableTenants.map((tenant) => {
-          return { key: tenant.tenantId, text: tenant.displayName };
-        })}
-        onChange={(ev, choice) => {
-          setTenant(choice?.key ?? '');
-        }}
-      />
+      <div css={{ maxHeight: 210, overflowY: 'auto' }}>
+        <ChoiceGroup
+          required
+          options={availableTenants.map((tenant) => {
+            return { key: tenant.tenantId, text: tenant.displayName };
+          })}
+          selectedKey={tenant}
+          onChange={(ev, choice) => {
+            setTenant(choice?.key ?? '');
+          }}
+        />
+      </div>
       <DialogFooter>
         <PrimaryButton
           disabled={!tenant}
