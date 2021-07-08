e43e4a34f160440ff0d7590a334feb2d43e12041
Andy Brown <asbrown002@gmail.com>
2021-07-08 09:15:29 -0700

    update input type options

--- Composer/packages/ui-plugins/schema-editor/src/Fields/ValueRefField.tsx
+++ Composer/packages/ui-plugins/schema-editor/src/Fields/ValueRefField.tsx
@@ -11,10 +11,12 @@ import { valueTypeDefinitions } from '../schema';
 
 export const ValueRefField: React.FC<FieldProps> = ({ description, id, label, value, required, onChange }) => {
   const options = useMemo<IDropdownOption[]>(() => {
-    return Object.entries(valueTypeDefinitions || {}).map(([key, value]) => ({
-      key: `#/definitions/${key}`,
-      text: value?.title || startCase(key),
-    }));
+    return Object.entries(valueTypeDefinitions || {})
+      .map(([key, value]) => ({
+        key: `#/definitions/${key}`,
+        text: value?.title || startCase(key),
+      }))
+      .filter((o) => !o.key.includes('equalsExpression')); // a value must be a type, not just an expression
   }, []);
 
   const handleChange = (_: React.FormEvent<HTMLDivElement>, option?: IDropdownOption) => {
