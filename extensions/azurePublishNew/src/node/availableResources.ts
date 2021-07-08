56f83892ed69c5a0f5b663611974147ff825555b
natalgar <natalie.garcia@microsoft.com>
2021-07-08 16:00:59 -0700

    add cosmosdb to available resources

--- extensions/azurePublishNew/src/node/availableResources.ts
+++ extensions/azurePublishNew/src/node/availableResources.ts
@@ -21,7 +21,7 @@ import {
 import { azureFunctionDefinition, getAzureFunctionsProvisionService } from './azureResources/azureFunction';
 import { blobStorageDefinition, getBlogStorageProvisionService } from './azureResources/blobStorage';
 import { botRegistrationDefinition, getBotChannelProvisionService } from './azureResources/botChannel';
-import { cosmosDbDefinition, getCosmosDbProvisionService } from './azureResources/cosmosDb';
+import { CosmosDbConfig, cosmosDbDefinition, getCosmosDbProvisionService } from './azureResources/cosmosDb';
 import { getLuisAuthoringProvisionService, luisAuthoringDefinition } from './azureResources/luisAuthoring';
 import { getLuisPredictionProvisionService, luisPredictionDefinition } from './azureResources/luisPrediction';
 import { getQnAProvisionService, qnaDefinition } from './azureResources/qna';
@@ -74,6 +74,8 @@ export const getResourceDependencies = (key: string) => {
       return servicePlanDefinition.dependencies;
     case AzureResourceTypes.APPINSIGHTS:
       return appInsightsDefinition.dependencies;
+    case AzureResourceTypes.COSMOSDB:
+      return cosmosDbDefinition.dependencies;
     default:
       return [];
   }
@@ -110,6 +112,16 @@ export const provisionConfigToResourceConfigMap = {
       resourceGroupName: config.resourceGroup,
       location: config.location,
       name: config.hostname,
+    };
+  },
+  cosmosDB: (config: ProvisioningConfig): CosmosDbConfig => {
+    return {
+      key: 'cosmosDb',
+      containerName: `botstate-container`,
+      databaseName: `botstate-db`,
+      displayName: config.hostname.replace(/_/g, '').substr(0, 31).toLowerCase(),
+      location: config.location,
+      resourceGroupName: config.resourceGroup,
     };
   },
 };
