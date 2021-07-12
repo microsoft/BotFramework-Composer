757d89142d12f391fe1691d976986f1f6ec6b83f
Srinaath Ravichandran <srravich@microsoft.com>
2021-07-12 12:55:26 -0700

    Update to WC 4.14.0
    Signed-off-by: Srinaath Ravichandran <srravich@microsoft.com>

--- Composer/packages/client/src/components/WebChat/utils/WebChatTypes.d.ts
+++ Composer/packages/client/src/components/WebChat/utils/WebChatTypes.d.ts
@@ -1,53 +1,53 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
+// // Copyright (c) Microsoft Corporation.
+// // Licensed under the MIT License.
 
-// TODO: Remove the types declaration file when https://github.com/microsoft/BotFramework-WebChat/issues/3856 is fixed.
-declare module 'botframework-webchat' {
-  export const renderWebChat: any;
-  export const createDirectLine: (options: any) => any;
-  export const hooks: any;
-  export const createStyleSet: any;
-  export const Components: {
-    AdaptiveCardContent: any;
-    AudioCardContent: any;
-    AnimationCardContent: any;
-    Composer: any;
-    HeroCardContent: any;
-    OAuthCardContent: any;
-    ReceiptCardContent: any;
-    SignInCardContent: any;
-    ThumbnailCardContent: any;
-    VideoCardContent: any;
-    BasicWebChat: any;
-    Localize: any;
-    AudioContent: any;
-    FileContent: any;
-    HTMLVideoContent: any;
-    ImageContent: any;
-    TextContent: any;
-    VideoContent: any;
-    VimeoContent: any;
-    YouTubeContent: any;
-    Avatar: any;
-    Bubble: any;
-    ErrorBox: any;
-    SendStatus: any;
-    SpeakActivity: any;
-    Timestamp: any;
-    connectSendStatus: any;
-    connectSpeakActivity: any;
-    DictationInterims: any;
-    MicrophoneButton: any;
-    SendButton: any;
-    SendTextBox: any;
-    SuggestedActions: any;
-    UploadButton: any;
-    connectDictationInterims: any;
-    connectMicrophoneButton: any;
-    connectSendButton: any;
-    connectSendTextBox: any;
-    UploadButton: any;
-    connectUploadButton: any;
-  };
-  export default ReactWebChat;
-}
+// // TODO: Remove the types declaration file when https://github.com/microsoft/BotFramework-WebChat/issues/3856 is fixed.
+// declare module 'botframework-webchat' {
+//   export const renderWebChat: any;
+//   export const createDirectLine: (options: any) => any;
+//   export const hooks: any;
+//   export const createStyleSet: any;
+//   export const Components: {
+//     AdaptiveCardContent: any;
+//     AudioCardContent: any;
+//     AnimationCardContent: any;
+//     Composer: any;
+//     HeroCardContent: any;
+//     OAuthCardContent: any;
+//     ReceiptCardContent: any;
+//     SignInCardContent: any;
+//     ThumbnailCardContent: any;
+//     VideoCardContent: any;
+//     BasicWebChat: any;
+//     Localize: any;
+//     AudioContent: any;
+//     FileContent: any;
+//     HTMLVideoContent: any;
+//     ImageContent: any;
+//     TextContent: any;
+//     VideoContent: any;
+//     VimeoContent: any;
+//     YouTubeContent: any;
+//     Avatar: any;
+//     Bubble: any;
+//     ErrorBox: any;
+//     SendStatus: any;
+//     SpeakActivity: any;
+//     Timestamp: any;
+//     connectSendStatus: any;
+//     connectSpeakActivity: any;
+//     DictationInterims: any;
+//     MicrophoneButton: any;
+//     SendButton: any;
+//     SendTextBox: any;
+//     SuggestedActions: any;
+//     UploadButton: any;
+//     connectDictationInterims: any;
+//     connectMicrophoneButton: any;
+//     connectSendButton: any;
+//     connectSendTextBox: any;
+//     UploadButton: any;
+//     connectUploadButton: any;
+//   };
+//   export default ReactWebChat;
+// }
