7645eb7cda0dcc86adbbf1611ca78b651babd4ff
Eugene Olonov <v-evolo@microsoft.com>
2021-12-09 11:34:24 +0200

    Fix losing keyboard focus after modal gets closed

--- Composer/packages/client/src/pages/botProject/RootBotExternalService.tsx
+++ Composer/packages/client/src/pages/botProject/RootBotExternalService.tsx
@@ -410,7 +410,6 @@ export const RootBotExternalService: React.FC<RootBotExternalServiceProps> = (pr
           )}
         </div>
         <PrimaryButton
-          disabled={displayManageLuis || displayManageQNA}
           styles={{ root: { width: '250px', marginTop: '15px' } }}
           text={formatMessage('Set up Language Understanding')}
           onClick={() => {
@@ -479,7 +478,6 @@ export const RootBotExternalService: React.FC<RootBotExternalServiceProps> = (pr
           />
         </div>
         <PrimaryButton
-          disabled={displayManageLuis || displayManageQNA}
           styles={{ root: { width: '250px', marginTop: '15px' } }}
           text={formatMessage('Set up QnA Maker')}
           onClick={() => {
