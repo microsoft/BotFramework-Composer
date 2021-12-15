d08df6b5d5303720a57046bc6c46112568d19822
Eugene <EOlonov@gmail.com>
2021-12-15 21:53:02 +0300

    fix: wrap text with formatMessage
    Co-authored-by: Andy Brown <abrown.d.ts@gmail.com>

--- Composer/packages/client/src/components/CreationFlow/FileSelector.tsx
+++ Composer/packages/client/src/components/CreationFlow/FileSelector.tsx
@@ -349,7 +349,7 @@ export const FileSelector: React.FC<FileSelectorProps> = (props) => {
     },
     {
       key: 'Edit',
-      name: 'Edit',
+      name: formatMessage('Edit'),
       fieldName: '',
       minWidth: 30,
       maxWidth: 30,
