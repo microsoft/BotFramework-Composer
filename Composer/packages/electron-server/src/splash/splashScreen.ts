226b4dfe4fdca12301e13fba317a7b2f640979fa
Eugene <EOlonov@gmail.com>
2022-03-17 10:51:24 -0700

    chore: update electron (#9056)
    * chore: update electron to currently supported version
    
    * Trigger build
    
    * Use Electron v14.2.6
    
    * Trigger build

--- Composer/packages/electron-server/src/splash/splashScreen.ts
+++ Composer/packages/electron-server/src/splash/splashScreen.ts
@@ -50,6 +50,7 @@ export const initSplashScreen = async ({
     webPreferences: {
       // This is necessary to enable loading local images in the url protocol (window.loadURL)
       webSecurity: false, // lgtm [js/disabling-electron-websecurity]
+      nativeWindowOpen: true,
     },
   });
 
