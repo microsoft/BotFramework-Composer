bf7ed7d3c4a4b51e8ccf55fcb3edfb2db7b69dbd
Eugene Olonov <EOlonov@gmail.com>
2022-06-09 11:52:07 -0700

    fix typo

--- scripts/switchYarnToProd.ps1
+++ scripts/switchYarnToProd.ps1
@@ -1,6 +1,6 @@
 <#
   Swap .yarnrc.yml with .yarnrc.prod.yml to enforce auth and repo settings for production build.
-  We want the local dev environment to use npm registry for a sake of simplicity and for productin builds run in CI we use Azure packages registry.
+  We want the local dev environment to use npm registry for a sake of simplicity and for production builds run in CI we use Azure packages registry.
   So we run this script to swap the .yarnrc.yml file so that the CI can use Azure registry without affecting the local dev environment.
 #>
 
